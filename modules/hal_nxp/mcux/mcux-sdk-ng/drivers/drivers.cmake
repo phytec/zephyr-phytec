zephyrproject-rtos/zephyr

boards: nxp: frdm_mcxe31b: Enable use of flash_c40
Configure DT for MCXE31B to use internal flash and its c40 controller.

Signed-off-by: Petr Buchta <petr.buchta@nxp.com>

modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
@@ -262,7 +262,7 @@ if(CONFIG_SOC_MCXW716C OR CONFIG_SOC_MCXW727C)
   endif()
 endif()
 
-if(CONFIG_SOC_S32K344)
+if(CONFIG_SOC_S32K344 OR CONFIG_SOC_MCXE31B)
   set_variable_ifdef(CONFIG_SOC_FLASH_MCUX_C40 CONFIG_MCUX_COMPONENT_driver.flash_c40)
 endif()
 

phytec/zephyr-phytec

modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
@@ -264,7 +264,7 @@ if(CONFIG_SOC_MCXW716C OR CONFIG_SOC_MCXW727C)
   endif()
 endif()
 
-if(CONFIG_SOC_S32K344)
+if(CONFIG_SOC_S32K344 OR CONFIG_SOC_SERIES_MCXE31X)
   set_variable_ifdef(CONFIG_SOC_FLASH_MCUX_C40 CONFIG_MCUX_COMPONENT_driver.flash_c40)
 endif()
 