zephyrproject-rtos/zephyr

boards: riscv: Convert HiFive1 to DTS
Adds DTS bindings for sifive,pwm0, sifive,uart0, sifive,spi0, and
riscv,plic0.

Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

drivers/gpio/gpio_fe310.c
@@ -358,7 +358,7 @@ static void gpio_fe310_cfg_0(void);
 
 static const struct gpio_fe310_config gpio_fe310_config0 = {
 	.gpio_base_addr    = FE310_GPIO_0_BASE_ADDR,
-	.gpio_irq_base     = FE310_GPIO_0_IRQ,
+	.gpio_irq_base     = FE310_GPIO_0_IRQ_0,
 	.gpio_cfg_func     = gpio_fe310_cfg_0,
 };
 
@@ -372,162 +372,162 @@ DEVICE_AND_API_INIT(gpio_fe310_0, CONFIG_GPIO_FE310_GPIO_NAME,
 
 static void gpio_fe310_cfg_0(void)
 {
-	IRQ_CONNECT(FE310_GPIO_0_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_0,
 		    CONFIG_GPIO_FE310_0_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_1_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_1,
 		    CONFIG_GPIO_FE310_1_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_2_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_2,
 		    CONFIG_GPIO_FE310_2_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_3_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_3,
 		    CONFIG_GPIO_FE310_3_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_4_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_4,
 		    CONFIG_GPIO_FE310_4_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_5_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_5,
 		    CONFIG_GPIO_FE310_5_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_6_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_6,
 		    CONFIG_GPIO_FE310_6_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_7_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_7,
 		    CONFIG_GPIO_FE310_7_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_8_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_8,
 		    CONFIG_GPIO_FE310_8_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_9_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_9,
 		    CONFIG_GPIO_FE310_9_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_10_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_10,
 		    CONFIG_GPIO_FE310_10_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_11_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_11,
 		    CONFIG_GPIO_FE310_11_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_12_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_12,
 		    CONFIG_GPIO_FE310_12_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_13_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_13,
 		    CONFIG_GPIO_FE310_13_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_14_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_14,
 		    CONFIG_GPIO_FE310_14_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_15_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_15,
 		    CONFIG_GPIO_FE310_15_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_16_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_16,
 		    CONFIG_GPIO_FE310_16_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_17_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_17,
 		    CONFIG_GPIO_FE310_17_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_18_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_18,
 		    CONFIG_GPIO_FE310_18_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_19_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_19,
 		    CONFIG_GPIO_FE310_19_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_20_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_20,
 		    CONFIG_GPIO_FE310_20_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_21_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_21,
 		    CONFIG_GPIO_FE310_21_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_22_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_22,
 		    CONFIG_GPIO_FE310_22_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_23_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_23,
 		    CONFIG_GPIO_FE310_23_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_24_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_24,
 		    CONFIG_GPIO_FE310_24_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_25_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_25,
 		    CONFIG_GPIO_FE310_25_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_26_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_26,
 		    CONFIG_GPIO_FE310_26_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_27_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_27,
 		    CONFIG_GPIO_FE310_27_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_28_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_28,
 		    CONFIG_GPIO_FE310_28_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_29_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_29,
 		    CONFIG_GPIO_FE310_29_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_30_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_30,
 		    CONFIG_GPIO_FE310_30_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_31_IRQ,
+	IRQ_CONNECT(FE310_GPIO_0_IRQ_31,
 		    CONFIG_GPIO_FE310_31_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),

phytec/zephyr-phytec

Signed-off-by: Palmer Dabbelt <palmer@sifive.com>

drivers/gpio/gpio_fe310.c
@@ -357,8 +357,8 @@ static int gpio_fe310_init(struct device *dev)
 static void gpio_fe310_cfg_0(void);
 
 static const struct gpio_fe310_config gpio_fe310_config0 = {
-	.gpio_base_addr    = FE310_GPIO_0_BASE_ADDR,
-	.gpio_irq_base     = FE310_GPIO_0_IRQ,
+	.gpio_base_addr    = CONFIG_FE310_GPIO_0_BASE_ADDR,
+	.gpio_irq_base     = CONFIG_FE310_GPIO_0_IRQ_0,
 	.gpio_cfg_func     = gpio_fe310_cfg_0,
 };
 
@@ -372,162 +372,162 @@ DEVICE_AND_API_INIT(gpio_fe310_0, CONFIG_GPIO_FE310_GPIO_NAME,
 
 static void gpio_fe310_cfg_0(void)
 {
-	IRQ_CONNECT(FE310_GPIO_0_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_0,
 		    CONFIG_GPIO_FE310_0_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_1_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_1,
 		    CONFIG_GPIO_FE310_1_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_2_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_2,
 		    CONFIG_GPIO_FE310_2_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_3_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_3,
 		    CONFIG_GPIO_FE310_3_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_4_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_4,
 		    CONFIG_GPIO_FE310_4_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_5_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_5,
 		    CONFIG_GPIO_FE310_5_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_6_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_6,
 		    CONFIG_GPIO_FE310_6_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_7_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_7,
 		    CONFIG_GPIO_FE310_7_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_8_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_8,
 		    CONFIG_GPIO_FE310_8_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_9_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_9,
 		    CONFIG_GPIO_FE310_9_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_10_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_10,
 		    CONFIG_GPIO_FE310_10_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_11_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_11,
 		    CONFIG_GPIO_FE310_11_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_12_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_12,
 		    CONFIG_GPIO_FE310_12_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_13_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_13,
 		    CONFIG_GPIO_FE310_13_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_14_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_14,
 		    CONFIG_GPIO_FE310_14_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_15_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_15,
 		    CONFIG_GPIO_FE310_15_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_16_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_16,
 		    CONFIG_GPIO_FE310_16_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_17_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_17,
 		    CONFIG_GPIO_FE310_17_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_18_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_18,
 		    CONFIG_GPIO_FE310_18_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_19_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_19,
 		    CONFIG_GPIO_FE310_19_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_20_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_20,
 		    CONFIG_GPIO_FE310_20_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_21_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_21,
 		    CONFIG_GPIO_FE310_21_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_22_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_22,
 		    CONFIG_GPIO_FE310_22_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_23_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_23,
 		    CONFIG_GPIO_FE310_23_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_24_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_24,
 		    CONFIG_GPIO_FE310_24_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_25_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_25,
 		    CONFIG_GPIO_FE310_25_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_26_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_26,
 		    CONFIG_GPIO_FE310_26_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_27_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_27,
 		    CONFIG_GPIO_FE310_27_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_28_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_28,
 		    CONFIG_GPIO_FE310_28_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_29_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_29,
 		    CONFIG_GPIO_FE310_29_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_30_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_30,
 		    CONFIG_GPIO_FE310_30_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),
 		    0);
-	IRQ_CONNECT(FE310_GPIO_31_IRQ,
+	IRQ_CONNECT(CONFIG_FE310_GPIO_0_IRQ_31,
 		    CONFIG_GPIO_FE310_31_PRIORITY,
 		    gpio_fe310_irq_handler,
 		    DEVICE_GET(gpio_fe310_0),