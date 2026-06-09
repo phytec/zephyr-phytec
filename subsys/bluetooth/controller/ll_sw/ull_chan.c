zephyrproject-rtos/zephyr

Bluetooth: controller: split: Moved chan map helper functions
Moved Channel Map helper functions into separate file.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/ull_chan.c
@@ -1,0 +1,40 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <string.h>
+
+#include <zephyr/types.h>
+
+#include "util/util.h"
+
+static u8_t map[5] = {0xFF, 0xFF, 0xFF, 0xFF, 0x1F};
+static u8_t count = 37U;
+
+int ull_chan_reset(void)
+{
+	/* initialise connection channel map */
+	map[0] = 0xFF;
+	map[1] = 0xFF;
+	map[2] = 0xFF;
+	map[3] = 0xFF;
+	map[4] = 0x1F;
+	count = 37U;
+
+	return 0;
+}
+
+u8_t ull_chan_map_get(u8_t *chan_map)
+{
+	memcpy(chan_map, map, sizeof(map));
+
+	return count;
+}
+
+void ull_chan_map_set(u8_t *chan_map)
+{
+	memcpy(map, chan_map, sizeof(map));
+	count = util_ones_count_get(map, sizeof(map));
+}

subsys/bluetooth/controller/ll_sw/ull_chan_internal.h
@@ -1,0 +1,9 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+int ull_chan_reset(void);
+u8_t ull_chan_map_get(u8_t *chan_map);
+void ull_chan_map_set(u8_t *chan_map);

phytec/zephyr-phytec

subsys/bluetooth/controller/ll_sw/ull_chan.c
@@ -1,0 +1,40 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <string.h>
+
+#include <zephyr/types.h>
+
+#include "util/util.h"
+
+static uint8_t map[5] = {0xFF, 0xFF, 0xFF, 0xFF, 0x1F};
+static uint8_t count = 37U;
+
+int ull_chan_reset(void)
+{
+	/* initialise connection channel map */
+	map[0] = 0xFF;
+	map[1] = 0xFF;
+	map[2] = 0xFF;
+	map[3] = 0xFF;
+	map[4] = 0x1F;
+	count = 37U;
+
+	return 0;
+}
+
+uint8_t ull_chan_map_get(uint8_t *chan_map)
+{
+	memcpy(chan_map, map, sizeof(map));
+
+	return count;
+}
+
+void ull_chan_map_set(uint8_t *chan_map)
+{
+	memcpy(map, chan_map, sizeof(map));
+	count = util_ones_count_get(map, sizeof(map));
+}

subsys/bluetooth/controller/ll_sw/ull_chan_internal.h
@@ -1,0 +1,9 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+int ull_chan_reset(void);
+uint8_t ull_chan_map_get(uint8_t *chan_map);
+void ull_chan_map_set(uint8_t *chan_map);