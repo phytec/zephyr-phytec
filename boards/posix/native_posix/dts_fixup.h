vanwinkeljan/zephyr

dts: posix: Add DTS support for POSIX architecture
Added device tree support for POSIX architecture based boards.

Signed-off-by: Jan Van Winkel <jan.van_winkel@dxplore.eu>

boards/posix/native_posix/dts_fixup.h
@@ -1,0 +1,9 @@
+/*
+ * Copyright (c) 2019 Jan Van Winkel (jan.van_winkel@dxplore.eu)
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define DT_FLASH_DEV_NAME   DT_SOC_NV_FLASH_0_LABEL
+#define DT_UART_0_DEV_NAME  DT_ZEPHYR_NATIVE_POSIX_UART_1_LABEL
+

drivers/serial/uart_native_posix.c
@@ -284,7 +284,7 @@ static int np_uart_tty_poll_in(struct device *dev, unsigned char *p_char)
 static struct native_uart_status native_uart_status_0;
 
 DEVICE_AND_API_INIT(uart_native_posix0,
-	    CONFIG_UART_NATIVE_POSIX_PORT_0_NAME, &np_uart_init,
+	    DT_UART_0_DEV_NAME, &np_uart_init,
 	    (void *)&native_uart_status_0, NULL,
 	    PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
 	    &np_uart_driver_api);

phytec/zephyr-phytec

boards/posix/native_posix/dts_fixup.h
@@ -1,0 +1,9 @@
+/*
+ * Copyright (c) 2019 Jan Van Winkel (jan.van_winkel@dxplore.eu)
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define DT_FLASH_DEV_NAME   DT_ZEPHYR_NATIVE_POSIX_FLASH_CONTROLLER_0_LABEL
+#define DT_UART_0_DEV_NAME  DT_ZEPHYR_NATIVE_POSIX_UART_1_LABEL
+

drivers/serial/uart_native_posix.c
@@ -201,8 +201,7 @@ static int np_uart_0_init(struct device *dev)
 	d = (struct native_uart_status *)dev->driver_data;
 
 	if (IS_ENABLED(CONFIG_NATIVE_UART_0_ON_OWN_PTY)) {
-		int tty_fn = open_tty(d, CONFIG_UART_NATIVE_POSIX_PORT_0_NAME,
-				      auto_attach);
+		int tty_fn = open_tty(d, DT_UART_0_DEV_NAME, auto_attach);
 
 		d->in_fd = tty_fn;
 		d->out_fd = tty_fn;
@@ -337,7 +336,7 @@ static int np_uart_tty_poll_in(struct device *dev, unsigned char *p_char)
 }
 
 DEVICE_AND_API_INIT(uart_native_posix0,
-	    CONFIG_UART_NATIVE_POSIX_PORT_0_NAME, &np_uart_0_init,
+	    DT_UART_0_DEV_NAME, &np_uart_0_init,
 	    (void *)&native_uart_status_0, NULL,
 	    PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
 	    &np_uart_driver_api_0);