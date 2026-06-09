Akadimia-Robot/zephyr

posix: options: replace kconfig for rw lock
- Update Kconfig from CONFIG_POSIX_READER_WRITER_LOCKS to POSIX_RW_LOCKS

Signed-off-by: Romain Pelletant <romainp@kickmaker.net>

include/zephyr/posix/sys/sysconf.h
@@ -172,7 +172,7 @@ enum {
 	COND_CODE_1(CONFIG_NET_SOCKETS_PACKET, (_POSIX_RAW_SOCKETS), (-1L))
 #define __z_posix_sysconf_SC_RE_DUP_MAX _POSIX_RE_DUP_MAX
 #define __z_posix_sysconf_SC_READER_WRITER_LOCKS                                                   \
-	COND_CODE_1(CONFIG_POSIX_READER_WRITER_LOCKS, (_POSIX_READER_WRITER_LOCKS), (-1L))
+	COND_CODE_1(CONFIG_POSIX_RW_LOCKS, (_POSIX_RW_LOCKS), (-1L))
 #define __z_posix_sysconf_SC_REALTIME_SIGNALS      (-1L)
 #define __z_posix_sysconf_SC_REGEXP                (-1L)
 #define __z_posix_sysconf_SC_SAVED_IDS             (-1L)

phytec/zephyr-phytec

include/zephyr/posix/sys/sysconf.h
@@ -181,7 +181,7 @@ enum {
 	COND_CODE_1(CONFIG_NET_SOCKETS_PACKET, (_POSIX_RAW_SOCKETS), (-1L))
 #define __z_posix_sysconf_SC_RE_DUP_MAX _POSIX_RE_DUP_MAX
 #define __z_posix_sysconf_SC_READER_WRITER_LOCKS                                                   \
-	COND_CODE_1(CONFIG_POSIX_READER_WRITER_LOCKS, (_POSIX_READER_WRITER_LOCKS), (-1L))
+	COND_CODE_1(CONFIG_POSIX_RW_LOCKS, (_POSIX_READER_WRITER_LOCKS), (-1L))
 #define __z_posix_sysconf_SC_REALTIME_SIGNALS      (-1L)
 #define __z_posix_sysconf_SC_REGEXP                (-1L)
 #define __z_posix_sysconf_SC_SAVED_IDS             (-1L)