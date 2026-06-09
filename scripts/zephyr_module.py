zephyrproject-rtos/zephyr

west: blobs: Allow explicit fetcher selection
Decouple fetcher selection from the URL by adding a new field
`fetcher` to the blob metadata. If not set, fall back to parsing
the schema from the URL.

Add an optional field `size` to declare the size of the blob in
bytes.

Update documentation to cover the new additions. Fix a typo
`uri`->`url` and document the `click-through` field together
with the new fields.

Signed-off-by: Aksel Skauge Mellbye <aksel.mellbye@silabs.com>

scripts/zephyr_module.py
@@ -156,6 +156,12 @@ mapping:
           doc-url:
             required: false
             type: str
+          fetcher:
+            required: false
+            type: str
+          size:
+            required: false
+            type: int
   security:
      required: false
      type: map

phytec/zephyr-phytec

scripts/zephyr_module.py
@@ -121,6 +121,10 @@ properties:
           type: string
         doc-url:
           type: string
+        fetcher:
+          type: string
+        size:
+          type: integer
       required:
         - path
         - sha256