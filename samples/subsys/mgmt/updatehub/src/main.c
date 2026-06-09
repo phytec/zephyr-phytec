nehalrohit/zephyr

samples: mgmt: updatehub: Add modem support
Add MODEM and Arduino header overlays.  This allows users to evaluate
UpdateHub using any MODEM supported by Zephyr which supports UDP and
PPP connection.

Signed-off-by: Gerson Fernando Budke <gerson.budke@ossystems.com.br>

samples/subsys/mgmt/updatehub/src/main.c
@@ -129,6 +129,12 @@ void main(void)
 		LOG_ERR("Error connecting to WiFi");
 		return;
 	}
+#elif defined(CONFIG_MODEM)
+	const struct device *uart_dev =
+		device_get_binding(CONFIG_MODEM_GSM_UART_NAME);
+
+	LOG_INF("APN '%s' UART '%s' device %p", CONFIG_MODEM_GSM_APN,
+		CONFIG_MODEM_GSM_UART_NAME, uart_dev);
 #endif
 
 	net_mgmt_init_event_callback(&mgmt_cb, event_handler, EVENT_MASK);

phytec/zephyr-phytec

samples/subsys/mgmt/updatehub/src/main.c
@@ -129,6 +129,12 @@ void main(void)
 		LOG_ERR("Error connecting to WiFi");
 		return;
 	}
+#elif defined(CONFIG_MODEM_GSM_PPP)
+	const struct device *uart_dev =
+		device_get_binding(CONFIG_MODEM_GSM_UART_NAME);
+
+	LOG_INF("APN '%s' UART '%s' device %p", CONFIG_MODEM_GSM_APN,
+		CONFIG_MODEM_GSM_UART_NAME, uart_dev);
 #endif
 
 	net_mgmt_init_event_callback(&mgmt_cb, event_handler, EVENT_MASK);