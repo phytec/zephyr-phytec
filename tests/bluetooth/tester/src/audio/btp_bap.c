zephyrproject-rtos/zephyr

tests: Bluetooth: Tester: Audio: Adds BRS
This commit adds API to tester to allow exposing of Broadcast Receive
State.

Signed-off-by: Omkar Kulkarni <omkar.kulkarni@nordicsemi.no>

tests/bluetooth/tester/src/audio/btp_bap.c
@@ -474,6 +474,11 @@ static const struct btp_handler bap_handlers[] = {
 		.expect_len = sizeof(struct btp_bap_send_past_cmd),
 		.func = btp_bap_broadcast_assistant_send_past,
 	},
+	{
+		.opcode = BTP_BAP_ADD_BROADCAST_RECEIVE_STATE,
+		.expect_len = BTP_HANDLER_LENGTH_VARIABLE,
+		.func = btp_bap_broadcast_add_receive_state,
+	},
 #endif /* CONFIG_BT_BAP_BROADCAST_SOURCE || CONFIG_BT_BAP_BROADCAST_SINK */
 };
 

tests/bluetooth/tester/src/audio/btp_bap_broadcast.h
@@ -120,3 +120,5 @@ uint8_t btp_bap_broadcast_assistant_set_broadcast_code(const void *cmd, uint16_t
 						       void *rsp, uint16_t *rsp_len);
 uint8_t btp_bap_broadcast_assistant_send_past(const void *cmd, uint16_t cmd_len,
 					      void *rsp, uint16_t *rsp_len);
+uint8_t btp_bap_broadcast_add_receive_state(const void *cmd, uint16_t cmd_len,
+					    void *rsp, uint16_t *rsp_len);

phytec/zephyr-phytec

tests/bluetooth/tester/src/audio/btp_bap.c
@@ -474,6 +474,11 @@ static const struct btp_handler bap_handlers[] = {
 		.expect_len = sizeof(struct btp_bap_send_past_cmd),
 		.func = btp_bap_broadcast_assistant_send_past,
 	},
+	{
+		.opcode = BTP_BAP_SCAN_DELEGATOR_ADD_SRC,
+		.expect_len = BTP_HANDLER_LENGTH_VARIABLE,
+		.func = btp_bap_scan_delegator_add_src,
+	},
 #endif /* CONFIG_BT_BAP_BROADCAST_SOURCE || CONFIG_BT_BAP_BROADCAST_SINK */
 };
 

tests/bluetooth/tester/src/audio/btp_bap_broadcast.h
@@ -120,3 +120,5 @@ uint8_t btp_bap_broadcast_assistant_set_broadcast_code(const void *cmd, uint16_t
 						       void *rsp, uint16_t *rsp_len);
 uint8_t btp_bap_broadcast_assistant_send_past(const void *cmd, uint16_t cmd_len,
 					      void *rsp, uint16_t *rsp_len);
+uint8_t btp_bap_scan_delegator_add_src(const void *cmd, uint16_t cmd_len,
+				       void *rsp, uint16_t *rsp_len);