ceolin/zephyr

power: device: void *context -> uint32_t *state
The context parameter used across device power management is
actually the power state. Just use it and avoid a lot of
unnecessary casts.

Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

drivers/gpio/gpio_stm32.c
@@ -602,7 +602,7 @@ static int gpio_stm32_set_power_state(const struct device *dev,
 
 static int gpio_stm32_device_pm_ctrl(const struct device *dev,
 				     uint32_t ctrl_command,
-				     void *context, device_pm_cb cb, void *arg)
+				     uint32_t *state, device_pm_cb cb, void *arg)
 {
 	struct gpio_stm32_data *data = dev->data;
 	uint32_t new_state;
@@ -610,13 +610,13 @@ static int gpio_stm32_device_pm_ctrl(const struct device *dev,
 
 	switch (ctrl_command) {
 	case DEVICE_PM_SET_POWER_STATE:
-		new_state = *((const uint32_t *)context);
+		new_state = *state;
 		if (new_state != data->power_state) {
 			ret = gpio_stm32_set_power_state(dev, new_state);
 		}
 		break;
 	case DEVICE_PM_GET_POWER_STATE:
-		*((uint32_t *)context) = gpio_stm32_get_power_state(dev);
+		*state = gpio_stm32_get_power_state(dev);
 		break;
 	default:
 		ret = -EINVAL;
@@ -624,7 +624,7 @@ static int gpio_stm32_device_pm_ctrl(const struct device *dev,
 	}
 
 	if (cb) {
-		cb(dev, ret, context, arg);
+		cb(dev, ret, state, arg);
 	}
 
 	return ret;

drivers/i2c/i2c_nrfx_twi.c
@@ -215,13 +215,13 @@ static int init_twi(const struct device *dev)
 #ifdef CONFIG_PM_DEVICE
 static int twi_nrfx_pm_control(const struct device *dev,
 				uint32_t ctrl_command,
-				void *context, device_pm_cb cb, void *arg)
+				uint32_t *state, device_pm_cb cb, void *arg)
 {
 	int ret = 0;
 	uint32_t pm_current_state = get_dev_data(dev)->pm_state;
 
 	if (ctrl_command == DEVICE_PM_SET_POWER_STATE) {
-		uint32_t new_state = *((const uint32_t *)context);
+		uint32_t new_state = *state;
 
 		if (new_state != pm_current_state) {
 			switch (new_state) {
@@ -251,11 +251,11 @@ static int twi_nrfx_pm_control(const struct device *dev,
 		}
 	} else {
 		__ASSERT_NO_MSG(ctrl_command == DEVICE_PM_GET_POWER_STATE);
-		*((uint32_t *)context) = get_dev_data(dev)->pm_state;
+		*state = get_dev_data(dev)->pm_state;
 	}
 
 	if (cb) {
-		cb(dev, ret, context, arg);
+		cb(dev, ret, state, arg);
 	}
 
 	return ret;

drivers/serial/uart_nrfx_uart.c
@@ -1154,12 +1154,12 @@ static void uart_nrfx_set_power_state(const struct device *dev,
 
 static int uart_nrfx_pm_control(const struct device *dev,
 				uint32_t ctrl_command,
-				void *context, device_pm_cb cb, void *arg)
+				uint32_t *state, device_pm_cb cb, void *arg)
 {
 	static uint32_t current_state = DEVICE_PM_ACTIVE_STATE;
 
 	if (ctrl_command == DEVICE_PM_SET_POWER_STATE) {
-		uint32_t new_state = *((const uint32_t *)context);
+		uint32_t new_state = *state;
 
 		if (new_state != current_state) {
 			uart_nrfx_set_power_state(dev, new_state);
@@ -1167,11 +1167,11 @@ static int uart_nrfx_pm_control(const struct device *dev,
 		}
 	} else {
 		__ASSERT_NO_MSG(ctrl_command == DEVICE_PM_GET_POWER_STATE);
-		*((uint32_t *)context) = current_state;
+		*state = current_state;
 	}
 
 	if (cb) {
-		cb(dev, 0, context, arg);
+		cb(dev, 0, state, arg);
 	}
 
 	return 0;

drivers/serial/uart_stm32.c
@@ -1461,24 +1461,24 @@ static int uart_stm32_set_power_state(const struct device *dev,
  */
 static int uart_stm32_pm_control(const struct device *dev,
 					 uint32_t ctrl_command,
-					 void *context, device_pm_cb cb,
+					 uint32_t *state, device_pm_cb cb,
 					 void *arg)
 {
 	struct uart_stm32_data *data = DEV_DATA(dev);
 
 	if (ctrl_command == DEVICE_PM_SET_POWER_STATE) {
-		uint32_t new_state = *((const uint32_t *)context);
+		uint32_t new_state = *state;
 
 		if (new_state != data->pm_state) {
 			uart_stm32_set_power_state(dev, new_state);
 		}
 	} else {
 		__ASSERT_NO_MSG(ctrl_command == DEVICE_PM_GET_POWER_STATE);
-		*((uint32_t *)context) = data->pm_state;
+		*state = data->pm_state;
 	}
 
 	if (cb) {
-		cb(dev, 0, context, arg);
+		cb(dev, 0, state, arg);
 	}
 
 	return 0;

drivers/timer/sys_clock_init.c
@@ -32,7 +32,7 @@ int __weak sys_clock_driver_init(const struct device *dev)
 
 int __weak sys_clock_device_ctrl(const struct device *dev,
 			       uint32_t ctrl_command,
-			       void *context, device_pm_cb cb, void *arg)
+			       uint32_t *context, device_pm_cb cb, void *arg)
 {
 	return -ENOTSUP;
 }

include/device.h
@@ -333,7 +333,7 @@ typedef int16_t device_handle_t;
 #define DEVICE_DECLARE(name) static const struct device DEVICE_NAME_GET(name)
 
 typedef void (*device_pm_cb)(const struct device *dev,
-			     int status, void *context, void *arg);
+			     int status, uint32_t *state, void *arg);
 
 /**
  * @brief Device PM info
@@ -419,7 +419,7 @@ struct device {
 #ifdef CONFIG_PM_DEVICE
 	/** Power Management function */
 	int (*device_pm_control)(const struct device *dev, uint32_t command,
-				 void *context, device_pm_cb cb, void *arg);
+				 uint32_t *state, device_pm_cb cb, void *arg);
 	/** Pointer to device instance power management data */
 	struct device_pm * const pm;
 #endif

samples/subsys/power/device_pm/src/dummy_parent.c
@@ -47,27 +47,27 @@ static int dummy_resume_from_suspend(const struct device *dev)
 
 static int dummy_parent_pm_ctrl(const struct device *dev,
 				uint32_t ctrl_command,
-				void *context, device_pm_cb cb, void *arg)
+				uint32_t *state, device_pm_cb cb, void *arg)
 {
 	int ret = 0;
 
 	switch (ctrl_command) {
 	case DEVICE_PM_SET_POWER_STATE:
-		if (*((uint32_t *)context) == DEVICE_PM_ACTIVE_STATE) {
+		if (*state == DEVICE_PM_ACTIVE_STATE) {
 			ret = dummy_resume_from_suspend(dev);
 		} else {
 			ret = dummy_suspend(dev);
 		}
 		break;
 	case DEVICE_PM_GET_POWER_STATE:
-		*((uint32_t *)context) = dummy_get_power_state(dev);
+		*state = dummy_get_power_state(dev);
 		break;
 	default:
 		ret = -EINVAL;
 
 	}
 
-	cb(dev, ret, context, arg);
+	cb(dev, ret, state, arg);
 	return ret;
 }
 

phytec/zephyr-phytec

drivers/gpio/gpio_stm32.c
@@ -605,7 +605,7 @@ static int gpio_stm32_set_power_state(const struct device *dev,
 
 static int gpio_stm32_pm_device_ctrl(const struct device *dev,
 				     uint32_t ctrl_command,
-				     void *context, pm_device_cb cb, void *arg)
+				     uint32_t *state, pm_device_cb cb, void *arg)
 {
 	struct gpio_stm32_data *data = dev->data;
 	uint32_t new_state;
@@ -613,13 +613,13 @@ static int gpio_stm32_pm_device_ctrl(const struct device *dev,
 
 	switch (ctrl_command) {
 	case PM_DEVICE_STATE_SET:
-		new_state = *((const uint32_t *)context);
+		new_state = *state;
 		if (new_state != data->power_state) {
 			ret = gpio_stm32_set_power_state(dev, new_state);
 		}
 		break;
 	case PM_DEVICE_STATE_GET:
-		*((uint32_t *)context) = gpio_stm32_get_power_state(dev);
+		*state = gpio_stm32_get_power_state(dev);
 		break;
 	default:
 		ret = -EINVAL;
@@ -627,7 +627,7 @@ static int gpio_stm32_pm_device_ctrl(const struct device *dev,
 	}
 
 	if (cb) {
-		cb(dev, ret, context, arg);
+		cb(dev, ret, state, arg);
 	}
 
 	return ret;

drivers/i2c/i2c_nrfx_twi.c
@@ -224,13 +224,13 @@ static int init_twi(const struct device *dev)
 #ifdef CONFIG_PM_DEVICE
 static int twi_nrfx_pm_control(const struct device *dev,
 				uint32_t ctrl_command,
-				void *context, pm_device_cb cb, void *arg)
+				uint32_t *state, pm_device_cb cb, void *arg)
 {
 	int ret = 0;
 	uint32_t pm_current_state = get_dev_data(dev)->pm_state;
 
 	if (ctrl_command == PM_DEVICE_STATE_SET) {
-		uint32_t new_state = *((const uint32_t *)context);
+		uint32_t new_state = *state;
 
 		if (new_state != pm_current_state) {
 			switch (new_state) {
@@ -260,11 +260,11 @@ static int twi_nrfx_pm_control(const struct device *dev,
 		}
 	} else {
 		__ASSERT_NO_MSG(ctrl_command == PM_DEVICE_STATE_GET);
-		*((uint32_t *)context) = get_dev_data(dev)->pm_state;
+		*state = get_dev_data(dev)->pm_state;
 	}
 
 	if (cb) {
-		cb(dev, ret, context, arg);
+		cb(dev, ret, state, arg);
 	}
 
 	return ret;

drivers/serial/uart_nrfx_uart.c
@@ -1158,12 +1158,12 @@ static void uart_nrfx_set_power_state(const struct device *dev,
 
 static int uart_nrfx_pm_control(const struct device *dev,
 				uint32_t ctrl_command,
-				void *context, pm_device_cb cb, void *arg)
+				uint32_t *state, pm_device_cb cb, void *arg)
 {
 	static uint32_t current_state = PM_DEVICE_ACTIVE_STATE;
 
 	if (ctrl_command == PM_DEVICE_STATE_SET) {
-		uint32_t new_state = *((const uint32_t *)context);
+		uint32_t new_state = *state;
 
 		if (new_state != current_state) {
 			uart_nrfx_set_power_state(dev, new_state);
@@ -1171,11 +1171,11 @@ static int uart_nrfx_pm_control(const struct device *dev,
 		}
 	} else {
 		__ASSERT_NO_MSG(ctrl_command == PM_DEVICE_STATE_GET);
-		*((uint32_t *)context) = current_state;
+		*state = current_state;
 	}
 
 	if (cb) {
-		cb(dev, 0, context, arg);
+		cb(dev, 0, state, arg);
 	}
 
 	return 0;

drivers/serial/uart_stm32.c
@@ -1461,24 +1461,24 @@ static int uart_stm32_set_power_state(const struct device *dev,
  */
 static int uart_stm32_pm_control(const struct device *dev,
 					 uint32_t ctrl_command,
-					 void *context, pm_device_cb cb,
+					 uint32_t *state, pm_device_cb cb,
 					 void *arg)
 {
 	struct uart_stm32_data *data = DEV_DATA(dev);
 
 	if (ctrl_command == PM_DEVICE_STATE_SET) {
-		uint32_t new_state = *((const uint32_t *)context);
+		uint32_t new_state = *state;
 
 		if (new_state != data->pm_state) {
 			uart_stm32_set_power_state(dev, new_state);
 		}
 	} else {
 		__ASSERT_NO_MSG(ctrl_command == PM_DEVICE_STATE_GET);
-		*((uint32_t *)context) = data->pm_state;
+		*state = data->pm_state;
 	}
 
 	if (cb) {
-		cb(dev, 0, context, arg);
+		cb(dev, 0, state, arg);
 	}
 
 	return 0;

drivers/timer/sys_clock_init.c
@@ -32,7 +32,7 @@ int __weak sys_clock_driver_init(const struct device *dev)
 
 int __weak sys_clock_device_ctrl(const struct device *dev,
 			       uint32_t ctrl_command,
-			       void *context, pm_device_cb cb, void *arg)
+			       uint32_t *context, pm_device_cb cb, void *arg)
 {
 	return -ENOTSUP;
 }

include/device.h
@@ -387,7 +387,7 @@ struct device {
 #ifdef CONFIG_PM_DEVICE
 	/** Power Management function */
 	int (*pm_control)(const struct device *dev, uint32_t command,
-				 void *context, pm_device_cb cb, void *arg);
+				 uint32_t *state, pm_device_cb cb, void *arg);
 	/** Pointer to device instance power management data */
 	struct pm_device * const pm;
 #endif

samples/subsys/pm/device_pm/src/dummy_parent.c
@@ -48,27 +48,27 @@ static int dummy_resume_from_suspend(const struct device *dev)
 
 static int dummy_parent_pm_ctrl(const struct device *dev,
 				uint32_t ctrl_command,
-				void *context, pm_device_cb cb, void *arg)
+				uint32_t *state, pm_device_cb cb, void *arg)
 {
 	int ret = 0;
 
 	switch (ctrl_command) {
 	case PM_DEVICE_STATE_SET:
-		if (*((uint32_t *)context) == PM_DEVICE_ACTIVE_STATE) {
+		if (*state == PM_DEVICE_ACTIVE_STATE) {
 			ret = dummy_resume_from_suspend(dev);
 		} else {
 			ret = dummy_suspend(dev);
 		}
 		break;
 	case PM_DEVICE_STATE_GET:
-		*((uint32_t *)context) = dummy_get_power_state(dev);
+		*state = dummy_get_power_state(dev);
 		break;
 	default:
 		ret = -EINVAL;
 
 	}
 
-	cb(dev, ret, context, arg);
+	cb(dev, ret, state, arg);
 	return ret;
 }
 