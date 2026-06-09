gxyescut/zephyr

soc: arm: ambiq: apollo4: Add support for Apollo4 Plus SoC
Add all required parts (new SoC family/series, device tree) for
the Ambiq Apollo4 Plus SoC.

Signed-off-by: Maciej Sobkowski <msobkowski@antmicro.com>

soc/arm/ambiq/apollo4x/soc.h
@@ -1,0 +1,12 @@
+/*
+ * Copyright (c) 2023 Antmicro <www.antmicro.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __SOC_H__
+#define __SOC_H__
+
+#include <am_mcu_apollo.h>
+
+#endif /* __SOC_H__ */

phytec/zephyr-phytec

soc/arm/ambiq/apollo4x/soc.h
@@ -1,0 +1,12 @@
+/*
+ * Copyright (c) 2023 Antmicro <www.antmicro.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __SOC_H__
+#define __SOC_H__
+
+#include <apollo4p.h>
+
+#endif /* __SOC_H__ */