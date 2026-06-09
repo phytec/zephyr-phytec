Johnsonyu0926/zephyr

Bluetooth: CCP: Initial CCP Client implemenation
Added initial CCP client implementation that simply
does discovery of TBS on a remote CCP server.

Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

tests/bsim/bluetooth/audio_samples/compile.sh
@@ -49,6 +49,11 @@ if [ "${BOARD_TS}" == "nrf5340bsim_nrf5340_cpuapp" ]; then
     conf_file=${sample}/prj.conf \
     conf_overlay=${sample}/boards/nrf5340_audio_dk_nrf5340_cpuapp.conf \
     exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile
+  app=tests/bsim/bluetooth/audio_samples/ccp/client \
+    sample=${ZEPHYR_BASE}/samples/bluetooth/ccp_client \
+    conf_file=${sample}/prj.conf \
+    conf_overlay=${sample}/boards/nrf5340_audio_dk_nrf5340_cpuapp.conf \
+    exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile
 else
   app=samples/bluetooth/bap_unicast_server conf_overlay=overlay-bt_ll_sw_split.conf \
     exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile
@@ -87,6 +92,11 @@ else
     exe_name=bs_${BOARD_TS}_${app}_unicast_prj_conf sysbuild=1 compile
   app=tests/bsim/bluetooth/audio_samples/ccp/server \
     sample=${ZEPHYR_BASE}/samples/bluetooth/ccp_server \
+    conf_file=${sample}/prj.conf \
+    conf_overlay=${sample}/overlay-bt_ll_sw_split.conf \
+    exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile
+  app=tests/bsim/bluetooth/audio_samples/ccp/client \
+    sample=${ZEPHYR_BASE}/samples/bluetooth/ccp_client \
     conf_file=${sample}/prj.conf \
     conf_overlay=${sample}/overlay-bt_ll_sw_split.conf \
     exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile

tests/bsim/bluetooth/audio_samples/ccp/tests_scripts/ccp.sh
@@ -14,7 +14,10 @@ cd ${BSIM_OUT_PATH}/bin
 Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_audio_samples_ccp_server_prj_conf \
   -v=${verbosity_level} -s=${simulation_id} -d=0 -RealEncryption=1 -testid=ccp_server
 
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_audio_samples_ccp_client_prj_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=1 -RealEncryption=1 -testid=ccp_client
+
 Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
-  -D=1 -sim_length=20e6 $@ -argschannel -at=40
+  -D=2 -sim_length=20e6 $@ -argschannel -at=40
 
 wait_for_background_jobs #Wait for all programs in background and return != 0 if any fails

phytec/zephyr-phytec

tests/bsim/bluetooth/audio_samples/ccp/compile.sh
@@ -15,9 +15,19 @@ if [ "${BOARD_TS}" == "nrf5340bsim_nrf5340_cpuapp" ]; then
     conf_file=${sample}/prj.conf \
     conf_overlay=${sample}/boards/nrf5340_audio_dk_nrf5340_cpuapp.conf \
     exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile
+  app=tests/bsim/bluetooth/audio_samples/ccp/call_control_client \
+    sample=${ZEPHYR_BASE}/samples/bluetooth/ccp_call_control_client \
+    conf_file=${sample}/prj.conf \
+    conf_overlay=${sample}/boards/nrf5340_audio_dk_nrf5340_cpuapp.conf \
+    exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile
 else
   app=tests/bsim/bluetooth/audio_samples/ccp/call_control_server \
     sample=${ZEPHYR_BASE}/samples/bluetooth/ccp_call_control_server \
+    conf_file=${sample}/prj.conf \
+    conf_overlay=${sample}/overlay-bt_ll_sw_split.conf \
+    exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile
+  app=tests/bsim/bluetooth/audio_samples/ccp/call_control_client \
+    sample=${ZEPHYR_BASE}/samples/bluetooth/ccp_call_control_client \
     conf_file=${sample}/prj.conf \
     conf_overlay=${sample}/overlay-bt_ll_sw_split.conf \
     exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile

tests/bsim/bluetooth/audio_samples/ccp/tests_scripts/ccp.sh
@@ -14,7 +14,10 @@ cd ${BSIM_OUT_PATH}/bin
 Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_audio_samples_ccp_call_control_server_prj_conf \
   -v=${verbosity_level} -s=${simulation_id} -d=0 -RealEncryption=1 -testid=ccp_call_control_server
 
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_audio_samples_ccp_call_control_client_prj_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=1 -RealEncryption=1 -testid=ccp_call_control_client
+
 Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
-  -D=1 -sim_length=20e6 $@ -argschannel -at=40
+  -D=2 -sim_length=20e6 $@ -argschannel -at=40
 
 wait_for_background_jobs #Wait for all programs in background and return != 0 if any fails