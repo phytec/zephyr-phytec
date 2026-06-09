tbursztyka/zephyr

drivers/sensors: Switch bme280 driver to new SPI API
Replacing legacy API calls by news ones.

Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

drivers/sensor/bme280/bme280.h
@@ -107,8 +107,7 @@ struct bme280_data {
 	struct device *i2c_master;
 	u16_t i2c_slave_addr;
 #elif defined CONFIG_BME280_DEV_TYPE_SPI
-	struct device *spi;
-	int spi_slave;
+	struct spi_config spi;
 #else
 #error "BME280 device type not specified"
 #endif

phytec/zephyr-phytec

drivers/sensor/bme280/bme280.h
@@ -108,7 +108,7 @@ struct bme280_data {
 	u16_t i2c_slave_addr;
 #elif defined CONFIG_BME280_DEV_TYPE_SPI
 	struct device *spi;
-	int spi_slave;
+	struct spi_config spi_cfg;
 #else
 #error "BME280 device type not specified"
 #endif