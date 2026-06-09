zephyrproject-rtos/zephyr

drivers: adc: Place API into iterable section
Move all adc driver api structs into an iterable section, this allows us
to verify if an api pointer is located in compatible linker section.

Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

drivers/adc/adc_ads1112.c
@@ -383,7 +383,7 @@ static int ads1112_init(const struct device *dev)
 	return rc;
 }
 
-static const struct adc_driver_api api = {
+static DRIVER_API(adc_driver_api, api) = {
 	.channel_setup = ads1112_channel_setup,
 	.read = ads1112_read,
 	.ref_internal = ADS1112_REF_INTERNAL,

drivers/adc/adc_nrfx_saadc.c
@@ -423,7 +423,7 @@ static int init_saadc(const struct device *dev)
 	return 0;
 }
 
-static const struct adc_driver_api adc_nrfx_driver_api = {
+static DRIVER_API(adc_driver_api, adc_nrfx_driver_api) = {
 	.channel_setup = adc_nrfx_channel_setup,
 	.read          = adc_nrfx_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_rpi_pico.c
@@ -353,7 +353,7 @@ static int adc_rpi_init(const struct device *dev)
 #define ADC_RPI_INIT(idx)                                                                          \
 	IRQ_CONFIGURE_FUNC(idx)                                                                    \
 	PINCTRL_DT_INST_DEFINE(idx);                                                               \
-	static struct adc_driver_api adc_rpi_api_##idx = {                                         \
+	static DRIVER_API(adc_driver_api, adc_rpi_api_##idx) = {                                   \
 		.channel_setup = adc_rpi_channel_setup,                                            \
 		.read = adc_rpi_read,                                                              \
 		.ref_internal = DT_INST_PROP(idx, vref_mv),                                        \

drivers/adc/adc_sam0.c
@@ -503,7 +503,7 @@ static int adc_sam0_read_async(const struct device *dev,
 }
 #endif
 
-static const struct adc_driver_api adc_sam0_api = {
+static DRIVER_API(adc_driver_api, adc_sam0_api) = {
 	.channel_setup = adc_sam0_channel_setup,
 	.read = adc_sam0_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_smartbond_gpadc.c
@@ -282,7 +282,7 @@ static int adc_smartbond_init(const struct device *dev)
 	return 0;
 }
 
-static const struct adc_driver_api adc_smartbond_driver_api = {
+static DRIVER_API(adc_driver_api, adc_smartbond_driver_api) = {
 	.channel_setup = adc_smartbond_channel_setup,
 	.read          = adc_smartbond_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_smartbond_sdadc.c
@@ -285,7 +285,7 @@ static int sdadc_smartbond_init(const struct device *dev)
 	return 0;
 }
 
-static const struct adc_driver_api sdadc_smartbond_driver_api = {
+static DRIVER_API(adc_driver_api, sdadc_smartbond_driver_api) = {
 	.channel_setup = sdadc_smartbond_channel_setup,
 	.read          = sdadc_smartbond_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_tla2021.c
@@ -285,7 +285,7 @@ static int tla2021_init(const struct device *dev)
 	return 0;
 }
 
-static const struct adc_driver_api tla2021_driver_api = {
+static DRIVER_API(adc_driver_api, tla2021_driver_api) = {
 	.channel_setup = tla2021_channel_setup,
 	.read = tla2021_read,
 	.ref_internal = 2048,

phytec/zephyr-phytec

drivers/adc/adc_ads1112.c
@@ -379,7 +379,7 @@ static int ads1112_init(const struct device *dev)
 	return rc;
 }
 
-static const struct adc_driver_api api = {
+static DEVICE_API(adc, api) = {
 	.channel_setup = ads1112_channel_setup,
 	.read = ads1112_read,
 	.ref_internal = ADS1112_REF_INTERNAL,

drivers/adc/adc_nrfx_saadc.c
@@ -661,7 +661,7 @@ static int init_saadc(const struct device *dev)
 	return 0;
 }
 
-static const struct adc_driver_api adc_nrfx_driver_api = {
+static DEVICE_API(adc, adc_nrfx_driver_api) = {
 	.channel_setup = adc_nrfx_channel_setup,
 	.read          = adc_nrfx_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_rpi_pico.c
@@ -354,7 +354,7 @@ static int adc_rpi_init(const struct device *dev)
 #define ADC_RPI_INIT(idx)                                                                          \
 	IRQ_CONFIGURE_FUNC(idx)                                                                    \
 	PINCTRL_DT_INST_DEFINE(idx);                                                               \
-	static struct adc_driver_api adc_rpi_api_##idx = {                                         \
+	static DEVICE_API(adc, adc_rpi_api_##idx) = {                                              \
 		.channel_setup = adc_rpi_channel_setup,                                            \
 		.read = adc_rpi_read,                                                              \
 		.ref_internal = DT_INST_PROP(idx, vref_mv),                                        \

drivers/adc/adc_sam0.c
@@ -503,7 +503,7 @@ static int adc_sam0_read_async(const struct device *dev,
 }
 #endif
 
-static const struct adc_driver_api adc_sam0_api = {
+static DEVICE_API(adc, adc_sam0_api) = {
 	.channel_setup = adc_sam0_channel_setup,
 	.read = adc_sam0_read,
 	.ref_internal = 1000U,			/* Fixed 1.0 V reference */

drivers/adc/adc_smartbond_gpadc.c
@@ -389,7 +389,7 @@ static int adc_smartbond_init(const struct device *dev)
 	return ret;
 }
 
-static const struct adc_driver_api adc_smartbond_driver_api = {
+static DEVICE_API(adc, adc_smartbond_driver_api) = {
 	.channel_setup = adc_smartbond_channel_setup,
 	.read          = adc_smartbond_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_smartbond_sdadc.c
@@ -393,7 +393,7 @@ static int sdadc_smartbond_init(const struct device *dev)
 	return ret;
 }
 
-static const struct adc_driver_api sdadc_smartbond_driver_api = {
+static DEVICE_API(adc, sdadc_smartbond_driver_api) = {
 	.channel_setup = sdadc_smartbond_channel_setup,
 	.read          = sdadc_smartbond_read,
 #ifdef CONFIG_ADC_ASYNC

drivers/adc/adc_tla2021.c
@@ -295,7 +295,7 @@ static int tla2021_init(const struct device *dev)
 	return 0;
 }
 
-static const struct adc_driver_api tla2021_driver_api = {
+static DEVICE_API(adc, tla2021_driver_api) = {
 	.channel_setup = tla2021_channel_setup,
 	.read = tla2021_read,
 	.ref_internal = 4096,