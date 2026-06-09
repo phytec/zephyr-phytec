ckhardin/zephyr

drivers: ethernet: lan9250: add in the reset gpio configurate
The reset gpio field was in the config structure but was not coded
into the initialization path. So, add the appropriate code to handle
the gpio setup when it is defined in the device tree.

Signed-off-by: Charles Hardin <ckhardin@gmail.com>

drivers/ethernet/eth_lan9250.c
@@ -679,17 +679,48 @@ static int lan9250_init(const struct device *dev)
 		return -EINVAL;
 	}
 
-	if (gpio_pin_configure_dt(&config->interrupt, GPIO_INPUT)) {
+	ret = gpio_pin_configure_dt(&config->interrupt, GPIO_INPUT);
+	if (ret < 0) {
 		LOG_ERR("Unable to configure GPIO pin %u", config->interrupt.pin);
-		return -EINVAL;
+		return ret;
 	}
 
-	gpio_init_callback(&(context->gpio_cb), lan9250_gpio_callback, BIT(config->interrupt.pin));
-	if (gpio_add_callback(config->interrupt.port, &(context->gpio_cb))) {
-		return -EINVAL;
+	gpio_init_callback(&(context->gpio_cb), lan9250_gpio_callback,
+			   BIT(config->interrupt.pin));
+	ret = gpio_add_callback(config->interrupt.port, &context->gpio_cb);
+	if (ret < 0) {
+		return ret;
 	}
 
-	gpio_pin_interrupt_configure_dt(&config->interrupt, GPIO_INT_EDGE_TO_ACTIVE);
+	ret = gpio_pin_interrupt_configure_dt(&config->interrupt,
+					      GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret < 0) {
+		LOG_ERR("Unable to enable GPIO INT %u", config->interrupt.pin);
+		return ret;
+	}
+
+	if (config->reset.port != NULL) {
+		if (!gpio_is_ready_dt(&config->reset)) {
+			LOG_ERR("GPIO port %s not ready", config->reset.port->name);
+			return -EINVAL;
+		}
+
+		ret = gpio_pin_configure_dt(&config->reset, GPIO_OUTPUT);
+		if (ret < 0) {
+			LOG_ERR("Unable to configure GPIO pin %u", config->reset.pin);
+			return ret;
+		}
+
+		/* See Section 19.6.3 from the LAN9250 Data Sheet
+		 *
+		 * trstia is 200 microseconds min (use 250 us)
+		 * tcfg is 15 milliseconds min (use 20 ms for after reset)
+		 */
+		gpio_pin_set_dt(&config->reset, 1);
+		k_usleep(250);
+		gpio_pin_set_dt(&config->reset, 0);
+		k_msleep(20);
+	}
 
 	/* Reset and wait for ready on the LAN9250 SPI device */
 	ret = lan9250_sw_reset(dev);
@@ -727,6 +758,7 @@ static int lan9250_init(const struct device *dev)
 	static const struct lan9250_config lan9250_##inst##_config = {                             \
 		.spi = SPI_DT_SPEC_INST_GET(inst, SPI_WORD_SET(8)),                                \
 		.interrupt = GPIO_DT_SPEC_INST_GET(inst, int_gpios),                               \
+		.reset = GPIO_DT_SPEC_INST_GET_OR(inst, reset_gpios, {0}),                         \
 		.timeout = CONFIG_ETH_LAN9250_BUF_ALLOC_TIMEOUT,                                   \
 		.random_mac = DT_INST_PROP(inst, zephyr_random_mac_address),                       \
 	};                                                                                         \

phytec/zephyr-phytec

drivers/ethernet/eth_lan9250.c
@@ -678,17 +678,49 @@ static int lan9250_init(const struct device *dev)
 		return -EINVAL;
 	}
 
-	if (gpio_pin_configure_dt(&config->interrupt, GPIO_INPUT)) {
+	ret = gpio_pin_configure_dt(&config->interrupt, GPIO_INPUT);
+	if (ret < 0) {
 		LOG_ERR("Unable to configure GPIO pin %u", config->interrupt.pin);
-		return -EINVAL;
+		return ret;
 	}
 
-	gpio_init_callback(&(context->gpio_cb), lan9250_gpio_callback, BIT(config->interrupt.pin));
-	if (gpio_add_callback(config->interrupt.port, &(context->gpio_cb))) {
-		return -EINVAL;
+	gpio_init_callback(&(context->gpio_cb), lan9250_gpio_callback,
+			   BIT(config->interrupt.pin));
+	ret = gpio_add_callback(config->interrupt.port, &context->gpio_cb);
+	if (ret < 0) {
+		LOG_ERR("Unable to add GPIO callback %u", config->interrupt.pin);
+		return ret;
 	}
 
-	gpio_pin_interrupt_configure_dt(&config->interrupt, GPIO_INT_EDGE_TO_ACTIVE);
+	ret = gpio_pin_interrupt_configure_dt(&config->interrupt,
+					      GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret < 0) {
+		LOG_ERR("Unable to enable GPIO INT %u", config->interrupt.pin);
+		return ret;
+	}
+
+	if (config->reset.port != NULL) {
+		if (!gpio_is_ready_dt(&config->reset)) {
+			LOG_ERR("GPIO port %s not ready", config->reset.port->name);
+			return -EINVAL;
+		}
+
+		ret = gpio_pin_configure_dt(&config->reset, GPIO_OUTPUT_INACTIVE);
+		if (ret < 0) {
+			LOG_ERR("Unable to configure GPIO pin %u", config->reset.pin);
+			return ret;
+		}
+
+		/* See Section 19.6.3 from the LAN9250 Data Sheet
+		 *
+		 * trstia is 200 microseconds min (use 250 us)
+		 * tcfg is 15 milliseconds min (use 20 ms for after reset)
+		 */
+		gpio_pin_set_dt(&config->reset, 1);
+		k_usleep(250);
+		gpio_pin_set_dt(&config->reset, 0);
+		k_msleep(20);
+	}
 
 	/* Reset and wait for ready on the LAN9250 SPI device */
 	ret = lan9250_sw_reset(dev);
@@ -720,6 +752,7 @@ static int lan9250_init(const struct device *dev)
 	static const struct lan9250_config lan9250_##inst##_config = {                             \
 		.spi = SPI_DT_SPEC_INST_GET(inst, SPI_WORD_SET(8)),                                \
 		.interrupt = GPIO_DT_SPEC_INST_GET(inst, int_gpios),                               \
+		.reset = GPIO_DT_SPEC_INST_GET_OR(inst, reset_gpios, {0}),                         \
 		.timeout = CONFIG_ETH_LAN9250_BUF_ALLOC_TIMEOUT,                                   \
 		.mac_cfg = NET_ETH_MAC_DT_INST_CONFIG_INIT(inst),                                  \
 	};                                                                                         \