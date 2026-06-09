nordic-krch/zephyr

logging: Adapt logger to support both versions
Adapted to be able to switch between v1 and v2.

Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>

include/sys/printk.h
@@ -45,8 +45,21 @@ extern "C" {
  * @return N/A
  */
 #ifdef CONFIG_PRINTK
+
+#if defined(CONFIG_LOG_PRINTK) && defined(CONFIG_LOG2)
+#include <logging/log.h>
+#define printk(...) Z_LOG2_PRINTK(__VA_ARGS__)
+static inline __printf_like(1, 0) void vprintk(const char *fmt, va_list ap)
+{
+	z_log_msg2_runtime_vcreate(CONFIG_LOG_DOMAIN_ID, NULL,
+				   LOG_LEVEL_INTERNAL_RAW_STRING, NULL, 0,
+				   fmt, ap);
+}
+#else
 extern __printf_like(1, 2) void printk(const char *fmt, ...);
 extern __printf_like(1, 0) void vprintk(const char *fmt, va_list ap);
+#endif /* defined(CONFIG_LOG_PRINTK) && defined(CONFIG_LOG) */
+
 #else
 static inline __printf_like(1, 2) void printk(const char *fmt, ...)
 {

phytec/zephyr-phytec

include/sys/printk.h
@@ -45,8 +45,21 @@ extern "C" {
  * @return N/A
  */
 #ifdef CONFIG_PRINTK
+
+#if defined(CONFIG_LOG_PRINTK) && defined(CONFIG_LOG2)
+#include <logging/log.h>
+#define printk(...) Z_LOG_PRINTK(__VA_ARGS__)
+static inline __printf_like(1, 0) void vprintk(const char *fmt, va_list ap)
+{
+	z_log_msg2_runtime_vcreate(CONFIG_LOG_DOMAIN_ID, NULL,
+				   LOG_LEVEL_INTERNAL_RAW_STRING, NULL, 0,
+				   fmt, ap);
+}
+#else
 extern __printf_like(1, 2) void printk(const char *fmt, ...);
 extern __printf_like(1, 0) void vprintk(const char *fmt, va_list ap);
+#endif /* defined(CONFIG_LOG_PRINTK) && defined(CONFIG_LOG) */
+
 #else
 static inline __printf_like(1, 2) void printk(const char *fmt, ...)
 {