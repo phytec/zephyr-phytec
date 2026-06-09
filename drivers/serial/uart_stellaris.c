zephyrproject-rtos/zephyr

drivers: uart_stellaris: Get clock frequency from DTS
The uart_stellaris driver used system clock frequency
as a base for baudrate calculation. This commit corrects
that by obtaining the needed value from DTS.

Signed-off-by: Piotr Zięcik <piotr.ziecik@nordicsemi.no>

drivers/serial/uart_stellaris.c
@@ -633,7 +633,7 @@ static void irq_config_func_0(struct device *port);
 
 static const struct uart_device_config uart_stellaris_dev_cfg_0 = {
 	.base = (u8_t *)DT_TI_STELLARIS_UART_4000C000_BASE_ADDRESS,
-	.sys_clk_freq = DT_UART_STELLARIS_CLK_FREQ,
+	.sys_clk_freq = DT_TI_STELLARIS_UART_4000C000_CLOCK_FREQUENCY,
 
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
 	.irq_config_func = irq_config_func_0,
@@ -671,7 +671,7 @@ static void irq_config_func_1(struct device *port);
 
 static struct uart_device_config uart_stellaris_dev_cfg_1 = {
 	.base = (u8_t *)DT_TI_STELLARIS_UART_4000D000_BASE_ADDRESS,
-	.sys_clk_freq = DT_UART_STELLARIS_CLK_FREQ,
+	.sys_clk_freq = DT_TI_STELLARIS_UART_4000D000_CLOCK_FREQUENCY,
 
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
 	.irq_config_func = irq_config_func_1,
@@ -709,7 +709,7 @@ static void irq_config_func_2(struct device *port);
 
 static const struct uart_device_config uart_stellaris_dev_cfg_2 = {
 	.base = (u8_t *)DT_TI_STELLARIS_UART_4000E000_BASE_ADDRESS,
-	.sys_clk_freq = DT_UART_STELLARIS_CLK_FREQ,
+	.sys_clk_freq = DT_TI_STELLARIS_UART_4000E000_CLOCK_FREQUENCY,
 
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
 	.irq_config_func = irq_config_func_2,

phytec/zephyr-phytec

drivers/serial/uart_stellaris.c
@@ -634,7 +634,7 @@ static void irq_config_func_0(struct device *port);
 
 static const struct uart_device_config uart_stellaris_dev_cfg_0 = {
 	.base = (u8_t *)DT_TI_STELLARIS_UART_4000C000_BASE_ADDRESS,
-	.sys_clk_freq = DT_UART_STELLARIS_CLK_FREQ,
+	.sys_clk_freq = DT_TI_STELLARIS_UART_4000C000_CLOCKS_CLOCK_FREQUENCY,
 
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
 	.irq_config_func = irq_config_func_0,
@@ -672,7 +672,7 @@ static void irq_config_func_1(struct device *port);
 
 static struct uart_device_config uart_stellaris_dev_cfg_1 = {
 	.base = (u8_t *)DT_TI_STELLARIS_UART_4000D000_BASE_ADDRESS,
-	.sys_clk_freq = DT_UART_STELLARIS_CLK_FREQ,
+	.sys_clk_freq = DT_TI_STELLARIS_UART_4000D000_CLOCKS_CLOCK_FREQUENCY,
 
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
 	.irq_config_func = irq_config_func_1,
@@ -710,7 +710,7 @@ static void irq_config_func_2(struct device *port);
 
 static const struct uart_device_config uart_stellaris_dev_cfg_2 = {
 	.base = (u8_t *)DT_TI_STELLARIS_UART_4000E000_BASE_ADDRESS,
-	.sys_clk_freq = DT_UART_STELLARIS_CLK_FREQ,
+	.sys_clk_freq = DT_TI_STELLARIS_UART_4000E000_CLOCKS_CLOCK_FREQUENCY,
 
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
 	.irq_config_func = irq_config_func_2,