mathieuchopstm/zephyr

scripts: runners: generalize --no-load argument
Move the existing "--no-load" argument from the OpenOCD and Intel Cyclone V
runners into an argument in the runners base class such that it can be used
by all runners. Also update the existing runners to work with the common
option instead of their own.

Signed-off-by: Mathieu Choplain <mathieu.choplain-ext@st.com>

scripts/west_commands/runners/intel_cyclonev.py
@@ -28,7 +28,7 @@ class IntelCycloneVBinaryRunner(ZephyrBinaryRunner):
                  tcl_port=DEFAULT_OPENOCD_TCL_PORT,
                  telnet_port=DEFAULT_OPENOCD_TELNET_PORT,
                  gdb_port=DEFAULT_OPENOCD_GDB_PORT,
-                 gdb_init=None, no_load=False):
+                 gdb_init=None, load=True):
         super().__init__(cfg)
 
         support = path.join(cfg.board_dir, 'support')
@@ -89,7 +89,7 @@ class IntelCycloneVBinaryRunner(ZephyrBinaryRunner):
         self.serial = ['-c set _ZEPHYR_BOARD_SERIAL ' + serial] if serial else []
         self.use_elf = use_elf
         self.gdb_init = gdb_init
-        self.load_arg = [] if no_load else ['-ex', 'load']
+        self.load_arg = ['-ex', 'load'] if load else []
 
     @classmethod
     def name(cls):
@@ -98,7 +98,7 @@ class IntelCycloneVBinaryRunner(ZephyrBinaryRunner):
     @classmethod
     def capabilities(cls):
         return RunnerCaps(commands={'flash', 'debug', 'attach'},
-                          dev_id=False, flash_addr=False, erase=False)
+                          dev_id=False, flash_addr=False, erase=False, debug_load=True)
 
     @classmethod
     def do_add_parser(cls, parser):
@@ -151,8 +151,6 @@ class IntelCycloneVBinaryRunner(ZephyrBinaryRunner):
                             help='if given, no init issued in gdb server cmd')
         parser.add_argument('--no-targets', action='store_true',
                             help='if given, no target issued in gdb server cmd')
-        parser.add_argument('--no-load', action='store_true',
-                            help='if given, no load issued in gdb server cmd')
 
     @classmethod
     def do_create(cls, cfg, args):
@@ -166,7 +164,7 @@ class IntelCycloneVBinaryRunner(ZephyrBinaryRunner):
             use_elf=args.use_elf, no_halt=args.no_halt, no_init=args.no_init,
             no_targets=args.no_targets, tcl_port=args.tcl_port,
             telnet_port=args.telnet_port, gdb_port=args.gdb_port,
-            gdb_init=args.gdb_init, no_load=args.no_load)
+            gdb_init=args.gdb_init, load=args.load)
 
     def print_gdbserver_message(self):
         if not self.thread_info_enabled:

scripts/west_commands/runners/openocd.py
@@ -57,7 +57,7 @@ class OpenOcdBinaryRunner(ZephyrBinaryRunner):
                  telnet_port=DEFAULT_OPENOCD_TELNET_PORT,
                  gdb_port=DEFAULT_OPENOCD_GDB_PORT,
                  gdb_client_port=DEFAULT_OPENOCD_GDB_PORT,
-                 gdb_init=None, no_load=False,
+                 gdb_init=None, load=True,
                  target_handle=DEFAULT_OPENOCD_TARGET_HANDLE,
                  rtt_port=DEFAULT_OPENOCD_RTT_PORT, rtt_server=False):
         super().__init__(cfg)
@@ -118,7 +118,7 @@ class OpenOcdBinaryRunner(ZephyrBinaryRunner):
         self.serial = ['-c set _ZEPHYR_BOARD_SERIAL ' + serial] if serial else []
         self.use_elf = use_elf
         self.gdb_init = gdb_init
-        self.load_arg = [] if no_load else ['-ex', 'load']
+        self.load_arg = ['-ex', 'load'] if load else []
         self.target_handle = target_handle
         self.rtt_port = rtt_port
         self.rtt_server = rtt_server
@@ -130,7 +130,7 @@ class OpenOcdBinaryRunner(ZephyrBinaryRunner):
     @classmethod
     def capabilities(cls):
         return RunnerCaps(commands={'flash', 'debug', 'debugserver', 'attach', 'rtt'},
-                          rtt=True, erase=True)
+                          rtt=True, erase=True, debug_load=True)
 
     @classmethod
     def do_add_parser(cls, parser):
@@ -188,8 +188,6 @@ class OpenOcdBinaryRunner(ZephyrBinaryRunner):
                             help='if given, no init issued in gdb server cmd')
         parser.add_argument('--no-targets', action='store_true',
                             help='if given, no target issued in gdb server cmd')
-        parser.add_argument('--no-load', action='store_true',
-                            help='if given, no load issued in gdb server cmd')
         parser.add_argument('--target-handle', default=DEFAULT_OPENOCD_TARGET_HANDLE,
                             help=f'''Internal handle used in openocd targets cfg
                             files, defaults to "{DEFAULT_OPENOCD_TARGET_HANDLE}".
@@ -215,7 +213,7 @@ class OpenOcdBinaryRunner(ZephyrBinaryRunner):
             no_targets=args.no_targets, tcl_port=args.tcl_port,
             telnet_port=args.telnet_port, gdb_port=args.gdb_port,
             gdb_client_port=args.gdb_client_port, gdb_init=args.gdb_init,
-            no_load=args.no_load, target_handle=args.target_handle,
+            load=args.load, target_handle=args.target_handle,
             rtt_port=args.rtt_port, rtt_server=args.rtt_server)
 
     def print_gdbserver_message(self):

phytec/zephyr-phytec

scripts/west_commands/runners/intel_cyclonev.py
@@ -28,7 +28,7 @@ class IntelCycloneVBinaryRunner(ZephyrBinaryRunner):
                  tcl_port=DEFAULT_OPENOCD_TCL_PORT,
                  telnet_port=DEFAULT_OPENOCD_TELNET_PORT,
                  gdb_port=DEFAULT_OPENOCD_GDB_PORT,
-                 gdb_init=None, no_load=False):
+                 gdb_init=None, load=True):
         super().__init__(cfg)
 
         support = path.join(cfg.board_dir, 'support')
@@ -89,7 +89,7 @@ class IntelCycloneVBinaryRunner(ZephyrBinaryRunner):
         self.serial = ['-c set _ZEPHYR_BOARD_SERIAL ' + serial] if serial else []
         self.use_elf = use_elf
         self.gdb_init = gdb_init
-        self.load_arg = [] if no_load else ['-ex', 'load']
+        self.load_arg = ['-ex', 'load'] if load else []
 
     @classmethod
     def name(cls):
@@ -98,7 +98,7 @@ class IntelCycloneVBinaryRunner(ZephyrBinaryRunner):
     @classmethod
     def capabilities(cls):
         return RunnerCaps(commands={'flash', 'debug', 'attach'},
-                          dev_id=False, flash_addr=False, erase=False)
+                          dev_id=False, flash_addr=False, erase=False, skip_load=True)
 
     @classmethod
     def do_add_parser(cls, parser):
@@ -151,8 +151,6 @@ class IntelCycloneVBinaryRunner(ZephyrBinaryRunner):
                             help='if given, no init issued in gdb server cmd')
         parser.add_argument('--no-targets', action='store_true',
                             help='if given, no target issued in gdb server cmd')
-        parser.add_argument('--no-load', action='store_true',
-                            help='if given, no load issued in gdb server cmd')
 
     @classmethod
     def do_create(cls, cfg, args):
@@ -166,7 +164,7 @@ class IntelCycloneVBinaryRunner(ZephyrBinaryRunner):
             use_elf=args.use_elf, no_halt=args.no_halt, no_init=args.no_init,
             no_targets=args.no_targets, tcl_port=args.tcl_port,
             telnet_port=args.telnet_port, gdb_port=args.gdb_port,
-            gdb_init=args.gdb_init, no_load=args.no_load)
+            gdb_init=args.gdb_init, load=args.load)
 
     def print_gdbserver_message(self):
         if not self.thread_info_enabled:

scripts/west_commands/runners/openocd.py
@@ -57,7 +57,7 @@ class OpenOcdBinaryRunner(ZephyrBinaryRunner):
                  telnet_port=DEFAULT_OPENOCD_TELNET_PORT,
                  gdb_port=DEFAULT_OPENOCD_GDB_PORT,
                  gdb_client_port=DEFAULT_OPENOCD_GDB_PORT,
-                 gdb_init=None, no_load=False,
+                 gdb_init=None, load=True,
                  target_handle=DEFAULT_OPENOCD_TARGET_HANDLE,
                  rtt_port=DEFAULT_OPENOCD_RTT_PORT, rtt_server=False):
         super().__init__(cfg)
@@ -118,7 +118,7 @@ class OpenOcdBinaryRunner(ZephyrBinaryRunner):
         self.serial = ['-c set _ZEPHYR_BOARD_SERIAL ' + serial] if serial else []
         self.use_elf = use_elf
         self.gdb_init = gdb_init
-        self.load_arg = [] if no_load else ['-ex', 'load']
+        self.load_arg = ['-ex', 'load'] if load else []
         self.target_handle = target_handle
         self.rtt_port = rtt_port
         self.rtt_server = rtt_server
@@ -130,7 +130,7 @@ class OpenOcdBinaryRunner(ZephyrBinaryRunner):
     @classmethod
     def capabilities(cls):
         return RunnerCaps(commands={'flash', 'debug', 'debugserver', 'attach', 'rtt'},
-                          rtt=True, erase=True)
+                          rtt=True, erase=True, skip_load=True)
 
     @classmethod
     def do_add_parser(cls, parser):
@@ -188,8 +188,6 @@ class OpenOcdBinaryRunner(ZephyrBinaryRunner):
                             help='if given, no init issued in gdb server cmd')
         parser.add_argument('--no-targets', action='store_true',
                             help='if given, no target issued in gdb server cmd')
-        parser.add_argument('--no-load', action='store_true',
-                            help='if given, no load issued in gdb server cmd')
         parser.add_argument('--target-handle', default=DEFAULT_OPENOCD_TARGET_HANDLE,
                             help=f'''Internal handle used in openocd targets cfg
                             files, defaults to "{DEFAULT_OPENOCD_TARGET_HANDLE}".
@@ -215,7 +213,7 @@ class OpenOcdBinaryRunner(ZephyrBinaryRunner):
             no_targets=args.no_targets, tcl_port=args.tcl_port,
             telnet_port=args.telnet_port, gdb_port=args.gdb_port,
             gdb_client_port=args.gdb_client_port, gdb_init=args.gdb_init,
-            no_load=args.no_load, target_handle=args.target_handle,
+            load=args.load, target_handle=args.target_handle,
             rtt_port=args.rtt_port, rtt_server=args.rtt_server)
 
     def print_gdbserver_message(self):