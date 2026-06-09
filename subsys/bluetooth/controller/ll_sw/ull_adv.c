zephyrproject-rtos/zephyr

Bluetooth: controller: split: Added Adv Random Address set
Added implementation to set advertising random address.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/ull_adv.c
@@ -480,9 +480,10 @@ u8_t ll_adv_enable(u8_t enable)
 
 		/* AdvA, fill here at enable */
 		if (h->adv_addr) {
-			u8_t *tx_addr = ll_addr_get(pdu_adv->tx_addr, NULL);
+			u8_t *tx_addr = ll_adv_aux_random_addr_get(adv, NULL);
 
-			/* TODO: Privacy check */
+			/* TODO: Privacy */
+
 			if (pdu_adv->tx_addr && !mem_nz(tx_addr, BDADDR_SIZE)) {
 				return BT_HCI_ERR_INVALID_PARAM;
 			}
@@ -502,10 +503,11 @@ u8_t ll_adv_enable(u8_t enable)
 			ps = (u8_t *)hs + sizeof(*hs);
 
 			if (hs->adv_addr) {
-				u8_t *tx_addr = ll_addr_get(pdu_aux->tx_addr,
-							    NULL);
+				u8_t *tx_addr =
+					ll_adv_aux_random_addr_get(adv,	NULL);
 
-				/* TODO: Privacy check */
+				/* TODO: Privacy */
+
 				if (pdu_aux->tx_addr &&
 				    !mem_nz(tx_addr, BDADDR_SIZE)) {
 					return BT_HCI_ERR_INVALID_PARAM;

subsys/bluetooth/controller/ll_sw/ull_adv_aux.c
@@ -53,14 +53,30 @@ static void *adv_aux_free;
 
 u8_t ll_adv_aux_random_addr_set(u8_t handle, u8_t *addr)
 {
-	/* TODO: store in adv set instance */
+	struct ll_adv_set *adv;
+
+	adv = ull_adv_is_created_get(handle);
+	if (!adv) {
+		return BT_HCI_ERR_CMD_DISALLOWED;
+	}
+
+	/* TODO: Fail if connectable advertising is enabled */
+	if (0) {
+		return BT_HCI_ERR_CMD_DISALLOWED;
+	}
+
+	memcpy(adv->rnd_addr, addr, BDADDR_SIZE);
+
 	return 0;
 }
 
-u8_t *ll_adv_aux_random_addr_get(u8_t handle, u8_t *addr)
+u8_t *ll_adv_aux_random_addr_get(struct ll_adv_set *adv, u8_t *addr)
 {
-	/* TODO: copy adv set instance addr into addr and/or return reference */
-	return NULL;
+	if (addr) {
+		memcpy(addr, adv->rnd_addr, BDADDR_SIZE);
+	}
+
+	return adv->rnd_addr;
 }
 
 #if (CONFIG_BT_CTLR_ADV_AUX_SET > 0)

subsys/bluetooth/controller/ll_sw/ull_adv_internal.h
@@ -37,6 +37,9 @@ u8_t scan_rsp_set(struct ll_adv_set *adv, u8_t len, u8_t const *const data);
 int ull_adv_aux_init(void);
 int ull_adv_aux_reset(void);
 
+/* Helper to read back random address */
+u8_t *ll_adv_aux_random_addr_get(struct ll_adv_set *adv, u8_t *addr);
+
 /* helper function to start auxiliary advertising */
 u32_t ull_adv_aux_start(struct ll_adv_aux_set *aux, u32_t ticks_anchor,
 			u32_t volatile *ret_cb);

subsys/bluetooth/controller/ll_sw/ull_adv_types.h
@@ -16,6 +16,7 @@ struct ll_adv_set {
 
 #if defined(CONFIG_BT_CTLR_ADV_EXT)
 	u32_t interval;
+	u8_t  rnd_addr[BDADDR_SIZE];
 	u8_t  sid:4;
 	u8_t  is_created:1;
 #else /* !CONFIG_BT_CTLR_ADV_EXT */

phytec/zephyr-phytec

subsys/bluetooth/controller/ll_sw/ull_adv.c
@@ -481,9 +481,11 @@ uint8_t ll_adv_enable(uint8_t enable)
 
 		/* AdvA, fill here at enable */
 		if (h->adv_addr) {
-			uint8_t *tx_addr = ll_addr_get(pdu_adv->tx_addr, NULL);
+			uint8_t *tx_addr =
+					ll_adv_aux_random_addr_get(adv, NULL);
 
-			/* TODO: Privacy check */
+			/* TODO: Privacy */
+
 			if (pdu_adv->tx_addr && !mem_nz(tx_addr, BDADDR_SIZE)) {
 				return BT_HCI_ERR_INVALID_PARAM;
 			}
@@ -503,10 +505,11 @@ uint8_t ll_adv_enable(uint8_t enable)
 			ps = (uint8_t *)hs + sizeof(*hs);
 
 			if (hs->adv_addr) {
-				uint8_t *tx_addr = ll_addr_get(pdu_aux->tx_addr,
-							    NULL);
+				uint8_t *tx_addr =
+					ll_adv_aux_random_addr_get(adv,	NULL);
 
-				/* TODO: Privacy check */
+				/* TODO: Privacy */
+
 				if (pdu_aux->tx_addr &&
 				    !mem_nz(tx_addr, BDADDR_SIZE)) {
 					return BT_HCI_ERR_INVALID_PARAM;

subsys/bluetooth/controller/ll_sw/ull_adv_aux.c
@@ -53,14 +53,30 @@ static void *adv_aux_free;
 
 uint8_t ll_adv_aux_random_addr_set(uint8_t handle, uint8_t *addr)
 {
-	/* TODO: store in adv set instance */
+	struct ll_adv_set *adv;
+
+	adv = ull_adv_is_created_get(handle);
+	if (!adv) {
+		return BT_HCI_ERR_CMD_DISALLOWED;
+	}
+
+	/* TODO: Fail if connectable advertising is enabled */
+	if (0) {
+		return BT_HCI_ERR_CMD_DISALLOWED;
+	}
+
+	memcpy(adv->rnd_addr, addr, BDADDR_SIZE);
+
 	return 0;
 }
 
-uint8_t *ll_adv_aux_random_addr_get(uint8_t handle, uint8_t *addr)
+uint8_t *ll_adv_aux_random_addr_get(struct ll_adv_set *adv, uint8_t *addr)
 {
-	/* TODO: copy adv set instance addr into addr and/or return reference */
-	return NULL;
+	if (addr) {
+		memcpy(addr, adv->rnd_addr, BDADDR_SIZE);
+	}
+
+	return adv->rnd_addr;
 }
 
 #if (CONFIG_BT_CTLR_ADV_AUX_SET > 0)

subsys/bluetooth/controller/ll_sw/ull_adv_internal.h
@@ -39,6 +39,9 @@ uint8_t scan_rsp_set(struct ll_adv_set *adv, uint8_t len,
 int ull_adv_aux_init(void);
 int ull_adv_aux_reset(void);
 
+/* Helper to read back random address */
+uint8_t *ll_adv_aux_random_addr_get(struct ll_adv_set *adv, uint8_t *addr);
+
 /* helper function to start auxiliary advertising */
 uint32_t ull_adv_aux_start(struct ll_adv_aux_set *aux, uint32_t ticks_anchor,
 			uint32_t volatile *ret_cb);

subsys/bluetooth/controller/ll_sw/ull_adv_types.h
@@ -16,6 +16,7 @@ struct ll_adv_set {
 
 #if defined(CONFIG_BT_CTLR_ADV_EXT)
 	uint32_t interval;
+	uint8_t  rnd_addr[BDADDR_SIZE];
 	uint8_t  sid:4;
 	uint8_t  is_created:1;
 #else /* !CONFIG_BT_CTLR_ADV_EXT */