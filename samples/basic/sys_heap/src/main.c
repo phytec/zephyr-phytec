ipankr/zephyr

kernel: sys_heap: stats: save heap pointers to an array during init
To request heap statistics, a pointer to a heap structure is required.
This is straightforward for a user-defined heap. However, such a pointer
is not known for heaps created by other components or libraries, like
libc. Therefore, it is not possible to calculate the total heap memory.

The proposed solution is to use an array of pointers, which is filled in
on every sys_heap_init() call. One can then iterate through it to sum up
the total memory allocated for all heaps.

The array size is configurable. The default array size is zero,
which means the feature is disabled. Any other integer greater then zero
defines the array size and enables the feature.

A list of pointers instead of an array could be another approach,
but it requeres a heap, which is not always available.

Signed-off-by: Ivan Pankratov <ivan.pankratov@silabs.com>

samples/basic/sys_heap/src/main.c
@@ -9,10 +9,13 @@
 
 #define HEAP_SIZE	256
 
+K_HEAP_DEFINE(my_kernel_heap, HEAP_SIZE);
+
 static char heap_mem[HEAP_SIZE];
 static struct sys_heap heap;
 
-static void print_sys_memory_stats(void);
+static void print_sys_memory_stats(struct sys_heap *);
+static void print_all_heaps(void);
 
 int main(void)
 {
@@ -21,26 +24,42 @@ int main(void)
 	printk("System heap sample\n\n");
 
 	sys_heap_init(&heap, heap_mem, HEAP_SIZE);
-	print_sys_memory_stats();
+	print_sys_memory_stats(&heap);
 
 	p = sys_heap_alloc(&heap, 150);
-	print_sys_memory_stats();
+	print_sys_memory_stats(&heap);
 
 	p = sys_heap_realloc(&heap, p, 100);
-	print_sys_memory_stats();
+	print_sys_memory_stats(&heap);
 
 	sys_heap_free(&heap, p);
-	print_sys_memory_stats();
+	print_sys_memory_stats(&heap);
+
+	print_all_heaps();
 	return 0;
 }
 
-static void print_sys_memory_stats(void)
+static void print_sys_memory_stats(struct sys_heap *hp)
 {
 	struct sys_memory_stats stats;
 
-	sys_heap_runtime_stats_get(&heap, &stats);
+	sys_heap_runtime_stats_get(hp, &stats);
 
 	printk("allocated %zu, free %zu, max allocated %zu, heap size %u\n",
 		stats.allocated_bytes, stats.free_bytes,
 		stats.max_allocated_bytes, HEAP_SIZE);
+}
+
+static void print_all_heaps(void)
+{
+	struct sys_heap **ha;
+	size_t i, n;
+
+	n = sys_heap_array_get(&ha);
+	printk("There are %d heaps allocated:\n", n);
+
+	for (i = 0; i < n; i++) {
+		printk("\t%d - address %p ", i, ha[i]);
+		print_sys_memory_stats(ha[i]);
+	}
 }

phytec/zephyr-phytec

samples/basic/sys_heap/src/main.c
@@ -9,10 +9,13 @@
 
 #define HEAP_SIZE	256
 
+K_HEAP_DEFINE(my_kernel_heap, HEAP_SIZE);
+
 static char heap_mem[HEAP_SIZE];
 static struct sys_heap heap;
 
-static void print_sys_memory_stats(void);
+static void print_sys_memory_stats(struct sys_heap *);
+static void print_all_heaps(void);
 
 int main(void)
 {
@@ -21,26 +24,42 @@ int main(void)
 	printk("System heap sample\n\n");
 
 	sys_heap_init(&heap, heap_mem, HEAP_SIZE);
-	print_sys_memory_stats();
+	print_sys_memory_stats(&heap);
 
 	p = sys_heap_alloc(&heap, 150);
-	print_sys_memory_stats();
+	print_sys_memory_stats(&heap);
 
 	p = sys_heap_realloc(&heap, p, 100);
-	print_sys_memory_stats();
+	print_sys_memory_stats(&heap);
 
 	sys_heap_free(&heap, p);
-	print_sys_memory_stats();
+	print_sys_memory_stats(&heap);
+
+	print_all_heaps();
 	return 0;
 }
 
-static void print_sys_memory_stats(void)
+static void print_sys_memory_stats(struct sys_heap *hp)
 {
 	struct sys_memory_stats stats;
 
-	sys_heap_runtime_stats_get(&heap, &stats);
+	sys_heap_runtime_stats_get(hp, &stats);
 
 	printk("allocated %zu, free %zu, max allocated %zu, heap size %u\n",
 		stats.allocated_bytes, stats.free_bytes,
 		stats.max_allocated_bytes, HEAP_SIZE);
+}
+
+static void print_all_heaps(void)
+{
+	struct sys_heap **ha;
+	size_t i, n;
+
+	n = sys_heap_array_get(&ha);
+	printk("There are %zu heaps allocated:\n", n);
+
+	for (i = 0; i < n; i++) {
+		printk("\t%zu - address %p ", i, ha[i]);
+		print_sys_memory_stats(ha[i]);
+	}
 }