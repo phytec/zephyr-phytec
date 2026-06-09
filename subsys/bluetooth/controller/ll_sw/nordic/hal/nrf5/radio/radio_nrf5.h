ioannisg/zephyr

bluetooth: controller: include nRF5340-specific radio header
Conditionally include nrf5340_radio.h when building the
BLE controller for nRF5340 SoC. Introduce the header for
nrf5340.

Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/radio/radio_nrf5.h
@@ -32,6 +32,8 @@
 #include "radio_nrf52832.h"
 #elif defined(CONFIG_SOC_NRF52840)
 #include "radio_nrf52840.h"
+#elif defined(CONFIG_SOC_NRF5340_CPU1)
+#include "radio_nrf5340.h"
 #endif
 
 #include "radio_nrf5_ppi.h"

phytec/zephyr-phytec

subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/radio/radio_nrf5.h
@@ -32,6 +32,8 @@
 #include "radio_nrf52832.h"
 #elif defined(CONFIG_SOC_NRF52840)
 #include "radio_nrf52840.h"
+#elif defined(CONFIG_SOC_NRF5340_CPUNET)
+#include "radio_nrf5340.h"
 #endif
 
 #include "radio_nrf5_ppi.h"