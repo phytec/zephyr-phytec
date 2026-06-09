smartin-vossloh/zephyr

settings fcb: Increase buffer for mcu with large write block size
Devices with write block size greater than 16 could not use settings_fcb
due to small buffer size.
Update value in test as well.

Signed-off-by: Nicolas VINCENT <nicolas.vincent@vossloh.com>

tests/subsys/settings/fcb/src/settings_test_fcb.c
@@ -351,7 +351,7 @@ void test_settings_check_target(void)
 	zassert_true(rc == 0, "Can't open storage flash area");
 
 	wbs = flash_area_align(fap);
-	zassert_true(wbs <= 16,
+	zassert_true(wbs <= 32,
 		"Flash driver is not compatible with the settings fcb-backend");
 }
 

phytec/zephyr-phytec

tests/subsys/settings/fcb/src/settings_test_fcb.c
@@ -354,7 +354,7 @@ void *settings_config_fcb_setup(void)
 	zassume_true(rc == 0, "Can't open storage flash area");
 
 	wbs = flash_area_align(fap);
-	zassume_true(wbs <= 16,
+	zassume_true(wbs <= 32,
 		"Flash driver is not compatible with the settings fcb-backend");
 	return NULL;
 }