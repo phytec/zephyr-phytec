zephyrproject-rtos/zephyr

drivers: uart_pl011: Get clock frequency from DTS
The uart_pl011 driver used system clock frequency
as a base for baudrate calculation. This commit corrects
that by obtaining the needed value from DTS.

Signed-off-by: Piotr Zięcik <piotr.ziecik@nordicsemi.no>

soc/arm/arm/musca_a/dts_fixup.h
@@ -17,6 +17,7 @@
 #define DT_PL011_PORT0_IRQ_RXTIM		DT_ARM_PL011_40101000_IRQ_RXTIM
 #define DT_PL011_PORT0_IRQ_ERR			DT_ARM_PL011_40101000_IRQ_ERR
 #define DT_PL011_PORT0_IRQ_PRI			DT_ARM_PL011_40101000_IRQ_0_PRIORITY
+#define DT_PL011_PORT0_CLOCK_FREQUENCY		DT_ARM_PL011_40101000_CLOCK_FREQUENCY
 #define DT_PL011_PORT0_BAUD_RATE		DT_ARM_PL011_40101000_CURRENT_SPEED
 #define DT_PL011_PORT0_NAME			DT_ARM_PL011_40101000_LABEL
 
@@ -26,6 +27,7 @@
 #define DT_PL011_PORT1_IRQ_RXTIM		DT_ARM_PL011_40102000_IRQ_RXTIM
 #define DT_PL011_PORT1_IRQ_ERR			DT_ARM_PL011_40102000_IRQ_ERR
 #define DT_PL011_PORT1_IRQ_PRI			DT_ARM_PL011_40102000_IRQ_0_PRIORITY
+#define DT_PL011_PORT1_CLOCK_FREQUENCY		DT_ARM_PL011_40102000_CLOCK_FREQUENCY
 #define DT_PL011_PORT1_BAUD_RATE		DT_ARM_PL011_40102000_CURRENT_SPEED
 #define DT_PL011_PORT1_NAME			DT_ARM_PL011_40102000_LABEL
 
@@ -41,6 +43,7 @@
 #define DT_PL011_PORT0_IRQ_RXTIM		DT_ARM_PL011_50101000_IRQ_RXTIM
 #define DT_PL011_PORT0_IRQ_ERR			DT_ARM_PL011_50101000_IRQ_ERR
 #define DT_PL011_PORT0_IRQ_PRI			DT_ARM_PL011_50101000_IRQ_0_PRIORITY
+#define DT_PL011_PORT0_CLOCK_FREQUENCY		DT_ARM_PL011_50101000_CLOCK_FREQUENCY
 #define DT_PL011_PORT0_BAUD_RATE		DT_ARM_PL011_50101000_CURRENT_SPEED
 #define DT_PL011_PORT0_NAME			DT_ARM_PL011_50101000_LABEL
 
@@ -50,6 +53,7 @@
 #define DT_PL011_PORT1_IRQ_RXTIM		DT_ARM_PL011_50102000_IRQ_RXTIM
 #define DT_PL011_PORT1_IRQ_ERR			DT_ARM_PL011_50102000_IRQ_ERR
 #define DT_PL011_PORT1_IRQ_PRI			DT_ARM_PL011_50102000_IRQ_0_PRIORITY
+#define DT_PL011_PORT1_CLOCK_FREQUENCY		DT_ARM_PL011_50102000_CLOCK_FREQUENCY
 #define DT_PL011_PORT1_BAUD_RATE		DT_ARM_PL011_50102000_CURRENT_SPEED
 #define DT_PL011_PORT1_NAME			DT_ARM_PL011_50102000_LABEL
 

soc/arm/arm/musca_b1/dts_fixup.h
@@ -17,6 +17,7 @@
 #define DT_PL011_PORT0_IRQ_RXTIM		DT_ARM_PL011_40105000_IRQ_RXTIM
 #define DT_PL011_PORT0_IRQ_ERR			DT_ARM_PL011_40105000_IRQ_ERR
 #define DT_PL011_PORT0_IRQ_PRI			DT_ARM_PL011_40105000_IRQ_0_PRIORITY
+#define DT_PL011_PORT0_CLOCK_FREQUENCY		DT_ARM_PL011_40105000_CLOCK_FREQUENCY
 #define DT_PL011_PORT0_BAUD_RATE		DT_ARM_PL011_40105000_CURRENT_SPEED
 #define DT_PL011_PORT0_NAME			DT_ARM_PL011_40105000_LABEL
 
@@ -26,6 +27,7 @@
 #define DT_PL011_PORT1_IRQ_RXTIM		DT_ARM_PL011_40106000_IRQ_RXTIM
 #define DT_PL011_PORT1_IRQ_ERR			DT_ARM_PL011_40106000_IRQ_ERR
 #define DT_PL011_PORT1_IRQ_PRI			DT_ARM_PL011_40106000_IRQ_0_PRIORITY
+#define DT_PL011_PORT1_CLOCK_FREQUENCY		DT_ARM_PL011_40106000_CLOCK_FREQUENCY
 #define DT_PL011_PORT1_BAUD_RATE		DT_ARM_PL011_40106000_CURRENT_SPEED
 #define DT_PL011_PORT1_NAME			DT_ARM_PL011_40106000_LABEL
 
@@ -41,6 +43,7 @@
 #define DT_PL011_PORT0_IRQ_RXTIM		DT_ARM_PL011_50105000_IRQ_RXTIM
 #define DT_PL011_PORT0_IRQ_ERR			DT_ARM_PL011_50105000_IRQ_ERR
 #define DT_PL011_PORT0_IRQ_PRI			DT_ARM_PL011_50105000_IRQ_0_PRIORITY
+#define DT_PL011_PORT0_CLOCK_FREQUENCY		DT_ARM_PL011_50105000_CLOCK_FREQUENCY
 #define DT_PL011_PORT0_BAUD_RATE		DT_ARM_PL011_50105000_CURRENT_SPEED
 #define DT_PL011_PORT0_NAME			DT_ARM_PL011_50105000_LABEL
 
@@ -50,6 +53,7 @@
 #define DT_PL011_PORT1_IRQ_RXTIM		DT_ARM_PL011_50106000_IRQ_RXTIM
 #define DT_PL011_PORT1_IRQ_ERR			DT_ARM_PL011_50106000_IRQ_ERR
 #define DT_PL011_PORT1_IRQ_PRI			DT_ARM_PL011_50106000_IRQ_0_PRIORITY
+#define DT_PL011_PORT1_CLOCK_FREQUENCY		DT_ARM_PL011_50106000_CLOCK_FREQUENCY
 #define DT_PL011_PORT1_BAUD_RATE		DT_ARM_PL011_50106000_CURRENT_SPEED
 #define DT_PL011_PORT1_NAME			DT_ARM_PL011_50106000_LABEL
 

phytec/zephyr-phytec

soc/arm/arm/musca_a/dts_fixup.h
@@ -19,6 +19,7 @@
 #define DT_PL011_PORT0_IRQ_RXTIM		DT_ARM_PL011_40101000_IRQ_RXTIM
 #define DT_PL011_PORT0_IRQ_ERR			DT_ARM_PL011_40101000_IRQ_ERR
 #define DT_PL011_PORT0_IRQ_PRI			DT_ARM_PL011_40101000_IRQ_0_PRIORITY
+#define DT_PL011_PORT0_CLOCK_FREQUENCY		DT_ARM_PL011_40101000_CLOCKS_CLOCK_FREQUENCY
 #define DT_PL011_PORT0_BAUD_RATE		DT_ARM_PL011_40101000_CURRENT_SPEED
 #define DT_PL011_PORT0_NAME			DT_ARM_PL011_40101000_LABEL
 
@@ -28,6 +29,7 @@
 #define DT_PL011_PORT1_IRQ_RXTIM		DT_ARM_PL011_40102000_IRQ_RXTIM
 #define DT_PL011_PORT1_IRQ_ERR			DT_ARM_PL011_40102000_IRQ_ERR
 #define DT_PL011_PORT1_IRQ_PRI			DT_ARM_PL011_40102000_IRQ_0_PRIORITY
+#define DT_PL011_PORT1_CLOCK_FREQUENCY		DT_ARM_PL011_40102000_CLOCKS_CLOCK_FREQUENCY
 #define DT_PL011_PORT1_BAUD_RATE		DT_ARM_PL011_40102000_CURRENT_SPEED
 #define DT_PL011_PORT1_NAME			DT_ARM_PL011_40102000_LABEL
 
@@ -43,6 +45,7 @@
 #define DT_PL011_PORT0_IRQ_RXTIM		DT_ARM_PL011_50101000_IRQ_RXTIM
 #define DT_PL011_PORT0_IRQ_ERR			DT_ARM_PL011_50101000_IRQ_ERR
 #define DT_PL011_PORT0_IRQ_PRI			DT_ARM_PL011_50101000_IRQ_0_PRIORITY
+#define DT_PL011_PORT0_CLOCK_FREQUENCY		DT_ARM_PL011_50101000_CLOCKS_CLOCK_FREQUENCY
 #define DT_PL011_PORT0_BAUD_RATE		DT_ARM_PL011_50101000_CURRENT_SPEED
 #define DT_PL011_PORT0_NAME			DT_ARM_PL011_50101000_LABEL
 
@@ -52,6 +55,7 @@
 #define DT_PL011_PORT1_IRQ_RXTIM		DT_ARM_PL011_50102000_IRQ_RXTIM
 #define DT_PL011_PORT1_IRQ_ERR			DT_ARM_PL011_50102000_IRQ_ERR
 #define DT_PL011_PORT1_IRQ_PRI			DT_ARM_PL011_50102000_IRQ_0_PRIORITY
+#define DT_PL011_PORT1_CLOCK_FREQUENCY		DT_ARM_PL011_50102000_CLOCKS_CLOCK_FREQUENCY
 #define DT_PL011_PORT1_BAUD_RATE		DT_ARM_PL011_50102000_CURRENT_SPEED
 #define DT_PL011_PORT1_NAME			DT_ARM_PL011_50102000_LABEL
 

soc/arm/arm/musca_b1/dts_fixup.h
@@ -19,6 +19,7 @@
 #define DT_PL011_PORT0_IRQ_RXTIM		DT_ARM_PL011_40105000_IRQ_RXTIM
 #define DT_PL011_PORT0_IRQ_ERR			DT_ARM_PL011_40105000_IRQ_ERR
 #define DT_PL011_PORT0_IRQ_PRI			DT_ARM_PL011_40105000_IRQ_0_PRIORITY
+#define DT_PL011_PORT0_CLOCK_FREQUENCY		DT_ARM_PL011_40105000_CLOCKS_CLOCK_FREQUENCY
 #define DT_PL011_PORT0_BAUD_RATE		DT_ARM_PL011_40105000_CURRENT_SPEED
 #define DT_PL011_PORT0_NAME			DT_ARM_PL011_40105000_LABEL
 
@@ -28,6 +29,7 @@
 #define DT_PL011_PORT1_IRQ_RXTIM		DT_ARM_PL011_40106000_IRQ_RXTIM
 #define DT_PL011_PORT1_IRQ_ERR			DT_ARM_PL011_40106000_IRQ_ERR
 #define DT_PL011_PORT1_IRQ_PRI			DT_ARM_PL011_40106000_IRQ_0_PRIORITY
+#define DT_PL011_PORT1_CLOCK_FREQUENCY		DT_ARM_PL011_40106000_CLOCKS_CLOCK_FREQUENCY
 #define DT_PL011_PORT1_BAUD_RATE		DT_ARM_PL011_40106000_CURRENT_SPEED
 #define DT_PL011_PORT1_NAME			DT_ARM_PL011_40106000_LABEL
 
@@ -43,6 +45,7 @@
 #define DT_PL011_PORT0_IRQ_RXTIM		DT_ARM_PL011_50105000_IRQ_RXTIM
 #define DT_PL011_PORT0_IRQ_ERR			DT_ARM_PL011_50105000_IRQ_ERR
 #define DT_PL011_PORT0_IRQ_PRI			DT_ARM_PL011_50105000_IRQ_0_PRIORITY
+#define DT_PL011_PORT0_CLOCK_FREQUENCY		DT_ARM_PL011_50105000_CLOCKS_CLOCK_FREQUENCY
 #define DT_PL011_PORT0_BAUD_RATE		DT_ARM_PL011_50105000_CURRENT_SPEED
 #define DT_PL011_PORT0_NAME			DT_ARM_PL011_50105000_LABEL
 
@@ -52,6 +55,7 @@
 #define DT_PL011_PORT1_IRQ_RXTIM		DT_ARM_PL011_50106000_IRQ_RXTIM
 #define DT_PL011_PORT1_IRQ_ERR			DT_ARM_PL011_50106000_IRQ_ERR
 #define DT_PL011_PORT1_IRQ_PRI			DT_ARM_PL011_50106000_IRQ_0_PRIORITY
+#define DT_PL011_PORT1_CLOCK_FREQUENCY		DT_ARM_PL011_50106000_CLOCKS_CLOCK_FREQUENCY
 #define DT_PL011_PORT1_BAUD_RATE		DT_ARM_PL011_50106000_CURRENT_SPEED
 #define DT_PL011_PORT1_NAME			DT_ARM_PL011_50106000_LABEL
 