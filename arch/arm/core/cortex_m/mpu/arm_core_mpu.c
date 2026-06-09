ioannisg/zephyr

arch: arm: make priv stack guard programming similar to normal guard
This commit aligns the programming of the privileged stack MPU
guard with that of the default stack guard (i.e of supervisor
threads). In particular:
- the guard is programmed BELOW the address indicated in
  arch.priv_stack_start; it is, therefore, similar to the
  default guard that is programmed BELOW stack_info.start.
- the stack fail check is updated accordningly
- arch.priv_stack_start is adjusted in arch_userspace_enter(),
  to make sure we account for a (possible) guard requirement,
  that is, if building with CONFIG_MPU_STACK_GUARD=y.

Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

arch/arm/core/cortex_m/mpu/arm_core_mpu.c
@@ -214,7 +214,11 @@ void z_arch_configure_dynamic_mpu_regions(struct k_thread *thread)
 	u32_t guard_start;
 #if defined(CONFIG_USERSPACE)
 	if (thread->arch.priv_stack_start) {
-		guard_start = thread->arch.priv_stack_start;
+		guard_start = thread->arch.priv_stack_start -
+			MPU_GUARD_ALIGN_AND_SIZE;
+		__ASSERT((u32_t)&_priv_stacks_ram_start <= guard_start,
+		"Guard start: (0x%x) below privilege stacks boundary: (0x%x)",
+		guard_start, (u32_t)&_priv_stacks_ram_start);
 	} else {
 		guard_start = thread->stack_info.start -
 			MPU_GUARD_ALIGN_AND_SIZE;

phytec/zephyr-phytec

  An ASSERT is added to confirm that the guard is programmed
  inside the thread privilege stack area.

arch/arm/core/cortex_m/mpu/arm_core_mpu.c
@@ -214,7 +214,11 @@ void z_arch_configure_dynamic_mpu_regions(struct k_thread *thread)
 	u32_t guard_start;
 #if defined(CONFIG_USERSPACE)
 	if (thread->arch.priv_stack_start) {
-		guard_start = thread->arch.priv_stack_start;
+		guard_start = thread->arch.priv_stack_start -
+			MPU_GUARD_ALIGN_AND_SIZE;
+		__ASSERT((u32_t)&z_priv_stacks_ram_start <= guard_start,
+		"Guard start: (0x%x) below privilege stacks boundary: (0x%x)",
+		guard_start, (u32_t)&z_priv_stacks_ram_start);
 	} else {
 		guard_start = thread->stack_info.start -
 			MPU_GUARD_ALIGN_AND_SIZE;