stephanosio/zephyr

tests: kernel: fp_sharing: Use ztest
This commit converts the `fp_sharing` tests to use the ztest framework.

In addition, this commit also introduces a behavioural change to run
the `pi` unit test separately from the `load_store` unit test, in order
to allow more manageable and diagnosable test execution.

Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

tests/kernel/fp_sharing/generic/src/load_store.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2011-2014 Wind River Systems, Inc.
+ * Copyright (c) 2020 Stephanos Ioannidis <root@stephanos.io>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -34,10 +35,7 @@
  * x87 FPU registers are being saved/restored.
  */
 
-#include <zephyr.h>
-#include <tc_util.h>
-#include <stddef.h>
-#include <string.h>
+#include <ztest.h>
 #include <debug/gcov.h>
 
 #if defined(CONFIG_X86)
@@ -64,29 +62,26 @@
 #include "test_common.h"
 
 /* space for float register load/store area used by low priority task */
-
 static struct fp_register_set float_reg_set_load;
 static struct fp_register_set float_reg_set_store;
 
 /* space for float register load/store area used by high priority thread */
-
 static struct fp_register_set float_reg_set;
-
-
-/* flag indicating that an error has occurred */
-
-int fpu_sharing_error;
 
 /*
  * Test counters are "volatile" because GCC may not update them properly
  * otherwise. (See description of pi calculation test for more details.)
  */
-
 static volatile unsigned int load_store_low_count;
 static volatile unsigned int load_store_high_count;
 
+/* Indicates that the load/store test exited */
+static bool test_exited;
+
+/* Semaphore for signaling end of test */
+static K_SEM_DEFINE(test_exit_sem, 0, 1);
+
 /**
- *
  * @brief Low priority FPU load/store thread
  *
  * @ingroup kernel_fpsharing_tests
@@ -94,58 +89,38 @@ static volatile unsigned int load_store_high_count;
  * @see k_sched_time_slice_set(), memset(),
  * _load_all_float_registers(), _store_all_float_registers()
  */
-
-void load_store_low(void)
+static void load_store_low(void)
 {
 	unsigned int i;
+	bool error = false;
 	unsigned char init_byte;
 	unsigned char *store_ptr = (unsigned char *)&float_reg_set_store;
 	unsigned char *load_ptr = (unsigned char *)&float_reg_set_load;
 
 	volatile char volatile_stack_var = 0;
 
-	PRINT_DATA("Floating point sharing tests started\n");
-	PRINT_LINE;
-
-	/*
-	 * The high priority thread has a sleep to get this (low pri) thread
-	 * running and here (low priority) we enable slicing and waste cycles
-	 * to run hi pri thread in between fp ops.
-	 *
-	 * Enable round robin scheduling to allow both the low priority pi
-	 * computation and load/store tasks to execute. The high priority pi
-	 * computation and load/store tasks will preempt the low priority tasks
-	 * periodically.
-	 */
-
-	k_sched_time_slice_set(10, THREAD_LOW_PRIORITY);
-
 	/*
 	 * Initialize floating point load buffer to known values;
 	 * these values must be different than the value used in other threads.
 	 */
-
 	init_byte = MAIN_FLOAT_REG_CHECK_BYTE;
-	for (i = 0U; i < SIZEOF_FP_REGISTER_SET; i++) {
+	for (i = 0; i < SIZEOF_FP_REGISTER_SET; i++) {
 		load_ptr[i] = init_byte++;
 	}
 
-	/* Keep cranking forever, or until an error is detected. */
-
-	for (load_store_low_count = 0U;; load_store_low_count++) {
+	/* Loop until the test finishes, or an error is detected. */
+	for (load_store_low_count = 0; !test_exited; load_store_low_count++) {
 
 		/*
 		 * Clear store buffer to erase all traces of any previous
 		 * floating point values that have been saved.
 		 */
-
 		(void)memset(&float_reg_set_store, 0, SIZEOF_FP_REGISTER_SET);
 
 		/*
 		 * Utilize an architecture specific function to load all the
 		 * floating point registers with known values.
 		 */
-
 		_load_all_float_registers(&float_reg_set_load);
 
 		/*
@@ -156,8 +131,7 @@ void load_store_low(void)
 		 * IMPORTANT: This logic requires that z_tick_get_32() not
 		 * perform any floating point operations!
 		 */
-
-		while ((z_tick_get_32() % 5) != 0U) {
+		while ((z_tick_get_32() % 5) != 0) {
 			/*
 			 * Use a volatile variable to prevent compiler
 			 * optimizing out the spin loop.
@@ -169,7 +143,6 @@ void load_store_low(void)
 		 * Utilize an architecture specific function to dump the
 		 * contents of all floating point registers to memory.
 		 */
-
 		_store_all_float_registers(&float_reg_set_store);
 
 		/*
@@ -180,32 +153,24 @@ void load_store_low(void)
 		 * Display error message and terminate if discrepancies are
 		 * detected.
 		 */
-
 		init_byte = MAIN_FLOAT_REG_CHECK_BYTE;
 
-		for (i = 0U; i < SIZEOF_FP_REGISTER_SET; i++) {
+		for (i = 0; i < SIZEOF_FP_REGISTER_SET; i++) {
 			if (store_ptr[i] != init_byte) {
-				TC_ERROR("load_store_low found 0x%x instead "
-					 "of 0x%x @ offset 0x%x\n",
+				TC_ERROR("Found 0x%x instead of 0x%x @ "
+					 "offset 0x%x\n",
 					 store_ptr[i],
 					 init_byte, i);
 				TC_ERROR("Discrepancy found during "
 					 "iteration %d\n",
 					 load_store_low_count);
-				fpu_sharing_error = 1;
+				error = true;
 			}
 			init_byte++;
 		}
 
-		/*
-		 * Terminate if a test error has been reported.
-		 */
-
-		if (fpu_sharing_error) {
-			TC_END_RESULT(TC_FAIL);
-			TC_END_REPORT(TC_FAIL);
-			return;
-		}
+		/* Terminate if a test error has been reported */
+		zassert_false(error, NULL);
 
 		/*
 		 * After every 1000 iterations (arbitrarily chosen), explicitly
@@ -230,7 +195,7 @@ void load_store_low(void)
 		 * is useful for testing automatic thread enabling of floating
 		 * point as soon as FP registers are used, again by the thread.
 		 */
-		if ((load_store_low_count % 1000) == 0U) {
+		if ((load_store_low_count % 1000) == 0) {
 			k_float_disable(k_current_get());
 		}
 #endif
@@ -238,23 +203,23 @@ void load_store_low(void)
 }
 
 /**
- *
  * @brief High priority FPU load/store thread
  *
  * @ingroup kernel_fpsharing_tests
  *
  * @see _load_then_store_all_float_registers()
  */
-
-void load_store_high(void)
+static void load_store_high(void)
 {
 	unsigned int i;
 	unsigned char init_byte;
 	unsigned char *reg_set_ptr = (unsigned char *)&float_reg_set;
 
-	/* test until the specified time limit, or until an error is detected */
+	/* Run the test until the specified maximum test count is reached */
+	for (load_store_high_count = 0;
+	     load_store_high_count <= MAX_TESTS;
+	     load_store_high_count++) {
 
-	while (1) {
 		/*
 		 * Initialize the float_reg_set structure by treating it as
 		 * a simple array of bytes (the arrangement and actual number
@@ -270,10 +235,9 @@ void load_store_high(void)
 		 * properly save/restore the floating point values during a
 		 * context switch.
 		 */
-
 		init_byte = FIBER_FLOAT_REG_CHECK_BYTE;
 
-		for (i = 0U; i < SIZEOF_FP_REGISTER_SET; i++) {
+		for (i = 0; i < SIZEOF_FP_REGISTER_SET; i++) {
 			reg_set_ptr[i] = init_byte++;
 		}
 
@@ -295,7 +259,6 @@ void load_store_high(void)
 		 * registers when the task was swapped out due to the
 		 * occurrence of the timer tick.
 		 */
-
 		_load_then_store_all_float_registers(&float_reg_set);
 
 		/*
@@ -308,33 +271,42 @@ void load_store_high(void)
 		 * kernel to provide a "clean" FPU state to this thread
 		 * once the sleep ends.
 		 */
-
 		k_sleep(K_MSEC(1));
 
-		/* periodically issue progress report */
-
-		if ((++load_store_high_count % 100) == 0U) {
+		/* Periodically issue progress report */
+		if ((load_store_high_count % 100) == 0) {
 			PRINT_DATA("Load and store OK after %u (high) "
 				   "+ %u (low) tests\n",
 				   load_store_high_count,
 				   load_store_low_count);
 		}
-
-		/* terminate testing if specified limit has been reached */
-
-		if (load_store_high_count == MAX_TESTS) {
-			TC_END_RESULT(TC_PASS);
-			TC_END_REPORT(TC_PASS);
-#ifdef CONFIG_COVERAGE_GCOV
-			gcov_coverage_dump();
-#endif
-			return;
-		}
 	}
+
+#ifdef CONFIG_COVERAGE_GCOV
+	gcov_coverage_dump();
+#endif
+
+	/* Signal end of test */
+	test_exited = true;
+	k_sem_give(&test_exit_sem);
 }
 
 K_THREAD_DEFINE(load_low, THREAD_STACK_SIZE, load_store_low, NULL, NULL, NULL,
-		THREAD_LOW_PRIORITY, THREAD_FP_FLAGS, K_NO_WAIT);
+		THREAD_LOW_PRIORITY, THREAD_FP_FLAGS, K_FOREVER);
 
 K_THREAD_DEFINE(load_high, THREAD_STACK_SIZE, load_store_high, NULL, NULL, NULL,
-		THREAD_HIGH_PRIORITY, THREAD_FP_FLAGS, K_NO_WAIT);
+		THREAD_HIGH_PRIORITY, THREAD_FP_FLAGS, K_FOREVER);
+
+void test_load_store(void)
+{
+	/* Initialise test states */
+	test_exited = false;
+	k_sem_reset(&test_exit_sem);
+
+	/* Start test threads */
+	k_thread_start(load_low);
+	k_thread_start(load_high);
+
+	/* Wait for test threads to exit */
+	k_sem_take(&test_exit_sem, K_FOREVER);
+}

tests/kernel/fp_sharing/generic/src/pi.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2011-2014 Wind River Systems, Inc.
+ * Copyright (c) 2020 Stephanos Ioannidis <root@stephanos.io>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -30,10 +31,7 @@
  * has occurred.
  */
 
-#include <zephyr.h>
-
-#include <stdio.h>
-#include <tc_util.h>
+#include <ztest.h>
 
 #include "float_context.h"
 #include "test_common.h"
@@ -42,8 +40,7 @@
  * PI_NUM_ITERATIONS: This macro is defined in the project's Makefile and
  * is configurable from the command line.
  */
-
-static double reference_pi = 0.0f;
+static float reference_pi = 0.0f;
 
 /*
  * Test counters are "volatile" because GCC wasn't properly updating
@@ -51,79 +48,79 @@ static double reference_pi = 0.0f;
  * in its error handling logic -- the value was incremented in a register,
  * but never written back to memory. (Seems to be a compiler bug!)
  */
-
 static volatile unsigned int calc_pi_low_count;
 static volatile unsigned int calc_pi_high_count;
 
+/* Indicates that the load/store test exited */
+static bool test_exited;
+
+/* Semaphore for signaling end of test */
+static K_SEM_DEFINE(test_exit_sem, 0, 1);
+
 /**
- *
  * @brief Entry point for the low priority pi compute task
  *
  * @ingroup kernel_fpsharing_tests
  */
-
-void calculate_pi_low(void)
+static void calculate_pi_low(void)
 {
-	volatile double pi; /* volatile to avoid optimizing out of loop */
-	double divisor = 3.0;
-	double sign = -1.0;
+	volatile float pi; /* volatile to avoid optimizing out of loop */
+	float divisor = 3.0f;
+	float sign = -1.0f;
 	unsigned int ix;
 
-	/* loop forever, unless an error is detected */
+	/* Loop until the test finishes, or an error is detected. */
+	for (calc_pi_low_count = 0; !test_exited; calc_pi_low_count++) {
 
-	while (1) {
+		sign = -1.0f;
+		pi = 1.0f;
+		divisor = 3.0f;
 
-		sign = -1.0;
-		pi = 1.0;
-		divisor = 3.0;
-
-		for (ix = 0U; ix < PI_NUM_ITERATIONS; ix++) {
+		for (ix = 0; ix < PI_NUM_ITERATIONS; ix++) {
 			pi += sign / divisor;
-			divisor += 2.0;
-			sign *= -1.0;
+			divisor += 2.0f;
+			sign *= -1.0f;
 		}
 
-		pi *= 4;
+		pi *= 4.0f;
 
 		if (reference_pi == 0.0f) {
 			reference_pi = pi;
 		} else if (reference_pi != pi) {
-			TC_ERROR("Computed pi %1.6f, reference pi %1.6f\n",
-				 pi, reference_pi);
-			fpu_sharing_error = 1;
-			return;
+			printf("Computed pi %1.6f, reference pi %1.6f\n",
+			       pi, reference_pi);
 		}
 
-		++calc_pi_low_count;
+		zassert_equal(reference_pi, pi,
+			      "pi computation error");
 	}
 }
 
 /**
- *
  * @brief Entry point for the high priority pi compute task
  *
  * @ingroup kernel_fpsharing_tests
  */
-
-void calculate_pi_high(void)
+static void calculate_pi_high(void)
 {
-	volatile double pi; /* volatile to avoid optimizing out of loop */
-	double divisor = 3.0;
-	double sign = -1.0;
+	volatile float pi; /* volatile to avoid optimizing out of loop */
+	float divisor = 3.0f;
+	float sign = -1.0f;
 	unsigned int ix;
 
-	/* loop forever, unless an error is detected */
+	/* Run the test until the specified maximum test count is reached */
+	for (calc_pi_high_count = 0;
+	     calc_pi_high_count <= MAX_TESTS;
+	     calc_pi_high_count++) {
 
-	while (1) {
+		sign = -1.0f;
+		pi = 1.0f;
+		divisor = 3.0f;
 
-		sign = -1.0;
-		pi = 1.0;
-		divisor = 3.0;
-
-		for (ix = 0U; ix < PI_NUM_ITERATIONS; ix++) {
+		for (ix = 0; ix < PI_NUM_ITERATIONS; ix++) {
 			pi += sign / divisor;
-			divisor += 2.0;
-			sign *= -1.0;
+			divisor += 2.0f;
+			sign *= -1.0f;
 		}
 
 		/*
@@ -136,32 +133,49 @@ void calculate_pi_high(void)
 		 * kernel to provide a "clean" FPU state to this thread
 		 * once the sleep ends.
 		 */
-
 		k_sleep(K_MSEC(10));
 
-		pi *= 4;
+		pi *= 4.0f;
 
 		if (reference_pi == 0.0f) {
 			reference_pi = pi;
 		} else if (reference_pi != pi) {
-			TC_ERROR("Computed pi %1.6f, reference pi %1.6f\n",
-				 pi, reference_pi);
-			fpu_sharing_error = 1;
-			return;
+			printf("Computed pi %1.6f, reference pi %1.6f\n",
+			       pi, reference_pi);
 		}
 
-		/* periodically issue progress report */
+		zassert_equal(reference_pi, pi,
+			      "pi computation error");
 
-		if ((++calc_pi_high_count % 100) == 50U) {
-			PRINT_DATA("Pi calculation OK after %u (high) +"
-				   " %u (low) tests (computed %1.6f)\n",
-				   calc_pi_high_count, calc_pi_low_count, pi);
+		/* Periodically issue progress report */
+		if ((calc_pi_high_count % 100) == 50) {
+			printf("Pi calculation OK after %u (high) +"
+			       " %u (low) tests (computed %1.6lf)\n",
+			       calc_pi_high_count, calc_pi_low_count, pi);
 		}
 	}
+
+	/* Signal end of test */
+	test_exited = true;
+	k_sem_give(&test_exit_sem);
 }
 
 K_THREAD_DEFINE(pi_low, THREAD_STACK_SIZE, calculate_pi_low, NULL, NULL, NULL,
-		THREAD_LOW_PRIORITY, THREAD_FP_FLAGS, K_NO_WAIT);
+		THREAD_LOW_PRIORITY, THREAD_FP_FLAGS, K_FOREVER);
 
 K_THREAD_DEFINE(pi_high, THREAD_STACK_SIZE, calculate_pi_high, NULL, NULL, NULL,
-		THREAD_HIGH_PRIORITY, THREAD_FP_FLAGS, K_NO_WAIT);
+		THREAD_HIGH_PRIORITY, THREAD_FP_FLAGS, K_FOREVER);
+
+void test_pi(void)
+{
+	/* Initialise test states */
+	test_exited = false;
+	k_sem_reset(&test_exit_sem);
+
+	/* Start test threads */
+	k_thread_start(pi_low);
+	k_thread_start(pi_high);
+
+	/* Wait for test threads to exit */
+	k_sem_take(&test_exit_sem, K_FOREVER);
+}

phytec/zephyr-phytec

tests/kernel/fp_sharing/generic/src/load_store.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2011-2014 Wind River Systems, Inc.
+ * Copyright (c) 2020 Stephanos Ioannidis <root@stephanos.io>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -34,10 +35,7 @@
  * x87 FPU registers are being saved/restored.
  */
 
-#include <zephyr.h>
-#include <tc_util.h>
-#include <stddef.h>
-#include <string.h>
+#include <ztest.h>
 #include <debug/gcov.h>
 
 #if defined(CONFIG_X86)
@@ -64,29 +62,26 @@
 #include "test_common.h"
 
 /* space for float register load/store area used by low priority task */
-
 static struct fp_register_set float_reg_set_load;
 static struct fp_register_set float_reg_set_store;
 
 /* space for float register load/store area used by high priority thread */
-
 static struct fp_register_set float_reg_set;
-
-
-/* flag indicating that an error has occurred */
-
-int fpu_sharing_error;
 
 /*
  * Test counters are "volatile" because GCC may not update them properly
  * otherwise. (See description of pi calculation test for more details.)
  */
-
 static volatile unsigned int load_store_low_count;
 static volatile unsigned int load_store_high_count;
 
+/* Indicates that the load/store test exited */
+static bool test_exited;
+
+/* Semaphore for signaling end of test */
+static K_SEM_DEFINE(test_exit_sem, 0, 1);
+
 /**
- *
  * @brief Low priority FPU load/store thread
  *
  * @ingroup kernel_fpsharing_tests
@@ -94,58 +89,38 @@ static volatile unsigned int load_store_high_count;
  * @see k_sched_time_slice_set(), memset(),
  * _load_all_float_registers(), _store_all_float_registers()
  */
-
-void load_store_low(void)
+static void load_store_low(void)
 {
 	unsigned int i;
+	bool error = false;
 	unsigned char init_byte;
 	unsigned char *store_ptr = (unsigned char *)&float_reg_set_store;
 	unsigned char *load_ptr = (unsigned char *)&float_reg_set_load;
 
 	volatile char volatile_stack_var = 0;
 
-	PRINT_DATA("Floating point sharing tests started\n");
-	PRINT_LINE;
-
-	/*
-	 * The high priority thread has a sleep to get this (low pri) thread
-	 * running and here (low priority) we enable slicing and waste cycles
-	 * to run hi pri thread in between fp ops.
-	 *
-	 * Enable round robin scheduling to allow both the low priority pi
-	 * computation and load/store tasks to execute. The high priority pi
-	 * computation and load/store tasks will preempt the low priority tasks
-	 * periodically.
-	 */
-
-	k_sched_time_slice_set(10, THREAD_LOW_PRIORITY);
-
 	/*
 	 * Initialize floating point load buffer to known values;
 	 * these values must be different than the value used in other threads.
 	 */
-
 	init_byte = MAIN_FLOAT_REG_CHECK_BYTE;
-	for (i = 0U; i < SIZEOF_FP_REGISTER_SET; i++) {
+	for (i = 0; i < SIZEOF_FP_REGISTER_SET; i++) {
 		load_ptr[i] = init_byte++;
 	}
 
-	/* Keep cranking forever, or until an error is detected. */
-
-	for (load_store_low_count = 0U;; load_store_low_count++) {
+	/* Loop until the test finishes, or an error is detected. */
+	for (load_store_low_count = 0; !test_exited; load_store_low_count++) {
 
 		/*
 		 * Clear store buffer to erase all traces of any previous
 		 * floating point values that have been saved.
 		 */
-
 		(void)memset(&float_reg_set_store, 0, SIZEOF_FP_REGISTER_SET);
 
 		/*
 		 * Utilize an architecture specific function to load all the
 		 * floating point registers with known values.
 		 */
-
 		_load_all_float_registers(&float_reg_set_load);
 
 		/*
@@ -156,8 +131,7 @@ void load_store_low(void)
 		 * IMPORTANT: This logic requires that z_tick_get_32() not
 		 * perform any floating point operations!
 		 */
-
-		while ((z_tick_get_32() % 5) != 0U) {
+		while ((z_tick_get_32() % 5) != 0) {
 			/*
 			 * Use a volatile variable to prevent compiler
 			 * optimizing out the spin loop.
@@ -169,7 +143,6 @@ void load_store_low(void)
 		 * Utilize an architecture specific function to dump the
 		 * contents of all floating point registers to memory.
 		 */
-
 		_store_all_float_registers(&float_reg_set_store);
 
 		/*
@@ -180,32 +153,24 @@ void load_store_low(void)
 		 * Display error message and terminate if discrepancies are
 		 * detected.
 		 */
-
 		init_byte = MAIN_FLOAT_REG_CHECK_BYTE;
 
-		for (i = 0U; i < SIZEOF_FP_REGISTER_SET; i++) {
+		for (i = 0; i < SIZEOF_FP_REGISTER_SET; i++) {
 			if (store_ptr[i] != init_byte) {
-				TC_ERROR("load_store_low found 0x%x instead "
-					 "of 0x%x @ offset 0x%x\n",
+				TC_ERROR("Found 0x%x instead of 0x%x @ "
+					 "offset 0x%x\n",
 					 store_ptr[i],
 					 init_byte, i);
 				TC_ERROR("Discrepancy found during "
 					 "iteration %d\n",
 					 load_store_low_count);
-				fpu_sharing_error = 1;
+				error = true;
 			}
 			init_byte++;
 		}
 
-		/*
-		 * Terminate if a test error has been reported.
-		 */
-
-		if (fpu_sharing_error) {
-			TC_END_RESULT(TC_FAIL);
-			TC_END_REPORT(TC_FAIL);
-			return;
-		}
+		/* Terminate if a test error has been reported */
+		zassert_false(error, NULL);
 
 		/*
 		 * After every 1000 iterations (arbitrarily chosen), explicitly
@@ -230,7 +195,7 @@ void load_store_low(void)
 		 * is useful for testing automatic thread enabling of floating
 		 * point as soon as FP registers are used, again by the thread.
 		 */
-		if ((load_store_low_count % 1000) == 0U) {
+		if ((load_store_low_count % 1000) == 0) {
 			k_float_disable(k_current_get());
 		}
 #endif
@@ -238,23 +203,23 @@ void load_store_low(void)
 }
 
 /**
- *
  * @brief High priority FPU load/store thread
  *
  * @ingroup kernel_fpsharing_tests
  *
  * @see _load_then_store_all_float_registers()
  */
-
-void load_store_high(void)
+static void load_store_high(void)
 {
 	unsigned int i;
 	unsigned char init_byte;
 	unsigned char *reg_set_ptr = (unsigned char *)&float_reg_set;
 
-	/* test until the specified time limit, or until an error is detected */
+	/* Run the test until the specified maximum test count is reached */
+	for (load_store_high_count = 0;
+	     load_store_high_count <= MAX_TESTS;
+	     load_store_high_count++) {
 
-	while (1) {
 		/*
 		 * Initialize the float_reg_set structure by treating it as
 		 * a simple array of bytes (the arrangement and actual number
@@ -270,10 +235,9 @@ void load_store_high(void)
 		 * properly save/restore the floating point values during a
 		 * context switch.
 		 */
-
 		init_byte = FIBER_FLOAT_REG_CHECK_BYTE;
 
-		for (i = 0U; i < SIZEOF_FP_REGISTER_SET; i++) {
+		for (i = 0; i < SIZEOF_FP_REGISTER_SET; i++) {
 			reg_set_ptr[i] = init_byte++;
 		}
 
@@ -295,7 +259,6 @@ void load_store_high(void)
 		 * registers when the task was swapped out due to the
 		 * occurrence of the timer tick.
 		 */
-
 		_load_then_store_all_float_registers(&float_reg_set);
 
 		/*
@@ -308,33 +271,42 @@ void load_store_high(void)
 		 * kernel to provide a "clean" FPU state to this thread
 		 * once the sleep ends.
 		 */
-
 		k_sleep(K_MSEC(1));
 
-		/* periodically issue progress report */
-
-		if ((++load_store_high_count % 100) == 0U) {
+		/* Periodically issue progress report */
+		if ((load_store_high_count % 100) == 0) {
 			PRINT_DATA("Load and store OK after %u (high) "
 				   "+ %u (low) tests\n",
 				   load_store_high_count,
 				   load_store_low_count);
 		}
-
-		/* terminate testing if specified limit has been reached */
-
-		if (load_store_high_count == MAX_TESTS) {
-			TC_END_RESULT(TC_PASS);
-			TC_END_REPORT(TC_PASS);
-#ifdef CONFIG_COVERAGE_GCOV
-			gcov_coverage_dump();
-#endif
-			return;
-		}
 	}
+
+#ifdef CONFIG_COVERAGE_GCOV
+	gcov_coverage_dump();
+#endif
+
+	/* Signal end of test */
+	test_exited = true;
+	k_sem_give(&test_exit_sem);
 }
 
 K_THREAD_DEFINE(load_low, THREAD_STACK_SIZE, load_store_low, NULL, NULL, NULL,
-		THREAD_LOW_PRIORITY, THREAD_FP_FLAGS, 0);
+		THREAD_LOW_PRIORITY, THREAD_FP_FLAGS, K_TICKS_FOREVER);
 
 K_THREAD_DEFINE(load_high, THREAD_STACK_SIZE, load_store_high, NULL, NULL, NULL,
-		THREAD_HIGH_PRIORITY, THREAD_FP_FLAGS, 0);
+		THREAD_HIGH_PRIORITY, THREAD_FP_FLAGS, K_TICKS_FOREVER);
+
+void test_load_store(void)
+{
+	/* Initialise test states */
+	test_exited = false;
+	k_sem_reset(&test_exit_sem);
+
+	/* Start test threads */
+	k_thread_start(load_low);
+	k_thread_start(load_high);
+
+	/* Wait for test threads to exit */
+	k_sem_take(&test_exit_sem, K_FOREVER);
+}

tests/kernel/fp_sharing/generic/src/pi.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2011-2014 Wind River Systems, Inc.
+ * Copyright (c) 2020 Stephanos Ioannidis <root@stephanos.io>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -30,10 +31,7 @@
  * has occurred.
  */
 
-#include <zephyr.h>
-
-#include <stdio.h>
-#include <tc_util.h>
+#include <ztest.h>
 
 #include "float_context.h"
 #include "test_common.h"
@@ -42,8 +40,7 @@
  * PI_NUM_ITERATIONS: This macro is defined in the project's Makefile and
  * is configurable from the command line.
  */
-
-static double reference_pi = 0.0f;
+static float reference_pi = 0.0f;
 
 /*
  * Test counters are "volatile" because GCC wasn't properly updating
@@ -51,79 +48,79 @@ static double reference_pi = 0.0f;
  * in its error handling logic -- the value was incremented in a register,
  * but never written back to memory. (Seems to be a compiler bug!)
  */
-
 static volatile unsigned int calc_pi_low_count;
 static volatile unsigned int calc_pi_high_count;
 
+/* Indicates that the load/store test exited */
+static bool test_exited;
+
+/* Semaphore for signaling end of test */
+static K_SEM_DEFINE(test_exit_sem, 0, 1);
+
 /**
- *
  * @brief Entry point for the low priority pi compute task
  *
  * @ingroup kernel_fpsharing_tests
  */
-
-void calculate_pi_low(void)
+static void calculate_pi_low(void)
 {
-	volatile double pi; /* volatile to avoid optimizing out of loop */
-	double divisor = 3.0;
-	double sign = -1.0;
+	volatile float pi; /* volatile to avoid optimizing out of loop */
+	float divisor = 3.0f;
+	float sign = -1.0f;
 	unsigned int ix;
 
-	/* loop forever, unless an error is detected */
+	/* Loop until the test finishes, or an error is detected. */
+	for (calc_pi_low_count = 0; !test_exited; calc_pi_low_count++) {
 
-	while (1) {
+		sign = -1.0f;
+		pi = 1.0f;
+		divisor = 3.0f;
 
-		sign = -1.0;
-		pi = 1.0;
-		divisor = 3.0;
-
-		for (ix = 0U; ix < PI_NUM_ITERATIONS; ix++) {
+		for (ix = 0; ix < PI_NUM_ITERATIONS; ix++) {
 			pi += sign / divisor;
-			divisor += 2.0;
-			sign *= -1.0;
+			divisor += 2.0f;
+			sign *= -1.0f;
 		}
 
-		pi *= 4;
+		pi *= 4.0f;
 
 		if (reference_pi == 0.0f) {
 			reference_pi = pi;
 		} else if (reference_pi != pi) {
-			TC_ERROR("Computed pi %1.6f, reference pi %1.6f\n",
-				 pi, reference_pi);
-			fpu_sharing_error = 1;
-			return;
+			printf("Computed pi %1.6f, reference pi %1.6f\n",
+			       pi, reference_pi);
 		}
 
-		++calc_pi_low_count;
+		zassert_equal(reference_pi, pi,
+			      "pi computation error");
 	}
 }
 
 /**
- *
  * @brief Entry point for the high priority pi compute task
  *
  * @ingroup kernel_fpsharing_tests
  */
-
-void calculate_pi_high(void)
+static void calculate_pi_high(void)
 {
-	volatile double pi; /* volatile to avoid optimizing out of loop */
-	double divisor = 3.0;
-	double sign = -1.0;
+	volatile float pi; /* volatile to avoid optimizing out of loop */
+	float divisor = 3.0f;
+	float sign = -1.0f;
 	unsigned int ix;
 
-	/* loop forever, unless an error is detected */
+	/* Run the test until the specified maximum test count is reached */
+	for (calc_pi_high_count = 0;
+	     calc_pi_high_count <= MAX_TESTS;
+	     calc_pi_high_count++) {
 
-	while (1) {
+		sign = -1.0f;
+		pi = 1.0f;
+		divisor = 3.0f;
 
-		sign = -1.0;
-		pi = 1.0;
-		divisor = 3.0;
-
-		for (ix = 0U; ix < PI_NUM_ITERATIONS; ix++) {
+		for (ix = 0; ix < PI_NUM_ITERATIONS; ix++) {
 			pi += sign / divisor;
-			divisor += 2.0;
-			sign *= -1.0;
+			divisor += 2.0f;
+			sign *= -1.0f;
 		}
 
 		/*
@@ -136,32 +133,49 @@ void calculate_pi_high(void)
 		 * kernel to provide a "clean" FPU state to this thread
 		 * once the sleep ends.
 		 */
-
 		k_sleep(K_MSEC(10));
 
-		pi *= 4;
+		pi *= 4.0f;
 
 		if (reference_pi == 0.0f) {
 			reference_pi = pi;
 		} else if (reference_pi != pi) {
-			TC_ERROR("Computed pi %1.6f, reference pi %1.6f\n",
-				 pi, reference_pi);
-			fpu_sharing_error = 1;
-			return;
+			printf("Computed pi %1.6f, reference pi %1.6f\n",
+			       pi, reference_pi);
 		}
 
-		/* periodically issue progress report */
+		zassert_equal(reference_pi, pi,
+			      "pi computation error");
 
-		if ((++calc_pi_high_count % 100) == 50U) {
-			PRINT_DATA("Pi calculation OK after %u (high) +"
-				   " %u (low) tests (computed %1.6f)\n",
-				   calc_pi_high_count, calc_pi_low_count, pi);
+		/* Periodically issue progress report */
+		if ((calc_pi_high_count % 100) == 50) {
+			printf("Pi calculation OK after %u (high) +"
+			       " %u (low) tests (computed %1.6lf)\n",
+			       calc_pi_high_count, calc_pi_low_count, pi);
 		}
 	}
+
+	/* Signal end of test */
+	test_exited = true;
+	k_sem_give(&test_exit_sem);
 }
 
 K_THREAD_DEFINE(pi_low, THREAD_STACK_SIZE, calculate_pi_low, NULL, NULL, NULL,
-		THREAD_LOW_PRIORITY, THREAD_FP_FLAGS, 0);
+		THREAD_LOW_PRIORITY, THREAD_FP_FLAGS, K_TICKS_FOREVER);
 
 K_THREAD_DEFINE(pi_high, THREAD_STACK_SIZE, calculate_pi_high, NULL, NULL, NULL,
-		THREAD_HIGH_PRIORITY, THREAD_FP_FLAGS, 0);
+		THREAD_HIGH_PRIORITY, THREAD_FP_FLAGS, K_TICKS_FOREVER);
+
+void test_pi(void)
+{
+	/* Initialise test states */
+	test_exited = false;
+	k_sem_reset(&test_exit_sem);
+
+	/* Start test threads */
+	k_thread_start(pi_low);
+	k_thread_start(pi_high);
+
+	/* Wait for test threads to exit */
+	k_sem_take(&test_exit_sem, K_FOREVER);
+}