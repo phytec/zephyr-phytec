mnasr17/zephyr

Bluetooth: host: Added scan support for periodic advertisement
Added the ext adv report interval to bt_le_scan_recv_info which
denotes whether or not there is periodic advertisement data
associated with the advertisement report.

Signed-off-by: Emil Gydesen <emil_gydesen@bose.com>

include/bluetooth/bluetooth.h
@@ -941,6 +941,13 @@ struct bt_le_scan_recv_info {
 	/** Advertising packet properties. */
 	u16_t adv_props;
 
+	/**
+	 * @brief Periodic advertising interval.
+	 *
+	 * If 0 there is no periodic advertisement.
+	 */
+	u16_t interval;
+
 	/** Primary advertising channel PHY. */
 	u8_t primary_phy;
 

phytec/zephyr-phytec

include/bluetooth/bluetooth.h
@@ -987,6 +987,13 @@ struct bt_le_scan_recv_info {
 	/** Advertising packet properties. */
 	uint16_t adv_props;
 
+	/**
+	 * @brief Periodic advertising interval.
+	 *
+	 * If 0 there is no periodic advertisement.
+	 */
+	uint16_t interval;
+
 	/** Primary advertising channel PHY. */
 	uint8_t primary_phy;
 