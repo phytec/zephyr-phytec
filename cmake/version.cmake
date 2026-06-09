SebastianBoe/zephyr

cmake: version: Misc. refactorings for readability
Misc. refactorings to increase the readability of cmake/version.cmake.

Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

cmake/version.cmake
@@ -16,23 +16,26 @@ set(PROJECT_VERSION_TWEAK ${CMAKE_MATCH_1})
 string(REGEX MATCH "EXTRAVERSION = ([a-z0-9]*)" _ ${ver})
 set(PROJECT_VERSION_EXTRA ${CMAKE_MATCH_1})
 
+# Temporary convenience variable
+set(PROJECT_VERSION_WITHOUT_TWEAK ${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH})
 
-message("Zephyr version: ${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}")
-
-set(PROJECT_VERSION ${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH})
 if(PROJECT_VERSION_TWEAK)
-  set(PROJECT_VERSION "${PROJECT_VERSION}.${PROJECT_VERSION_TWEAK}")
+  set(PROJECT_VERSION ${PROJECT_VERSION_WITHOUT_TWEAK}.${PROJECT_VERSION_TWEAK})
+else()
+  set(PROJECT_VERSION ${PROJECT_VERSION_WITHOUT_TWEAK})
 endif()
+
+message("Zephyr version: ${PROJECT_VERSION_WITHOUT_TWEAK}")
 
 set(MAJOR ${PROJECT_VERSION_MAJOR}) # Temporary convenience variable
 set(MINOR ${PROJECT_VERSION_MINOR}) # Temporary convenience variable
 set(PATCH ${PROJECT_VERSION_PATCH}) # Temporary convenience variable
 
-math(EXPR ZEPHYR_VERSION_CODE                      "(${MAJOR} << 16) + (${MINOR} << 8) + (${PATCH})")
-math(EXPR KERNEL_VERSION_NUMBER_INT                "(${MAJOR} << 16) + (${MINOR} << 8) + (${PATCH})")
-math(EXPR KERNELVERSION_INT "(${MAJOR} << 24) + (${MINOR} << 16) + (${PATCH} << 8) + ${PROJECT_VERSION_TWEAK}")
+math(EXPR ZEPHYR_VERSION_CODE       "(${MAJOR} << 16) + (${MINOR} << 8)  + (${PATCH})")
+math(EXPR KERNEL_VERSION_NUMBER_INT "(${MAJOR} << 16) + (${MINOR} << 8)  + (${PATCH})")
+math(EXPR KERNELVERSION_INT         "(${MAJOR} << 24) + (${MINOR} << 16) + (${PATCH} << 8) + (${PROJECT_VERSION_TWEAK})")
 
-to_hex(${KERNELVERSION_INT} KERNELVERSION)
+to_hex(${KERNELVERSION_INT}         KERNELVERSION)
 to_hex(${KERNEL_VERSION_NUMBER_INT} KERNEL_VERSION_NUMBER)
 
 unset(MAJOR)
@@ -42,9 +45,15 @@ unset(PATCH)
 set(KERNEL_VERSION_MAJOR      ${PROJECT_VERSION_MAJOR})
 set(KERNEL_VERSION_MINOR      ${PROJECT_VERSION_MINOR})
 set(KERNEL_PATCHLEVEL         ${PROJECT_VERSION_PATCH})
+
 if(PROJECT_VERSION_EXTRA)
-set(KERNEL_VERSION_STRING     "\"${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}-${PROJECT_VERSION_EXTRA}\"")
+  set(KERNEL_VERSION_STRING     "\"${PROJECT_VERSION_WITHOUT_TWEAK}-${PROJECT_VERSION_EXTRA}\"")
 else()
-set(KERNEL_VERSION_STRING     "\"${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}\"")
+  set(KERNEL_VERSION_STRING     "\"${PROJECT_VERSION_WITHOUT_TWEAK}\"")
 endif()
 
+# Cleanup convenience variables
+unset(MAJOR)
+unset(MINOR)
+unset(PATCH)
+unset(PROJECT_VERSION_WITHOUT_TWEAK)

phytec/zephyr-phytec

cmake/version.cmake
@@ -16,35 +16,41 @@ set(PROJECT_VERSION_TWEAK ${CMAKE_MATCH_1})
 string(REGEX MATCH "EXTRAVERSION = ([a-z0-9]*)" _ ${ver})
 set(PROJECT_VERSION_EXTRA ${CMAKE_MATCH_1})
 
+# Temporary convenience variable
+set(PROJECT_VERSION_WITHOUT_TWEAK ${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH})
 
-message("Zephyr version: ${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}")
-
-set(PROJECT_VERSION ${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH})
 if(PROJECT_VERSION_TWEAK)
-  set(PROJECT_VERSION "${PROJECT_VERSION}.${PROJECT_VERSION_TWEAK}")
+  set(PROJECT_VERSION ${PROJECT_VERSION_WITHOUT_TWEAK}.${PROJECT_VERSION_TWEAK})
+else()
+  set(PROJECT_VERSION ${PROJECT_VERSION_WITHOUT_TWEAK})
 endif()
+
+message("Zephyr version: ${PROJECT_VERSION}")
 
 set(MAJOR ${PROJECT_VERSION_MAJOR}) # Temporary convenience variable
 set(MINOR ${PROJECT_VERSION_MINOR}) # Temporary convenience variable
 set(PATCH ${PROJECT_VERSION_PATCH}) # Temporary convenience variable
 
-math(EXPR ZEPHYR_VERSION_CODE                      "(${MAJOR} << 16) + (${MINOR} << 8) + (${PATCH})")
-math(EXPR KERNEL_VERSION_NUMBER_INT                "(${MAJOR} << 16) + (${MINOR} << 8) + (${PATCH})")
-math(EXPR KERNELVERSION_INT "(${MAJOR} << 24) + (${MINOR} << 16) + (${PATCH} << 8) + ${PROJECT_VERSION_TWEAK}")
+math(EXPR KERNEL_VERSION_NUMBER_INT "(${MAJOR} << 16) + (${MINOR} << 8)  + (${PATCH})")
+math(EXPR KERNELVERSION_INT         "(${MAJOR} << 24) + (${MINOR} << 16) + (${PATCH} << 8) + (${PROJECT_VERSION_TWEAK})")
 
-to_hex(${KERNELVERSION_INT} KERNELVERSION)
 to_hex(${KERNEL_VERSION_NUMBER_INT} KERNEL_VERSION_NUMBER)
-
-unset(MAJOR)
-unset(MINOR)
-unset(PATCH)
+to_hex(${KERNELVERSION_INT}         KERNELVERSION)
 
 set(KERNEL_VERSION_MAJOR      ${PROJECT_VERSION_MAJOR})
 set(KERNEL_VERSION_MINOR      ${PROJECT_VERSION_MINOR})
 set(KERNEL_PATCHLEVEL         ${PROJECT_VERSION_PATCH})
+
 if(PROJECT_VERSION_EXTRA)
-set(KERNEL_VERSION_STRING     "\"${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}-${PROJECT_VERSION_EXTRA}\"")
+  set(KERNEL_VERSION_STRING     "\"${PROJECT_VERSION_WITHOUT_TWEAK}-${PROJECT_VERSION_EXTRA}\"")
 else()
-set(KERNEL_VERSION_STRING     "\"${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}\"")
+  set(KERNEL_VERSION_STRING     "\"${PROJECT_VERSION_WITHOUT_TWEAK}\"")
 endif()
 
+set(ZEPHYR_VERSION_CODE ${KERNEL_VERSION_NUMBER_INT})
+
+# Cleanup convenience variables
+unset(MAJOR)
+unset(MINOR)
+unset(PATCH)
+unset(PROJECT_VERSION_WITHOUT_TWEAK)