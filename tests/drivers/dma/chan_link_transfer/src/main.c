hakehuang/zephyr

tests: add mcux edma channel link test
test minor/major link mode on frdm_k64f and mimxrt1060_evk

log as below:

Running test suite dma_m2m_link_test
===================================================================
START - test_dma_m2m_chan0_1_major_link
Preparing DMA Controller: Chan_ID=1, BURST_LEN=1
Starting the transfer
DMA transfer done ch 0
It is harder to be kind than to be wise........
It is harder to
 PASS - test_dma_m2m_chan0_1_major_link
===================================================================
START - test_dma_m2m_chan0_1_minor_link
Preparing DMA Controller: Chan_ID=1, BURST_LEN=1
Starting the transfer
DMA transfer done ch 0
It is harder to be kind than to be wise........
It is harder to be kind than to
 PASS - test_dma_m2m_chan0_1_minor_link
===================================================================
START - test_dma_m2m_chan0_1_minor_major_link
Preparing DMA Controller: Chan_ID=1, BURST_LEN=1
Starting the transfer
DMA transfer done ch 0
DMA transfer done ch 1
It is harder to be kind than to be wise........
It is harder to be kind than to be wise........
 PASS - test_dma_m2m_chan0_1_minor_major_link
===================================================================
Test suite dma_m2m_link_test succeeded
===================================================================
PROJECT EXECUTION SUCCESSFUL

Signed-off-by: Hake Huang <hake.huang@oss.nxp.com>

tests/drivers/dma/chan_link_transfer/src/main.c
@@ -1,0 +1,39 @@
+/*
+ * Copyright (c) 2020 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
+#include <zephyr.h>
+#include <ztest.h>
+
+extern void test_dma_m2m_chan0_1_major_link(void);
+extern void test_dma_m2m_chan0_1_minor_link(void);
+extern void test_dma_m2m_chan0_1_minor_major_link(void);
+
+
+#ifdef CONFIG_SHELL
+TC_CMD_DEFINE(test_dma_m2m_chan0_1_major_link)
+TC_CMD_DEFINE(test_dma_m2m_chan0_1_minmor_link)
+
+
+SHELL_CMD_REGISTER(test_dma_m2m_chan0_1_major_link, NULL, NULL,
+			TC_CMD_ITEM(test_dma_m2m_chan0_1_major_link));
+SHELL_CMD_REGISTER(test_dma_m2m_chan0_1_minor_link, NULL, NULL,
+			TC_CMD_ITEM(test_dma_m2m_chan0_1_minor_link));
+SHELL_CMD_REGISTER(test_dma_m2m_chan0_1_minor_major_link, NULL, NULL,
+            TC_CMD_ITEM(test_dma_m2m_chan0_1_minor_major_link));
+
+#endif
+
+void test_main(void)
+{
+#ifndef CONFIG_SHELL
+	ztest_test_suite(dma_m2m_link_test,
+			 ztest_unit_test(test_dma_m2m_chan0_1_major_link),
+			 ztest_unit_test(test_dma_m2m_chan0_1_minor_link),
+             ztest_unit_test(test_dma_m2m_chan0_1_minor_major_link));
+	ztest_run_test_suite(dma_m2m_link_test);
+#endif
+}

phytec/zephyr-phytec

tests/drivers/dma/chan_link_transfer/src/main.c
@@ -1,0 +1,38 @@
+/*
+ * Copyright (c) 2020 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <ztest.h>
+
+extern void test_dma_m2m_chan0_1_major_link(void);
+extern void test_dma_m2m_chan0_1_minor_link(void);
+extern void test_dma_m2m_chan0_1_minor_major_link(void);
+
+#ifdef CONFIG_SHELL
+TC_CMD_DEFINE(test_dma_m2m_chan0_1_major_link)
+TC_CMD_DEFINE(test_dma_m2m_chan0_1_minor_link)
+TC_CMD_DEFINE(test_dma_m2m_chan0_1_minor_major_link)
+
+SHELL_CMD_REGISTER(test_dma_m2m_chan0_1_major_link, NULL, NULL,
+		   TC_CMD_ITEM(test_dma_m2m_chan0_1_major_link));
+SHELL_CMD_REGISTER(test_dma_m2m_chan0_1_minor_link, NULL, NULL,
+		   TC_CMD_ITEM(test_dma_m2m_chan0_1_minor_link));
+SHELL_CMD_REGISTER(test_dma_m2m_chan0_1_minor_major_link, NULL, NULL,
+		   TC_CMD_ITEM(test_dma_m2m_chan0_1_minor_major_link));
+
+#endif
+
+void test_main(void)
+{
+#ifndef CONFIG_SHELL
+	ztest_test_suite(
+		dma_m2m_link_test,
+		ztest_unit_test(test_dma_m2m_chan0_1_major_link),
+		ztest_unit_test(test_dma_m2m_chan0_1_minor_link),
+		ztest_unit_test(test_dma_m2m_chan0_1_minor_major_link));
+	ztest_run_test_suite(dma_m2m_link_test);
+#endif
+}