zephyrproject-rtos/zephyr

tests: drivers: device clock controls for stm32n6 series
Add device clock configuration tests for STM32N6 series boards.
These tests cover ADC, I2C and SPI, using ADC1, I2C1 and SPI5 that are
connected on nucleo_n657x0_q and stm32n6570_dk boards.

Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

tests/drivers/clock_control/stm32_clock_configuration/stm32n6_devices/src/test_stm32_clock_configuration_adc.c
@@ -1,0 +1,127 @@
+/*
+ * Copyright (c) 2022 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/ztest.h>
+#include <soc.h>
+#include <zephyr/drivers/clock_control.h>
+#include <zephyr/drivers/clock_control/stm32_clock_control.h>
+#include <zephyr/logging/log.h>
+
+ZTEST(stm32n6_devices_clocks, test_adc_clk_config)
+{
+	static const struct stm32_pclken pclken[] = STM32_DT_CLOCKS(DT_NODELABEL(adc1));
+
+	uint32_t dev_dt_clk_freq, dev_actual_clk_freq, dev_actual_clk_src;
+	enum clock_control_status status;
+	int r;
+
+	status = clock_control_get_status(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					  (clock_control_subsys_t)&pclken[0]);
+	zassert_true((status == CLOCK_CONTROL_STATUS_OFF),
+		     "ADC1 gating clock should be off initially");
+
+	/* Test clock_on(gating clock) */
+	r = clock_control_on(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+				(clock_control_subsys_t) &pclken[0]);
+	zassert_true((r == 0), "Could not enable ADC1 gating clock");
+
+	/* Check via HAL as well as via get_status API */
+	zassert_true(__HAL_RCC_ADC12_IS_CLK_ENABLED(), "[HAL] ADC1 gating clock should be on");
+
+	status = clock_control_get_status(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					  (clock_control_subsys_t)&pclken[0]);
+	zassert_true((status == CLOCK_CONTROL_STATUS_ON),
+		     "[Zephyr] ADC1 gating clock should be on");
+
+	if (DT_NUM_CLOCKS(DT_NODELABEL(adc1)) > 1) {
+		/* Test clock_on(domain_clk) */
+		r = clock_control_configure(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					    (clock_control_subsys_t) &pclken[1],
+					    NULL);
+		zassert_true((r == 0), "Could not enable ADC1 domain clock");
+
+		/* Test clock source */
+		dev_actual_clk_src = __HAL_RCC_GET_ADC_SOURCE();
+
+		switch (pclken[1].bus) {
+		case STM32_SRC_HCLK:
+			zassert_equal(dev_actual_clk_src, RCC_ADCCLKSOURCE_HCLK,
+					"Expected ADC1 src: HCLK (0x%lx). Actual ADC1 src: 0x%x",
+					RCC_ADCCLKSOURCE_HCLK, dev_actual_clk_src);
+			break;
+		case STM32_SRC_CKPER:
+			zassert_equal(dev_actual_clk_src, RCC_ADCCLKSOURCE_CLKP,
+					"Expected ADC1 src: CKPER (0x%lx). Actual ADC1 src: 0x%x",
+					RCC_ADCCLKSOURCE_CLKP, dev_actual_clk_src);
+			break;
+		case STM32_SRC_IC7:
+			zassert_equal(dev_actual_clk_src, RCC_ADCCLKSOURCE_IC7,
+					"Expected ADC1 src: IC7 (0x%lx). Actual ADC1 src: 0x%x",
+					RCC_ADCCLKSOURCE_IC7, dev_actual_clk_src);
+			break;
+		case STM32_SRC_IC8:
+			zassert_equal(dev_actual_clk_src, RCC_ADCCLKSOURCE_IC8,
+					"Expected ADC1 src: IC8 (0x%lx). Actual ADC1 src: 0x%x",
+					RCC_ADCCLKSOURCE_IC8, dev_actual_clk_src);
+			break;
+		case STM32_SRC_MSI:
+			zassert_equal(dev_actual_clk_src, RCC_ADCCLKSOURCE_MSI,
+					"Expected ADC1 src: MSI (0x%lx). Actual ADC1 src: 0x%x",
+					RCC_ADCCLKSOURCE_MSI, dev_actual_clk_src);
+			break;
+		case STM32_SRC_HSI:
+			zassert_equal(dev_actual_clk_src, RCC_ADCCLKSOURCE_HSI,
+					"Expected ADC1 src: HSI (0x%lx). Actual ADC1 src: 0x%x",
+					RCC_ADCCLKSOURCE_HSI, dev_actual_clk_src);
+			break;
+		case STM32_SRC_I2S_CKIN:
+			zassert_equal(dev_actual_clk_src, RCC_ADCCLKSOURCE_PIN,
+					"Expected ADC1 src: I2S_CKIN (0x%lx). Actual ADC1 src: 0x%x",
+					RCC_ADCCLKSOURCE_PIN, dev_actual_clk_src);
+			break;
+		case STM32_SRC_TIMG:
+			zassert_equal(dev_actual_clk_src, RCC_ADCCLKSOURCE_TIMG,
+					"Expected ADC1 src: TIMG (0x%lx). Actual ADC1 src: 0x%x",
+					RCC_ADCCLKSOURCE_TIMG, dev_actual_clk_src);
+			break;
+		default:
+			zassert_true(0, "Unexpected src clk (%d)", dev_actual_clk_src);
+		}
+
+		/* Test status of the used clk source */
+		status = clock_control_get_status(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					     (clock_control_subsys_t)&pclken[1]);
+		zassert_true((status == CLOCK_CONTROL_STATUS_ON), "ADC1 clk src must to be on");
+
+		/* Test get_rate(srce clk) */
+		r = clock_control_get_rate(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					(clock_control_subsys_t) &pclken[1],
+					&dev_dt_clk_freq);
+		zassert_true((r == 0), "Could not get ADC1 clk srce freq");
+
+		dev_actual_clk_freq = HAL_RCCEx_GetPeriphCLKFreq(RCC_PERIPHCLK_ADC);
+		zassert_equal(dev_dt_clk_freq, dev_actual_clk_freq,
+				"Expected DT freq: %d Hz. Actual freq: %d Hz",
+				dev_dt_clk_freq, dev_actual_clk_freq);
+
+	} else {
+		zassert_true((DT_NUM_CLOCKS(DT_NODELABEL(adc1)) == 1), "test config issue");
+		/* No domain clock available, don't check gating clock as for adc there is no
+		 * uniform way to verify via hal.
+		 */
+		TC_PRINT("ADC1 no domain clock defined. Skipped check\n");
+	}
+
+	/* Test clock_off(reg_clk) */
+	r = clock_control_off(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+				(clock_control_subsys_t) &pclken[0]);
+	zassert_true((r == 0), "Could not disable ADC1 gating clk");
+
+	zassert_true(!__HAL_RCC_ADC12_IS_CLK_ENABLED(), "ADC1 gating clk should be off");
+
+	/* Test clock_off(domain clk) */
+	/* Not supported today */
+}

tests/drivers/clock_control/stm32_clock_configuration/stm32n6_devices/src/test_stm32_clock_configuration_i2c.c
@@ -1,0 +1,133 @@
+/*
+ * Copyright (c) 2022 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/ztest.h>
+#include <soc.h>
+#include <zephyr/drivers/clock_control.h>
+#include <zephyr/drivers/clock_control/stm32_clock_control.h>
+#include <zephyr/logging/log.h>
+
+static void i2c_set_clock(const struct stm32_pclken *clk)
+{
+	uint32_t dev_actual_clk_src, dev_dt_clk_freq, dev_actual_clk_freq;
+	enum clock_control_status status;
+
+	/* Test clock_on(domain_clk) */
+	int r = clock_control_configure(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+				    (clock_control_subsys_t) clk, NULL);
+	zassert_true((r == 0), "Could not enable I2C domain clock");
+
+	/* Test clock source */
+	dev_actual_clk_src = __HAL_RCC_GET_I2C1_SOURCE();
+
+	switch (clk->bus) {
+	case STM32_SRC_HSI:
+		zassert_equal(dev_actual_clk_src, RCC_I2C1CLKSOURCE_HSI,
+				"Expected I2C src: HSI (0x%lx). Actual I2C src: 0x%x",
+				RCC_I2C1CLKSOURCE_HSI, dev_actual_clk_src);
+		break;
+	case STM32_SRC_MSI:
+		zassert_equal(dev_actual_clk_src, RCC_I2C1CLKSOURCE_MSI,
+				"Expected I2C src: MSI (0x%lx). Actual I2C src: 0x%x",
+				RCC_I2C1CLKSOURCE_MSI, dev_actual_clk_src);
+		break;
+	case STM32_SRC_CKPER:
+		zassert_equal(dev_actual_clk_src, RCC_I2C1CLKSOURCE_CLKP,
+				"Expected I2C src: CKPER (0x%lx). Actual I2C src: 0x%x",
+				RCC_I2C1CLKSOURCE_CLKP, dev_actual_clk_src);
+		break;
+	case STM32_SRC_IC10:
+		zassert_equal(dev_actual_clk_src, RCC_I2C1CLKSOURCE_IC10,
+				"Expected I2C src: IC10 (0x%lx). Actual I2C src: 0x%x",
+				RCC_I2C1CLKSOURCE_IC10, dev_actual_clk_src);
+		break;
+	case STM32_SRC_IC15:
+		zassert_equal(dev_actual_clk_src, RCC_I2C1CLKSOURCE_IC15,
+				"Expected I2C src: IC15 (0x%lx). Actual I2C src: 0x%x",
+				RCC_I2C1CLKSOURCE_IC15, dev_actual_clk_src);
+		break;
+	default:
+		zassert_true(0, "Unexpected domain clk (0x%x)", dev_actual_clk_src);
+		break;
+	}
+
+	/* Test status of the used clk source */
+	status = clock_control_get_status(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					  (clock_control_subsys_t)clk);
+	zassert_true((status == CLOCK_CONTROL_STATUS_ON), "I2C1 clk src must to be on");
+
+	/* Test get_rate(srce clk) */
+	r = clock_control_get_rate(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+				(clock_control_subsys_t) clk,
+				&dev_dt_clk_freq);
+	zassert_true((r == 0), "Could not get I2C clk srce freq");
+
+	dev_actual_clk_freq = HAL_RCCEx_GetPeriphCLKFreq(RCC_PERIPHCLK_I2C1);
+	zassert_equal(dev_dt_clk_freq, dev_actual_clk_freq,
+			"Expected freq: %d Hz. Actual clk: %d Hz",
+			dev_dt_clk_freq, dev_actual_clk_freq);
+}
+
+ZTEST(stm32n6_devices_clocks, test_i2c_clk_config)
+{
+	static const struct stm32_pclken pclken[] = STM32_DT_CLOCKS(DT_NODELABEL(i2c1));
+
+	uint32_t dev_dt_clk_freq, dev_actual_clk_freq;
+	enum clock_control_status status;
+	int r;
+
+	status = clock_control_get_status(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					  (clock_control_subsys_t)&pclken[0]);
+
+	zassert_true((status == CLOCK_CONTROL_STATUS_OFF),
+		     "I2C Gating clock should be off initially");
+
+	/* Test clock_on(gating clock) */
+	r = clock_control_on(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+				(clock_control_subsys_t) &pclken[0]);
+	zassert_true((r == 0), "Could not enable I2C gating clock");
+
+	/* Check via HAL as well as via get_status API */
+	zassert_true(__HAL_RCC_I2C1_IS_CLK_ENABLED(), "[HAL] I2C1 gating clock should be on");
+	status = clock_control_get_status(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					  (clock_control_subsys_t)&pclken[0]);
+	zassert_true((status == CLOCK_CONTROL_STATUS_ON),
+		     "[Zephyr] I2C1 gating clock should be on");
+
+	if (DT_NUM_CLOCKS(DT_NODELABEL(i2c1)) > 1) {
+		if (DT_NUM_CLOCKS(DT_NODELABEL(i2c1)) > 2) {
+			/* set a dummy clock first, to check if the register is set correctly even
+			 * if not in reset state
+			 */
+			i2c_set_clock(&pclken[2]);
+		}
+		i2c_set_clock(&pclken[1]);
+	} else {
+		zassert_true((DT_NUM_CLOCKS(DT_NODELABEL(i2c1)) == 1), "test config issue");
+		/* No domain clock available, get rate from gating clock */
+
+		/* Test get_rate */
+		r = clock_control_get_rate(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					(clock_control_subsys_t) &pclken[0],
+					&dev_dt_clk_freq);
+		zassert_true((r == 0), "Could not get I2C clk freq");
+
+		dev_actual_clk_freq = HAL_RCCEx_GetPeriphCLKFreq(RCC_PERIPHCLK_I2C1);
+		zassert_equal(dev_dt_clk_freq, dev_actual_clk_freq,
+				"Expected freq: %d Hz. Actual freq: %d Hz",
+				dev_dt_clk_freq, dev_actual_clk_freq);
+	}
+
+	/* Test clock_off(gating clk) */
+	r = clock_control_off(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+				(clock_control_subsys_t) &pclken[0]);
+	zassert_true((r == 0), "Could not disable I2C gating clk");
+
+	zassert_true(!__HAL_RCC_I2C1_IS_CLK_ENABLED(), "I2C1 gating clk should be off");
+
+	/* Test clock_off(srce) */
+	/* Not supported today */
+}

tests/drivers/clock_control/stm32_clock_configuration/stm32n6_devices/src/test_stm32_clock_configuration_spi.c
@@ -1,0 +1,101 @@
+/*
+ * Copyright (c) 2022 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/ztest.h>
+#include <soc.h>
+#include <zephyr/drivers/clock_control.h>
+#include <zephyr/drivers/clock_control/stm32_clock_control.h>
+#include <zephyr/logging/log.h>
+
+#if (DT_NUM_CLOCKS(DT_NODELABEL(spi5)) != 2)
+#error "SPI node requires 2 clocks: bus clock and kernel clock"
+#endif
+
+ZTEST(stm32n6_devices_clocks, test_spi_clk_config)
+{
+	const struct stm32_pclken pclken[] = STM32_DT_CLOCKS(DT_NODELABEL(spi5));
+	uint32_t spi_actual_domain_clk;
+	uint32_t spi_dt_clk_freq, spi_actual_clk_freq;
+	int r;
+
+	/* Test clock_on(reg_clk) */
+	r = clock_control_on(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+				(clock_control_subsys_t) &pclken[0]);
+	zassert_true((r == 0), "Could not enable SPI gating clock");
+
+	zassert_true(__HAL_RCC_SPI5_IS_CLK_ENABLED(), "SPI5 gating clock should be on");
+
+	/* Test clock_on(domain source) */
+	r = clock_control_configure(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+				    (clock_control_subsys_t) &pclken[1],
+				    NULL);
+	zassert_true((r == 0), "Could not configure SPI domain clk");
+
+	/* Test clk source */
+	spi_actual_domain_clk = __HAL_RCC_GET_SPI5_SOURCE();
+
+	switch (pclken[1].bus) {
+	case STM32_SRC_PCLK2:
+		zassert_equal(spi_actual_domain_clk, RCC_SPI5CLKSOURCE_PCLK2,
+			      "Expected SPI src: PCLK2 (0x%x). Actual: 0x%x",
+			      RCC_SPI5CLKSOURCE_PCLK2, spi_actual_domain_clk);
+		break;
+	case STM32_SRC_CKPER:
+		zassert_equal(spi_actual_domain_clk, RCC_SPI5CLKSOURCE_CLKP,
+			      "Expected SPI src: PERCLK (0x%x). Actual: 0x%x",
+			      RCC_SPI5CLKSOURCE_CLKP, spi_actual_domain_clk);
+		break;
+	case STM32_SRC_IC9:
+		zassert_equal(spi_actual_domain_clk, RCC_SPI5CLKSOURCE_IC9,
+			      "Expected SPI src: IC9 (0x%x). Actual: 0x%x",
+			      RCC_SPI5CLKSOURCE_IC9, spi_actual_domain_clk);
+		break;
+	case STM32_SRC_IC14:
+		zassert_equal(spi_actual_domain_clk, RCC_SPI5CLKSOURCE_IC14,
+			      "Expected SPI src: IC14 (0x%x). Actual: 0x%x",
+			      RCC_SPI5CLKSOURCE_IC14, spi_actual_domain_clk);
+		break;
+	case STM32_SRC_MSI:
+		zassert_equal(spi_actual_domain_clk, RCC_SPI5CLKSOURCE_MSI,
+			      "Expected SPI src: MSI (0x%x). Actual: 0x%x",
+			      RCC_SPI5CLKSOURCE_MSI, spi_actual_domain_clk);
+		break;
+	case STM32_SRC_HSI:
+		zassert_equal(spi_actual_domain_clk, RCC_SPI5CLKSOURCE_HSI,
+			      "Expected SPI src: HSI (0x%x). Actual: 0x%x",
+			      RCC_SPI5CLKSOURCE_HSI, spi_actual_domain_clk);
+		break;
+	case STM32_SRC_HSE:
+		zassert_equal(spi_actual_domain_clk, RCC_SPI5CLKSOURCE_HSE,
+			      "Expected SPI src: HSE (0x%x). Actual: 0x%x",
+			      RCC_SPI5CLKSOURCE_HSE, spi_actual_domain_clk);
+		break;
+	default:
+		zassert_true(1, "Unexpected clk src (0x%x)", spi_actual_domain_clk);
+		break;
+	}
+
+	/* Test get_rate(source clk) */
+	r = clock_control_get_rate(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+				(clock_control_subsys_t) &pclken[1],
+				&spi_dt_clk_freq);
+	zassert_true((r == 0), "Could not get SPI clk freq");
+
+	spi_actual_clk_freq = HAL_RCCEx_GetPeriphCLKFreq(RCC_PERIPHCLK_SPI5);
+	zassert_equal(spi_dt_clk_freq, spi_actual_clk_freq,
+			"Expected SPI clk: %d. Actual: %d",
+			spi_dt_clk_freq, spi_actual_clk_freq);
+
+	/* Test clock_off(gating clock) */
+	r = clock_control_off(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+			      (clock_control_subsys_t) &pclken[0]);
+	zassert_true((r == 0), "Could not disable SPI5 reg_clk");
+
+	zassert_true(!__HAL_RCC_SPI5_IS_CLK_ENABLED(), "SPI5 gating clock should be off");
+
+	/* Test clock_off(domain clk) */
+	/* Not supported today */
+}

phytec/zephyr-phytec

tests/drivers/clock_control/stm32_clock_configuration/stm32n6_devices/src/test_stm32_clock_configuration_adc.c
@@ -1,0 +1,120 @@
+/*
+ * Copyright (c) 2025 STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <soc.h>
+#include <zephyr/drivers/clock_control.h>
+#include <zephyr/drivers/clock_control/stm32_clock_control.h>
+#include <zephyr/ztest.h>
+
+ZTEST(stm32n6_devices_clocks, test_adc_clk_config)
+{
+	static const struct stm32_pclken pclken[] = STM32_DT_CLOCKS(DT_NODELABEL(adc1));
+
+	uint32_t dev_dt_clk_freq, dev_actual_clk_freq, dev_actual_clk_src;
+	enum clock_control_status status;
+	int r;
+
+	status = clock_control_get_status(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					  (clock_control_subsys_t)&pclken[0]);
+	zassert_true((status == CLOCK_CONTROL_STATUS_OFF),
+		     "ADC1 gating clock should be off initially");
+
+	/* Test clock_on(gating clock) */
+	r = clock_control_on(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+				(clock_control_subsys_t) &pclken[0]);
+	zassert_true((r == 0), "Could not enable ADC1 gating clock");
+
+	/* Check via HAL as well as via get_status API */
+	zassert_true(__HAL_RCC_ADC12_IS_CLK_ENABLED(), "[HAL] ADC1 gating clock should be on");
+
+	status = clock_control_get_status(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					  (clock_control_subsys_t)&pclken[0]);
+	zassert_true((status == CLOCK_CONTROL_STATUS_ON),
+		     "[Zephyr] ADC1 gating clock should be on");
+
+	if (DT_NUM_CLOCKS(DT_NODELABEL(adc1)) > 1) {
+		/* Test clock_on(domain_clk) */
+		r = clock_control_configure(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					    (clock_control_subsys_t) &pclken[1],
+					    NULL);
+		zassert_true((r == 0), "Could not enable ADC1 domain clock");
+
+		/* Test clock source */
+		dev_actual_clk_src = __HAL_RCC_GET_ADC_SOURCE();
+
+		switch (pclken[1].bus) {
+		case STM32_SRC_HCLK1:
+			zassert_equal(dev_actual_clk_src, RCC_ADCCLKSOURCE_HCLK,
+					"Expected ADC1 src: HCLK1 (0x%lx). Actual ADC1 src: 0x%x",
+					RCC_ADCCLKSOURCE_HCLK, dev_actual_clk_src);
+			break;
+		case STM32_SRC_CKPER:
+			zassert_equal(dev_actual_clk_src, RCC_ADCCLKSOURCE_CLKP,
+					"Expected ADC1 src: CKPER (0x%lx). Actual ADC1 src: 0x%x",
+					RCC_ADCCLKSOURCE_CLKP, dev_actual_clk_src);
+			break;
+		case STM32_SRC_IC7:
+			zassert_equal(dev_actual_clk_src, RCC_ADCCLKSOURCE_IC7,
+					"Expected ADC1 src: IC7 (0x%lx). Actual ADC1 src: 0x%x",
+					RCC_ADCCLKSOURCE_IC7, dev_actual_clk_src);
+			break;
+		case STM32_SRC_IC8:
+			zassert_equal(dev_actual_clk_src, RCC_ADCCLKSOURCE_IC8,
+					"Expected ADC1 src: IC8 (0x%lx). Actual ADC1 src: 0x%x",
+					RCC_ADCCLKSOURCE_IC8, dev_actual_clk_src);
+			break;
+		case STM32_SRC_MSI:
+			zassert_equal(dev_actual_clk_src, RCC_ADCCLKSOURCE_MSI,
+					"Expected ADC1 src: MSI (0x%lx). Actual ADC1 src: 0x%x",
+					RCC_ADCCLKSOURCE_MSI, dev_actual_clk_src);
+			break;
+		case STM32_SRC_HSI_DIV:
+			zassert_equal(dev_actual_clk_src, RCC_ADCCLKSOURCE_HSI,
+					"Expected ADC1 src: HSI_DIV (0x%lx). Actual ADC1 src: 0x%x",
+					RCC_ADCCLKSOURCE_HSI, dev_actual_clk_src);
+			break;
+		case STM32_SRC_TIMG:
+			zassert_equal(dev_actual_clk_src, RCC_ADCCLKSOURCE_TIMG,
+					"Expected ADC1 src: TIMG (0x%lx). Actual ADC1 src: 0x%x",
+					RCC_ADCCLKSOURCE_TIMG, dev_actual_clk_src);
+			break;
+		default:
+			zassert_true(0, "Unexpected src clk (%d)", dev_actual_clk_src);
+		}
+
+		/* Test status of the used clk source */
+		status = clock_control_get_status(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					     (clock_control_subsys_t)&pclken[1]);
+		zassert_true((status == CLOCK_CONTROL_STATUS_ON), "ADC1 clk src must to be on");
+
+		/* Test get_rate(srce clk) */
+		r = clock_control_get_rate(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					(clock_control_subsys_t) &pclken[1],
+					&dev_dt_clk_freq);
+		zassert_true((r == 0), "Could not get ADC1 clk srce freq");
+
+		dev_actual_clk_freq = HAL_RCCEx_GetPeriphCLKFreq(RCC_PERIPHCLK_ADC);
+		zassert_equal(dev_dt_clk_freq, dev_actual_clk_freq,
+				"Expected DT freq: %d Hz. Actual freq: %d Hz",
+				dev_dt_clk_freq, dev_actual_clk_freq);
+
+	} else {
+		zassert_true((DT_NUM_CLOCKS(DT_NODELABEL(adc1)) == 1), "test config issue");
+		/* No domain clock available, don't check gating clock as for adc there is no
+		 * uniform way to verify via hal.
+		 */
+	}
+
+	/* Test clock_off(reg_clk) */
+	r = clock_control_off(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+				(clock_control_subsys_t) &pclken[0]);
+	zassert_true((r == 0), "Could not disable ADC1 gating clk");
+
+	zassert_true(!__HAL_RCC_ADC12_IS_CLK_ENABLED(), "ADC1 gating clk should be off");
+
+	/* Test clock_off(domain clk) */
+	/* Not supported today */
+}

tests/drivers/clock_control/stm32_clock_configuration/stm32n6_devices/src/test_stm32_clock_configuration_i2c.c
@@ -1,0 +1,132 @@
+/*
+ * Copyright (c) 2025 STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <soc.h>
+#include <zephyr/drivers/clock_control.h>
+#include <zephyr/drivers/clock_control/stm32_clock_control.h>
+#include <zephyr/ztest.h>
+
+static void i2c_set_clock(const struct stm32_pclken *clk)
+{
+	uint32_t dev_actual_clk_src, dev_dt_clk_freq, dev_actual_clk_freq;
+	enum clock_control_status status;
+
+	/* Test clock_on(domain_clk) */
+	int r = clock_control_configure(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					(clock_control_subsys_t) clk, NULL);
+	zassert_true((r == 0), "Could not enable I2C domain clock");
+
+	/* Test clock source */
+	dev_actual_clk_src = __HAL_RCC_GET_I2C1_SOURCE();
+
+	switch (clk->bus) {
+	case STM32_SRC_HSI_DIV:
+		zassert_equal(dev_actual_clk_src, RCC_I2C1CLKSOURCE_HSI,
+				"Expected I2C src: HSI_DIV (0x%lx). Actual I2C src: 0x%x",
+				RCC_I2C1CLKSOURCE_HSI, dev_actual_clk_src);
+		break;
+	case STM32_SRC_MSI:
+		zassert_equal(dev_actual_clk_src, RCC_I2C1CLKSOURCE_MSI,
+				"Expected I2C src: MSI (0x%lx). Actual I2C src: 0x%x",
+				RCC_I2C1CLKSOURCE_MSI, dev_actual_clk_src);
+		break;
+	case STM32_SRC_CKPER:
+		zassert_equal(dev_actual_clk_src, RCC_I2C1CLKSOURCE_CLKP,
+				"Expected I2C src: CKPER (0x%lx). Actual I2C src: 0x%x",
+				RCC_I2C1CLKSOURCE_CLKP, dev_actual_clk_src);
+		break;
+	case STM32_SRC_IC10:
+		zassert_equal(dev_actual_clk_src, RCC_I2C1CLKSOURCE_IC10,
+				"Expected I2C src: IC10 (0x%lx). Actual I2C src: 0x%x",
+				RCC_I2C1CLKSOURCE_IC10, dev_actual_clk_src);
+		break;
+	case STM32_SRC_IC15:
+		zassert_equal(dev_actual_clk_src, RCC_I2C1CLKSOURCE_IC15,
+				"Expected I2C src: IC15 (0x%lx). Actual I2C src: 0x%x",
+				RCC_I2C1CLKSOURCE_IC15, dev_actual_clk_src);
+		break;
+	default:
+		zassert_true(0, "Unexpected domain clk (0x%x)", dev_actual_clk_src);
+		break;
+	}
+
+	/* Test status of the used clk source */
+	status = clock_control_get_status(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					  (clock_control_subsys_t)clk);
+	zassert_true((status == CLOCK_CONTROL_STATUS_ON), "I2C1 clk src must to be on");
+
+	/* Test get_rate(srce clk) */
+	r = clock_control_get_rate(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+				   (clock_control_subsys_t) clk,
+				   &dev_dt_clk_freq);
+	zassert_true((r == 0), "Could not get I2C clk srce freq");
+
+	dev_actual_clk_freq = HAL_RCCEx_GetPeriphCLKFreq(RCC_PERIPHCLK_I2C1);
+	zassert_equal(dev_dt_clk_freq, dev_actual_clk_freq,
+			"Expected freq: %d Hz. Actual clk: %d Hz",
+			dev_dt_clk_freq, dev_actual_clk_freq);
+}
+
+ZTEST(stm32n6_devices_clocks, test_i2c_clk_config)
+{
+	static const struct stm32_pclken pclken[] = STM32_DT_CLOCKS(DT_NODELABEL(i2c1));
+
+	uint32_t dev_dt_clk_freq, dev_actual_clk_freq;
+	enum clock_control_status status;
+	int r;
+
+	status = clock_control_get_status(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					  (clock_control_subsys_t)&pclken[0]);
+
+	zassert_true((status == CLOCK_CONTROL_STATUS_OFF),
+		     "I2C Gating clock should be off initially");
+
+	/* Test clock_on(gating clock) */
+	r = clock_control_on(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+			     (clock_control_subsys_t) &pclken[0]);
+	zassert_true((r == 0), "Could not enable I2C gating clock");
+
+	/* Check via HAL as well as via get_status API */
+	zassert_true(__HAL_RCC_I2C1_IS_CLK_ENABLED(), "[HAL] I2C1 gating clock should be on");
+	status = clock_control_get_status(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					  (clock_control_subsys_t)&pclken[0]);
+	zassert_true((status == CLOCK_CONTROL_STATUS_ON),
+		     "[Zephyr] I2C1 gating clock should be on");
+
+	if (DT_NUM_CLOCKS(DT_NODELABEL(i2c1)) > 1) {
+		if (DT_NUM_CLOCKS(DT_NODELABEL(i2c1)) > 2) {
+			/* set a dummy clock first, to check if the register is set correctly even
+			 * if not in reset state
+			 */
+			i2c_set_clock(&pclken[2]);
+		}
+		i2c_set_clock(&pclken[1]);
+	} else {
+		zassert_true((DT_NUM_CLOCKS(DT_NODELABEL(i2c1)) == 1), "test config issue");
+		/* No domain clock available, get rate from gating clock */
+
+		/* Test get_rate */
+		r = clock_control_get_rate(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+					   (clock_control_subsys_t) &pclken[0],
+					   &dev_dt_clk_freq);
+		zassert_true((r == 0), "Could not get I2C clk freq");
+
+		dev_actual_clk_freq = HAL_RCCEx_GetPeriphCLKFreq(RCC_PERIPHCLK_I2C1);
+		zassert_equal(dev_dt_clk_freq, dev_actual_clk_freq,
+				"Expected freq: %d Hz. Actual freq: %d Hz",
+				dev_dt_clk_freq, dev_actual_clk_freq);
+	}
+
+	/* Test clock_off(gating clk) */
+	r = clock_control_off(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+			      (clock_control_subsys_t) &pclken[0]);
+	zassert_true((r == 0), "Could not disable I2C gating clk");
+
+	zassert_true(!__HAL_RCC_I2C1_IS_CLK_ENABLED(), "I2C1 gating clk should be off");
+
+	/* Test clock_off(srce) */
+	/* Not supported today */
+}

tests/drivers/clock_control/stm32_clock_configuration/stm32n6_devices/src/test_stm32_clock_configuration_spi.c
@@ -1,0 +1,100 @@
+/*
+ * Copyright (c) 2025 STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <soc.h>
+#include <zephyr/drivers/clock_control.h>
+#include <zephyr/drivers/clock_control/stm32_clock_control.h>
+#include <zephyr/ztest.h>
+
+#if (DT_NUM_CLOCKS(DT_NODELABEL(spi5)) != 2)
+#error "SPI node requires 2 clocks: bus clock and kernel clock"
+#endif
+
+ZTEST(stm32n6_devices_clocks, test_spi_clk_config)
+{
+	const struct stm32_pclken pclken[] = STM32_DT_CLOCKS(DT_NODELABEL(spi5));
+	uint32_t spi_actual_domain_clk;
+	uint32_t spi_dt_clk_freq, spi_actual_clk_freq;
+	int r;
+
+	/* Test clock_on(reg_clk) */
+	r = clock_control_on(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+			     (clock_control_subsys_t) &pclken[0]);
+	zassert_true((r == 0), "Could not enable SPI gating clock");
+
+	zassert_true(__HAL_RCC_SPI5_IS_CLK_ENABLED(), "SPI5 gating clock should be on");
+
+	/* Test clock_on(domain source) */
+	r = clock_control_configure(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+				    (clock_control_subsys_t) &pclken[1],
+				    NULL);
+	zassert_true((r == 0), "Could not configure SPI domain clk");
+
+	/* Test clk source */
+	spi_actual_domain_clk = __HAL_RCC_GET_SPI5_SOURCE();
+
+	switch (pclken[1].bus) {
+	case STM32_SRC_PCLK2:
+		zassert_equal(spi_actual_domain_clk, RCC_SPI5CLKSOURCE_PCLK2,
+			      "Expected SPI src: PCLK2 (0x%x). Actual: 0x%x",
+			      RCC_SPI5CLKSOURCE_PCLK2, spi_actual_domain_clk);
+		break;
+	case STM32_SRC_CKPER:
+		zassert_equal(spi_actual_domain_clk, RCC_SPI5CLKSOURCE_CLKP,
+			      "Expected SPI src: PERCLK (0x%x). Actual: 0x%x",
+			      RCC_SPI5CLKSOURCE_CLKP, spi_actual_domain_clk);
+		break;
+	case STM32_SRC_IC9:
+		zassert_equal(spi_actual_domain_clk, RCC_SPI5CLKSOURCE_IC9,
+			      "Expected SPI src: IC9 (0x%x). Actual: 0x%x",
+			      RCC_SPI5CLKSOURCE_IC9, spi_actual_domain_clk);
+		break;
+	case STM32_SRC_IC14:
+		zassert_equal(spi_actual_domain_clk, RCC_SPI5CLKSOURCE_IC14,
+			      "Expected SPI src: IC14 (0x%x). Actual: 0x%x",
+			      RCC_SPI5CLKSOURCE_IC14, spi_actual_domain_clk);
+		break;
+	case STM32_SRC_MSI:
+		zassert_equal(spi_actual_domain_clk, RCC_SPI5CLKSOURCE_MSI,
+			      "Expected SPI src: MSI (0x%x). Actual: 0x%x",
+			      RCC_SPI5CLKSOURCE_MSI, spi_actual_domain_clk);
+		break;
+	case STM32_SRC_HSI_DIV:
+		zassert_equal(spi_actual_domain_clk, RCC_SPI5CLKSOURCE_HSI,
+			      "Expected SPI src: HSI_DIV (0x%x). Actual: 0x%x",
+			      RCC_SPI5CLKSOURCE_HSI, spi_actual_domain_clk);
+		break;
+	case STM32_SRC_HSE:
+		zassert_equal(spi_actual_domain_clk, RCC_SPI5CLKSOURCE_HSE,
+			      "Expected SPI src: HSE (0x%x). Actual: 0x%x",
+			      RCC_SPI5CLKSOURCE_HSE, spi_actual_domain_clk);
+		break;
+	default:
+		zassert_true(1, "Unexpected clk src (0x%x)", spi_actual_domain_clk);
+		break;
+	}
+
+	/* Test get_rate(source clk) */
+	r = clock_control_get_rate(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+				   (clock_control_subsys_t) &pclken[1],
+				   &spi_dt_clk_freq);
+	zassert_true((r == 0), "Could not get SPI clk freq");
+
+	spi_actual_clk_freq = HAL_RCCEx_GetPeriphCLKFreq(RCC_PERIPHCLK_SPI5);
+	zassert_equal(spi_dt_clk_freq, spi_actual_clk_freq,
+			"Expected SPI clk: %d. Actual: %d",
+			spi_dt_clk_freq, spi_actual_clk_freq);
+
+	/* Test clock_off(gating clock) */
+	r = clock_control_off(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
+			      (clock_control_subsys_t) &pclken[0]);
+	zassert_true((r == 0), "Could not disable SPI5 reg_clk");
+
+	zassert_true(!__HAL_RCC_SPI5_IS_CLK_ENABLED(), "SPI5 gating clock should be off");
+
+	/* Test clock_off(domain clk) */
+	/* Not supported today */
+}