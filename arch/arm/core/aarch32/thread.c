bbolen/zephyr

arch: arm: aarch32: cortex_a_r: Add shared FPU support
This adds lazy floating point context switching.  On svc/irq entrance,
the VFP is disabled and a pointer to the exception stack frame is saved
away.  If the esf pointer is still valid on exception exit, then no
other context used the VFP so the context is still valid and nothing
needs to be restored.  If the esf pointer is NULL on exception exit,
then some other context used the VFP and the floating point context is
restored from the esf.

The undefined instruction handler is responsible for saving away the
floating point context if needed.  If the handler is in the first
irq/svc context and the current thread uses the VFP, then the float
context needs to be saved.  Also, if the handler is in a nested context
and the previous context was using the FVP, save the float context.

arch/arm/core/aarch32/thread.c
@@ -94,6 +94,12 @@ void arch_new_thread(struct k_thread *thread, k_thread_stack_t *stack,
 
 	z_new_thread_init(thread, pStackMem, stackSize);
 
+#if defined(CONFIG_CPU_CORTEX_R) && defined(CONFIG_FPU_SHARING)
+	pInitCtx = (struct __esf *)(Z_STACK_PTR_ALIGN(stackEnd -
+		(char *)top_of_stack_offset - sizeof(struct __esf)));
+
+	memset(pInitCtx, 0, sizeof(struct __esf));
+#else
 	/* Carve the thread entry struct from the "base" of the stack
 	 *
 	 * The initial carved stack frame only needs to contain the basic
@@ -102,6 +108,7 @@ void arch_new_thread(struct k_thread *thread, k_thread_stack_t *stack,
 	 */
 	pInitCtx = (struct __esf *)(Z_STACK_PTR_ALIGN(stackEnd -
 		(char *)top_of_stack_offset - sizeof(struct __basic_sf) - sizeof(struct __return_sf)));
+#endif
 
 #if defined(CONFIG_USERSPACE)
 	if ((options & K_USER) != 0) {
@@ -355,7 +362,11 @@ int arch_float_disable(struct k_thread *thread)
 
 	thread->base.user_options &= ~K_FP_REGS;
 
+#if defined(CONFIG_CPU_CORTEX_M)
 	__set_CONTROL(__get_CONTROL() & (~CONTROL_FPCA_Msk));
+#else
+	__set_FPEXC(0);
+#endif
 
 	/* No need to add an ISB barrier after setting the CONTROL
 	 * register; arch_irq_unlock() already adds one.
@@ -378,7 +389,7 @@ void arch_switch_to_main_thread(struct k_thread *main_thread,
 	 * initialized at thread creation for threads that make use of the FP).
 	 */
 	__set_FPSCR(0);
-#if defined(CONFIG_FPU_SHARING)
+#if defined(CONFIG_CPU_CORTEX_M) && defined(CONFIG_FPU_SHARING)
 	/* In Sharing mode clearing FPSCR may set the CONTROL.FPCA flag. */
 	__set_CONTROL(__get_CONTROL() & (~(CONTROL_FPCA_Msk)));
 	__ISB();

phytec/zephyr-phytec

Signed-off-by: Bradley Bolen <bbolen@lexmark.com>

arch/arm/core/aarch32/thread.c
@@ -112,6 +112,13 @@ void arch_new_thread(struct k_thread *thread, k_thread_stack_t *stack,
 #endif /* CONFIG_COMPILER_ISA_THUMB2 */
 #endif /* CONFIG_CPU_CORTEX_M */
 
+#if !defined(CONFIG_CPU_CORTEX_M) \
+	&& defined(CONFIG_FPU) && defined(CONFIG_FPU_SHARING)
+	iframe = (struct __basic_sf *)
+		((uintptr_t)iframe - sizeof(struct __fpu_sf));
+	memset(iframe, 0, sizeof(struct __fpu_sf));
+#endif
+
 	thread->callee_saved.psp = (uint32_t)iframe;
 	thread->arch.basepri = 0;
 
@@ -470,7 +477,11 @@ int arch_float_disable(struct k_thread *thread)
 
 	thread->base.user_options &= ~K_FP_REGS;
 
+#if defined(CONFIG_CPU_CORTEX_M)
 	__set_CONTROL(__get_CONTROL() & (~CONTROL_FPCA_Msk));
+#else
+	__set_FPEXC(0);
+#endif
 
 	/* No need to add an ISB barrier after setting the CONTROL
 	 * register; arch_irq_unlock() already adds one.
@@ -483,7 +494,7 @@ int arch_float_disable(struct k_thread *thread)
 
 int arch_float_enable(struct k_thread *thread, unsigned int options)
 {
-	/* This is not supported in Cortex-M and Cortex-R does not have FPU */
+	/* This is not supported in Cortex-M */
 	return -ENOTSUP;
 }
 #endif /* CONFIG_FPU && CONFIG_FPU_SHARING */
@@ -508,7 +519,7 @@ static void z_arm_prepare_switch_to_main(void)
 #else
 	__set_FPSCR(0);
 #endif
-#if defined(CONFIG_FPU_SHARING)
+#if defined(CONFIG_CPU_CORTEX_M) && defined(CONFIG_FPU_SHARING)
 	/* In Sharing mode clearing FPSCR may set the CONTROL.FPCA flag. */
 	__set_CONTROL(__get_CONTROL() & (~(CONTROL_FPCA_Msk)));
 	__ISB();