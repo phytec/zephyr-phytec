Balaklaka/zephyr

Bluetooth: Mesh: Add composition data page 128
Composition data page 128 represents the new composition data after a
composition refresh procedure. The implementation stores the old
composition data (before applying a DFU or similar), and uses that as
page 0 if present. As the device has already rebooted by the time page
128 becomes active, its active composition data is page 128, while the
backed up data is the "old" data, which should be shown as page 0.

Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>

subsys/bluetooth/mesh/access.c
@@ -1084,6 +1084,20 @@ static int vnd_mod_set(const char *name, size_t len_rd,
 
 BT_MESH_SETTINGS_DEFINE(vnd_mod, "v", vnd_mod_set);
 
+static int comp_set(const char *name, size_t len_rd, settings_read_cb read_cb,
+		    void *cb_arg)
+{
+	/* Only need to know that the entry exists. Will load the contents on
+	 * demand.
+	 */
+	if (len_rd > 0) {
+		atomic_set_bit(bt_mesh.flags, BT_MESH_COMP_DIRTY);
+	}
+
+	return 0;
+}
+BT_MESH_SETTINGS_DEFINE(comp, "cmp", comp_set);
+
 static void encode_mod_path(struct bt_mesh_model *mod, bool vnd,
 			    const char *key, char *path, size_t path_len)
 {
@@ -1226,6 +1240,85 @@ void bt_mesh_model_pub_store(struct bt_mesh_model *mod)
 {
 	mod->flags |= BT_MESH_MOD_PUB_PENDING;
 	bt_mesh_settings_store_schedule(BT_MESH_SETTINGS_MOD_PENDING);
+}
+
+int bt_mesh_comp_store(void)
+{
+	NET_BUF_SIMPLE_DEFINE(buf, BT_MESH_TX_SDU_MAX);
+	int err;
+
+	err = bt_mesh_comp_get_page_0(&buf);
+	if (err) {
+		BT_ERR("Failed to read composition data: %d", err);
+		return err;
+	}
+
+	err = settings_save_one("bt/mesh/cmp", buf.data, buf.len);
+	if (err) {
+		BT_ERR("Failed to store composition data: %d", err);
+	} else {
+		BT_DBG("Stored composition data");
+	}
+
+	return err;
+}
+
+int bt_mesh_comp_change_prepare(void)
+{
+	return bt_mesh_comp_store();
+}
+
+void bt_mesh_comp_clear(void)
+{
+	int err;
+
+	err = settings_delete("bt/mesh/cmp");
+	if (err) {
+		BT_ERR("Failed to clear composition data: %d", err);
+	} else {
+		BT_DBG("Cleared composition data");
+	}
+
+	atomic_clear_bit(bt_mesh.flags, BT_MESH_COMP_DIRTY);
+}
+
+static int read_comp_cb(const char *key, size_t len, settings_read_cb read_cb,
+			void *cb_arg, void *param)
+{
+	struct net_buf_simple *buf = param;
+
+	if (len > net_buf_simple_tailroom(buf)) {
+		return -ENOBUFS;
+	}
+
+	len = read_cb(cb_arg, net_buf_simple_tail(buf), len);
+	if (len > 0) {
+		net_buf_simple_add(buf, len);
+	}
+
+	return -EALREADY;
+}
+
+int bt_mesh_comp_read(struct net_buf_simple *buf)
+{
+	size_t original_len = buf->len;
+	int err;
+
+	if (!IS_ENABLED(CONFIG_BT_SETTINGS)) {
+		return -ENOTSUP;
+	}
+
+	err = settings_load_subtree_direct("bt/mesh/cmp", read_comp_cb, buf);
+	if (err) {
+		BT_ERR("Failed reading composition data: %d", err);
+		return err;
+	}
+
+	if (buf->len == original_len) {
+		return -ENOENT;
+	}
+
+	return 0;
 }
 
 int bt_mesh_model_data_store(struct bt_mesh_model *mod, bool vnd,

subsys/bluetooth/mesh/cfg_srv.c
@@ -76,7 +76,7 @@ static int comp_add_elem(struct net_buf_simple *buf, struct bt_mesh_elem *elem,
 	return 0;
 }
 
-static int comp_get_page_0(struct net_buf_simple *buf)
+int bt_mesh_comp_get_page_0(struct net_buf_simple *buf)
 {
 	uint16_t feat = 0U;
 	const struct bt_mesh_comp *comp;
@@ -131,7 +131,11 @@ static int dev_comp_data_get(struct bt_mesh_model *model,
 	       bt_hex(buf->data, buf->len));
 
 	page = net_buf_simple_pull_u8(buf);
-	if (page != 0U) {
+
+	if (page >= 128U && atomic_test_bit(bt_mesh.flags, BT_MESH_COMP_DIRTY)) {
+		BT_DBG("Composition data page 128");
+		page = 128U;
+	} else if (page != 0U) {
 		BT_DBG("Composition page %u not available", page);
 		page = 0U;
 	}
@@ -139,10 +143,21 @@ static int dev_comp_data_get(struct bt_mesh_model *model,
 	bt_mesh_model_msg_init(&sdu, OP_DEV_COMP_DATA_STATUS);
 
 	net_buf_simple_add_u8(&sdu, page);
-	err = comp_get_page_0(&sdu);
-	if (err) {
-		BT_ERR("Unable to get composition page 0");
-		return err;
+	if (atomic_test_bit(bt_mesh.flags, BT_MESH_COMP_DIRTY) == (page == 0U)) {
+		sdu.size -= BT_MESH_MIC_SHORT;
+		err = bt_mesh_comp_read(&sdu);
+		if (err) {
+			BT_ERR("Unable to get stored composition data");
+			return err;
+		}
+
+		sdu.size += BT_MESH_MIC_SHORT;
+	} else {
+		err = bt_mesh_comp_get_page_0(&sdu);
+		if (err < 0) {
+			BT_ERR("Unable to get composition page 0");
+			return err;
+		}
 	}
 
 	if (bt_mesh_model_send(model, ctx, &sdu, NULL, NULL)) {

phytec/zephyr-phytec

Co-authored-by: Ingar Kulbrandstad <ingar.kulbrandstad@nordicsemi.no>
Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

subsys/bluetooth/mesh/access.c
@@ -1078,6 +1078,20 @@ static int vnd_mod_set(const char *name, size_t len_rd,
 
 BT_MESH_SETTINGS_DEFINE(vnd_mod, "v", vnd_mod_set);
 
+static int comp_set(const char *name, size_t len_rd, settings_read_cb read_cb,
+		    void *cb_arg)
+{
+	/* Only need to know that the entry exists. Will load the contents on
+	 * demand.
+	 */
+	if (len_rd > 0) {
+		atomic_set_bit(bt_mesh.flags, BT_MESH_COMP_DIRTY);
+	}
+
+	return 0;
+}
+BT_MESH_SETTINGS_DEFINE(comp, "cmp", comp_set);
+
 static void encode_mod_path(struct bt_mesh_model *mod, bool vnd,
 			    const char *key, char *path, size_t path_len)
 {
@@ -1220,6 +1234,89 @@ void bt_mesh_model_pub_store(struct bt_mesh_model *mod)
 {
 	mod->flags |= BT_MESH_MOD_PUB_PENDING;
 	bt_mesh_settings_store_schedule(BT_MESH_SETTINGS_MOD_PENDING);
+}
+
+int bt_mesh_comp_store(void)
+{
+	NET_BUF_SIMPLE_DEFINE(buf, BT_MESH_TX_SDU_MAX);
+	int err;
+
+	err = bt_mesh_comp_get_page_0(&buf);
+	if (err) {
+		LOG_ERR("Failed to read composition data: %d", err);
+		return err;
+	}
+
+	err = settings_save_one("bt/mesh/cmp", buf.data, buf.len);
+	if (err) {
+		LOG_ERR("Failed to store composition data: %d", err);
+	} else {
+		LOG_DBG("Stored composition data");
+	}
+
+	return err;
+}
+
+int bt_mesh_comp_change_prepare(void)
+{
+	if (!IS_ENABLED(CONFIG_BT_SETTINGS)) {
+		return -ENOTSUP;
+	}
+
+	return bt_mesh_comp_store();
+}
+
+void bt_mesh_comp_clear(void)
+{
+	int err;
+
+	err = settings_delete("bt/mesh/cmp");
+	if (err) {
+		LOG_ERR("Failed to clear composition data: %d", err);
+	} else {
+		LOG_DBG("Cleared composition data page 128");
+	}
+
+	atomic_clear_bit(bt_mesh.flags, BT_MESH_COMP_DIRTY);
+}
+
+static int read_comp_cb(const char *key, size_t len, settings_read_cb read_cb,
+			void *cb_arg, void *param)
+{
+	struct net_buf_simple *buf = param;
+
+	if (len > net_buf_simple_tailroom(buf)) {
+		return -ENOBUFS;
+	}
+
+	len = read_cb(cb_arg, net_buf_simple_tail(buf), len);
+	if (len > 0) {
+		net_buf_simple_add(buf, len);
+	}
+
+	return -EALREADY;
+}
+
+int bt_mesh_comp_read(struct net_buf_simple *buf)
+{
+	size_t original_len = buf->len;
+	int err;
+
+	if (!IS_ENABLED(CONFIG_BT_SETTINGS)) {
+		return -ENOTSUP;
+	}
+
+	err = settings_load_subtree_direct("bt/mesh/cmp", read_comp_cb, buf);
+	if (err) {
+		LOG_ERR("Failed reading composition data: %d", err);
+		return err;
+	}
+
+	if (buf->len == original_len) {
+		return -ENOENT;
+	}
+
+	return 0;
 }
 
 int bt_mesh_model_data_store(struct bt_mesh_model *mod, bool vnd,

subsys/bluetooth/mesh/cfg_srv.c
@@ -132,7 +132,11 @@ static int dev_comp_data_get(struct bt_mesh_model *model,
 		ctx->addr, buf->len, bt_hex(buf->data, buf->len));
 
 	page = net_buf_simple_pull_u8(buf);
-	if (page != 0U) {
+
+	if (page >= 128U && atomic_test_bit(bt_mesh.flags, BT_MESH_COMP_DIRTY)) {
+		LOG_DBG("Composition data page 128");
+		page = 128U;
+	} else if (page != 0U) {
 		LOG_DBG("Composition page %u not available", page);
 		page = 0U;
 	}
@@ -140,10 +144,21 @@ static int dev_comp_data_get(struct bt_mesh_model *model,
 	bt_mesh_model_msg_init(&sdu, OP_DEV_COMP_DATA_STATUS);
 
 	net_buf_simple_add_u8(&sdu, page);
-	err = bt_mesh_comp_get_page_0(&sdu);
-	if (err) {
-		LOG_ERR("Unable to get composition page 0");
-		return err;
+	if (atomic_test_bit(bt_mesh.flags, BT_MESH_COMP_DIRTY) == (page == 0U)) {
+		sdu.size -= BT_MESH_MIC_SHORT;
+		err = bt_mesh_comp_read(&sdu);
+		if (err) {
+			LOG_ERR("Unable to get stored composition data");
+			return err;
+		}
+
+		sdu.size += BT_MESH_MIC_SHORT;
+	} else {
+		err = bt_mesh_comp_get_page_0(&sdu);
+		if (err < 0) {
+			LOG_ERR("Unable to get composition page 0");
+			return err;
+		}
 	}
 
 	if (bt_mesh_model_send(model, ctx, &sdu, NULL, NULL)) {