zephyrproject-rtos/zephyr

boards: silabs: introduction of siwx917_dk2605a board
This commit introduce siwx917_dk2605 development kit board

Signed-off-by: Martin Hoff <martin.hoff@silabs.com>

boards/silabs/dev_kits/siwx917_dk2605a/board.cmake
@@ -1,0 +1,12 @@
+# Copyright (c) 2025 Silicon Laboratories Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(silabs_commander "--device=SiWG917M111GTBA")
+include(${ZEPHYR_BASE}/boards/common/silabs_commander.board.cmake)
+
+# It is not possible to load/flash a firmware using JLink, but it is possible to
+# debug a firmware with:
+#    west attach -r jlink
+# Once started, it should be possible to reset the device with "monitor reset"
+board_runner_args(jlink "--device=Si917" "--speed=10000")
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)

phytec/zephyr-phytec

boards/silabs/dev_kits/siwx917_dk2605a/board.cmake
@@ -1,0 +1,12 @@
+# Copyright (c) 2025 Silicon Laboratories Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(silabs_commander "--device=SiWG917M111MGTBA")
+include(${ZEPHYR_BASE}/boards/common/silabs_commander.board.cmake)
+
+# It is not possible to load/flash a firmware using JLink, but it is possible to
+# debug a firmware with:
+#    west attach -r jlink
+# Once started, it should be possible to reset the device with "monitor reset"
+board_runner_args(jlink "--device=Si917" "--speed=10000")
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)