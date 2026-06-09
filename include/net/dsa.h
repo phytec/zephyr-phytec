arvinf/zephyr

dts: ksz8974: add support for ksz8863
Add DSA support for KSZ8863 chip.

Signed-off-by: Arvin Farahmand <arvinf@ip-logix.com>

include/net/dsa.h
@@ -28,7 +28,7 @@
  * Size of the DSA TAG:
  * - KSZ8794 - 1 byte
  */
-#if defined(CONFIG_DSA_KSZ8794) && defined(CONFIG_DSA_KSZ8794_TAIL_TAGGING)
+#if defined(CONFIG_DSA_KSZ8794) && defined(CONFIG_DSA_TAIL_TAGGING)
 #define DSA_TAG_SIZE 1
 #else
 #define DSA_TAG_SIZE 0

phytec/zephyr-phytec

include/net/dsa.h
@@ -28,7 +28,7 @@
  * Size of the DSA TAG:
  * - KSZ8794 - 1 byte
  */
-#if defined(CONFIG_DSA_KSZ8794) && defined(CONFIG_DSA_KSZ8794_TAIL_TAGGING)
+#if defined(CONFIG_DSA_KSZ8794) && defined(DSA_KSZ_TAIL_TAGGING)
 #define DSA_TAG_SIZE 1
 #else
 #define DSA_TAG_SIZE 0