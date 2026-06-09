kz-space/zephyr

net: ptp: Add Kconfig symbols and preprocessor guards for some states
Based on IEEE 1588-2019 some of PTP Port states might not be used
if the option of 17.7 is implemented. This patch prepares state
machine implementation for such scenario.

Signed-off-by: Adam Wojasinski <awojasinski@baylibre.com>

subsys/net/lib/ptp/state_machine.c
@@ -23,19 +23,23 @@ enum ptp_port_state ptp_state_machine(enum ptp_port_state state,
 	case PTP_PS_INITIALIZING:
 		switch (event) {
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_INIT_COMPLETE:
-			new_state = PTP_PS_LISTENING;
+			new_state = IS_ENABLED(CONFIG_PTP_LISTENING_PRESENT) ?
+					PTP_PS_LISTENING : PTP_PS_MASTER;
 			break;
 		default:
 			break;
 		}
 		break;
+#if CONFIG_PTP_FAULTY_PRESENT
 	case PTP_PS_FAULTY:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_CLEARED:
 			new_state = PTP_PS_INITIALIZING;
@@ -44,24 +48,31 @@ enum ptp_port_state ptp_state_machine(enum ptp_port_state state,
 			break;
 		}
 		break;
+#endif /* CONFIG_PTP_FAULTY_PRESENT */
+#if CONFIG_PTP_DISABLED_PRESENT
 	case PTP_PS_DISABLED:
 		if (event == PTP_EVT_DESIGNATED_ENABLED) {
 			new_state = PTP_PS_INITIALIZING;
 		}
 		break;
+#endif /* CONFIG_PTP_DISABLED_PRESENT */
+#if CONFIG_PTP_LISTENING_PRESENT
 	case PTP_PS_LISTENING:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_ANNOUNCE_RECEIPT_TIMEOUT_EXPIRES:
 			new_state = PTP_PS_MASTER;
 			break;
 		case PTP_EVT_RS_MASTER:
-			new_state = PTP_PS_PRE_MASTER;
+			new_state = IS_ENABLED(CONFIG_PTP_PRE_MASTER_PRESENT) ?
+					PTP_PS_PRE_MASTER : PTP_PS_MASTER;
 			break;
 		case PTP_EVT_RS_GRAND_MASTER:
 			new_state = PTP_PS_GRAND_MASTER;
@@ -70,19 +81,24 @@ enum ptp_port_state ptp_state_machine(enum ptp_port_state state,
 			new_state = PTP_PS_PASSIVE;
 			break;
 		case PTP_EVT_RS_SLAVE:
-			new_state = PTP_PS_UNCALIBRATED;
+			new_state = IS_ENABLED(CONFIG_PTP_UNCALIBRATED_PRESENT) ?
+					PTP_PS_UNCALIBRATED : PTP_PS_SLAVE;
 			break;
 		default:
 			break;
 		}
 		break;
+#endif /* CONFIG_PTP_LISTENING_PRESENT */
+#if CONFIG_PTP_PRE_MASTER_PRESENT
 	case PTP_PS_PRE_MASTER:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_QUALIFICATION_TIMEOUT_EXPIRES:
 			new_state = PTP_PS_MASTER;
@@ -91,25 +107,30 @@ enum ptp_port_state ptp_state_machine(enum ptp_port_state state,
 			new_state = PTP_PS_PASSIVE;
 			break;
 		case PTP_EVT_RS_SLAVE:
-			new_state = PTP_PS_UNCALIBRATED;
+			new_state = IS_ENABLED(CONFIG_PTP_UNCALIBRATED_PRESENT) ?
+					PTP_PS_UNCALIBRATED : PTP_PS_SLAVE;
 			break;
 		default:
 			break;
 		}
 		break;
+#endif /* CONFIG_PTP_PRE_MASTER_PRESENT */
 	case PTP_PS_MASTER:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_RS_PASSIVE:
 			new_state = PTP_PS_PASSIVE;
 			break;
 		case PTP_EVT_RS_SLAVE:
-			new_state = PTP_PS_UNCALIBRATED;
+			new_state = IS_ENABLED(CONFIG_PTP_UNCALIBRATED_PRESENT) ?
+					PTP_PS_UNCALIBRATED : PTP_PS_SLAVE;
 			break;
 		default:
 			break;
@@ -118,31 +139,38 @@ enum ptp_port_state ptp_state_machine(enum ptp_port_state state,
 	case PTP_PS_PASSIVE:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_RS_MASTER:
-			new_state = PTP_PS_PRE_MASTER;
+			new_state = IS_ENABLED(CONFIG_PTP_PRE_MASTER_PRESENT) ?
+					PTP_PS_PRE_MASTER : PTP_PS_MASTER;
 			break;
 		case PTP_EVT_RS_GRAND_MASTER:
 			new_state = PTP_PS_GRAND_MASTER;
 			break;
 		case PTP_EVT_RS_SLAVE:
-			new_state = PTP_PS_UNCALIBRATED;
+			new_state = IS_ENABLED(CONFIG_PTP_UNCALIBRATED_PRESENT) ?
+					PTP_PS_UNCALIBRATED : PTP_PS_SLAVE;
 			break;
 		default:
 			break;
 		}
 		break;
+#if CONFIG_PTP_UNCALIBRATED_PRESENT
 	case PTP_PS_UNCALIBRATED:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_ANNOUNCE_RECEIPT_TIMEOUT_EXPIRES:
 			new_state = PTP_PS_MASTER;
@@ -151,7 +179,8 @@ enum ptp_port_state ptp_state_machine(enum ptp_port_state state,
 			new_state = PTP_PS_SLAVE;
 			break;
 		case PTP_EVT_RS_MASTER:
-			new_state = PTP_PS_PRE_MASTER;
+			new_state = IS_ENABLED(CONFIG_PTP_PRE_MASTER_PRESENT) ?
+					PTP_PS_PRE_MASTER : PTP_PS_MASTER;
 			break;
 		case PTP_EVT_RS_GRAND_MASTER:
 			new_state = PTP_PS_GRAND_MASTER;
@@ -163,22 +192,27 @@ enum ptp_port_state ptp_state_machine(enum ptp_port_state state,
 			break;
 		}
 		break;
+#endif /* CONFIG_PTP_UNCALIBRATED_PRESENT */
 	case PTP_PS_SLAVE:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_SYNCHRONIZATION_FAULT:
-			new_state = PTP_PS_UNCALIBRATED;
+			new_state = IS_ENABLED(CONFIG_PTP_UNCALIBRATED_PRESENT) ?
+					PTP_PS_UNCALIBRATED : new_state;
 			break;
 		case PTP_EVT_ANNOUNCE_RECEIPT_TIMEOUT_EXPIRES:
 			new_state = PTP_PS_MASTER;
 			break;
 		case PTP_EVT_RS_MASTER:
-			new_state = PTP_PS_PRE_MASTER;
+			new_state = IS_ENABLED(CONFIG_PTP_PRE_MASTER_PRESENT) ?
+					PTP_PS_PRE_MASTER : PTP_PS_MASTER;
 			break;
 		case PTP_EVT_RS_GRAND_MASTER:
 			new_state = PTP_PS_GRAND_MASTER;
@@ -188,7 +222,8 @@ enum ptp_port_state ptp_state_machine(enum ptp_port_state state,
 			break;
 		case PTP_EVT_RS_SLAVE:
 			if (master_diff) {
-				new_state = PTP_PS_UNCALIBRATED;
+				new_state = IS_ENABLED(CONFIG_PTP_UNCALIBRATED_PRESENT) ?
+						PTP_PS_UNCALIBRATED : new_state;
 			}
 			break;
 		default:
@@ -217,19 +252,23 @@ enum ptp_port_state ptp_so_state_machine(enum ptp_port_state state,
 	case PTP_PS_INITIALIZING:
 		switch (event) {
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_INIT_COMPLETE:
-			new_state = PTP_PS_LISTENING;
+			new_state = IS_ENABLED(CONFIG_PTP_LISTENING_PRESENT) ?
+					PTP_PS_LISTENING : PTP_PS_SLAVE;
 			break;
 		default:
 			break;
 		}
 		break;
+#if CONFIG_PTP_FAULTY_PRESENT
 	case PTP_PS_FAULTY:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_CLEARED:
 			new_state = PTP_PS_INITIALIZING;
@@ -238,39 +277,51 @@ enum ptp_port_state ptp_so_state_machine(enum ptp_port_state state,
 			break;
 		}
 		break;
+#endif
+#if CONFIG_PTP_DISABLED_PRESENT
 	case PTP_PS_DISABLED:
 		if (event == PTP_EVT_DESIGNATED_ENABLED) {
 			new_state = PTP_PS_INITIALIZING;
 		}
 		break;
+#endif
+#if CONFIG_PTP_LISTENING_PRESENT
 	case PTP_PS_LISTENING:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_RS_SLAVE:
-			new_state = PTP_PS_UNCALIBRATED;
+			new_state = IS_ENABLED(CONFIG_PTP_UNCALIBRATED_PRESENT) ?
+					PTP_PS_UNCALIBRATED : PTP_PS_SLAVE;
 			break;
 		default:
 			break;
 		}
 		break;
+#endif
+#if CONFIG_PTP_UNCALIBRATED_PRESENT
 	case PTP_PS_UNCALIBRATED:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_ANNOUNCE_RECEIPT_TIMEOUT_EXPIRES:
 		case PTP_EVT_RS_MASTER:
 		case PTP_EVT_RS_GRAND_MASTER:
 		case PTP_EVT_RS_PASSIVE:
-			new_state = PTP_PS_LISTENING;
+			new_state = IS_ENABLED(CONFIG_PTP_LISTENING_PRESENT) ?
+					PTP_PS_LISTENING : new_state;
 			break;
 		case PTP_EVT_MASTER_CLOCK_SELECTED:
 			new_state = PTP_PS_SLAVE;
@@ -278,26 +329,32 @@ enum ptp_port_state ptp_so_state_machine(enum ptp_port_state state,
 			break;
 		}
 		break;
+#endif
 	case PTP_PS_SLAVE:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_SYNCHRONIZATION_FAULT:
-			new_state = PTP_PS_UNCALIBRATED;
+			new_state = IS_ENABLED(CONFIG_PTP_UNCALIBRATED_PRESENT) ?
+					PTP_PS_UNCALIBRATED : new_state;
 			break;
 		case PTP_EVT_ANNOUNCE_RECEIPT_TIMEOUT_EXPIRES:
 		case PTP_EVT_RS_MASTER:
 		case PTP_EVT_RS_GRAND_MASTER:
 		case PTP_EVT_RS_PASSIVE:
-			new_state = PTP_PS_LISTENING;
+			new_state = IS_ENABLED(CONFIG_PTP_LISTENING_PRESENT) ?
+					PTP_PS_LISTENING : new_state;
 			break;
 		case PTP_EVT_RS_SLAVE:
 			if (master_diff) {
-				new_state = PTP_PS_UNCALIBRATED;
+				new_state = IS_ENABLED(CONFIG_PTP_UNCALIBRATED_PRESENT) ?
+						PTP_PS_UNCALIBRATED : new_state;
 			}
 			break;
 		default:

phytec/zephyr-phytec

subsys/net/lib/ptp/state_machine.c
@@ -23,19 +23,23 @@ enum ptp_port_state ptp_state_machine(enum ptp_port_state state,
 	case PTP_PS_INITIALIZING:
 		switch (event) {
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_INIT_COMPLETE:
-			new_state = PTP_PS_LISTENING;
+			new_state = IS_ENABLED(CONFIG_PTP_LISTENING_PRESENT) ?
+					PTP_PS_LISTENING : PTP_PS_TIME_TRANSMITTER;
 			break;
 		default:
 			break;
 		}
 		break;
+#if CONFIG_PTP_FAULTY_PRESENT
 	case PTP_PS_FAULTY:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_CLEARED:
 			new_state = PTP_PS_INITIALIZING;
@@ -44,24 +48,31 @@ enum ptp_port_state ptp_state_machine(enum ptp_port_state state,
 			break;
 		}
 		break;
+#endif /* CONFIG_PTP_FAULTY_PRESENT */
+#if CONFIG_PTP_DISABLED_PRESENT
 	case PTP_PS_DISABLED:
 		if (event == PTP_EVT_DESIGNATED_ENABLED) {
 			new_state = PTP_PS_INITIALIZING;
 		}
 		break;
+#endif /* CONFIG_PTP_DISABLED_PRESENT */
+#if CONFIG_PTP_LISTENING_PRESENT
 	case PTP_PS_LISTENING:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_ANNOUNCE_RECEIPT_TIMEOUT_EXPIRES:
 			new_state = PTP_PS_TIME_TRANSMITTER;
 			break;
 		case PTP_EVT_RS_TIME_TRANSMITTER:
-			new_state = PTP_PS_PRE_TIME_TRANSMITTER;
+			new_state = IS_ENABLED(CONFIG_PTP_PRE_TIME_TRANSMITTER_PRESENT) ?
+					PTP_PS_PRE_TIME_TRANSMITTER : PTP_PS_TIME_TRANSMITTER;
 			break;
 		case PTP_EVT_RS_GRAND_MASTER:
 			new_state = PTP_PS_GRAND_MASTER;
@@ -70,19 +81,24 @@ enum ptp_port_state ptp_state_machine(enum ptp_port_state state,
 			new_state = PTP_PS_PASSIVE;
 			break;
 		case PTP_EVT_RS_TIME_RECEIVER:
-			new_state = PTP_PS_UNCALIBRATED;
+			new_state = IS_ENABLED(CONFIG_PTP_UNCALIBRATED_PRESENT) ?
+					PTP_PS_UNCALIBRATED : PTP_PS_TIME_RECEIVER;
 			break;
 		default:
 			break;
 		}
 		break;
+#endif /* CONFIG_PTP_LISTENING_PRESENT */
+#if CONFIG_PTP_PRE_TIME_TRANSMITTER_PRESENT
 	case PTP_PS_PRE_TIME_TRANSMITTER:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_QUALIFICATION_TIMEOUT_EXPIRES:
 			new_state = PTP_PS_TIME_TRANSMITTER;
@@ -91,25 +107,30 @@ enum ptp_port_state ptp_state_machine(enum ptp_port_state state,
 			new_state = PTP_PS_PASSIVE;
 			break;
 		case PTP_EVT_RS_TIME_RECEIVER:
-			new_state = PTP_PS_UNCALIBRATED;
+			new_state = IS_ENABLED(CONFIG_PTP_UNCALIBRATED_PRESENT) ?
+					PTP_PS_UNCALIBRATED : PTP_PS_TIME_RECEIVER;
 			break;
 		default:
 			break;
 		}
 		break;
+#endif /* CONFIG_PTP_PRE_TIME_TRANSMITTER_PRESENT */
 	case PTP_PS_TIME_TRANSMITTER:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_RS_PASSIVE:
 			new_state = PTP_PS_PASSIVE;
 			break;
 		case PTP_EVT_RS_TIME_RECEIVER:
-			new_state = PTP_PS_UNCALIBRATED;
+			new_state = IS_ENABLED(CONFIG_PTP_UNCALIBRATED_PRESENT) ?
+					PTP_PS_UNCALIBRATED : PTP_PS_TIME_RECEIVER;
 			break;
 		default:
 			break;
@@ -118,31 +139,38 @@ enum ptp_port_state ptp_state_machine(enum ptp_port_state state,
 	case PTP_PS_PASSIVE:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_RS_TIME_TRANSMITTER:
-			new_state = PTP_PS_PRE_TIME_TRANSMITTER;
+			new_state = IS_ENABLED(CONFIG_PTP_PRE_TIME_TRANSMITTER_PRESENT) ?
+					PTP_PS_PRE_TIME_TRANSMITTER : PTP_PS_TIME_TRANSMITTER;
 			break;
 		case PTP_EVT_RS_GRAND_MASTER:
 			new_state = PTP_PS_GRAND_MASTER;
 			break;
 		case PTP_EVT_RS_TIME_RECEIVER:
-			new_state = PTP_PS_UNCALIBRATED;
+			new_state = IS_ENABLED(CONFIG_PTP_UNCALIBRATED_PRESENT) ?
+					PTP_PS_UNCALIBRATED : PTP_PS_TIME_RECEIVER;
 			break;
 		default:
 			break;
 		}
 		break;
+#if CONFIG_PTP_UNCALIBRATED_PRESENT
 	case PTP_PS_UNCALIBRATED:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_ANNOUNCE_RECEIPT_TIMEOUT_EXPIRES:
 			new_state = PTP_PS_TIME_TRANSMITTER;
@@ -151,7 +179,8 @@ enum ptp_port_state ptp_state_machine(enum ptp_port_state state,
 			new_state = PTP_PS_TIME_RECEIVER;
 			break;
 		case PTP_EVT_RS_TIME_TRANSMITTER:
-			new_state = PTP_PS_PRE_TIME_TRANSMITTER;
+			new_state = IS_ENABLED(CONFIG_PTP_PRE_TIME_TRANSMITTER_PRESENT) ?
+					PTP_PS_PRE_TIME_TRANSMITTER : PTP_PS_TIME_TRANSMITTER;
 			break;
 		case PTP_EVT_RS_GRAND_MASTER:
 			new_state = PTP_PS_GRAND_MASTER;
@@ -163,22 +192,27 @@ enum ptp_port_state ptp_state_machine(enum ptp_port_state state,
 			break;
 		}
 		break;
+#endif /* CONFIG_PTP_UNCALIBRATED_PRESENT */
 	case PTP_PS_TIME_RECEIVER:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_SYNCHRONIZATION_FAULT:
-			new_state = PTP_PS_UNCALIBRATED;
+			new_state = IS_ENABLED(CONFIG_PTP_UNCALIBRATED_PRESENT) ?
+					PTP_PS_UNCALIBRATED : new_state;
 			break;
 		case PTP_EVT_ANNOUNCE_RECEIPT_TIMEOUT_EXPIRES:
 			new_state = PTP_PS_TIME_TRANSMITTER;
 			break;
 		case PTP_EVT_RS_TIME_TRANSMITTER:
-			new_state = PTP_PS_PRE_TIME_TRANSMITTER;
+			new_state = IS_ENABLED(CONFIG_PTP_PRE_TIME_TRANSMITTER_PRESENT) ?
+					PTP_PS_PRE_TIME_TRANSMITTER : PTP_PS_TIME_TRANSMITTER;
 			break;
 		case PTP_EVT_RS_GRAND_MASTER:
 			new_state = PTP_PS_GRAND_MASTER;
@@ -188,7 +222,8 @@ enum ptp_port_state ptp_state_machine(enum ptp_port_state state,
 			break;
 		case PTP_EVT_RS_TIME_RECEIVER:
 			if (tt_diff) {
-				new_state = PTP_PS_UNCALIBRATED;
+				new_state = IS_ENABLED(CONFIG_PTP_UNCALIBRATED_PRESENT) ?
+						PTP_PS_UNCALIBRATED : new_state;
 			}
 			break;
 		default:
@@ -217,19 +252,23 @@ enum ptp_port_state ptp_tr_state_machine(enum ptp_port_state state,
 	case PTP_PS_INITIALIZING:
 		switch (event) {
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_INIT_COMPLETE:
-			new_state = PTP_PS_LISTENING;
+			new_state = IS_ENABLED(CONFIG_PTP_LISTENING_PRESENT) ?
+					PTP_PS_LISTENING : PTP_PS_TIME_RECEIVER;
 			break;
 		default:
 			break;
 		}
 		break;
+#if CONFIG_PTP_FAULTY_PRESENT
 	case PTP_PS_FAULTY:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_CLEARED:
 			new_state = PTP_PS_INITIALIZING;
@@ -238,33 +277,44 @@ enum ptp_port_state ptp_tr_state_machine(enum ptp_port_state state,
 			break;
 		}
 		break;
+#endif
+#if CONFIG_PTP_DISABLED_PRESENT
 	case PTP_PS_DISABLED:
 		if (event == PTP_EVT_DESIGNATED_ENABLED) {
 			new_state = PTP_PS_INITIALIZING;
 		}
 		break;
+#endif
+#if CONFIG_PTP_LISTENING_PRESENT
 	case PTP_PS_LISTENING:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_RS_TIME_RECEIVER:
-			new_state = PTP_PS_UNCALIBRATED;
+			new_state = IS_ENABLED(CONFIG_PTP_UNCALIBRATED_PRESENT) ?
+					PTP_PS_UNCALIBRATED : PTP_PS_TIME_RECEIVER;
 			break;
 		default:
 			break;
 		}
 		break;
+#endif
+#if CONFIG_PTP_UNCALIBRATED_PRESENT
 	case PTP_PS_UNCALIBRATED:
 		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
-			new_state = PTP_PS_DISABLED;
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
 			break;
 		case PTP_EVT_FAULT_DETECTED:
-			new_state = PTP_PS_FAULTY;
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_ANNOUNCE_RECEIPT_TIMEOUT_EXPIRES:
 			__fallthrough;
@@ -273,7 +323,8 @@ enum ptp_port_state ptp_tr_state_machine(enum ptp_port_state state,
 		case PTP_EVT_RS_GRAND_MASTER:
 			__fallthrough;
 		case PTP_EVT_RS_PASSIVE:
-			new_state = PTP_PS_LISTENING;
+			new_state = IS_ENABLED(CONFIG_PTP_LISTENING_PRESENT) ?
+					PTP_PS_LISTENING : new_state;
 			break;
 		case PTP_EVT_TIME_TRANSMITTER_CLOCK_SELECTED:
 			new_state = PTP_PS_TIME_RECEIVER;
@@ -281,11 +332,20 @@ enum ptp_port_state ptp_tr_state_machine(enum ptp_port_state state,
 			break;
 		}
 		break;
+#endif
 	case PTP_PS_TIME_RECEIVER:
+		switch (event) {
 		case PTP_EVT_DESIGNATED_DISABLED:
+			new_state = IS_ENABLED(CONFIG_PTP_DISABLED_PRESENT) ?
+					PTP_PS_DISABLED : new_state;
+			break;
+		case PTP_EVT_FAULT_DETECTED:
+			new_state = IS_ENABLED(CONFIG_PTP_FAULTY_PRESENT) ?
+					PTP_PS_FAULTY : new_state;
 			break;
 		case PTP_EVT_SYNCHRONIZATION_FAULT:
-			new_state = PTP_PS_UNCALIBRATED;
+			new_state = IS_ENABLED(CONFIG_PTP_UNCALIBRATED_PRESENT) ?
+					PTP_PS_UNCALIBRATED : new_state;
 			break;
 		case PTP_EVT_ANNOUNCE_RECEIPT_TIMEOUT_EXPIRES:
 			__fallthrough;
@@ -294,11 +354,13 @@ enum ptp_port_state ptp_tr_state_machine(enum ptp_port_state state,
 		case PTP_EVT_RS_GRAND_MASTER:
 			__fallthrough;
 		case PTP_EVT_RS_PASSIVE:
-			new_state = PTP_PS_LISTENING;
+			new_state = IS_ENABLED(CONFIG_PTP_LISTENING_PRESENT) ?
+					PTP_PS_LISTENING : new_state;
 			break;
 		case PTP_EVT_RS_TIME_RECEIVER:
 			if (tt_diff) {
-				new_state = PTP_PS_UNCALIBRATED;
+				new_state = IS_ENABLED(CONFIG_PTP_UNCALIBRATED_PRESENT) ?
+						PTP_PS_UNCALIBRATED : new_state;
 			}
 			break;
 		default: