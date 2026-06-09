vected/zephyr

drivers: fpga_ice40: rework spinlock usage
The driver uses spinlocks for synchronization (due to strict timing
requirements) which makes it defacto incompatible with almost all SPI
backend drivers, as most of them use `k_sem_take` internally.

This change reworks the driver in the following way to solve this
problem:

- Replace driver locks with `k_mutex`
- Run the `load` functions on the system workqueue to execute them on
  a cooperative priority
- Use additional local spinlock in the `bitbang` driver variant to ensure
  precise timing

Signed-off-by: Martin Stumpf <finomnis@gmail.com>

drivers/fpga/fpga_ice40_bitbang.c
@@ -115,27 +115,36 @@ static void fpga_ice40_spi_send_data(size_t delay, volatile gpio_port_pins_t *se
  *
  * https://www.latticesemi.com/~/media/LatticeSemi/Documents/Handbooks/iCE40FamilyHandbook.pdf
  */
-static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32_t img_size)
+static void fpga_ice40_load_handler(struct k_work *item)
 {
+	struct fpga_ice40_work_item *work_item =
+		CONTAINER_OF(item, struct fpga_ice40_work_item, work);
+	const struct device *dev = work_item->dev;
+	const uint32_t *image_ptr = work_item->image_ptr;
+	const uint32_t image_size = work_item->image_size;
+
 	int ret;
 	uint32_t crc;
 	gpio_port_pins_t cs;
 	gpio_port_pins_t clk;
-	k_spinlock_key_t key;
 	gpio_port_pins_t pico;
 	gpio_port_pins_t creset;
+	struct k_spinlock spinlock;
+	k_spinlock_key_t spinlock_key;
 	struct fpga_ice40_data *data = dev->data;
 	const struct fpga_ice40_config *config = dev->config;
 	const struct fpga_ice40_config_bitbang *config_bitbang = config->derived_config;
 
 	if (!device_is_ready(config_bitbang->clk.port)) {
 		LOG_ERR("%s: GPIO for clk is not ready", dev->name);
-		return -ENODEV;
+		ret = -ENODEV;
+		goto exit;
 	}
 
 	if (!device_is_ready(config_bitbang->pico.port)) {
 		LOG_ERR("%s: GPIO for pico is not ready", dev->name);
-		return -ENODEV;
+		ret = -ENODEV;
+		goto exit;
 	}
 
 	/* prepare masks */
@@ -145,12 +154,13 @@ static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32
 	creset = BIT(config->creset.pin);
 
 	/* crc check */
-	crc = crc32_ieee((uint8_t *)image_ptr, img_size);
+	crc = crc32_ieee((uint8_t *)image_ptr, image_size);
 	if (data->loaded && crc == data->crc) {
 		LOG_WRN("already loaded with image CRC32c: 0x%08x", data->crc);
 	}
 
-	key = k_spin_lock(&data->lock);
+	/* spinlock to prevent interruptions; this entire function is very timing sensitive */
+	spinlock_key = k_spin_lock(&spinlock);
 
 	/* clear crc */
 	data->crc = 0;
@@ -204,7 +214,7 @@ static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32
 	LOG_DBG("Set SPI_CS high");
 	fpga_ice40_spi_send_data(config_bitbang->mhz_delay_count, config_bitbang->set,
 				 config_bitbang->clear, cs, clk, pico, (uint8_t *)image_ptr,
-				 img_size);
+				 image_size);
 
 	LOG_DBG("Send %u clocks", config->trailing_clocks);
 	fpga_ice40_send_clocks(config_bitbang->mhz_delay_count, config_bitbang->set,
@@ -234,9 +244,13 @@ static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32
 	}
 
 unlock:
-	k_spin_unlock(&data->lock, key);
+	k_spin_unlock(&spinlock, spinlock_key);
 
-	return ret;
+exit:
+	work_item->result = ret;
+	k_sem_give(&work_item->finished);
+
+	return;
 }
 
 static DEVICE_API(fpga, fpga_ice40_api) = {
@@ -261,7 +275,8 @@ static DEVICE_API(fpga, fpga_ice40_api) = {
 		.mhz_delay_count = DT_INST_PROP(inst, mhz_delay_count),                            \
 	};                                                                                         \
                                                                                                    \
-	FPGA_ICE40_CONFIG_DEFINE(inst, &fpga_ice40_config_bitbang_##inst);                         \
+	FPGA_ICE40_CONFIG_DEFINE(inst, fpga_ice40_load_handler,                                    \
+				 &fpga_ice40_config_bitbang_##inst);                               \
                                                                                                    \
 	DEVICE_DT_INST_DEFINE(inst, fpga_ice40_init, NULL, &fpga_ice40_data_##inst,                \
 			      &fpga_ice40_config_##inst, POST_KERNEL, CONFIG_FPGA_INIT_PRIORITY,   \

drivers/fpga/fpga_ice40_spi.c
@@ -17,11 +17,16 @@
 
 LOG_MODULE_DECLARE(fpga_ice40, CONFIG_FPGA_LOG_LEVEL);
 
-static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32_t img_size)
+static void fpga_ice40_load_handler(struct k_work *item)
 {
+	struct fpga_ice40_work_item *work_item =
+		CONTAINER_OF(item, struct fpga_ice40_work_item, work);
+	const struct device *dev = work_item->dev;
+	uint32_t *image_ptr = work_item->image_ptr;
+	const uint32_t image_size = work_item->image_size;
+
 	int ret;
 	uint32_t crc;
-	k_spinlock_key_t key;
 	struct spi_buf tx_buf;
 	const struct spi_buf_set tx_bufs = {
 		.buffers = &tx_buf,
@@ -31,8 +36,6 @@ static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32
 	uint8_t clock_buf[(UINT8_MAX + 1) / BITS_PER_BYTE];
 	const struct fpga_ice40_config *config = dev->config;
 	struct spi_dt_spec bus;
-
-	key = k_spin_lock(&data->lock);
 
 	/*
 	 * Disable the automatism for chip select within the SPI driver,
@@ -52,7 +55,7 @@ static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32
 	}
 
 	/* crc check */
-	crc = crc32_ieee((uint8_t *)image_ptr, img_size);
+	crc = crc32_ieee((uint8_t *)image_ptr, image_size);
 	if (data->loaded && crc == data->crc) {
 		LOG_WRN("already loaded with image CRC32c: 0x%08x", data->crc);
 	}
@@ -72,31 +75,31 @@ static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32
 	ret = gpio_pin_configure_dt(&config->creset, GPIO_OUTPUT_LOW);
 	if (ret < 0) {
 		LOG_ERR("failed to set CRESET low: %d", ret);
-		goto unlock;
+		goto cleanup;
 	}
 
 	LOG_DBG("Set SPI_CS low");
 	ret = gpio_pin_configure_dt(&config->bus.config.cs.gpio, GPIO_OUTPUT_LOW);
 	if (ret < 0) {
 		LOG_ERR("failed to set SPI_CS low: %d", ret);
-		goto unlock;
+		goto cleanup;
 	}
 
 	/* Wait a minimum of 200ns */
 	LOG_DBG("Delay %u us", config->creset_delay_us);
-	k_usleep(config->creset_delay_us);
+	k_busy_wait(config->creset_delay_us);
 
 	if (gpio_pin_get_dt(&config->cdone) != 0) {
 		LOG_ERR("CDONE should be low after the reset");
 		ret = -EIO;
-		goto unlock;
+		goto cleanup;
 	}
 
 	LOG_DBG("Set CRESET high");
 	ret = gpio_pin_configure_dt(&config->creset, GPIO_OUTPUT_HIGH);
 	if (ret < 0) {
 		LOG_ERR("failed to set CRESET high: %d", ret);
-		goto unlock;
+		goto cleanup;
 	}
 
 	LOG_DBG("Delay %u us", config->config_delay_us);
@@ -106,7 +109,7 @@ static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32
 	ret = gpio_pin_configure_dt(&config->bus.config.cs.gpio, GPIO_OUTPUT_HIGH);
 	if (ret < 0) {
 		LOG_ERR("failed to set SPI_CS high: %d", ret);
-		goto unlock;
+		goto cleanup;
 	}
 
 	LOG_DBG("Send %u clocks", config->leading_clocks);
@@ -115,30 +118,30 @@ static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32
 	ret = spi_write_dt(&bus, &tx_bufs);
 	if (ret < 0) {
 		LOG_ERR("Failed to send leading %u clocks: %d", config->leading_clocks, ret);
-		goto unlock;
+		goto cleanup;
 	}
 
 	LOG_DBG("Set SPI_CS low");
 	ret = gpio_pin_configure_dt(&config->bus.config.cs.gpio, GPIO_OUTPUT_LOW);
 	if (ret < 0) {
 		LOG_ERR("failed to set SPI_CS low: %d", ret);
-		goto unlock;
+		goto cleanup;
 	}
 
 	LOG_DBG("Send bin file");
 	tx_buf.buf = image_ptr;
-	tx_buf.len = img_size;
+	tx_buf.len = image_size;
 	ret = spi_write_dt(&bus, &tx_bufs);
 	if (ret < 0) {
 		LOG_ERR("Failed to send bin file: %d", ret);
-		goto unlock;
+		goto cleanup;
 	}
 
 	LOG_DBG("Set SPI_CS high");
 	ret = gpio_pin_configure_dt(&config->bus.config.cs.gpio, GPIO_OUTPUT_HIGH);
 	if (ret < 0) {
 		LOG_ERR("failed to set SPI_CS high: %d", ret);
-		goto unlock;
+		goto cleanup;
 	}
 
 	LOG_DBG("Send %u clocks", config->trailing_clocks);
@@ -147,18 +150,18 @@ static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32
 	ret = spi_write_dt(&bus, &tx_bufs);
 	if (ret < 0) {
 		LOG_ERR("Failed to send trailing %u clocks: %d", config->trailing_clocks, ret);
-		goto unlock;
+		goto cleanup;
 	}
 
 	LOG_DBG("checking CDONE");
 	ret = gpio_pin_get_dt(&config->cdone);
 	if (ret < 0) {
 		LOG_ERR("failed to read CDONE: %d", ret);
-		goto unlock;
+		goto cleanup;
 	} else if (ret != 1) {
 		ret = -EIO;
 		LOG_ERR("CDONE did not go high");
-		goto unlock;
+		goto cleanup;
 	}
 
 	ret = 0;
@@ -166,13 +169,14 @@ static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32
 	fpga_ice40_crc_to_str(crc, data->info);
 	LOG_INF("Loaded image with CRC32 0x%08x", crc);
 
-unlock:
+cleanup:
 	(void)gpio_pin_configure_dt(&config->creset, GPIO_OUTPUT_HIGH);
 	(void)gpio_pin_configure_dt(&config->bus.config.cs.gpio, GPIO_OUTPUT_HIGH);
 
-	k_spin_unlock(&data->lock, key);
+	work_item->result = ret;
+	k_sem_give(&work_item->finished);
 
-	return ret;
+	return;
 }
 
 static DEVICE_API(fpga, fpga_ice40_api) = {
@@ -187,7 +191,7 @@ static DEVICE_API(fpga, fpga_ice40_api) = {
 #define FPGA_ICE40_DEFINE(inst)                                                                    \
 	static struct fpga_ice40_data fpga_ice40_data_##inst;                                      \
                                                                                                    \
-	FPGA_ICE40_CONFIG_DEFINE(inst, NULL);                                                      \
+	FPGA_ICE40_CONFIG_DEFINE(inst, fpga_ice40_load_handler, NULL);                             \
                                                                                                    \
 	DEVICE_DT_INST_DEFINE(inst, fpga_ice40_init, NULL, &fpga_ice40_data_##inst,                \
 			      &fpga_ice40_config_##inst, POST_KERNEL, CONFIG_FPGA_INIT_PRIORITY,   \

phytec/zephyr-phytec

drivers/fpga/fpga_ice40_bitbang.c
@@ -115,13 +115,20 @@ static void fpga_ice40_spi_send_data(size_t delay, volatile gpio_port_pins_t *se
  *
  * https://www.latticesemi.com/~/media/LatticeSemi/Documents/Handbooks/iCE40FamilyHandbook.pdf
  */
-static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32_t img_size)
+static void fpga_ice40_load_handler(struct k_work *item)
 {
+	struct fpga_ice40_work_item *work_item =
+		CONTAINER_OF(item, struct fpga_ice40_work_item, work);
+	const struct device *dev = work_item->dev;
+	uint32_t *image_ptr = work_item->image_ptr;
+	const uint32_t img_size = work_item->image_size;
+
 	int ret;
 	uint32_t crc;
 	gpio_port_pins_t cs;
 	gpio_port_pins_t clk;
 	k_spinlock_key_t key;
+	struct k_spinlock spinlock;
 	gpio_port_pins_t pico;
 	gpio_port_pins_t creset;
 	struct fpga_ice40_data *data = dev->data;
@@ -130,12 +137,14 @@ static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32
 
 	if (!device_is_ready(config_bitbang->clk.port)) {
 		LOG_ERR("%s: GPIO for clk is not ready", dev->name);
-		return -ENODEV;
+		ret = -ENODEV;
+		goto exit;
 	}
 
 	if (!device_is_ready(config_bitbang->pico.port)) {
 		LOG_ERR("%s: GPIO for pico is not ready", dev->name);
-		return -ENODEV;
+		ret = -ENODEV;
+		goto exit;
 	}
 
 	/* prepare masks */
@@ -150,7 +159,8 @@ static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32
 		LOG_WRN("already loaded with image CRC32c: 0x%08x", data->crc);
 	}
 
-	key = k_spin_lock(&data->lock);
+	/* spinlock to prevent interruptions; this entire function is very timing sensitive */
+	key = k_spin_lock(&spinlock);
 
 	/* clear crc */
 	data->crc = 0;
@@ -234,9 +244,13 @@ static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32
 	}
 
 unlock:
-	k_spin_unlock(&data->lock, key);
+	k_spin_unlock(&spinlock, key);
 
-	return ret;
+exit:
+	work_item->result = ret;
+	k_sem_give(&work_item->finished);
+
+	return;
 }
 
 static DEVICE_API(fpga, fpga_ice40_api) = {
@@ -261,7 +275,8 @@ static DEVICE_API(fpga, fpga_ice40_api) = {
 		.mhz_delay_count = DT_INST_PROP(inst, mhz_delay_count),                            \
 	};                                                                                         \
                                                                                                    \
-	FPGA_ICE40_CONFIG_DEFINE(inst, &fpga_ice40_config_bitbang_##inst);                         \
+	FPGA_ICE40_CONFIG_DEFINE(inst, fpga_ice40_load_handler,                                    \
+				 &fpga_ice40_config_bitbang_##inst);                               \
                                                                                                    \
 	DEVICE_DT_INST_DEFINE(inst, fpga_ice40_init, NULL, &fpga_ice40_data_##inst,                \
 			      &fpga_ice40_config_##inst, POST_KERNEL, CONFIG_FPGA_INIT_PRIORITY,   \

drivers/fpga/fpga_ice40_spi.c
@@ -17,11 +17,16 @@
 
 LOG_MODULE_DECLARE(fpga_ice40, CONFIG_FPGA_LOG_LEVEL);
 
-static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32_t img_size)
+static void fpga_ice40_load_handler(struct k_work *item)
 {
+	struct fpga_ice40_work_item *work_item =
+		CONTAINER_OF(item, struct fpga_ice40_work_item, work);
+	const struct device *dev = work_item->dev;
+	uint32_t *image_ptr = work_item->image_ptr;
+	const uint32_t img_size = work_item->image_size;
+
 	int ret;
 	uint32_t crc;
-	k_spinlock_key_t key;
 	struct spi_buf tx_buf;
 	const struct spi_buf_set tx_bufs = {
 		.buffers = &tx_buf,
@@ -31,8 +36,6 @@ static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32
 	uint8_t clock_buf[(UINT8_MAX + 1) / BITS_PER_BYTE];
 	const struct fpga_ice40_config *config = dev->config;
 	struct spi_dt_spec bus;
-
-	key = k_spin_lock(&data->lock);
 
 	/*
 	 * Disable the automatism for chip select within the SPI driver,
@@ -84,7 +87,7 @@ static int fpga_ice40_load(const struct device *dev, uint32_t *image_ptr, uint32
 
 	/* Wait a minimum of 200ns */
 	LOG_DBG("Delay %u us", config->creset_delay_us);
-	k_usleep(config->creset_delay_us);
+	k_busy_wait(config->creset_delay_us);
 
 	if (gpio_pin_get_dt(&config->cdone) != 0) {
 		LOG_ERR("CDONE should be low after the reset");
@@ -170,9 +173,10 @@ unlock:
 	(void)gpio_pin_configure_dt(&config->creset, GPIO_OUTPUT_HIGH);
 	(void)gpio_pin_configure_dt(&config->bus.config.cs.gpio, GPIO_OUTPUT_HIGH);
 
-	k_spin_unlock(&data->lock, key);
+	work_item->result = ret;
+	k_sem_give(&work_item->finished);
 
-	return ret;
+	return;
 }
 
 static DEVICE_API(fpga, fpga_ice40_api) = {
@@ -187,7 +191,7 @@ static DEVICE_API(fpga, fpga_ice40_api) = {
 #define FPGA_ICE40_DEFINE(inst)                                                                    \
 	static struct fpga_ice40_data fpga_ice40_data_##inst;                                      \
                                                                                                    \
-	FPGA_ICE40_CONFIG_DEFINE(inst, NULL);                                                      \
+	FPGA_ICE40_CONFIG_DEFINE(inst, fpga_ice40_load_handler, NULL);                             \
                                                                                                    \
 	DEVICE_DT_INST_DEFINE(inst, fpga_ice40_init, NULL, &fpga_ice40_data_##inst,                \
 			      &fpga_ice40_config_##inst, POST_KERNEL, CONFIG_FPGA_INIT_PRIORITY,   \