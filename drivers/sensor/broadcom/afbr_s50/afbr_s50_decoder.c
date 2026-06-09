zephyrproject-rtos/zephyr

sensor: afbr_s50: Add private channel to obtain pixel matrix
Each time this sensor gets a reading, it contains a matrix of 4 x 32
pixels containing distance readings, from which the 1-D result is
calculated. The private channel would expose this array through
Sensor APIs.

Signed-off-by: Luis Ubieda <luisf@croxel.com>

drivers/sensor/broadcom/afbr_s50/afbr_s50_decoder.c
@@ -9,6 +9,7 @@
 
 #include <zephyr/drivers/sensor_clock.h>
 #include <api/argus_res.h>
+#include <zephyr/drivers/sensor/afbr_s50.h>
 
 #include "afbr_s50_decoder.h"
 
@@ -20,6 +21,8 @@ uint8_t afbr_s50_encode_channel(uint16_t chan)
 	switch (chan) {
 	case SENSOR_CHAN_DISTANCE:
 		return BIT(0);
+	case SENSOR_CHAN_AFBR_S50_PIXELS:
+		return BIT(1);
 	default:
 		return 0;
 	}
@@ -47,7 +50,8 @@ static int afbr_s50_decoder_get_frame_count(const uint8_t *buffer,
 
 	switch (chan_spec.chan_type) {
 	case SENSOR_CHAN_DISTANCE:
-		if (edata->header.channels & afbr_s50_encode_channel(SENSOR_CHAN_DISTANCE)) {
+	case SENSOR_CHAN_AFBR_S50_PIXELS:
+		if (edata->header.channels & afbr_s50_encode_channel(chan_spec.chan_type)) {
 			*frame_count = 1;
 			return 0;
 		}
@@ -68,6 +72,11 @@ static int afbr_s50_decoder_get_size_info(struct sensor_chan_spec chan_spec,
 	case SENSOR_CHAN_DISTANCE:
 		*base_size = sizeof(struct sensor_q31_data);
 		*frame_size = sizeof(struct sensor_q31_sample_data);
+		return 0;
+	case SENSOR_CHAN_AFBR_S50_PIXELS:
+		*base_size = sizeof(struct sensor_q31_data) +
+			     31 * sizeof(struct sensor_q31_sample_data);
+		*frame_size = 32 * sizeof(struct sensor_q31_sample_data);
 		return 0;
 	default:
 		return -ENOTSUP;
@@ -104,6 +113,36 @@ static int afbr_s50_decoder_decode(const uint8_t *buffer,
 		/* Result comes encoded in Q9.22 format */
 		out->shift = 9;
 		out->readings[0].value = edata->payload.Bin.Range;
+
+		*fit = 1;
+		return 1;
+	}
+	case SENSOR_CHAN_AFBR_S50_PIXELS: {
+		struct sensor_q31_data *out = data_out;
+
+		if ((edata->header.channels &
+		     afbr_s50_encode_channel(SENSOR_CHAN_AFBR_S50_PIXELS)) == 0) {
+			return -ENODATA;
+		}
+
+		out->header.base_timestamp_ns = edata->header.timestamp;
+		out->header.reading_count = 32;
+		/* Result comes encoded in Q9.22 format */
+		out->shift = 9;
+
+		for (size_t i = 0 ; i < 32 ; i++) {
+			if (edata->payload.Pixels[i].Amplitude == 0xFFFF ||
+			    edata->payload.Pixels[i].Status != PIXEL_OK) {
+				LOG_DBG("Invalid pixel: %d, Amplitude: %d, Status: %d", i,
+					edata->payload.Pixels[i].Amplitude,
+					edata->payload.Pixels[i].Status);
+
+				out->readings[i].value = AFBR_PIXEL_INVALID_VALUE;
+			} else {
+				out->readings[i].value = edata->payload.Pixels[i].Range;
+			}
+			out->readings[i].timestamp_delta = 0;
+		}
 
 		*fit = 1;
 		return 1;

phytec/zephyr-phytec

drivers/sensor/broadcom/afbr_s50/afbr_s50_decoder.c
@@ -9,6 +9,7 @@
 
 #include <zephyr/drivers/sensor_clock.h>
 #include <api/argus_res.h>
+#include <zephyr/drivers/sensor/afbr_s50.h>
 
 #include "afbr_s50_decoder.h"
 
@@ -20,6 +21,8 @@ uint8_t afbr_s50_encode_channel(uint16_t chan)
 	switch (chan) {
 	case SENSOR_CHAN_DISTANCE:
 		return BIT(0);
+	case SENSOR_CHAN_AFBR_S50_PIXELS:
+		return BIT(1);
 	default:
 		return 0;
 	}
@@ -46,7 +49,8 @@ static int afbr_s50_decoder_get_frame_count(const uint8_t *buffer,
 
 	switch (chan_spec.chan_type) {
 	case SENSOR_CHAN_DISTANCE:
-		if (edata->header.channels & afbr_s50_encode_channel(SENSOR_CHAN_DISTANCE)) {
+	case SENSOR_CHAN_AFBR_S50_PIXELS:
+		if (edata->header.channels & afbr_s50_encode_channel(chan_spec.chan_type)) {
 			*frame_count = 1;
 			return 0;
 		}
@@ -67,6 +71,11 @@ static int afbr_s50_decoder_get_size_info(struct sensor_chan_spec chan_spec,
 	case SENSOR_CHAN_DISTANCE:
 		*base_size = sizeof(struct sensor_q31_data);
 		*frame_size = sizeof(struct sensor_q31_sample_data);
+		return 0;
+	case SENSOR_CHAN_AFBR_S50_PIXELS:
+		*base_size = sizeof(struct sensor_q31_data) +
+			     31 * sizeof(struct sensor_q31_sample_data);
+		*frame_size = 32 * sizeof(struct sensor_q31_sample_data);
 		return 0;
 	default:
 		return -ENOTSUP;
@@ -103,6 +112,36 @@ static int afbr_s50_decoder_decode(const uint8_t *buffer,
 		/* Result comes encoded in Q9.22 format */
 		out->shift = 9;
 		out->readings[0].value = edata->payload.Bin.Range;
+
+		*fit = 1;
+		return 1;
+	}
+	case SENSOR_CHAN_AFBR_S50_PIXELS: {
+		struct sensor_q31_data *out = data_out;
+
+		if ((edata->header.channels &
+		     afbr_s50_encode_channel(SENSOR_CHAN_AFBR_S50_PIXELS)) == 0) {
+			return -ENODATA;
+		}
+
+		out->header.base_timestamp_ns = edata->header.timestamp;
+		out->header.reading_count = 32;
+		/* Result comes encoded in Q9.22 format */
+		out->shift = 9;
+
+		for (size_t i = 0 ; i < 32 ; i++) {
+			if (edata->payload.Pixels[i].Amplitude == 0xFFFF ||
+			    edata->payload.Pixels[i].Status != PIXEL_OK) {
+				LOG_DBG("Invalid pixel: %zu, Amplitude: %u, Status: %u", i,
+					edata->payload.Pixels[i].Amplitude,
+					edata->payload.Pixels[i].Status);
+
+				out->readings[i].value = AFBR_PIXEL_INVALID_VALUE;
+			} else {
+				out->readings[i].value = edata->payload.Pixels[i].Range;
+			}
+			out->readings[i].timestamp_delta = 0;
+		}
 
 		*fit = 1;
 		return 1;