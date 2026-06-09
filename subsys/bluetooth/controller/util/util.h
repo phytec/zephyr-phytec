zephyrproject-rtos/zephyr

Bluetooth: controller: split: Rework with defines for adv set states
Review rework, added comments, TODOs, FIXMEs and converted
magic number use in advertising set state flags to defines.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/util/util.h
@@ -17,4 +17,4 @@
 
 u8_t util_ones_count_get(u8_t *octets, u8_t octets_len);
 int util_rand(void *buf, size_t len);
-int util_aa_to_le32(u8_t *dst);
+int util_aa_le32(u8_t *dst);

phytec/zephyr-phytec

subsys/bluetooth/controller/util/util.h
@@ -17,4 +17,4 @@
 
 uint8_t util_ones_count_get(uint8_t *octets, uint8_t octets_len);
 int util_rand(void *buf, size_t len);
-int util_aa_to_le32(uint8_t *dst);
+int util_aa_le32(uint8_t *dst);