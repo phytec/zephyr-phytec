zephyrproject-rtos/zephyr

arch: riscv: custom: add OpenISA RI5CY and Zero-RISCY CSR support
Update OpenISA RI5CY and Zero-RISCY CSR handling to use RISC-V custom
CSR common code. Move these stuff to 'arch/riscv/custom/openisa':

1. Rename 'soc_ri5cy.h' to 'ri5cy_csr.h' for CSR definitions.
2. Rename 'soc_zero_riscy.h' to 'zero_riscy_csr.h' for CSR definitions.
3. Move CSR context to common macro '__custom_csr_save/restore_context'.
4. Move compiler option '-march=rv32imcxpulpv2' to common code.

Signed-off-by: Jimmy Zheng <jimmyzhe@andestech.com>

soc/openisa/rv32m1/soc.h
@@ -96,10 +96,10 @@ static inline uint32_t rv32m1_intmux_line(unsigned int irq)
 
 #endif	/* !_ASMLANGUAGE */
 
-#if defined(CONFIG_SOC_OPENISA_RV32M1_RI5CY)
-#include "soc_ri5cy.h"
-#elif defined(CONFIG_SOC_OPENISA_RV32M1_ZERO_RISCY)
-#include "soc_zero_riscy.h"
+#if defined(CONFIG_RISCV_CUSTOM_CSR_RI5CY)
+#include <ri5cy_csr.h>
+#elif defined(CONFIG_RISCV_CUSTOM_CSR_ZERO_RISCY)
+#include <zero_riscy_csr.h>
 #endif
 
 /* helper macro to convert from a DT_INST to HAL clock_ip_name */

phytec/zephyr-phytec

soc/openisa/rv32m1/soc.h
@@ -96,10 +96,10 @@ static inline uint32_t rv32m1_intmux_line(unsigned int irq)
 
 #endif	/* !_ASMLANGUAGE */
 
-#if defined(CONFIG_SOC_OPENISA_RV32M1_RI5CY)
-#include "soc_ri5cy.h"
-#elif defined(CONFIG_SOC_OPENISA_RV32M1_ZERO_RISCY)
-#include "soc_zero_riscy.h"
+#if defined(CONFIG_DT_HAS_OPENISA_RI5CY_ENABLED)
+#include <ri5cy_csr.h>
+#elif defined(CONFIG_DT_HAS_OPENISA_ZERO_RI5CY_ENABLED)
+#include <zero_riscy_csr.h>
 #endif
 
 /* helper macro to convert from a DT_INST to HAL clock_ip_name */