zephyrproject-rtos/zephyr

drivers: clock_control: clock_control_mcux_syscon: add LCDIC clock
Add LCDIC clock support to MCUX SYSCON clock driver

Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

include/zephyr/dt-bindings/clock/mcux_lpc_syscon_clock.h
@@ -74,4 +74,6 @@
 #define MCUX_PORT4_CLK			MCUX_LPC_CLK_ID(0x0C, 0x04)
 #define MCUX_PORT5_CLK			MCUX_LPC_CLK_ID(0x0C, 0x05)
 
+#define MCUX_LCDIC_CLK			MCUX_LPC_CLK_ID(0x0D, 0x00)
+
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_MCUX_LPC_SYSCON_H_ */

phytec/zephyr-phytec

include/zephyr/dt-bindings/clock/mcux_lpc_syscon_clock.h
@@ -76,4 +76,6 @@
 
 #define MCUX_ENET_QOS_CLK		MCUX_LPC_CLK_ID(0x0D, 0x00)
 
+#define MCUX_LCDIC_CLK			MCUX_LPC_CLK_ID(0x0E, 0x00)
+
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_MCUX_LPC_SYSCON_H_ */