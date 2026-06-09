zephyrproject-rtos/zephyr

bluetooth: a2dp: implement get_config
implement get configuration

Signed-off-by: Mark Wang <yichang.wang@nxp.com>

subsys/bluetooth/host/classic/shell/a2dp.c
@@ -475,6 +475,25 @@ void app_delay_report_rsp(struct bt_a2dp_stream *stream, uint8_t rsp_err_code)
 	}
 }
 
+int app_get_config_req(struct bt_a2dp_stream *stream, uint8_t *rsp_err_code)
+{
+	*rsp_err_code = 0;
+	bt_shell_print("receive get config request and accept");
+	return 0;
+}
+
+void app_get_config_rsp(struct bt_a2dp_stream *stream, struct bt_a2dp_codec_cfg *codec_cfg,
+			uint8_t rsp_err_code)
+{
+	bt_shell_print("get config result: %d", rsp_err_code);
+
+	if (rsp_err_code == 0) {
+		uint32_t sample_rate = bt_a2dp_sbc_get_sampling_frequency(
+			(struct bt_a2dp_codec_sbc_params *)&codec_cfg->codec_config->codec_ie[0]);
+		bt_shell_print("sample rate %dHz", sample_rate);
+	}
+}
+
 struct bt_a2dp_cb a2dp_cb = {
 	.connected = app_connected,
 	.disconnected = app_disconnected,
@@ -489,6 +508,8 @@ struct bt_a2dp_cb a2dp_cb = {
 	.suspend_req = app_suspend_req,
 	.suspend_rsp = app_suspend_rsp,
 	.reconfig_req = app_reconfig_req,
+	.get_config_req = app_get_config_req,
+	.get_config_rsp = app_get_config_rsp,
 #ifdef CONFIG_BT_A2DP_DELAY_REPORT
 #if defined(CONFIG_BT_A2DP_SOURCE)
 	.delay_report_req = app_delay_report_req,
@@ -842,6 +863,19 @@ static int cmd_send_delay_report(const struct shell *sh, int32_t argc, char *arg
 }
 #endif
 
+static int cmd_get_config(const struct shell *sh, int32_t argc, char *argv[])
+{
+	if (a2dp_initied == 0) {
+		shell_print(sh, "need to register a2dp connection callbacks");
+		return -ENOEXEC;
+	}
+
+	if (bt_a2dp_stream_get_config(&sbc_stream) != 0) {
+		shell_print(sh, "fail");
+	}
+	return 0;
+}
+
 #define HELP_NONE "[none]"
 
 SHELL_STATIC_SUBCMD_SET_CREATE(a2dp_cmds,
@@ -863,6 +897,7 @@ SHELL_STATIC_SUBCMD_SET_CREATE(a2dp_cmds,
 #ifdef CONFIG_BT_A2DP_DELAY_REPORT
 	SHELL_CMD_ARG(send_delay_report, NULL, HELP_NONE, cmd_send_delay_report, 1, 0),
 #endif
+	SHELL_CMD_ARG(get_config, NULL, HELP_NONE, cmd_get_config, 1, 0),
 	SHELL_SUBCMD_SET_END
 );
 

phytec/zephyr-phytec

(cherry picked from commit 54cae2e75bcb7ee38ec4d7c24cef8835e3836a05)

subsys/bluetooth/host/classic/shell/a2dp.c
@@ -470,6 +470,25 @@ void app_delay_report_rsp(struct bt_a2dp_stream *stream, uint8_t rsp_err_code)
 	}
 }
 
+int app_get_config_req(struct bt_a2dp_stream *stream, uint8_t *rsp_err_code)
+{
+	*rsp_err_code = 0;
+	bt_shell_print("receive get config request and accept");
+	return 0;
+}
+
+void app_get_config_rsp(struct bt_a2dp_stream *stream, struct bt_a2dp_codec_cfg *codec_cfg,
+			uint8_t rsp_err_code)
+{
+	bt_shell_print("get config result: %d", rsp_err_code);
+
+	if (rsp_err_code == 0) {
+		uint32_t sample_rate = bt_a2dp_sbc_get_sampling_frequency(
+			(struct bt_a2dp_codec_sbc_params *)&codec_cfg->codec_config->codec_ie[0]);
+		bt_shell_print("sample rate %dHz", sample_rate);
+	}
+}
+
 struct bt_a2dp_cb a2dp_cb = {
 	.connected = app_connected,
 	.disconnected = app_disconnected,
@@ -484,6 +503,8 @@ struct bt_a2dp_cb a2dp_cb = {
 	.suspend_req = app_suspend_req,
 	.suspend_rsp = app_suspend_rsp,
 	.reconfig_req = app_reconfig_req,
+	.get_config_req = app_get_config_req,
+	.get_config_rsp = app_get_config_rsp,
 #if defined(CONFIG_BT_A2DP_SOURCE)
 	.delay_report_req = app_delay_report_req,
 #endif
@@ -840,6 +861,19 @@ static int cmd_send_delay_report(const struct shell *sh, int32_t argc, char *arg
 	return 0;
 }
 
+static int cmd_get_config(const struct shell *sh, int32_t argc, char *argv[])
+{
+	if (a2dp_initied == 0) {
+		shell_print(sh, "need to register a2dp connection callbacks");
+		return -ENOEXEC;
+	}
+
+	if (bt_a2dp_stream_get_config(&sbc_stream) != 0) {
+		shell_error(sh, "fail");
+	}
+	return 0;
+}
+
 #define HELP_NONE "[none]"
 
 SHELL_STATIC_SUBCMD_SET_CREATE(a2dp_cmds,
@@ -859,6 +893,7 @@ SHELL_STATIC_SUBCMD_SET_CREATE(a2dp_cmds,
 	SHELL_CMD_ARG(abort, NULL, "\"abort the stream\"", cmd_abort, 1, 0),
 	SHELL_CMD_ARG(send_media, NULL, HELP_NONE, cmd_send_media, 1, 0),
 	SHELL_CMD_ARG(send_delay_report, NULL, HELP_NONE, cmd_send_delay_report, 1, 0),
+	SHELL_CMD_ARG(get_config, NULL, HELP_NONE, cmd_get_config, 1, 0),
 	SHELL_SUBCMD_SET_END
 );
 