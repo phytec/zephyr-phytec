zephyrproject-rtos/zephyr

samples: bluetooth: peripheral_identity: Stop scanner in privacy
When CONFIG_BT_PRIVACY is enabled, we cannot start an advertiser with an
RPA generated for a different identity than scanner role. Therefore, we
need to stop scanning.

Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

samples/bluetooth/peripheral_identity/src/peripheral_identity.c
@@ -19,6 +19,7 @@ static uint8_t conn_count_max;
 static uint8_t volatile conn_count;
 static uint8_t id_current;
 static bool volatile is_disconnecting;
+static bool scanning;
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
@@ -60,6 +61,21 @@ static void adv_start(struct k_work *work)
 
 	printk("Using current id: %u\n", id_current);
 	adv_param.id = id_current;
+
+#if defined(CONFIG_BT_PRIVACY)
+	/* When privacy is enabled, we cannot start an advertiser with an RPA generated for a
+	 * different identity than scanner role. Therefore, we need to stop scanning.
+	 */
+	if (adv_param.id != BT_ID_DEFAULT && scanning) {
+		err = bt_le_scan_stop();
+		if (err) {
+			printk("Failed to stop scanning (err %d)\n", err);
+			return;
+		}
+
+		scanning = false;
+	}
+#endif
 
 	err = bt_le_adv_start(&adv_param, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
 	if (err) {
@@ -273,6 +289,8 @@ int init_peripheral(uint8_t max_conn, uint8_t iterations)
 		printk("Scan start failed (%d).\n", err);
 		return err;
 	}
+
+	scanning = true;
 
 	printk("Scanning start success.\n");
 #endif /* CONFIG_BT_OBSERVER */

phytec/zephyr-phytec

samples/bluetooth/peripheral_identity/src/peripheral_identity.c
@@ -19,6 +19,9 @@ static uint8_t conn_count_max;
 static uint8_t volatile conn_count;
 static uint8_t id_current;
 static bool volatile is_disconnecting;
+#if defined(CONFIG_BT_OBSERVER)
+static bool scanning;
+#endif
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
@@ -60,6 +63,21 @@ static void adv_start(struct k_work *work)
 
 	printk("Using current id: %u\n", id_current);
 	adv_param.id = id_current;
+
+#if defined(CONFIG_BT_OBSERVER)
+	/* When privacy is enabled, we cannot start a legacy advertiser with an RPA generated for a
+	 * different identity than scanner role. Therefore, we need to stop scanning.
+	 */
+	if (adv_param.id != BT_ID_DEFAULT && scanning) {
+		err = bt_le_scan_stop();
+		if (err) {
+			printk("Failed to stop scanning (err %d)\n", err);
+			return;
+		}
+
+		scanning = false;
+	}
+#endif
 
 	err = bt_le_adv_start(&adv_param, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
 	if (err) {
@@ -273,6 +291,8 @@ int init_peripheral(uint8_t max_conn, uint8_t iterations)
 		printk("Scan start failed (%d).\n", err);
 		return err;
 	}
+
+	scanning = true;
 
 	printk("Scanning start success.\n");
 #endif /* CONFIG_BT_OBSERVER */