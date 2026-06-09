zephyrproject-rtos/zephyr

drivers/interrupt_controller: Add fault event support on Intel VT-D
IR faults are non-recoverable, so it's good to know why. Thus let's
handle the fault event and print the fault.

Other faults are printed as well.

Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

drivers/interrupt_controller/intc_intel_vtd.c
@@ -20,8 +20,34 @@
 #include <arch/x86/intel_vtd.h>
 #include <drivers/interrupt_controller/intel_vtd.h>
 #include <drivers/interrupt_controller/ioapic.h>
+#include <drivers/pcie/msi.h>
+
+#include <kernel_arch_func.h>
 
 #include "intc_intel_vtd.h"
+
+static void vtd_clear_bit(const struct device *dev,
+			  uint16_t reg, unsigned int bit)
+{
+	uintptr_t base_address = DEVICE_MMIO_GET(dev);
+
+	sys_clear_bit((base_address + reg), bit);
+}
+
+static void vtd_write_reg32(const struct device *dev,
+			    uint16_t reg, uint32_t value)
+{
+	uintptr_t base_address = DEVICE_MMIO_GET(dev);
+
+	sys_write32(value, (base_address + reg));
+}
+
+static uint32_t vtd_read_reg(const struct device *dev, uint16_t reg)
+{
+	uintptr_t base_address = DEVICE_MMIO_GET(dev);
+
+	return sys_read32(base_address + reg);
+}
 
 static void vtd_write_reg64(const struct device *dev,
 			    uint16_t reg, uint64_t value)
@@ -31,11 +57,11 @@ static void vtd_write_reg64(const struct device *dev,
 	sys_write64(value, (base_address + reg));
 }
 
-static uint32_t vtd_read_reg(const struct device *dev, uint16_t reg)
+static uint64_t vtd_read_reg64(const struct device *dev, uint16_t reg)
 {
 	uintptr_t base_address = DEVICE_MMIO_GET(dev);
 
-	return sys_read32(base_address + reg);
+	return sys_read64(base_address + reg);
 }
 
 static void vtd_send_cmd(const struct device *dev,
@@ -49,6 +75,117 @@ static void vtd_send_cmd(const struct device *dev,
 			     status_bit)) {
 		/* Do nothing */
 	}
+}
+
+static void fault_status_description(uint32_t status)
+{
+	if (status & VTD_FSTS_PFO) {
+		printk("Primary Fault Overflow (PFO)\n");
+	}
+
+	if (status & VTD_FSTS_AFO) {
+		printk("Advanced Fault Overflow (AFO)\n");
+	}
+
+	if (status & VTD_FSTS_APF) {
+		printk("Advanced Primary Fault (APF)\n");
+	}
+
+	if (status & VTD_FSTS_IQE) {
+		printk("Invalidation Queue Error (IQE)\n");
+	}
+
+	if (status & VTD_FSTS_ICE) {
+		printk("Invalidation Completion Error (ICE)\n");
+	}
+
+	if (status & VTD_FSTS_ITE) {
+		printk("Invalidation Timeout Error\n");
+	}
+
+	if (status & VTD_FSTS_PPF) {
+		printk("Primary Pending Fault (PPF) %u\n",
+		       VTD_FSTS_FRI(status));
+	}
+}
+
+static void fault_record_description(uint64_t low, uint64_t high)
+{
+	printk("Fault %s request: Reason 0x%x info 0x%llx src 0x%x\n",
+	       (high & VTD_FRCD_T) ? "Read/Atomic" : "Write/Page",
+	       VTD_FRCD_FR(high), VTD_FRCD_FI(low), VTD_FRCD_SID(high));
+}
+
+static void fault_event_isr(const void *arg)
+{
+	const struct device *dev = arg;
+	struct vtd_ictl_data *data = dev->data;
+	uint32_t status;
+	uint8_t f_idx;
+
+	status = vtd_read_reg(dev, VTD_FSTS_REG);
+	fault_status_description(status);
+
+	if (!(status & VTD_FSTS_PPF)) {
+		goto out;
+	}
+
+	f_idx = VTD_FSTS_FRI(status);
+	while (f_idx < data->fault_record_num) {
+		uint64_t fault_l, fault_h;
+
+		/* Reading fault's 64 lowest bits */
+		fault_l = vtd_read_reg64(dev, data->fault_record_reg +
+					 (VTD_FRCD_REG_SIZE * f_idx));
+		/* Reading fault's 64 highest bits */
+		fault_h = vtd_read_reg64(dev, data->fault_record_reg +
+				       (VTD_FRCD_REG_SIZE * f_idx) + 8);
+
+		if (fault_h & VTD_FRCD_F) {
+			fault_record_description(fault_l, fault_h);
+		}
+
+		/* Clearing the fault */
+		vtd_write_reg64(dev, data->fault_record_reg +
+				(VTD_FRCD_REG_SIZE * f_idx), fault_l);
+		vtd_write_reg64(dev, data->fault_record_reg +
+				(VTD_FRCD_REG_SIZE * f_idx) + 8, fault_h);
+		f_idx++;
+	}
+out:
+	/* Clearing fault status */
+	vtd_write_reg32(dev, VTD_FSTS_REG, VTD_FSTS_CLEAR(status));
+}
+
+static void vtd_fault_event_init(const struct device *dev)
+{
+	struct vtd_ictl_data *data = dev->data;
+	uint64_t value;
+
+	value = vtd_read_reg64(dev, VTD_CAP_REG);
+	data->fault_record_num = VTD_CAP_NFR(value) + 1;
+	data->fault_record_reg = DEVICE_MMIO_GET(dev) +
+		(uintptr_t)(16 * VTD_CAP_FRO(value));
+
+	/* Allocating IRQ & vector and connecting the ISR handler,
+	 * by-passing remapping by using x86 functions directly.
+	 */
+	data->fault_irq = arch_irq_allocate();
+	data->fault_vector = z_x86_allocate_vector(0, -1);
+
+	vtd_write_reg32(dev, VTD_FEDATA_REG, data->fault_vector);
+	vtd_write_reg32(dev, VTD_FEADDR_REG,
+			pcie_msi_map(data->fault_irq, NULL));
+	vtd_write_reg32(dev, VTD_FEUADDR_REG, 0);
+
+	z_x86_irq_connect_on_vector(data->fault_irq, data->fault_vector,
+				    fault_event_isr, dev);
+
+	vtd_write_reg32(dev, VTD_FSTS_REG,
+			VTD_FSTS_CLEAR(vtd_read_reg32(dev, VTD_FSTS_REG)));
+
+	/* Unmasking interrupts */
+	vtd_clear_bit(dev, VTD_FECTL_REG, VTD_FECTL_REG_IM);
 }
 
 static int vtd_ictl_allocate_entries(const struct device *dev,
@@ -182,18 +319,20 @@ static int vtd_ictl_init(const struct device *dev)
 	struct vtd_ictl_data *data = dev->data;
 	unsigned int key = irq_lock();
 	uint64_t eime = 0;
-	uint64_t irta;
+	uint64_t value;
 
 	DEVICE_MMIO_MAP(dev, K_MEM_CACHE_NONE);
+
+	vtd_fault_event_init(dev);
 
 	if (IS_ENABLED(CONFIG_X2APIC)) {
 		eime = VTD_IRTA_EIME;
 	}
 
-	irta = VTD_IRTA_REG_GEN_CONTENT((uintptr_t)data->irte,
-					IRTA_SIZE, eime);
+	value = VTD_IRTA_REG_GEN_CONTENT((uintptr_t)data->irte,
+					 IRTA_SIZE, eime);
 
-	vtd_write_reg64(dev, VTD_IRTA_REG, irta);
+	vtd_write_reg64(dev, VTD_IRTA_REG, value);
 
 	if (!IS_ENABLED(CONFIG_X2APIC) &&
 	    IS_ENABLED(CONFIG_INTEL_VTD_ICTL_XAPIC_PASSTHROUGH)) {

phytec/zephyr-phytec

drivers/interrupt_controller/intc_intel_vtd.c
@@ -20,8 +20,26 @@
 #include <arch/x86/intel_vtd.h>
 #include <drivers/interrupt_controller/intel_vtd.h>
 #include <drivers/interrupt_controller/ioapic.h>
+#include <drivers/pcie/msi.h>
+
+#include <kernel_arch_func.h>
 
 #include "intc_intel_vtd.h"
+
+static void vtd_write_reg32(const struct device *dev,
+			    uint16_t reg, uint32_t value)
+{
+	uintptr_t base_address = DEVICE_MMIO_GET(dev);
+
+	sys_write32(value, (base_address + reg));
+}
+
+static uint32_t vtd_read_reg(const struct device *dev, uint16_t reg)
+{
+	uintptr_t base_address = DEVICE_MMIO_GET(dev);
+
+	return sys_read32(base_address + reg);
+}
 
 static void vtd_write_reg64(const struct device *dev,
 			    uint16_t reg, uint64_t value)
@@ -31,11 +49,11 @@ static void vtd_write_reg64(const struct device *dev,
 	sys_write64(value, (base_address + reg));
 }
 
-static uint32_t vtd_read_reg(const struct device *dev, uint16_t reg)
+static uint64_t vtd_read_reg64(const struct device *dev, uint16_t reg)
 {
 	uintptr_t base_address = DEVICE_MMIO_GET(dev);
 
-	return sys_read32(base_address + reg);
+	return sys_read64(base_address + reg);
 }
 
 static void vtd_send_cmd(const struct device *dev,
@@ -49,6 +67,120 @@ static void vtd_send_cmd(const struct device *dev,
 			     status_bit)) {
 		/* Do nothing */
 	}
+}
+
+static void fault_status_description(uint32_t status)
+{
+	if (status & VTD_FSTS_PFO) {
+		printk("Primary Fault Overflow (PFO)\n");
+	}
+
+	if (status & VTD_FSTS_AFO) {
+		printk("Advanced Fault Overflow (AFO)\n");
+	}
+
+	if (status & VTD_FSTS_APF) {
+		printk("Advanced Primary Fault (APF)\n");
+	}
+
+	if (status & VTD_FSTS_IQE) {
+		printk("Invalidation Queue Error (IQE)\n");
+	}
+
+	if (status & VTD_FSTS_ICE) {
+		printk("Invalidation Completion Error (ICE)\n");
+	}
+
+	if (status & VTD_FSTS_ITE) {
+		printk("Invalidation Timeout Error\n");
+	}
+
+	if (status & VTD_FSTS_PPF) {
+		printk("Primary Pending Fault (PPF) %u\n",
+		       VTD_FSTS_FRI(status));
+	}
+}
+
+static void fault_record_description(uint64_t low, uint64_t high)
+{
+	printk("Fault %s request: Reason 0x%x info 0x%llx src 0x%x\n",
+	       (high & VTD_FRCD_T) ? "Read/Atomic" : "Write/Page",
+	       VTD_FRCD_FR(high), VTD_FRCD_FI(low), VTD_FRCD_SID(high));
+}
+
+static void fault_event_isr(const void *arg)
+{
+	const struct device *dev = arg;
+	struct vtd_ictl_data *data = dev->data;
+	uint32_t status;
+	uint8_t f_idx;
+
+	status = vtd_read_reg(dev, VTD_FSTS_REG);
+	fault_status_description(status);
+
+	if (!(status & VTD_FSTS_PPF)) {
+		goto out;
+	}
+
+	f_idx = VTD_FSTS_FRI(status);
+	while (f_idx < data->fault_record_num) {
+		uint64_t fault_l, fault_h;
+
+		/* Reading fault's 64 lowest bits */
+		fault_l = vtd_read_reg64(dev, data->fault_record_reg +
+					 (VTD_FRCD_REG_SIZE * f_idx));
+		/* Reading fault's 64 highest bits */
+		fault_h = vtd_read_reg64(dev, data->fault_record_reg +
+				       (VTD_FRCD_REG_SIZE * f_idx) + 8);
+
+		if (fault_h & VTD_FRCD_F) {
+			fault_record_description(fault_l, fault_h);
+		}
+
+		/* Clearing the fault */
+		vtd_write_reg64(dev, data->fault_record_reg +
+				(VTD_FRCD_REG_SIZE * f_idx), fault_l);
+		vtd_write_reg64(dev, data->fault_record_reg +
+				(VTD_FRCD_REG_SIZE * f_idx) + 8, fault_h);
+		f_idx++;
+	}
+out:
+	/* Clearing fault status */
+	vtd_write_reg32(dev, VTD_FSTS_REG, VTD_FSTS_CLEAR(status));
+}
+
+static void vtd_fault_event_init(const struct device *dev)
+{
+	struct vtd_ictl_data *data = dev->data;
+	uint64_t value;
+	uint32_t reg;
+
+	value = vtd_read_reg64(dev, VTD_CAP_REG);
+	data->fault_record_num = VTD_CAP_NFR(value) + 1;
+	data->fault_record_reg = DEVICE_MMIO_GET(dev) +
+		(uintptr_t)(16 * VTD_CAP_FRO(value));
+
+	/* Allocating IRQ & vector and connecting the ISR handler,
+	 * by-passing remapping by using x86 functions directly.
+	 */
+	data->fault_irq = arch_irq_allocate();
+	data->fault_vector = z_x86_allocate_vector(0, -1);
+
+	vtd_write_reg32(dev, VTD_FEDATA_REG, data->fault_vector);
+	vtd_write_reg32(dev, VTD_FEADDR_REG,
+			pcie_msi_map(data->fault_irq, NULL, 0));
+	vtd_write_reg32(dev, VTD_FEUADDR_REG, 0);
+
+	z_x86_irq_connect_on_vector(data->fault_irq, data->fault_vector,
+				    fault_event_isr, dev);
+
+	vtd_write_reg32(dev, VTD_FSTS_REG,
+			VTD_FSTS_CLEAR(vtd_read_reg32(dev, VTD_FSTS_REG)));
+
+	/* Unmasking interrupts */
+	reg = vtd_read_reg32(dev, VTD_FECTL_REG);
+	reg &= ~BIT(VTD_FECTL_REG_IM);
+	vtd_write_reg32(dev, VTD_FECTL_REG, reg);
 }
 
 static int vtd_ictl_allocate_entries(const struct device *dev,
@@ -185,18 +317,20 @@ static int vtd_ictl_init(const struct device *dev)
 	struct vtd_ictl_data *data = dev->data;
 	unsigned int key = irq_lock();
 	uint64_t eime = 0;
-	uint64_t irta;
+	uint64_t value;
 
 	DEVICE_MMIO_MAP(dev, K_MEM_CACHE_NONE);
+
+	vtd_fault_event_init(dev);
 
 	if (IS_ENABLED(CONFIG_X2APIC)) {
 		eime = VTD_IRTA_EIME;
 	}
 
-	irta = VTD_IRTA_REG_GEN_CONTENT((uintptr_t)data->irte,
-					IRTA_SIZE, eime);
+	value = VTD_IRTA_REG_GEN_CONTENT((uintptr_t)data->irte,
+					 IRTA_SIZE, eime);
 
-	vtd_write_reg64(dev, VTD_IRTA_REG, irta);
+	vtd_write_reg64(dev, VTD_IRTA_REG, value);
 
 	if (!IS_ENABLED(CONFIG_X2APIC) &&
 	    IS_ENABLED(CONFIG_INTEL_VTD_ICTL_XAPIC_PASSTHROUGH)) {