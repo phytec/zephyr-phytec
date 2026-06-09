zephyrproject-rtos/zephyr

Bluetooth: BAP: Add missing check for unicast server ep
We check for all other roles when checking whether
an endpoint can send or receive, except for the unicast server.
It is technically implied, but if the ep is not a valid pointer,
then we may access invalid memory.

Add check for bt_bap_ep_is_unicast_server (which uses the new
bt_ascs_has_ep function).

Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

subsys/bluetooth/audio/bap_unicast_server.c
@@ -222,3 +222,8 @@ void bt_bap_unicast_server_foreach_ep(struct bt_conn *conn, bt_bap_ep_func_t fun
 {
 	bt_ascs_foreach_ep(conn, func, user_data);
 }
+
+bool bt_bap_ep_is_unicast_server(const struct bt_bap_ep *ep)
+{
+	return bt_ascs_has_ep(ep);
+}

phytec/zephyr-phytec

subsys/bluetooth/audio/bap_unicast_server.c
@@ -222,3 +222,8 @@ void bt_bap_unicast_server_foreach_ep(struct bt_conn *conn, bt_bap_ep_func_t fun
 {
 	bt_ascs_foreach_ep(conn, func, user_data);
 }
+
+bool bt_bap_ep_is_unicast_server(const struct bt_bap_ep *ep)
+{
+	return bt_ascs_is_ase_ep(ep);
+}