Mani-Sadhasivam/zephyr

arch: arm64: Introduce ARM64 (AArch64) architecture
Introduce the basic ARM64 architecture support.

A new CONFIG_ARM64 symbol is introduced for the new architecture and new
cmake / Kconfig files are added to switch between ARM and ARM64.

Signed-off-by: Carlo Caione <ccaione@baylibre.com>

include/arch/arm/aarch64/arch.h
@@ -1,0 +1,51 @@
+/*
+ * Copyright (c) 2019 Carlo Caione <ccaione@baylibre.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief ARM64 specific kernel interface header
+ *
+ * This header contains the ARM64 specific kernel interface.  It is
+ * included by the kernel interface architecture-abstraction header
+ * (include/arm/aarch64/cpu.h)
+ */
+
+#ifndef ZEPHYR_INCLUDE_ARCH_ARM_AARCH64_ARCH_H_
+#define ZEPHYR_INCLUDE_ARCH_ARM_AARCH64_ARCH_H_
+
+/* Add include for DTS generated information */
+#include <generated_dts_board.h>
+
+#include <arch/arm/aarch64/thread.h>
+#include <arch/arm/aarch64/exc.h>
+#include <arch/arm/aarch64/irq.h>
+#include <arch/arm/aarch64/misc.h>
+#include <arch/arm/aarch64/asm_inline.h>
+#include <arch/arm/aarch64/cpu.h>
+#include <arch/arm/aarch64/sys_io.h>
+#include <arch/arm/aarch64/timer.h>
+#include <arch/common/addr_types.h>
+#include <arch/common/ffs.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Declare the STACK_ALIGN_SIZE
+ *
+ * Denotes the required alignment of the stack pointer on public API
+ * boundaries
+ *
+ */
+#define STACK_ALIGN		16
+#define STACK_ALIGN_SIZE	STACK_ALIGN
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_INCLUDE_ARCH_ARM_AARCH64_ARCH_H_ */

include/arch/arm/aarch64/timer.h
@@ -1,0 +1,61 @@
+/*
+ * Copyright (c) 2019 Carlo Caione <ccaione@baylibre.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_ARCH_ARM_AARCH64_TIMER_H_
+#define ZEPHYR_INCLUDE_ARCH_ARM_AARCH64_TIMER_H_
+
+#ifndef _ASMLANGUAGE
+
+#include <drivers/timer/arm_arch_timer.h>
+#include <zephyr/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define ARM_ARCH_TIMER_IRQ	((ARM_TIMER_NON_SECURE_IRQ + 1) << 8)
+#define CNTV_CTL_ENABLE		((1) << 0)
+
+
+static ALWAYS_INLINE void arm_arch_timer_set_compare(u64_t val)
+{
+	__asm__ volatile("msr cntp_cval_el0, %0\n\t"
+			 : : "r" (val) : "memory");
+}
+
+static ALWAYS_INLINE void arm_arch_timer_enable(unsigned char enable)
+{
+	u32_t cntv_ctl;
+
+	__asm__ volatile("mrs %0, cntp_ctl_el0\n\t"
+			 : "=r" (cntv_ctl) :  : "memory");
+
+	if (enable)
+		cntv_ctl |= CNTV_CTL_ENABLE;
+	else
+		cntv_ctl &= ~CNTV_CTL_ENABLE;
+
+	__asm__ volatile("msr cntp_ctl_el0, %0\n\t"
+			 : : "r" (cntv_ctl) : "memory");
+}
+
+static ALWAYS_INLINE u64_t arm_arch_timer_count(void)
+{
+	u64_t cntvct_el0;
+
+	__asm__ volatile("mrs %0, cntpct_el0\n\t"
+			 : "=r" (cntvct_el0) : : "memory");
+
+	return cntvct_el0;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _ASMLANGUAGE */
+
+#endif /* ZEPHYR_INCLUDE_ARCH_ARM_AARCH64_TIMER_H_ */

phytec/zephyr-phytec

include/arch/arm/aarch64/arch.h
@@ -1,0 +1,51 @@
+/*
+ * Copyright (c) 2019 Carlo Caione <ccaione@baylibre.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief ARM64 specific kernel interface header
+ *
+ * This header contains the ARM64 specific kernel interface.  It is
+ * included by the kernel interface architecture-abstraction header
+ * (include/arm/aarch64/cpu.h)
+ */
+
+#ifndef ZEPHYR_INCLUDE_ARCH_ARM_AARCH64_ARCH_H_
+#define ZEPHYR_INCLUDE_ARCH_ARM_AARCH64_ARCH_H_
+
+/* Add include for DTS generated information */
+#include <devicetree.h>
+
+#include <arch/arm/aarch64/thread.h>
+#include <arch/arm/aarch64/exc.h>
+#include <arch/arm/aarch64/irq.h>
+#include <arch/arm/aarch64/misc.h>
+#include <arch/arm/aarch64/asm_inline.h>
+#include <arch/arm/aarch64/cpu.h>
+#include <arch/arm/aarch64/sys_io.h>
+#include <arch/arm/aarch64/timer.h>
+#include <arch/common/addr_types.h>
+#include <arch/common/ffs.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Declare the STACK_ALIGN_SIZE
+ *
+ * Denotes the required alignment of the stack pointer on public API
+ * boundaries
+ *
+ */
+#define STACK_ALIGN		16
+#define STACK_ALIGN_SIZE	STACK_ALIGN
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_INCLUDE_ARCH_ARM_AARCH64_ARCH_H_ */

include/arch/arm/aarch64/timer.h
@@ -1,0 +1,61 @@
+/*
+ * Copyright (c) 2019 Carlo Caione <ccaione@baylibre.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_ARCH_ARM_AARCH64_TIMER_H_
+#define ZEPHYR_INCLUDE_ARCH_ARM_AARCH64_TIMER_H_
+
+#ifndef _ASMLANGUAGE
+
+#include <drivers/timer/arm_arch_timer.h>
+#include <zephyr/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define ARM_ARCH_TIMER_IRQ	((ARM_TIMER_VIRTUAL_IRQ + 1) << 8)
+#define CNTV_CTL_ENABLE		((1) << 0)
+
+
+static ALWAYS_INLINE void arm_arch_timer_set_compare(u64_t val)
+{
+	__asm__ volatile("msr cntv_cval_el0, %0\n\t"
+			 : : "r" (val) : "memory");
+}
+
+static ALWAYS_INLINE void arm_arch_timer_enable(unsigned char enable)
+{
+	u32_t cntv_ctl;
+
+	__asm__ volatile("mrs %0, cntv_ctl_el0\n\t"
+			 : "=r" (cntv_ctl) :  : "memory");
+
+	if (enable)
+		cntv_ctl |= CNTV_CTL_ENABLE;
+	else
+		cntv_ctl &= ~CNTV_CTL_ENABLE;
+
+	__asm__ volatile("msr cntv_ctl_el0, %0\n\t"
+			 : : "r" (cntv_ctl) : "memory");
+}
+
+static ALWAYS_INLINE u64_t arm_arch_timer_count(void)
+{
+	u64_t cntvct_el0;
+
+	__asm__ volatile("mrs %0, cntvct_el0\n\t"
+			 : "=r" (cntvct_el0) : : "memory");
+
+	return cntvct_el0;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _ASMLANGUAGE */
+
+#endif /* ZEPHYR_INCLUDE_ARCH_ARM_AARCH64_TIMER_H_ */