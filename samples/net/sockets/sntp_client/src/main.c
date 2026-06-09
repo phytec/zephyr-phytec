Embeint/zephyr

samples: net: sntp_client: SNTP server lookup
Lookup the address of the SNTP server through DNS instead of requiring
a server to be running locally.

Signed-off-by: Jordan Yates <jordan@embeint.com>

samples/net/sockets/sntp_client/src/main.c
@@ -11,77 +11,83 @@ LOG_MODULE_REGISTER(net_sntp_client_sample, LOG_LEVEL_DBG);
 #include <zephyr/net/sntp.h>
 #include <arpa/inet.h>
 
-#include "config.h"
 #include "net_sample_common.h"
 
-#define SNTP_PORT 123
-
-int main(void)
+int dns_query(const char *host, uint16_t port, int family, int socktype, struct sockaddr *addr,
+			  socklen_t *addrlen)
 {
-	struct sntp_ctx ctx;
-	struct sockaddr_in addr;
-#if defined(CONFIG_NET_IPV6)
-	struct sockaddr_in6 addr6;
-#endif
-	struct sntp_time sntp_time;
+	struct addrinfo hints = {
+		.ai_family = family,
+		.ai_socktype = socktype,
+	};
+	struct addrinfo *res = NULL;
+	char addr_str[INET6_ADDRSTRLEN] = {0};
 	int rv;
 
-	wait_for_network();
-
-	/* ipv4 */
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(SNTP_PORT);
-	inet_pton(AF_INET, SERVER_ADDR, &addr.sin_addr);
-
-	rv = sntp_init(&ctx, (struct sockaddr *) &addr,
-		       sizeof(struct sockaddr_in));
+	/* Perform DNS query */
+	rv = getaddrinfo(host, NULL, &hints, &res);
 	if (rv < 0) {
-		LOG_ERR("Failed to init SNTP IPv4 ctx: %d", rv);
+		LOG_ERR("getaddrinfo failed (%d, errno %d)", rv, errno);
+		return rv;
+	}
+	/* Store the first result */
+	*addr = *res->ai_addr;
+	*addrlen = res->ai_addrlen;
+	/* Free the allocated memory */
+	freeaddrinfo(res);
+	/* Store the port */
+	net_sin(addr)->sin_port = htons(port);
+	/* Print the found address */
+	inet_ntop(addr->sa_family, &net_sin(addr)->sin_addr, addr_str, sizeof(addr_str));
+	LOG_INF("%s -> %s", host, addr_str);
+	return 0;
+}
+
+static void do_sntp(int family)
+{
+	char *family_str = family == AF_INET ? "IPv4" : "IPv6";
+	struct sntp_time s_time;
+	struct sntp_ctx ctx;
+	struct sockaddr addr;
+	socklen_t addrlen;
+	int rv;
+
+	/* Get SNTP server */
+	rv = dns_query(CONFIG_NET_SAMPLE_SNTP_SERVER_ADDRESS, CONFIG_NET_SAMPLE_SNTP_SERVER_PORT,
+				   family, SOCK_DGRAM, &addr, &addrlen);
+	if (rv != 0) {
+		LOG_ERR("Failed to lookup %s SNTP server (%d)", family_str, rv);
+		return;
+	}
+
+	rv = sntp_init(&ctx, &addr, addrlen);
+	if (rv < 0) {
+		LOG_ERR("Failed to init SNTP %s ctx: %d", family_str, rv);
 		goto end;
 	}
 
-	LOG_INF("Sending SNTP IPv4 request...");
-	rv = sntp_query(&ctx, 4 * MSEC_PER_SEC, &sntp_time);
+	LOG_INF("Sending SNTP %s request...", family_str);
+	rv = sntp_query(&ctx, 4 * MSEC_PER_SEC, &s_time);
 	if (rv < 0) {
-		LOG_ERR("SNTP IPv4 request failed: %d", rv);
+		LOG_ERR("SNTP %s request failed: %d", family_str, rv);
 		goto end;
 	}
 
-	LOG_INF("status: %d", rv);
-	LOG_INF("time since Epoch: high word: %u, low word: %u",
-		(uint32_t)(sntp_time.seconds >> 32), (uint32_t)sntp_time.seconds);
-
-#if defined(CONFIG_NET_IPV6)
-	sntp_close(&ctx);
-
-	/* ipv6 */
-	memset(&addr6, 0, sizeof(addr6));
-	addr6.sin6_family = AF_INET6;
-	addr6.sin6_port = htons(SNTP_PORT);
-	inet_pton(AF_INET6, SERVER_ADDR6, &addr6.sin6_addr);
-
-	rv = sntp_init(&ctx, (struct sockaddr *) &addr6,
-		       sizeof(struct sockaddr_in6));
-	if (rv < 0) {
-		LOG_ERR("Failed to init SNTP IPv6 ctx: %d", rv);
-		goto end;
-	}
-
-	LOG_INF("Sending SNTP IPv6 request...");
-	/* With such a timeout, this is expected to fail. */
-	rv = sntp_query(&ctx, 0, &sntp_time);
-	if (rv < 0) {
-		LOG_ERR("SNTP IPv6 request: %d", rv);
-		goto end;
-	}
-
-	LOG_INF("status: %d", rv);
-	LOG_INF("time since Epoch: high word: %u, low word: %u",
-		(uint32_t)(sntp_time.seconds >> 32), (uint32_t)sntp_time.seconds);
-#endif
+	LOG_INF("SNTP Time: %ld", s_time.seconds);
 
 end:
 	sntp_close(&ctx);
+}
+
+int main(void)
+{
+	wait_for_network();
+
+	do_sntp(AF_INET);
+
+#if defined(CONFIG_NET_IPV6)
+	do_sntp(AF_INET6);
+#endif
+
 	return 0;
 }

phytec/zephyr-phytec

samples/net/sockets/sntp_client/src/main.c
@@ -11,77 +11,83 @@ LOG_MODULE_REGISTER(net_sntp_client_sample, LOG_LEVEL_DBG);
 #include <zephyr/net/sntp.h>
 #include <arpa/inet.h>
 
-#include "config.h"
 #include "net_sample_common.h"
 
-#define SNTP_PORT 123
-
-int main(void)
+int dns_query(const char *host, uint16_t port, int family, int socktype, struct sockaddr *addr,
+			  socklen_t *addrlen)
 {
-	struct sntp_ctx ctx;
-	struct sockaddr_in addr;
-#if defined(CONFIG_NET_IPV6)
-	struct sockaddr_in6 addr6;
-#endif
-	struct sntp_time sntp_time;
+	struct addrinfo hints = {
+		.ai_family = family,
+		.ai_socktype = socktype,
+	};
+	struct addrinfo *res = NULL;
+	char addr_str[INET6_ADDRSTRLEN] = {0};
 	int rv;
 
-	wait_for_network();
-
-	/* ipv4 */
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(SNTP_PORT);
-	inet_pton(AF_INET, SERVER_ADDR, &addr.sin_addr);
-
-	rv = sntp_init(&ctx, (struct sockaddr *) &addr,
-		       sizeof(struct sockaddr_in));
+	/* Perform DNS query */
+	rv = getaddrinfo(host, NULL, &hints, &res);
 	if (rv < 0) {
-		LOG_ERR("Failed to init SNTP IPv4 ctx: %d", rv);
+		LOG_ERR("getaddrinfo failed (%d, errno %d)", rv, errno);
+		return rv;
+	}
+	/* Store the first result */
+	*addr = *res->ai_addr;
+	*addrlen = res->ai_addrlen;
+	/* Free the allocated memory */
+	freeaddrinfo(res);
+	/* Store the port */
+	net_sin(addr)->sin_port = htons(port);
+	/* Print the found address */
+	inet_ntop(addr->sa_family, &net_sin(addr)->sin_addr, addr_str, sizeof(addr_str));
+	LOG_INF("%s -> %s", host, addr_str);
+	return 0;
+}
+
+static void do_sntp(int family)
+{
+	char *family_str = family == AF_INET ? "IPv4" : "IPv6";
+	struct sntp_time s_time;
+	struct sntp_ctx ctx;
+	struct sockaddr addr;
+	socklen_t addrlen;
+	int rv;
+
+	/* Get SNTP server */
+	rv = dns_query(CONFIG_NET_SAMPLE_SNTP_SERVER_ADDRESS, CONFIG_NET_SAMPLE_SNTP_SERVER_PORT,
+				   family, SOCK_DGRAM, &addr, &addrlen);
+	if (rv != 0) {
+		LOG_ERR("Failed to lookup %s SNTP server (%d)", family_str, rv);
+		return;
+	}
+
+	rv = sntp_init(&ctx, &addr, addrlen);
+	if (rv < 0) {
+		LOG_ERR("Failed to init SNTP %s ctx: %d", family_str, rv);
 		goto end;
 	}
 
-	LOG_INF("Sending SNTP IPv4 request...");
-	rv = sntp_query(&ctx, 4 * MSEC_PER_SEC, &sntp_time);
+	LOG_INF("Sending SNTP %s request...", family_str);
+	rv = sntp_query(&ctx, 4 * MSEC_PER_SEC, &s_time);
 	if (rv < 0) {
-		LOG_ERR("SNTP IPv4 request failed: %d", rv);
+		LOG_ERR("SNTP %s request failed: %d", family_str, rv);
 		goto end;
 	}
 
-	LOG_INF("status: %d", rv);
-	LOG_INF("time since Epoch: high word: %u, low word: %u",
-		(uint32_t)(sntp_time.seconds >> 32), (uint32_t)sntp_time.seconds);
-
-#if defined(CONFIG_NET_IPV6)
-	sntp_close(&ctx);
-
-	/* ipv6 */
-	memset(&addr6, 0, sizeof(addr6));
-	addr6.sin6_family = AF_INET6;
-	addr6.sin6_port = htons(SNTP_PORT);
-	inet_pton(AF_INET6, SERVER_ADDR6, &addr6.sin6_addr);
-
-	rv = sntp_init(&ctx, (struct sockaddr *) &addr6,
-		       sizeof(struct sockaddr_in6));
-	if (rv < 0) {
-		LOG_ERR("Failed to init SNTP IPv6 ctx: %d", rv);
-		goto end;
-	}
-
-	LOG_INF("Sending SNTP IPv6 request...");
-	/* With such a timeout, this is expected to fail. */
-	rv = sntp_query(&ctx, 0, &sntp_time);
-	if (rv < 0) {
-		LOG_ERR("SNTP IPv6 request: %d", rv);
-		goto end;
-	}
-
-	LOG_INF("status: %d", rv);
-	LOG_INF("time since Epoch: high word: %u, low word: %u",
-		(uint32_t)(sntp_time.seconds >> 32), (uint32_t)sntp_time.seconds);
-#endif
+	LOG_INF("SNTP Time: %llu", s_time.seconds);
 
 end:
 	sntp_close(&ctx);
+}
+
+int main(void)
+{
+	wait_for_network();
+
+	do_sntp(AF_INET);
+
+#if defined(CONFIG_NET_IPV6)
+	do_sntp(AF_INET6);
+#endif
+
 	return 0;
 }