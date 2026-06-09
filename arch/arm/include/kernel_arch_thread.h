agross-linaro/zephyr

arm: userspace: Add ARM userspace infrastructure
This patch adds support for userspace on ARM architectures.  Arch
specific calls for transitioning threads to user mode, system calls,
and associated handlers.

Signed-off-by: Andy Gross <andy.gross@linaro.org>

arch/arm/include/kernel_arch_thread.h
@@ -94,6 +94,12 @@ struct _thread_arch {
 	 */
 	struct _preempt_float  preempt_float;
 #endif
+
+#ifdef CONFIG_ARM_USERSPACE
+	u32_t mode;
+	u32_t priv_stack_start;
+	u32_t priv_stack_size;
+#endif
 };
 
 typedef struct _thread_arch _thread_arch_t;

phytec/zephyr-phytec

arch/arm/include/kernel_arch_thread.h
@@ -94,6 +94,12 @@ struct _thread_arch {
 	 */
 	struct _preempt_float  preempt_float;
 #endif
+
+#ifdef CONFIG_USERSPACE
+	u32_t mode;
+	u32_t priv_stack_start;
+	u32_t priv_stack_size;
+#endif
 };
 
 typedef struct _thread_arch _thread_arch_t;