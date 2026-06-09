zephyrproject-rtos/zephyr

net: mqtt: Improve disconnect error notification for MQTT 5.0
As MQTT 5.0 allows to specify the disconnect reason in the Disconnect
packet, use this new feature to improve error notification to the
broker, according to the error guidelines in the MQTT 5.0 spec.
For most cases, a generic arbitrary mapping between errno values and
reason codes is used, however the parser can specify the disconnect
reason code manually to better handle certain corner cases (like invalid
topic alias used).

Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

include/zephyr/net/mqtt.h
@@ -838,6 +838,9 @@ struct mqtt_internal {
 #if defined(CONFIG_MQTT_VERSION_5_0)
 	/** Internal. MQTT 5.0 topic alias mapping. */
 	struct mqtt_topic_alias topic_aliases[CONFIG_MQTT_TOPIC_ALIAS_MAX];
+
+	/** Internal. MQTT 5.0 disconnect reason set in case of processing errors. */
+	enum mqtt_disconnect_reason disconnect_reason;
 #endif /* CONFIG_MQTT_VERSION_5_0 */
 };
 

subsys/net/lib/mqtt/mqtt_internal.h
@@ -496,6 +496,28 @@ int disconnect_decode(const struct mqtt_client *client, struct buf_ctx *buf,
  */
 int auth_decode(const struct mqtt_client *client, struct buf_ctx *buf,
 		struct mqtt_auth_param *param);
+
+/**@brief Set MQTT 5.0 disconnect reason.
+ *
+ * Packet parser can use this function to set a custom disconnect reason code,
+ * if not set, the client will use the default mapping between errno values and
+ * reason codes.
+ *
+ * @param[inout] MQTT client.
+ * @param[in] reason MQTT 5.0 disconnect reason code.
+ */
+static inline void set_disconnect_reason(struct mqtt_client *client,
+					 enum mqtt_disconnect_reason reason)
+{
+	client->internal.disconnect_reason = reason;
+}
+#else
+static inline void set_disconnect_reason(struct mqtt_client *client,
+					 enum mqtt_disconnect_reason reason)
+{
+	ARG_UNUSED(client);
+	ARG_UNUSED(reason);
+}
 #endif /* CONFIG_MQTT_VERSION_5_0 */
 
 /**
@@ -511,6 +533,10 @@ int auth_decode(const struct mqtt_client *client, struct buf_ctx *buf,
  * @retval -EAGAIN if the buffer would be exceeded during the read.
  */
 int unpack_variable_int(struct buf_ctx *buf, uint32_t *val);
+
+
+
+
 
 #ifdef __cplusplus
 }

phytec/zephyr-phytec

include/zephyr/net/mqtt.h
@@ -873,6 +873,9 @@ struct mqtt_internal {
 #if defined(CONFIG_MQTT_VERSION_5_0)
 	/** Internal. MQTT 5.0 topic alias mapping. */
 	struct mqtt_topic_alias topic_aliases[CONFIG_MQTT_TOPIC_ALIAS_MAX];
+
+	/** Internal. MQTT 5.0 disconnect reason set in case of processing errors. */
+	enum mqtt_disconnect_reason_code disconnect_reason;
 #endif /* CONFIG_MQTT_VERSION_5_0 */
 };
 

subsys/net/lib/mqtt/mqtt_internal.h
@@ -496,6 +496,28 @@ int disconnect_decode(const struct mqtt_client *client, struct buf_ctx *buf,
  */
 int auth_decode(const struct mqtt_client *client, struct buf_ctx *buf,
 		struct mqtt_auth_param *param);
+
+/**@brief Set MQTT 5.0 disconnect reason.
+ *
+ * Packet parser can use this function to set a custom disconnect reason code,
+ * if not set, the client will use the default mapping between errno values and
+ * reason codes.
+ *
+ * @param[inout] MQTT client.
+ * @param[in] reason MQTT 5.0 disconnect reason code.
+ */
+static inline void set_disconnect_reason(struct mqtt_client *client,
+					 enum mqtt_disconnect_reason_code reason)
+{
+	client->internal.disconnect_reason = reason;
+}
+#else
+static inline void set_disconnect_reason(struct mqtt_client *client,
+					 enum mqtt_disconnect_reason_code reason)
+{
+	ARG_UNUSED(client);
+	ARG_UNUSED(reason);
+}
 #endif /* CONFIG_MQTT_VERSION_5_0 */
 
 /**