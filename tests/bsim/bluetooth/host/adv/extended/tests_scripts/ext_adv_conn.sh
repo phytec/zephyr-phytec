ubieda/zephyr

Tests: Bluetooth: Host: Added Test for extended Advertisements
Where extended advertising is restablished through the use of
recycled() callback, registers over bt_conn_cb_register().

Signed-off-by: Luis Ubieda <luisf@croxel.com>

tests/bsim/bluetooth/host/adv/extended/tests_scripts/ext_adv_conn.sh
@@ -1,0 +1,31 @@
+#!/usr/bin/env bash
+# Copyright (c) 2024 Croxel, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+# Extended advertising test:
+#
+# - Connectable: In addition to broadcasting advertisements, it is connectable
+#   and restarts advertisements once disconnected. The scanner/central scans
+#   for the packets and establishes the connection, to then disconnect
+#   shortly-after.
+
+source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
+
+simulation_id="ext_adv"
+verbosity_level=2
+EXECUTE_TIMEOUT=10
+
+cd ${BSIM_OUT_PATH}/bin
+
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_host_adv_extended_prj_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=0 -RealEncryption=0 \
+  -testid=ext_adv_conn_advertiser -rs=23
+
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_host_adv_extended_prj_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=1 -RealEncryption=0 \
+  -testid=ext_adv_conn_scanner -rs=6
+
+Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
+  -D=2 -sim_length=10e6 $@
+
+wait_for_background_jobs

phytec/zephyr-phytec

tests/bsim/bluetooth/host/adv/extended/tests_scripts/ext_adv_conn.sh
@@ -1,0 +1,31 @@
+#!/usr/bin/env bash
+# Copyright (c) 2024 Croxel, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+# Extended advertising test:
+#
+# - Connectable: In addition to broadcasting advertisements, it is connectable
+#   and restarts advertisements once disconnected. The scanner/central scans
+#   for the packets and establishes the connection, to then disconnect
+#   shortly-after.
+
+source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
+
+simulation_id="ext_adv_conn"
+verbosity_level=2
+EXECUTE_TIMEOUT=10
+
+cd ${BSIM_OUT_PATH}/bin
+
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_host_adv_extended_prj_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=0 -RealEncryption=0 \
+  -testid=ext_adv_conn_advertiser -rs=23
+
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_host_adv_extended_prj_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=1 -RealEncryption=0 \
+  -testid=ext_adv_conn_scanner -rs=6
+
+Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
+  -D=2 -sim_length=10e6 $@
+
+wait_for_background_jobs