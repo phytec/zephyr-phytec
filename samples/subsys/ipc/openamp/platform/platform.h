zephyrproject-rtos/zephyr

sample: add OpenAMP sample application
This commit adds a sample application using OpenAMP for remote procedure
calls on the LPCXpresso54114. It is adapted from the RPMsg-Lite sample
application added in PR #5960, and uses the IPM driver to provide
interprocessor interrupts.

Instructions for how to compile the application, including OpenAMP and
libmetal is provided in the README.md file in the application's
toplevel directory.

Signed-off-by: Kristian Klomsten Skordal <kristian.skordal@nordicsemi.no>

samples/subsys/ipc/openamp/platform/platform.h
@@ -1,0 +1,25 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef PLATFORM_H__
+#define PLATFORM_H__
+
+#include <openamp/open_amp.h>
+
+#define SHM_START_ADDRESS       0x04000400
+#define SHM_SIZE                0x7c00
+#define SHM_DEVICE_NAME         "sramx.shm"
+
+#define VRING_COUNT             2
+#define VRING_RX_ADDRESS        0x04000400
+#define VRING_TX_ADDRESS        0x04000800
+#define VRING_ALIGNMENT         4
+#define VRING_SIZE              32
+
+struct hil_proc *platform_init(int role);
+
+#endif
+

samples/subsys/ipc/openamp/platform/platform_ops.c
@@ -1,0 +1,99 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <ipm.h>
+
+#include <openamp/open_amp.h>
+#include "platform.h"
+
+static K_SEM_DEFINE(data_sem, 0, 1);
+static struct device *ipm_handle = NULL;
+
+static void platform_ipm_callback(void *context, u32_t id, volatile void *data)
+{
+	k_sem_give(&data_sem);
+}
+
+static int enable_interrupt(struct proc_intr *intr)
+{
+	return ipm_set_enabled(ipm_handle, 1);
+}
+
+static void notify(struct hil_proc *proc, struct proc_intr *intr_info)
+{
+	uint32_t dummy_data = 0x12345678; /* Some data must be provided */
+
+	ipm_send(ipm_handle, 0, 0, &dummy_data, sizeof(dummy_data));
+}
+
+static int boot_cpu(struct hil_proc *proc, unsigned int load_addr)
+{
+	return -1;
+}
+
+static void shutdown_cpu(struct hil_proc *proc)
+{
+}
+
+static int poll(struct hil_proc *proc, int nonblock)
+{
+	int status = k_sem_take(&data_sem, nonblock ? K_NO_WAIT : K_FOREVER);
+
+	if (status == 0) {
+		hil_notified(proc, 0xffffffff);
+	}
+
+	return status;
+}
+
+static struct metal_io_region *alloc_shm(struct hil_proc *proc,
+					 metal_phys_addr_t physical, size_t size,
+					 struct metal_device **device)
+{
+	int status = metal_device_open("generic", SHM_DEVICE_NAME, device);
+
+	if (status != 0) {
+		return NULL;
+	}
+
+	return metal_device_io_region(*device, 0);
+}
+
+static void release_shm(struct hil_proc *proc, struct metal_device *device,
+			struct metal_io_region *io)
+{
+	metal_device_close(device);
+}
+
+static int initialize(struct hil_proc *proc)
+{
+	ipm_handle = device_get_binding("MAILBOX_0");
+	if (!ipm_handle) {
+		return -1;
+	}
+
+	ipm_register_callback(ipm_handle, platform_ipm_callback, NULL);
+	return 0;
+}
+
+static void release(struct hil_proc *proc)
+{
+	ipm_set_enabled(ipm_handle, 0);
+}
+
+struct hil_platform_ops platform_ops = {
+	.enable_interrupt = enable_interrupt,
+	.notify = notify,
+	.boot_cpu = boot_cpu,
+	.shutdown_cpu = shutdown_cpu,
+	.poll = poll,
+	.alloc_shm = alloc_shm,
+	.release_shm = release_shm,
+	.initialize = initialize,
+	.release = release
+};
+

phytec/zephyr-phytec

Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

samples/subsys/ipc/openamp/platform/platform.h
@@ -1,0 +1,25 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef PLATFORM_H__
+#define PLATFORM_H__
+
+#include <openamp/open_amp.h>
+
+#define SHM_START_ADDRESS       0x04000400
+#define SHM_SIZE                0x7c00
+#define SHM_DEVICE_NAME         "sramx.shm"
+
+#define VRING_COUNT             2
+#define VRING_RX_ADDRESS        0x04007800
+#define VRING_TX_ADDRESS        0x04007C00
+#define VRING_ALIGNMENT         4
+#define VRING_SIZE              16
+
+struct hil_proc *platform_init(int role);
+
+#endif
+

samples/subsys/ipc/openamp/platform/platform_ops.c
@@ -1,0 +1,100 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <ipm.h>
+
+#include <openamp/open_amp.h>
+#include "platform.h"
+
+static K_SEM_DEFINE(data_sem, 0, 1);
+static struct device *ipm_handle;
+
+static void platform_ipm_callback(void *context, u32_t id, volatile void *data)
+{
+	k_sem_give(&data_sem);
+}
+
+static int enable_interrupt(struct proc_intr *intr)
+{
+	return ipm_set_enabled(ipm_handle, 1);
+}
+
+static void notify(struct hil_proc *proc, struct proc_intr *intr_info)
+{
+	u32_t dummy_data = 0x12345678; /* Some data must be provided */
+
+	ipm_send(ipm_handle, 0, 0, &dummy_data, sizeof(dummy_data));
+}
+
+static int boot_cpu(struct hil_proc *proc, unsigned int load_addr)
+{
+	return -1;
+}
+
+static void shutdown_cpu(struct hil_proc *proc)
+{
+}
+
+static int poll(struct hil_proc *proc, int nonblock)
+{
+	int status = k_sem_take(&data_sem, nonblock ? K_NO_WAIT : K_FOREVER);
+
+	if (status == 0) {
+		hil_notified(proc, 0xffffffff);
+	}
+
+	return status;
+}
+
+static struct metal_io_region *alloc_shm(struct hil_proc *proc,
+					 metal_phys_addr_t physical,
+					 size_t size,
+					 struct metal_device **device)
+{
+	int status = metal_device_open("generic", SHM_DEVICE_NAME, device);
+
+	if (status != 0) {
+		return NULL;
+	}
+
+	return metal_device_io_region(*device, 0);
+}
+
+static void release_shm(struct hil_proc *proc, struct metal_device *device,
+			struct metal_io_region *io)
+{
+	metal_device_close(device);
+}
+
+static int initialize(struct hil_proc *proc)
+{
+	ipm_handle = device_get_binding("MAILBOX_0");
+	if (!ipm_handle) {
+		return -1;
+	}
+
+	ipm_register_callback(ipm_handle, platform_ipm_callback, NULL);
+	return 0;
+}
+
+static void release(struct hil_proc *proc)
+{
+	ipm_set_enabled(ipm_handle, 0);
+}
+
+struct hil_platform_ops platform_ops = {
+	.enable_interrupt = enable_interrupt,
+	.notify = notify,
+	.boot_cpu = boot_cpu,
+	.shutdown_cpu = shutdown_cpu,
+	.poll = poll,
+	.alloc_shm = alloc_shm,
+	.release_shm = release_shm,
+	.initialize = initialize,
+	.release = release
+};
+