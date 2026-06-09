tbursztyka/zephyr

drivers/flash: Switch W25QXXDV driver to new SPI API
This saves quite some memory.

Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

drivers/flash/spi_flash_w25qxxdv.h
@@ -15,9 +15,7 @@
 
 
 struct spi_flash_data {
-	struct device *spi;
-	u8_t buf[CONFIG_SPI_FLASH_W25QXXDV_MAX_DATA_LEN +
-		    W25QXXDV_LEN_CMD_ADDRESS];
+	struct spi_config spi;
 	struct k_sem sem;
 };
 

phytec/zephyr-phytec

drivers/flash/spi_flash_w25qxxdv.h
@@ -16,8 +16,7 @@
 
 struct spi_flash_data {
 	struct device *spi;
-	u8_t buf[CONFIG_SPI_FLASH_W25QXXDV_MAX_DATA_LEN +
-		    W25QXXDV_LEN_CMD_ADDRESS];
+	struct spi_config spi_cfg;
 	struct k_sem sem;
 };
 