zephyrproject-rtos/zephyr

Bluetooth: controller: split: Port openisa files
Port OpenISA LLL files to reflect some of the changes done
in Nordic LLL.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/openisa/lll/lll.c
@@ -293,6 +293,12 @@ bool lll_is_done(void *param)
 	return !event.curr.abort_cb;
 }
 
+int lll_is_abort_cb(void *next, int prio, void *curr,
+			 lll_prepare_cb_t *resume_cb, int *resume_prio)
+{
+	return -ECANCELED;
+}
+
 int lll_clk_on(void)
 {
 	int err;
@@ -404,6 +410,16 @@ void lll_chan_set(u32_t chan)
 u32_t lll_radio_is_idle(void)
 {
 	return radio_is_idle();
+}
+
+u32_t lll_radio_tx_ready_delay_get(u8_t phy, u8_t flags)
+{
+	return radio_tx_ready_delay_get(phy, flags);
+}
+
+u32_t lll_radio_rx_ready_delay_get(u8_t phy, u8_t flags)
+{
+	return radio_rx_ready_delay_get(phy, flags);
 }
 
 

subsys/bluetooth/controller/ll_sw/openisa/lll/lll_scan.h
@@ -39,9 +39,13 @@ struct lll_scan {
 	u32_t ticks_window;
 };
 
+struct lll_scan_aux {
+	struct lll_hdr hdr;
+};
+
 int lll_scan_init(void);
 int lll_scan_reset(void);
 
 void lll_scan_prepare(void *param);
 
-extern u16_t ull_scan_lll_handle_get(struct lll_scan *lll);
+extern u8_t ull_scan_lll_handle_get(struct lll_scan *lll);

phytec/zephyr-phytec

subsys/bluetooth/controller/ll_sw/openisa/lll/lll.c
@@ -293,6 +293,12 @@ bool lll_is_done(void *param)
 	return !event.curr.abort_cb;
 }
 
+int lll_is_abort_cb(void *next, int prio, void *curr,
+			 lll_prepare_cb_t *resume_cb, int *resume_prio)
+{
+	return -ECANCELED;
+}
+
 int lll_clk_on(void)
 {
 	int err;
@@ -404,6 +410,16 @@ void lll_chan_set(uint32_t chan)
 uint32_t lll_radio_is_idle(void)
 {
 	return radio_is_idle();
+}
+
+uint32_t lll_radio_tx_ready_delay_get(uint8_t phy, uint8_t flags)
+{
+	return radio_tx_ready_delay_get(phy, flags);
+}
+
+uint32_t lll_radio_rx_ready_delay_get(uint8_t phy, uint8_t flags)
+{
+	return radio_rx_ready_delay_get(phy, flags);
 }
 
 

subsys/bluetooth/controller/ll_sw/openisa/lll/lll_scan.h
@@ -39,9 +39,13 @@ struct lll_scan {
 	uint32_t ticks_window;
 };
 
+struct lll_scan_aux {
+	struct lll_hdr hdr;
+};
+
 int lll_scan_init(void);
 int lll_scan_reset(void);
 
 void lll_scan_prepare(void *param);
 
-extern uint16_t ull_scan_lll_handle_get(struct lll_scan *lll);
+extern uint8_t ull_scan_lll_handle_get(struct lll_scan *lll);