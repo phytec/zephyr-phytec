zephyrproject-rtos/zephyr

Bluetooth: controller: Fix to use advertiser random address
Fix legacy advertising started using Extended Advertising
sets to use the set advertising random address.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/ull_adv.c
@@ -555,7 +555,15 @@ u8_t ll_adv_enable(u8_t enable)
 #endif /* !CONFIG_BT_CTLR_PRIVACY */
 
 		if (!priv) {
-			u8_t *tx_addr = ll_addr_get(pdu_adv->tx_addr, NULL);
+			u8_t *tx_addr;
+#if defined(CONFIG_BT_CTLR_ADV_EXT)
+			if ((adv->is_created & BIT(1)) && pdu_adv->tx_addr) {
+				tx_addr = ll_adv_aux_random_addr_get(adv, NULL);
+			} else
+#endif /* CONFIG_BT_CTLR_ADV_EXT */
+			{
+				tx_addr = ll_addr_get(pdu_adv->tx_addr, NULL);
+			}
 
 			memcpy(&pdu_adv->adv_ind.addr[0], tx_addr,
 			       BDADDR_SIZE);

phytec/zephyr-phytec

subsys/bluetooth/controller/ll_sw/ull_adv.c
@@ -557,7 +557,15 @@ uint8_t ll_adv_enable(uint8_t enable)
 #endif /* !CONFIG_BT_CTLR_PRIVACY */
 
 		if (!priv) {
-			uint8_t *tx_addr = ll_addr_get(pdu_adv->tx_addr, NULL);
+			uint8_t *tx_addr;
+#if defined(CONFIG_BT_CTLR_ADV_EXT)
+			if ((adv->is_created & BIT(1)) && pdu_adv->tx_addr) {
+				tx_addr = ll_adv_aux_random_addr_get(adv, NULL);
+			} else
+#endif /* CONFIG_BT_CTLR_ADV_EXT */
+			{
+				tx_addr = ll_addr_get(pdu_adv->tx_addr, NULL);
+			}
 
 			memcpy(&pdu_adv->adv_ind.addr[0], tx_addr,
 			       BDADDR_SIZE);