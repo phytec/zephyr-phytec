zephyrproject-rtos/zephyr

boards: nxp: imx943_evk: enable m70 and m71
Enable m70 and m71 for imx943_evk

Signed-off-by: Biwen Li <biwen.li@nxp.com>

modules/hal_nxp/mcux/mcux-sdk-ng/device/device.cmake
@@ -52,7 +52,7 @@ if(CONFIG_SOC_SERIES_IMXRT10XX OR CONFIG_SOC_SERIES_IMXRT11XX)
   set(CONFIG_MCUX_COMPONENT_device.boot_header ON)
 endif()
 
-if(NOT CONFIG_SOC_MIMX94398_M33)
+if(NOT (CONFIG_SOC_MIMX94398_M33 OR CONFIG_SOC_MIMX94398_M70 OR CONFIG_SOC_MIMX94398_M71))
   set(CONFIG_MCUX_COMPONENT_device.system ON)
 endif()
 set(CONFIG_MCUX_COMPONENT_device.CMSIS ON)

phytec/zephyr-phytec

modules/hal_nxp/mcux/mcux-sdk-ng/device/device.cmake
@@ -54,7 +54,7 @@ if(CONFIG_SOC_SERIES_IMXRT10XX OR CONFIG_SOC_SERIES_IMXRT11XX)
   set(CONFIG_MCUX_COMPONENT_device.boot_header ON)
 endif()
 
-if(NOT CONFIG_SOC_MIMX94398_M33)
+if(NOT (CONFIG_SOC_MIMX94398_M33 OR CONFIG_SOC_MIMX94398_M7_0 OR CONFIG_SOC_MIMX94398_M7_1))
   set(CONFIG_MCUX_COMPONENT_device.system ON)
 endif()
 set(CONFIG_MCUX_COMPONENT_device.CMSIS ON)