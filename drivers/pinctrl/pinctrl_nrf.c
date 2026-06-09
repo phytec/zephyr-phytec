karstenkoenig/sdk-zephyr

drivers: pinctrl_nrf: Add coresight tpiu pins
Pinctrl needs to set the needed drive and direction of the pins. Also
this later allows automatically setting the clock bit for the traceclk
pin.

Signed-off-by: Karsten Koenig <karsten.koenig@nordicsemi.no>

drivers/pinctrl/pinctrl_nrf.c
@@ -582,6 +582,17 @@ int pinctrl_configure_pins(const pinctrl_soc_pin_t *pins, uint8_t pin_cnt,
 			input = NRF_GPIO_PIN_INPUT_CONNECT;
 			break;
 #endif /* defined(NRF_PSEL_SDP_MSPI) */
+#if DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_coresight)
+		/* Pin routing is controlled by secure domain, via UICR */
+		case NRF_FUN_TPIU_CLOCK:
+		case NRF_FUN_TPIU_DATA0:
+		case NRF_FUN_TPIU_DATA1:
+		case NRF_FUN_TPIU_DATA2:
+		case NRF_FUN_TPIU_DATA3:
+			dir = NRF_GPIO_PIN_DIR_OUTPUT;
+			input = NRF_GPIO_PIN_INPUT_DISCONNECT;
+			break;
+#endif /* DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_coresight) */
 		default:
 			return -ENOTSUP;
 		}

include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h
@@ -242,6 +242,16 @@
 #define NRF_FUN_TDM_SDOUT        76U
 /** TDM MCK */
 #define NRF_FUN_TDM_MCK          77U
+/** TPIU CLOCK */
+#define NRF_FUN_TPIU_CLOCK       78U
+/** TPIU DATA0 */
+#define NRF_FUN_TPIU_DATA0       79U
+/** TPIU DATA1 */
+#define NRF_FUN_TPIU_DATA1       80U
+/** TPIU DATA2 */
+#define NRF_FUN_TPIU_DATA2       81U
+/** TPIU DATA3 */
+#define NRF_FUN_TPIU_DATA3       82U
 
 /** @} */
 

phytec/zephyr-phytec

drivers/pinctrl/pinctrl_nrf.c
@@ -557,6 +557,17 @@ int pinctrl_configure_pins(const pinctrl_soc_pin_t *pins, uint8_t pin_cnt,
 			input = NRF_GPIO_PIN_INPUT_CONNECT;
 			break;
 #endif /* defined(NRF_PSEL_TWIS) */
+#if DT_HAS_COMPAT_STATUS_OKAY(nordic_coresight_nrf)
+		/* Pin routing is controlled by secure domain, via UICR */
+		case NRF_FUN_TPIU_CLOCK:
+		case NRF_FUN_TPIU_DATA0:
+		case NRF_FUN_TPIU_DATA1:
+		case NRF_FUN_TPIU_DATA2:
+		case NRF_FUN_TPIU_DATA3:
+			dir = NRF_GPIO_PIN_DIR_OUTPUT;
+			input = NRF_GPIO_PIN_INPUT_DISCONNECT;
+			break;
+#endif /* DT_HAS_COMPAT_STATUS_OKAY(nordic_coresight_nrf) */
 		default:
 			return -ENOTSUP;
 		}

include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h
@@ -187,7 +187,17 @@
 /** TDM MCK */
 #define NRF_FUN_TDM_MCK          77U
 /** SPI master CSN */
-#define NRF_FUN_SPIM_CSN 78U
+#define NRF_FUN_SPIM_CSN         78U
+/** TPIU CLOCK */
+#define NRF_FUN_TPIU_CLOCK       79U
+/** TPIU DATA0 */
+#define NRF_FUN_TPIU_DATA0       80U
+/** TPIU DATA1 */
+#define NRF_FUN_TPIU_DATA1       81U
+/** TPIU DATA2 */
+#define NRF_FUN_TPIU_DATA2       82U
+/** TPIU DATA3 */
+#define NRF_FUN_TPIU_DATA3       83U
 
 /** @} */
 