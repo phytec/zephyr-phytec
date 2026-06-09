zephyrproject-rtos/zephyr

ztest: Fix userspace ztests in new API
Update the new API to use K_USER as the flags for both
CONFIG_USERSPACE and CONFIG_TEST_USERSPACE. Also, fix the linker
script to properly include the suites, tests, and rules.

Fixes #44108

Signed-off-by: Yuval Peress <peress@google.com>

subsys/testsuite/ztest/include/ztest_test_new.h
@@ -17,6 +17,12 @@
 #include <init.h>
 #include <stdbool.h>
 
+#if defined(CONFIG_USERSPACE) || defined(CONFIG_TEST_USERSPACE)
+#define __USERSPACE_FLAGS (K_USER)
+#else
+#define __USERSPACE_FLAGS (0)
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -49,31 +55,31 @@ struct ztest_suite_stats {
  */
 struct ztest_suite_node {
 	/** The name of the test suite. */
-	const char *name;
+	const char * const name;
 	/**
 	 * Setup function to run before running this suite
 	 *
 	 * @return Pointer to the data structure that will be used throughout this test suite
 	 */
-	void *(*setup)(void);
+	void *(*const setup)(void);
 	/**
 	 * Function to run before each test in this suite
 	 *
 	 * @param data The test suite's data returned from setup()
 	 */
-	void (*before)(void *data);
+	void (*const before)(void *data);
 	/**
 	 * Function to run after each test in this suite
 	 *
 	 * @param data The test suite's data returned from setup()
 	 */
-	void (*after)(void *data);
+	void (*const after)(void *data);
 	/**
 	 * Teardown function to run after running this suite
 	 *
 	 * @param data The test suite's data returned from setup()
 	 */
-	void (*teardown)(void *data);
+	void (*const teardown)(void *data);
 	/**
 	 * An optional predicate function to determine if the test should run. If NULL, then the
 	 * test will only run once on the first attempt.
@@ -81,9 +87,9 @@ struct ztest_suite_node {
 	 * @param state The current state of the test application.
 	 * @return True if the suite should be run; false to skip.
 	 */
-	bool (*predicate)(const void *state);
+	bool (*const predicate)(const void *state);
 	/** Stats */
-	struct ztest_suite_stats stats;
+	struct ztest_suite_stats * const stats;
 };
 
 extern struct ztest_suite_node _ztest_suite_node_list_start[];
@@ -103,17 +109,18 @@ extern struct ztest_suite_node _ztest_suite_node_list_end[];
  * @param after_fn The function to call after each unit test in this suite
  * @param teardown_fn The function to call after running all the tests in this suite
  */
-#define ZTEST_SUITE(SUITE_NAME, PREDICATE, setup_fn, before_fn, after_fn, teardown_fn)	\
-	static STRUCT_SECTION_ITERABLE(ztest_suite_node,				\
-				       UTIL_CAT(z_ztest_test_node_, SUITE_NAME)) = {	\
-		.name = STRINGIFY(SUITE_NAME),						\
-		.setup = (setup_fn),							\
-		.before = (before_fn),							\
-		.after = (after_fn),							\
-		.teardown = (teardown_fn),						\
-		.predicate = PREDICATE,							\
+#define ZTEST_SUITE(SUITE_NAME, PREDICATE, setup_fn, before_fn, after_fn, teardown_fn)  \
+	static struct ztest_suite_stats UTIL_CAT(z_ztest_test_node_stats_, SUITE_NAME); \
+	static const STRUCT_SECTION_ITERABLE(ztest_suite_node,				\
+				       UTIL_CAT(z_ztest_test_node_, SUITE_NAME)) = {    \
+		.name = STRINGIFY(SUITE_NAME),                                          \
+		.setup = (setup_fn),                                                    \
+		.before = (before_fn),                                                  \
+		.after = (after_fn),                                                    \
+		.teardown = (teardown_fn),                                              \
+		.predicate = PREDICATE,                                                 \
+		.stats = &UTIL_CAT(z_ztest_test_node_stats_, SUITE_NAME),               \
 	}
-
 /**
  * Run the registered unit tests which return true from their pragma function.
  *
@@ -230,7 +237,7 @@ static inline void unit_test_noop(void)
  * @param suite The name of the test suite to attach this test
  * @param fn The test function to call.
  */
-#define ZTEST_USER(suite, fn) Z_ZTEST(suite, fn, COND_CODE_1(CONFIG_USERSPACE, (K_USER), (0)))
+#define ZTEST_USER(suite, fn) Z_ZTEST(suite, fn, __USERSPACE_FLAGS)
 
 /**
  * @brief Define a test function
@@ -252,7 +259,7 @@ static inline void unit_test_noop(void)
  * @param suite The name of the test suite to attach this test
  * @param fn The test function to call.
  */
-#define ZTEST_USER_F(suite, fn) Z_ZTEST_F(suite, fn, COND_CODE_1(CONFIG_USERSPACE, (K_USER), (0)))
+#define ZTEST_USER_F(suite, fn) Z_ZTEST_F(suite, fn, __USERSPACE_FLAGS)
 
 /**
  * @brief Test rule callback function signature

phytec/zephyr-phytec

subsys/testsuite/ztest/include/ztest_test_new.h
@@ -17,6 +17,12 @@
 #include <zephyr/init.h>
 #include <stdbool.h>
 
+#if defined(CONFIG_USERSPACE)
+#define __USERSPACE_FLAGS (K_USER)
+#else
+#define __USERSPACE_FLAGS (0)
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -30,6 +36,7 @@ struct ztest_unit_test {
 
 extern struct ztest_unit_test _ztest_unit_test_list_start[];
 extern struct ztest_unit_test _ztest_unit_test_list_end[];
+#define ZTEST_TEST_COUNT (_ztest_unit_test_list_end - _ztest_unit_test_list_start)
 
 /**
  * Stats about a ztest suite
@@ -49,31 +56,31 @@ struct ztest_suite_stats {
  */
 struct ztest_suite_node {
 	/** The name of the test suite. */
-	const char *name;
+	const char * const name;
 	/**
 	 * Setup function to run before running this suite
 	 *
 	 * @return Pointer to the data structure that will be used throughout this test suite
 	 */
-	void *(*setup)(void);
+	void *(*const setup)(void);
 	/**
 	 * Function to run before each test in this suite
 	 *
 	 * @param data The test suite's data returned from setup()
 	 */
-	void (*before)(void *data);
+	void (*const before)(void *data);
 	/**
 	 * Function to run after each test in this suite
 	 *
 	 * @param data The test suite's data returned from setup()
 	 */
-	void (*after)(void *data);
+	void (*const after)(void *data);
 	/**
 	 * Teardown function to run after running this suite
 	 *
 	 * @param data The test suite's data returned from setup()
 	 */
-	void (*teardown)(void *data);
+	void (*const teardown)(void *data);
 	/**
 	 * An optional predicate function to determine if the test should run. If NULL, then the
 	 * test will only run once on the first attempt.
@@ -81,13 +88,15 @@ struct ztest_suite_node {
 	 * @param state The current state of the test application.
 	 * @return True if the suite should be run; false to skip.
 	 */
-	bool (*predicate)(const void *state);
+	bool (*const predicate)(const void *state);
 	/** Stats */
-	struct ztest_suite_stats stats;
+	struct ztest_suite_stats * const stats;
 };
 
 extern struct ztest_suite_node _ztest_suite_node_list_start[];
 extern struct ztest_suite_node _ztest_suite_node_list_end[];
+#define ZTEST_SUITE_COUNT (_ztest_suite_node_list_end - _ztest_suite_node_list_start)
+
 
 /**
  * Create and register a ztest suite. Using this macro creates a new test suite (using
@@ -103,24 +112,27 @@ extern struct ztest_suite_node _ztest_suite_node_list_end[];
  * @param after_fn The function to call after each unit test in this suite
  * @param teardown_fn The function to call after running all the tests in this suite
  */
-#define ZTEST_SUITE(SUITE_NAME, PREDICATE, setup_fn, before_fn, after_fn, teardown_fn)	\
-	static STRUCT_SECTION_ITERABLE(ztest_suite_node,				\
-				       UTIL_CAT(z_ztest_test_node_, SUITE_NAME)) = {	\
-		.name = STRINGIFY(SUITE_NAME),						\
-		.setup = (setup_fn),							\
-		.before = (before_fn),							\
-		.after = (after_fn),							\
-		.teardown = (teardown_fn),						\
-		.predicate = PREDICATE,							\
+#define ZTEST_SUITE(SUITE_NAME, PREDICATE, setup_fn, before_fn, after_fn, teardown_fn)  \
+	struct ztest_suite_stats UTIL_CAT(z_ztest_test_node_stats_, SUITE_NAME);        \
+	static const STRUCT_SECTION_ITERABLE(ztest_suite_node,				\
+				       UTIL_CAT(z_ztest_test_node_, SUITE_NAME)) = {    \
+		.name = STRINGIFY(SUITE_NAME),                                          \
+		.setup = (setup_fn),                                                    \
+		.before = (before_fn),                                                  \
+		.after = (after_fn),                                                    \
+		.teardown = (teardown_fn),                                              \
+		.predicate = PREDICATE,                                                 \
+		.stats = &UTIL_CAT(z_ztest_test_node_stats_, SUITE_NAME),               \
 	}
-
 /**
  * Run the registered unit tests which return true from their pragma function.
  *
  * @param state The current state of the machine as it relates to the test executable.
  * @return The number of tests that ran.
  */
-int ztest_run_test_suites(const void *state);
+__syscall int ztest_run_test_suites(const void *state);
+
+#include <syscalls/ztest_test_new.h>
 
 /**
  * @brief Fails the test if any of the registered tests did not run.
@@ -240,7 +252,7 @@ static inline void unit_test_noop(void)
  * @param suite The name of the test suite to attach this test
  * @param fn The test function to call.
  */
-#define ZTEST_USER(suite, fn) Z_ZTEST(suite, fn, COND_CODE_1(CONFIG_USERSPACE, (K_USER), (0)))
+#define ZTEST_USER(suite, fn) Z_ZTEST(suite, fn, K_USER)
 
 /**
  * @brief Define a test function
@@ -262,7 +274,7 @@ static inline void unit_test_noop(void)
  * @param suite The name of the test suite to attach this test
  * @param fn The test function to call.
  */
-#define ZTEST_USER_F(suite, fn) Z_ZTEST_F(suite, fn, COND_CODE_1(CONFIG_USERSPACE, (K_USER), (0)))
+#define ZTEST_USER_F(suite, fn) Z_ZTEST_F(suite, fn, K_USER)
 
 /**
  * @brief Test rule callback function signature
@@ -351,5 +363,7 @@ extern struct k_mem_partition ztest_mem_partition;
 #ifdef __cplusplus
 }
 #endif
+
+#include <syscalls/ztest_test_new.h>
 
 #endif /* ZEPHYR_TESTSUITE_ZTEST_TEST_H_ */