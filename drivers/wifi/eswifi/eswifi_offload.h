loicpoulain/zephyr

driver: wifi: Add inventek es-WiFi driver
es-WiFi compatible modules use IWIN AT command set.
This driver is split into several layers:
- bus layer: interface to transmit AT commands (SPI, USB, UART...)
- core layer: es-WiFi module management (state, scan...)
- offload layer: TCP/IP offload operations (connect, listen...)

This driver has been tested with stm32l4 disco iot board
(disco_l475_iot1) and the wifi sample:

$ select wifi
$ scan
$ connect "CISCO" 5 password
$ select net
$ tcp connect 192.168.1.21 4242
$ tcp send HelloWorld!

Signed-off-by: Loic Poulain <loic.poulain@linaro.org>

drivers/wifi/eswifi/eswifi_offload.h
@@ -1,0 +1,37 @@
+/**
+ * Copyright (c) 2018 Linaro
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_WIFI_ESWIFI_ESWIFI_OFFLOAD_H_
+#define ZEPHYR_DRIVERS_WIFI_ESWIFI_ESWIFI_OFFLOAD_H_
+
+#include <net/net_offload.h>
+
+#include "eswifi.h"
+
+#define ESWIFI_OFFLOAD_MAX_SOCKETS 4
+
+enum eswifi_transport_type {
+	ESWIFI_TRANSPORT_TCP,
+	ESWIFI_TRANSPORT_UDP,
+	ESWIFI_TRANSPORT_UDP_LITE,
+	ESWIFI_TRANSPORT_TCP_SSL,
+};
+
+enum eswifi_socket_state {
+	ESWIFI_SOCKET_STATE_NONE,
+	ESWIFI_SOCKET_STATE_CONNECTED,
+};
+
+struct eswifi_off_socket {
+	uint8_t index;
+	enum eswifi_transport_type type;
+	enum eswifi_socket_state state;
+	struct net_context *context;
+	net_context_recv_cb_t recv_cb;
+	void *user_data;
+};
+
+#endif

phytec/zephyr-phytec

drivers/wifi/eswifi/eswifi_offload.h
@@ -1,0 +1,37 @@
+/**
+ * Copyright (c) 2018 Linaro
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_WIFI_ESWIFI_ESWIFI_OFFLOAD_H_
+#define ZEPHYR_DRIVERS_WIFI_ESWIFI_ESWIFI_OFFLOAD_H_
+
+#include <net/net_offload.h>
+
+#include "eswifi.h"
+
+#define ESWIFI_OFFLOAD_MAX_SOCKETS 4
+
+enum eswifi_transport_type {
+	ESWIFI_TRANSPORT_TCP,
+	ESWIFI_TRANSPORT_UDP,
+	ESWIFI_TRANSPORT_UDP_LITE,
+	ESWIFI_TRANSPORT_TCP_SSL,
+};
+
+enum eswifi_socket_state {
+	ESWIFI_SOCKET_STATE_NONE,
+	ESWIFI_SOCKET_STATE_CONNECTED,
+};
+
+struct eswifi_off_socket {
+	u8_t index;
+	enum eswifi_transport_type type;
+	enum eswifi_socket_state state;
+	struct net_context *context;
+	net_context_recv_cb_t recv_cb;
+	void *user_data;
+};
+
+#endif