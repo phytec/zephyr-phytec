zephyrproject-rtos/zephyr

Bluetooth: Host: More bsim refactoring
Commit does the following changes:
* Use functionality from the `babbelkit` library for common functions
  related to flags, test progression (failing, passing etc.) and
  synchronization between two devices. Locally defined equivalents are
  removed.
* Removes the files containing only functionality that is provided
  by the `babblekit` library.
* Remove the `test_pre_init_f` and `test_tick_f` functions (commonly
  implemented as `test_init` and `test_tick`) from the modified tests.
  These functions are not needed as they were only used to fail the test
  if a device didn't complete the test within a certain time frame. This
  is already handled by the `sim_length` argument used in the test
  scripts.

Signed-off-by: Håvard Reierstad <haavard.reierstad@nordicsemi.no>

tests/bsim/bluetooth/host/att/read_fill_buf/server/main.c
@@ -10,7 +10,7 @@
 
 #include <testlib/adv.h>
 
-#include "../bs_macro.h"
+#include "babblekit/testcase.h"
 #include "../common_defs.h"
 
 LOG_MODULE_REGISTER(server, LOG_LEVEL_DBG);
@@ -34,7 +34,7 @@ BT_GATT_SERVICE_DEFINE(long_attr_svc, BT_GATT_PRIMARY_SERVICE(MTU_VALIDATION_SVC
 					      BT_GATT_PERM_READ, read_mtu_validation_chrc, NULL,
 					      NULL));
 
-void the_test(void)
+static void test_srv_main(void)
 {
 	int err;
 
@@ -48,5 +48,27 @@ void the_test(void)
 	err = bt_testlib_adv_conn(NULL, BT_ID_DEFAULT, bt_get_name());
 	__ASSERT_NO_MSG(!err);
 
-	PASS("PASS\n");
+	TEST_PASS("TEST_PASS");
+}
+
+static const struct bst_test_instance test_def[] = {
+	{
+		.test_id = "srv",
+		.test_main_f = test_srv_main,
+	},
+	BSTEST_END_MARKER,
+};
+
+static struct bst_test_list *install(struct bst_test_list *tests)
+{
+	return bst_add_tests(tests, test_def);
+};
+
+bst_test_install_t test_installers[] = {install, NULL};
+
+int main(void)
+{
+	bst_main();
+
+	return 0;
 }

tests/bsim/bluetooth/host/att/retry_on_sec_err/client/main.c
@@ -11,15 +11,17 @@
 #include <zephyr/logging/log.h>
 
 #include "../common_defs.h"
-#include "../test_utils.h"
 
 #include <testlib/conn.h>
 #include "testlib/scan.h"
 #include "testlib/security.h"
 
+#include "babblekit/testcase.h"
+#include "babblekit/flags.h"
+
 LOG_MODULE_REGISTER(client, LOG_LEVEL_DBG);
 
-DEFINE_FLAG(flag_attr_read_success);
+DEFINE_FLAG_STATIC(flag_attr_read_success);
 
 static uint8_t gatt_attr_read_cb(struct bt_conn *conn, uint8_t att_err,
 				 struct bt_gatt_read_params *params, const void *data, uint16_t len)
@@ -96,7 +98,7 @@ static void test_client(void)
 	bt_conn_unref(conn);
 	conn = NULL;
 
-	PASS("PASS\n");
+	TEST_PASS("TEST_PASS");
 }
 
 DEFINE_FLAG(flag_pairing_in_progress);
@@ -158,20 +160,16 @@ static void test_client_security_request(void)
 	bt_conn_unref(conn);
 	conn = NULL;
 
-	PASS("PASS\n");
+	TEST_PASS("TEST_PASS");
 }
 
 static const struct bst_test_instance client_tests[] = {
 	{
 		.test_id = "test_client",
-		.test_pre_init_f = test_init,
-		.test_tick_f = test_tick,
 		.test_main_f = test_client,
 	},
 	{
 		.test_id = "test_client_security_request",
-		.test_pre_init_f = test_init,
-		.test_tick_f = test_tick,
 		.test_main_f = test_client_security_request,
 	},
 	BSTEST_END_MARKER,

tests/bsim/bluetooth/host/att/retry_on_sec_err/server/main.c
@@ -12,8 +12,10 @@
 #include "testlib/adv.h"
 #include "testlib/security.h"
 
+#include "babblekit/testcase.h"
+
 #include "../common_defs.h"
-#include "../test_utils.h"
+
 
 LOG_MODULE_REGISTER(server, LOG_LEVEL_DBG);
 
@@ -47,7 +49,7 @@ static void test_server(void)
 {
 	test_common(NULL);
 
-	PASS("PASS\n");
+	TEST_PASS("TEST_PASS");
 }
 
 static void test_server_security_request(void)
@@ -60,20 +62,16 @@ static void test_server_security_request(void)
 	err = bt_testlib_secure(conn, BT_SECURITY_L2);
 	__ASSERT(!err, "err %d", err);
 
-	PASS("PASS\n");
+	TEST_PASS("TEST_PASS");
 }
 
 static const struct bst_test_instance server_tests[] = {
 	{
 		.test_id = "test_server",
-		.test_pre_init_f = test_init,
-		.test_tick_f = test_tick,
 		.test_main_f = test_server,
 	},
 	{
 		.test_id = "test_server_security_request",
-		.test_pre_init_f = test_init,
-		.test_tick_f = test_tick,
 		.test_main_f = test_server_security_request,
 	},
 	BSTEST_END_MARKER,

tests/bsim/bluetooth/host/security/bond_per_connection/src/central.c
@@ -12,6 +12,9 @@
 
 #include <zephyr/bluetooth/bluetooth.h>
 
+#include "babblekit/testcase.h"
+#include "babblekit/flags.h"
+
 void central(void)
 {
 	bs_bt_utils_setup();
@@ -51,5 +54,5 @@ void central(void)
 	wait_disconnected();
 	clear_g_conn();
 
-	PASS("PASS\n");
+	TEST_PASS("TEST_PASS");
 }

tests/bsim/bluetooth/host/security/bond_per_connection/src/peripheral.c
@@ -13,6 +13,9 @@
 #include <stdint.h>
 #include <string.h>
 
+#include "babblekit/testcase.h"
+#include "babblekit/flags.h"
+
 void peripheral(void)
 {
 	bs_bt_utils_setup();
@@ -21,10 +24,10 @@ void peripheral(void)
 	int id_b;
 
 	id_a = bt_id_create(NULL, NULL);
-	ASSERT(id_a >= 0, "bt_id_create id_a failed (err %d)\n", id_a);
+	TEST_ASSERT(id_a >= 0, "bt_id_create id_a failed (err %d)", id_a);
 
 	id_b = bt_id_create(NULL, NULL);
-	ASSERT(id_b >= 0, "bt_id_create id_b failed (err %d)\n", id_b);
+	TEST_ASSERT(id_b >= 0, "bt_id_create id_b failed (err %d)", id_b);
 
 	printk("== Bonding id a - global bondable mode ==\n");
 	BUILD_ASSERT(CONFIG_BT_BONDABLE, "CONFIG_BT_BONDABLE must be enabled by default.");
@@ -58,5 +61,5 @@ void peripheral(void)
 	TAKE_FLAG(flag_pairing_complete);
 	TAKE_FLAG(flag_not_bonded);
 
-	PASS("PASS\n");
+	TEST_PASS("TEST_PASS");
 }

tests/bsim/bluetooth/host/security/id_addr_update/central/src/central.c
@@ -13,6 +13,8 @@
 
 #include <zephyr/bluetooth/bluetooth.h>
 
+#include "babblekit/testcase.h"
+
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(central, LOG_LEVEL_INF);
 
@@ -60,14 +62,12 @@ void central(void)
 	wait_disconnected();
 	clear_conn(conn_b);
 
-	PASS("PASS\n");
+	TEST_PASS("TEST_PASS");
 }
 
 static const struct bst_test_instance test_to_add[] = {
 	{
 		.test_id = "central",
-		.test_pre_init_f = test_init,
-		.test_tick_f = test_tick,
 		.test_main_f = central,
 	},
 	BSTEST_END_MARKER,

tests/bsim/bluetooth/host/security/id_addr_update/peripheral/src/peripheral.c
@@ -14,6 +14,8 @@
 #include <stdint.h>
 #include <string.h>
 
+#include "babblekit/testcase.h"
+
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(peripheral, LOG_LEVEL_INF);
 
@@ -24,12 +26,12 @@ static void verify_equal_address(struct bt_conn *conn_a, struct bt_conn *conn_b)
 	struct bt_conn_info info_b;
 
 	err = bt_conn_get_info(conn_a, &info_a);
-	ASSERT(!err, "Unexpected info_a result.");
+	TEST_ASSERT(!err, "Unexpected info_a result.");
 
 	err = bt_conn_get_info(conn_b, &info_b);
-	ASSERT(!err, "Unexpected info_b result.");
+	TEST_ASSERT(!err, "Unexpected info_b result.");
 
-	ASSERT(bt_addr_le_eq(info_a.le.dst, info_b.le.dst),
+	TEST_ASSERT(bt_addr_le_eq(info_a.le.dst, info_b.le.dst),
 	       "Conn A address is not equal with the conn B address");
 }
 
@@ -45,10 +47,10 @@ void peripheral(void)
 
 	/* Create two identities that will simultaneously connect with the same central peer. */
 	id_a = bt_id_create(NULL, NULL);
-	ASSERT(id_a >= 0, "bt_id_create id_a failed (err %d)\n", id_a);
+	TEST_ASSERT(id_a >= 0, "bt_id_create id_a failed (err %d)", id_a);
 
 	id_b = bt_id_create(NULL, NULL);
-	ASSERT(id_b >= 0, "bt_id_create id_b failed (err %d)\n", id_b);
+	TEST_ASSERT(id_b >= 0, "bt_id_create id_b failed (err %d)", id_b);
 
 	/* Connect with the first identity. */
 	LOG_INF("adv");
@@ -82,14 +84,12 @@ void peripheral(void)
 	wait_disconnected();
 	clear_conn(conn_b);
 
-	PASS("PASS\n");
+	TEST_PASS("TEST_PASS");
 }
 
 static const struct bst_test_instance test_to_add[] = {
 	{
 		.test_id = "peripheral",
-		.test_pre_init_f = test_init,
-		.test_tick_f = test_tick,
 		.test_main_f = peripheral,
 	},
 	BSTEST_END_MARKER,

tests/bsim/bluetooth/host/security/security_changed_callback/src/peripheral.c
@@ -13,6 +13,7 @@
 #include <zephyr/bluetooth/bluetooth.h>
 
 #include "bs_bt_utils.h"
+#include "babblekit/testcase.h"
 
 LOG_MODULE_REGISTER(test_peripheral, LOG_LEVEL_DBG);
 
@@ -20,7 +21,7 @@ BUILD_ASSERT(CONFIG_BT_BONDABLE, "CONFIG_BT_BONDABLE must be enabled by default.
 
 static void pairing_complete_unpair(struct bt_conn *conn, bool bonded)
 {
-	FAIL("Pairing succeed\n");
+	TEST_FAIL("Pairing succeed");
 }
 
 static void peripheral_security_changed_unpair(struct bt_conn *conn,
@@ -51,7 +52,7 @@ void peripheral_unpair_in_sec_cb(void)
 
 	bt_conn_cb_register(&peripheral_cb);
 	err = bt_conn_auth_info_cb_register(&peripheral_auth_info_cb);
-	ASSERT(!err, "bt_conn_auth_info_cb_register failed.\n");
+	TEST_ASSERT(!err, "bt_conn_auth_info_cb_register failed.");
 
 	advertise_connectable(BT_ID_DEFAULT, NULL);
 	wait_connected();
@@ -60,12 +61,12 @@ void peripheral_unpair_in_sec_cb(void)
 
 	clear_g_conn();
 
-	PASS("PASS\n");
+	TEST_PASS("TEST_PASS");
 }
 
 static void pairing_failed_disconnect(struct bt_conn *conn, enum bt_security_err err)
 {
-	FAIL("Pairing failed\n");
+	TEST_FAIL("Pairing failed");
 }
 
 static void peripheral_security_changed_disconnect(struct bt_conn *conn,
@@ -95,7 +96,7 @@ void peripheral_disconnect_in_sec_cb(void)
 
 	bt_conn_cb_register(&peripheral_cb);
 	err = bt_conn_auth_info_cb_register(&peripheral_auth_info_cb);
-	ASSERT(!err, "bt_conn_auth_info_cb_register failed.\n");
+	TEST_ASSERT(!err, "bt_conn_auth_info_cb_register failed.");
 
 	advertise_connectable(BT_ID_DEFAULT, NULL);
 	wait_connected();
@@ -104,5 +105,5 @@ void peripheral_disconnect_in_sec_cb(void)
 
 	clear_g_conn();
 
-	PASS("PASS\n");
+	TEST_PASS("TEST_PASS");
 }

phytec/zephyr-phytec

tests/bsim/bluetooth/host/att/read_fill_buf/server/main.c
@@ -10,7 +10,7 @@
 
 #include <testlib/adv.h>
 
-#include "../bs_macro.h"
+#include "babblekit/testcase.h"
 #include "../common_defs.h"
 
 LOG_MODULE_REGISTER(server, LOG_LEVEL_DBG);
@@ -34,7 +34,7 @@ BT_GATT_SERVICE_DEFINE(long_attr_svc, BT_GATT_PRIMARY_SERVICE(MTU_VALIDATION_SVC
 					      BT_GATT_PERM_READ, read_mtu_validation_chrc, NULL,
 					      NULL));
 
-void the_test(void)
+static void test_srv_main(void)
 {
 	int err;
 
@@ -48,5 +48,27 @@ void the_test(void)
 	err = bt_testlib_adv_conn(NULL, BT_ID_DEFAULT, bt_get_name());
 	__ASSERT_NO_MSG(!err);
 
-	PASS("PASS\n");
+	TEST_PASS("PASS");
+}
+
+static const struct bst_test_instance test_def[] = {
+	{
+		.test_id = "srv",
+		.test_main_f = test_srv_main,
+	},
+	BSTEST_END_MARKER,
+};
+
+static struct bst_test_list *install(struct bst_test_list *tests)
+{
+	return bst_add_tests(tests, test_def);
+};
+
+bst_test_install_t test_installers[] = {install, NULL};
+
+int main(void)
+{
+	bst_main();
+
+	return 0;
 }

tests/bsim/bluetooth/host/att/retry_on_sec_err/client/main.c
@@ -11,15 +11,17 @@
 #include <zephyr/logging/log.h>
 
 #include "../common_defs.h"
-#include "../test_utils.h"
 
 #include <testlib/conn.h>
 #include "testlib/scan.h"
 #include "testlib/security.h"
 
+#include "babblekit/testcase.h"
+#include "babblekit/flags.h"
+
 LOG_MODULE_REGISTER(client, LOG_LEVEL_DBG);
 
-DEFINE_FLAG(flag_attr_read_success);
+DEFINE_FLAG_STATIC(flag_attr_read_success);
 
 static uint8_t gatt_attr_read_cb(struct bt_conn *conn, uint8_t att_err,
 				 struct bt_gatt_read_params *params, const void *data, uint16_t len)
@@ -96,7 +98,7 @@ static void test_client(void)
 	bt_conn_unref(conn);
 	conn = NULL;
 
-	PASS("PASS\n");
+	TEST_PASS("PASS");
 }
 
 DEFINE_FLAG(flag_pairing_in_progress);
@@ -158,20 +160,16 @@ static void test_client_security_request(void)
 	bt_conn_unref(conn);
 	conn = NULL;
 
-	PASS("PASS\n");
+	TEST_PASS("PASS");
 }
 
 static const struct bst_test_instance client_tests[] = {
 	{
 		.test_id = "test_client",
-		.test_pre_init_f = test_init,
-		.test_tick_f = test_tick,
 		.test_main_f = test_client,
 	},
 	{
 		.test_id = "test_client_security_request",
-		.test_pre_init_f = test_init,
-		.test_tick_f = test_tick,
 		.test_main_f = test_client_security_request,
 	},
 	BSTEST_END_MARKER,

tests/bsim/bluetooth/host/att/retry_on_sec_err/server/main.c
@@ -12,8 +12,10 @@
 #include "testlib/adv.h"
 #include "testlib/security.h"
 
+#include "babblekit/testcase.h"
+
 #include "../common_defs.h"
-#include "../test_utils.h"
+
 
 LOG_MODULE_REGISTER(server, LOG_LEVEL_DBG);
 
@@ -47,7 +49,7 @@ static void test_server(void)
 {
 	test_common(NULL);
 
-	PASS("PASS\n");
+	TEST_PASS("PASS");
 }
 
 static void test_server_security_request(void)
@@ -60,20 +62,16 @@ static void test_server_security_request(void)
 	err = bt_testlib_secure(conn, BT_SECURITY_L2);
 	__ASSERT(!err, "err %d", err);
 
-	PASS("PASS\n");
+	TEST_PASS("PASS");
 }
 
 static const struct bst_test_instance server_tests[] = {
 	{
 		.test_id = "test_server",
-		.test_pre_init_f = test_init,
-		.test_tick_f = test_tick,
 		.test_main_f = test_server,
 	},
 	{
 		.test_id = "test_server_security_request",
-		.test_pre_init_f = test_init,
-		.test_tick_f = test_tick,
 		.test_main_f = test_server_security_request,
 	},
 	BSTEST_END_MARKER,

tests/bsim/bluetooth/host/security/bond_per_connection/src/central.c
@@ -12,6 +12,9 @@
 
 #include <zephyr/bluetooth/bluetooth.h>
 
+#include "babblekit/testcase.h"
+#include "babblekit/flags.h"
+
 void central(void)
 {
 	bs_bt_utils_setup();
@@ -51,5 +54,5 @@ void central(void)
 	wait_disconnected();
 	clear_g_conn();
 
-	PASS("PASS\n");
+	TEST_PASS("PASS");
 }

tests/bsim/bluetooth/host/security/bond_per_connection/src/peripheral.c
@@ -13,6 +13,9 @@
 #include <stdint.h>
 #include <string.h>
 
+#include "babblekit/testcase.h"
+#include "babblekit/flags.h"
+
 void peripheral(void)
 {
 	bs_bt_utils_setup();
@@ -21,10 +24,10 @@ void peripheral(void)
 	int id_b;
 
 	id_a = bt_id_create(NULL, NULL);
-	ASSERT(id_a >= 0, "bt_id_create id_a failed (err %d)\n", id_a);
+	TEST_ASSERT(id_a >= 0, "bt_id_create id_a failed (err %d)", id_a);
 
 	id_b = bt_id_create(NULL, NULL);
-	ASSERT(id_b >= 0, "bt_id_create id_b failed (err %d)\n", id_b);
+	TEST_ASSERT(id_b >= 0, "bt_id_create id_b failed (err %d)", id_b);
 
 	printk("== Bonding id a - global bondable mode ==\n");
 	BUILD_ASSERT(CONFIG_BT_BONDABLE, "CONFIG_BT_BONDABLE must be enabled by default.");
@@ -58,5 +61,5 @@ void peripheral(void)
 	TAKE_FLAG(flag_pairing_complete);
 	TAKE_FLAG(flag_not_bonded);
 
-	PASS("PASS\n");
+	TEST_PASS("PASS");
 }

tests/bsim/bluetooth/host/security/id_addr_update/central/src/central.c
@@ -13,6 +13,8 @@
 
 #include <zephyr/bluetooth/bluetooth.h>
 
+#include "babblekit/testcase.h"
+
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(central, LOG_LEVEL_INF);
 
@@ -60,14 +62,12 @@ void central(void)
 	wait_disconnected();
 	clear_conn(conn_b);
 
-	PASS("PASS\n");
+	TEST_PASS("PASS");
 }
 
 static const struct bst_test_instance test_to_add[] = {
 	{
 		.test_id = "central",
-		.test_pre_init_f = test_init,
-		.test_tick_f = test_tick,
 		.test_main_f = central,
 	},
 	BSTEST_END_MARKER,

tests/bsim/bluetooth/host/security/id_addr_update/peripheral/src/peripheral.c
@@ -14,6 +14,8 @@
 #include <stdint.h>
 #include <string.h>
 
+#include "babblekit/testcase.h"
+
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(peripheral, LOG_LEVEL_INF);
 
@@ -24,12 +26,12 @@ static void verify_equal_address(struct bt_conn *conn_a, struct bt_conn *conn_b)
 	struct bt_conn_info info_b;
 
 	err = bt_conn_get_info(conn_a, &info_a);
-	ASSERT(!err, "Unexpected info_a result.");
+	TEST_ASSERT(!err, "Unexpected info_a result.");
 
 	err = bt_conn_get_info(conn_b, &info_b);
-	ASSERT(!err, "Unexpected info_b result.");
+	TEST_ASSERT(!err, "Unexpected info_b result.");
 
-	ASSERT(bt_addr_le_eq(info_a.le.dst, info_b.le.dst),
+	TEST_ASSERT(bt_addr_le_eq(info_a.le.dst, info_b.le.dst),
 	       "Conn A address is not equal with the conn B address");
 }
 
@@ -45,10 +47,10 @@ void peripheral(void)
 
 	/* Create two identities that will simultaneously connect with the same central peer. */
 	id_a = bt_id_create(NULL, NULL);
-	ASSERT(id_a >= 0, "bt_id_create id_a failed (err %d)\n", id_a);
+	TEST_ASSERT(id_a >= 0, "bt_id_create id_a failed (err %d)", id_a);
 
 	id_b = bt_id_create(NULL, NULL);
-	ASSERT(id_b >= 0, "bt_id_create id_b failed (err %d)\n", id_b);
+	TEST_ASSERT(id_b >= 0, "bt_id_create id_b failed (err %d)", id_b);
 
 	/* Connect with the first identity. */
 	LOG_INF("adv");
@@ -82,14 +84,12 @@ void peripheral(void)
 	wait_disconnected();
 	clear_conn(conn_b);
 
-	PASS("PASS\n");
+	TEST_PASS("PASS");
 }
 
 static const struct bst_test_instance test_to_add[] = {
 	{
 		.test_id = "peripheral",
-		.test_pre_init_f = test_init,
-		.test_tick_f = test_tick,
 		.test_main_f = peripheral,
 	},
 	BSTEST_END_MARKER,

tests/bsim/bluetooth/host/security/security_changed_callback/src/peripheral.c
@@ -13,6 +13,7 @@
 #include <zephyr/bluetooth/bluetooth.h>
 
 #include "bs_bt_utils.h"
+#include "babblekit/testcase.h"
 
 LOG_MODULE_REGISTER(test_peripheral, LOG_LEVEL_DBG);
 
@@ -20,7 +21,7 @@ BUILD_ASSERT(CONFIG_BT_BONDABLE, "CONFIG_BT_BONDABLE must be enabled by default.
 
 static void pairing_complete_unpair(struct bt_conn *conn, bool bonded)
 {
-	FAIL("Pairing succeed\n");
+	TEST_FAIL("Pairing succeed");
 }
 
 static void peripheral_security_changed_unpair(struct bt_conn *conn,
@@ -51,7 +52,7 @@ void peripheral_unpair_in_sec_cb(void)
 
 	bt_conn_cb_register(&peripheral_cb);
 	err = bt_conn_auth_info_cb_register(&peripheral_auth_info_cb);
-	ASSERT(!err, "bt_conn_auth_info_cb_register failed.\n");
+	TEST_ASSERT(!err, "bt_conn_auth_info_cb_register failed.");
 
 	advertise_connectable(BT_ID_DEFAULT, NULL);
 	wait_connected();
@@ -60,12 +61,12 @@ void peripheral_unpair_in_sec_cb(void)
 
 	clear_g_conn();
 
-	PASS("PASS\n");
+	TEST_PASS("PASS");
 }
 
 static void pairing_failed_disconnect(struct bt_conn *conn, enum bt_security_err err)
 {
-	FAIL("Pairing failed\n");
+	TEST_FAIL("Pairing failed");
 }
 
 static void peripheral_security_changed_disconnect(struct bt_conn *conn,
@@ -95,7 +96,7 @@ void peripheral_disconnect_in_sec_cb(void)
 
 	bt_conn_cb_register(&peripheral_cb);
 	err = bt_conn_auth_info_cb_register(&peripheral_auth_info_cb);
-	ASSERT(!err, "bt_conn_auth_info_cb_register failed.\n");
+	TEST_ASSERT(!err, "bt_conn_auth_info_cb_register failed.");
 
 	advertise_connectable(BT_ID_DEFAULT, NULL);
 	wait_connected();
@@ -104,5 +105,5 @@ void peripheral_disconnect_in_sec_cb(void)
 
 	clear_g_conn();
 
-	PASS("PASS\n");
+	TEST_PASS("PASS");
 }