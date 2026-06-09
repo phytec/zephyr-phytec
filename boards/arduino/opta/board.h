zephyrproject-rtos/zephyr

boards: opta: external flash and BLE support
This set of changes adds support for QSPI-based external flash and
Bluetooth to the device tree. This make it possible to correctly build and
execute the fatfs and several Bluetooth samples out of the box.

Also added a function to read the external flash OTP to extract
information about the Opta model and hardware features and a second
function to retrieve the "official" Opta serial number.

Signed-off-by: Federico Di Gregorio <fog@dndg.it>

boards/arduino/opta/board.h
@@ -1,0 +1,38 @@
+/*
+ * Copyright (c) 2024 DNDG srl
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+ #ifndef __INC_BOARD_H
+ #define __INC_BOARD_H
+
+#include <stdint.h>
+
+#define OPTA_OTP_MAGIC 0xB5
+#define OPTA_SERIAL_NUMBER_SIZE 24
+
+struct __packed opta_board_info {
+	uint8_t magic;
+	uint8_t version;
+	union {
+		uint16_t board_functionalities;
+		struct {
+			uint8_t wifi: 1;
+			uint8_t rs485: 1;
+			uint8_t ethernet: 1;
+		} _board_functionalities_bits;
+	};
+	uint16_t revision;
+	uint8_t external_flash_size;
+	uint16_t vid;
+	uint16_t pid;
+	uint8_t mac_address[6];
+	uint8_t mac_address_wifi[6];
+};
+
+const struct opta_board_info *const opta_get_board_info(void);
+
+const char *const opta_get_serial_number(void);
+
+#endif /* __INC_BOARD_H */

phytec/zephyr-phytec

boards/arduino/opta/board.h
@@ -1,0 +1,39 @@
+/*
+ * Copyright (c) 2024 DNDG srl
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __ARDUINO_OPTA_BOARD_H
+#define __ARDUINO_OPTA_BOARD_H
+
+#include <stdint.h>
+
+#define OPTA_OTP_MAGIC 0xB5
+
+#define OPTA_SERIAL_NUMBER_SIZE 24
+
+struct __packed opta_board_info {
+	uint8_t magic;
+	uint8_t version;
+	union {
+		uint16_t board_functionalities;
+		struct {
+			uint8_t wifi: 1;
+			uint8_t rs485: 1;
+			uint8_t ethernet: 1;
+		} _board_functionalities_bits;
+	};
+	uint16_t revision;
+	uint8_t external_flash_size;
+	uint16_t vid;
+	uint16_t pid;
+	uint8_t mac_address[6];
+	uint8_t mac_address_wifi[6];
+};
+
+const struct opta_board_info *const opta_get_board_info(void);
+
+const char *const opta_get_serial_number(void);
+
+#endif /* __ARDUINO_OPTA_BOARD_H */