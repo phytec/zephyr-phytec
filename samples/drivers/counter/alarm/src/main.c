MCHP-MPU-Solutions-SHA/zephyr

samples: drivers: counter: alarm: add tc device for sama7g5
Add tc0_ch0 device for sama7g5 in alarm sample

Signed-off-by: CHEN Xing <xing.chen@microchip.com>

samples/drivers/counter/alarm/src/main.c
@@ -72,6 +72,8 @@ struct counter_alarm_cfg alarm_cfg;
 #define TIMER DT_INST(0, renesas_rz_gtm_counter)
 #elif defined(CONFIG_COUNTER_CC23X0_RTC)
 #define TIMER DT_NODELABEL(rtc0)
+#elif defined(CONFIG_COUNTER_MCHP_SAM_TC)
+#define TIMER DT_NODELABEL(tc0_ch0)
 #else
 #error Unable to find a counter device node in devicetree
 #endif

phytec/zephyr-phytec

samples/drivers/counter/alarm/src/main.c
@@ -78,6 +78,8 @@ struct counter_alarm_cfg alarm_cfg;
 #define SAMPLE_TIMER DT_NODELABEL(pit64b1)
 #undef ALARM_FLAGS
 #define ALARM_FLAGS COUNTER_ALARM_CFG_ABSOLUTE
+#elif defined(CONFIG_COUNTER_MCHP_TC_G2)
+#define SAMPLE_TIMER DT_NODELABEL(tc0_ch0)
 #elif defined(CONFIG_COUNTER_MCUX_RTC_JDP)
 #define SAMPLE_TIMER DT_NODELABEL(rtc)
 #elif defined(CONFIG_COUNTER_MCUX_RTC)