ioannisg/zephyr

arch: arm: introduce/implement API for configuring static MPU regions
This commit introduces the generic ARM (core) API, which allows
the user to program a set of static (fixed) MPU regions at boot
time. The API function is invoked upon initialization, in the
ARM-specific call of _arch_switch_to_main_thread(). The API
implementation is provided in arm_core_mpu.c.

Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

arch/arm/core/cortex_m/mpu/arm_core_mpu.c
@@ -10,10 +10,98 @@
 #include <soc.h>
 
 #include <arch/arm/cortex_m/mpu/arm_core_mpu_dev.h>
+#include <linker/linker-defs.h>
 
 #define LOG_LEVEL CONFIG_MPU_LOG_LEVEL
 #include <logging/log.h>
 LOG_MODULE_REGISTER(mpu);
+
+
+#if defined(CONFIG_APP_SHARED_MEM)
+#define _MPU_DYNAMIC_REGIONS_AREA_START ((u32_t)&_app_smem_start)
+#else
+#define _MPU_DYNAMIC_REGIONS_AREA_START ((u32_t)&__kernel_ram_start)
+#endif /* CONFIG_APP_SHARED_MEM */
+#define _MPU_DYNAMIC_REGIONS_AREA_SIZE ((u32_t)&__kernel_ram_end - \
+		_MPU_DYNAMIC_REGIONS_AREA_START)
+
+/**
+ * @brief Use the HW-specific MPU driver to program
+ *        the static MPU regions.
+ *
+ * Program the static MPU regions using the HW-specific MPU driver. The
+ * function is meant to be invoked only once upon system initialization.
+ *
+ * If the function attempts to configure a number of regions beyond the
+ * MPU HW limitations, the system behavior will be undefined.
+ *
+ * For some MPU architectures, such as the unmodified ARMv8-M MPU,
+ * the function must execute with MPU enabled.
+ */
+void _arch_configure_static_mpu_regions(void)
+{
+	/* Define a constant array of k_mem_partition objects
+	 * to hold the configuration of the respective static
+	 * MPU regions.
+	 */
+	const struct k_mem_partition static_regions[] = {
+#if defined(CONFIG_APPLICATION_MEMORY)
+		{
+		.start = (u32_t)&__app_ram_start,
+		.size = (u32_t)&__app_ram_end - (u32_t)&__app_ram_start,
+		.attr = K_MEM_PARTITION_P_RW_U_RW,
+		},
+#endif /* CONFIG_APPLICATION_MEMORY */
+#if defined(CONFIG_COVERAGE_GCOV) && defined(CONFIG_USERSPACE)
+		{
+		.start = (u32_t)&__gcov_bss_start,
+		.size = (u32_t)&__gcov_bss_end - (u32_t)&__gcov_bss_start,
+		.attr = K_MEM_PARTITION_P_RW_U_RW,
+		},
+#endif /* CONFIG_COVERAGE_GCOV && CONFIG_USERSPACE */
+#if defined(CONFIG_NOCACHE_MEMORY)
+		{
+		.start = (u32_t)&_nocache_ram_start,
+		.size = (u32_t)&_nocache_ram_end - (u32_t)&_nocache_ram_start,
+		.attr = K_MEM_PARTITION_P_RW_U_RW_NOCACHE,
+		}
+#endif /* CONFIG_NOCACHE_MEMORY */
+	};
+
+	/* Configure the static MPU regions within firmware SRAM boundaries.
+	 * Start address of the image is given by _image_ram_start. The end
+	 * of the firmware SRAM area is marked by __kernel_ram_end, taking
+	 * into account the unused SRAM area, as well.
+	 */
+	arm_core_mpu_configure_static_mpu_regions(static_regions,
+		ARRAY_SIZE(static_regions),
+		(u32_t)&_image_ram_start,
+		(u32_t)&__kernel_ram_end);
+
+#if defined(CONFIG_MPU_REQUIRES_NON_OVERLAPPING_REGIONS)
+	/* Define a constant array of k_mem_partition objects that holds the
+	 * boundaries of the areas, inside which dynamic region programming
+	 * is allowed. The information is passed to the underlying driver at
+	 * initialization.
+	 */
+	const struct k_mem_partition dyn_region_areas[] = {
+#if defined(CONFIG_APPLICATION_MEMORY)
+	/* Dynamic areas are also allowed in Application Memory. */
+		{
+		.start = (u32_t)&__app_ram_start,
+		.size = (u32_t)&__app_ram_end - (u32_t)&__app_ram_start,
+		},
+#endif /* CONFIG_APPLICATION_MEMORY */
+		{
+		.start = _MPU_DYNAMIC_REGIONS_AREA_START,
+		.size =  _MPU_DYNAMIC_REGIONS_AREA_SIZE,
+		}
+	};
+	arm_core_mpu_mark_areas_for_dynamic_regions(dyn_region_areas,
+		ARRAY_SIZE(dyn_region_areas));
+#endif /* CONFIG_MPU_REQUIRES_NON_OVERLAPPING_REGIONS */
+}
+
 
 #if defined(CONFIG_MPU_STACK_GUARD)
 /*

phytec/zephyr-phytec

arch/arm/core/cortex_m/mpu/arm_core_mpu.c
@@ -10,10 +10,98 @@
 #include <soc.h>
 
 #include <arch/arm/cortex_m/mpu/arm_core_mpu_dev.h>
+#include <linker/linker-defs.h>
 
 #define LOG_LEVEL CONFIG_MPU_LOG_LEVEL
 #include <logging/log.h>
 LOG_MODULE_REGISTER(mpu);
+
+
+#if defined(CONFIG_APP_SHARED_MEM)
+#define _MPU_DYNAMIC_REGIONS_AREA_START ((u32_t)&_app_smem_start)
+#else
+#define _MPU_DYNAMIC_REGIONS_AREA_START ((u32_t)&__kernel_ram_start)
+#endif /* CONFIG_APP_SHARED_MEM */
+#define _MPU_DYNAMIC_REGIONS_AREA_SIZE ((u32_t)&__kernel_ram_end - \
+		_MPU_DYNAMIC_REGIONS_AREA_START)
+
+/**
+ * @brief Use the HW-specific MPU driver to program
+ *        the static MPU regions.
+ *
+ * Program the static MPU regions using the HW-specific MPU driver. The
+ * function is meant to be invoked only once upon system initialization.
+ *
+ * If the function attempts to configure a number of regions beyond the
+ * MPU HW limitations, the system behavior will be undefined.
+ *
+ * For some MPU architectures, such as the unmodified ARMv8-M MPU,
+ * the function must execute with MPU enabled.
+ */
+void _arch_configure_static_mpu_regions(void)
+{
+	/* Define a constant array of k_mem_partition objects
+	 * to hold the configuration of the respective static
+	 * MPU regions.
+	 */
+	const struct k_mem_partition static_regions[] = {
+#if defined(CONFIG_APPLICATION_MEMORY)
+		{
+		.start = (u32_t)&__app_ram_start,
+		.size = (u32_t)&__app_ram_end - (u32_t)&__app_ram_start,
+		.attr = K_MEM_PARTITION_P_RW_U_RW,
+		},
+#endif /* CONFIG_APPLICATION_MEMORY */
+#if defined(CONFIG_COVERAGE_GCOV) && defined(CONFIG_USERSPACE)
+		{
+		.start = (u32_t)&__gcov_bss_start,
+		.size = (u32_t)&__gcov_bss_end - (u32_t)&__gcov_bss_start,
+		.attr = K_MEM_PARTITION_P_RW_U_RW,
+		},
+#endif /* CONFIG_COVERAGE_GCOV && CONFIG_USERSPACE */
+#if defined(CONFIG_NOCACHE_MEMORY)
+		{
+		.start = (u32_t)&_nocache_ram_start,
+		.size = (u32_t)&_nocache_ram_end - (u32_t)&_nocache_ram_start,
+		.attr = K_MEM_PARTITION_P_RW_U_NA_NOCACHE,
+		}
+#endif /* CONFIG_NOCACHE_MEMORY */
+	};
+
+	/* Configure the static MPU regions within firmware SRAM boundaries.
+	 * Start address of the image is given by _image_ram_start. The end
+	 * of the firmware SRAM area is marked by __kernel_ram_end, taking
+	 * into account the unused SRAM area, as well.
+	 */
+	arm_core_mpu_configure_static_mpu_regions(static_regions,
+		ARRAY_SIZE(static_regions),
+		(u32_t)&_image_ram_start,
+		(u32_t)&__kernel_ram_end);
+
+#if defined(CONFIG_MPU_REQUIRES_NON_OVERLAPPING_REGIONS)
+	/* Define a constant array of k_mem_partition objects that holds the
+	 * boundaries of the areas, inside which dynamic region programming
+	 * is allowed. The information is passed to the underlying driver at
+	 * initialization.
+	 */
+	const struct k_mem_partition dyn_region_areas[] = {
+#if defined(CONFIG_APPLICATION_MEMORY)
+	/* Dynamic areas are also allowed in Application Memory. */
+		{
+		.start = (u32_t)&__app_ram_start,
+		.size = (u32_t)&__app_ram_end - (u32_t)&__app_ram_start,
+		},
+#endif /* CONFIG_APPLICATION_MEMORY */
+		{
+		.start = _MPU_DYNAMIC_REGIONS_AREA_START,
+		.size =  _MPU_DYNAMIC_REGIONS_AREA_SIZE,
+		}
+	};
+	arm_core_mpu_mark_areas_for_dynamic_regions(dyn_region_areas,
+		ARRAY_SIZE(dyn_region_areas));
+#endif /* CONFIG_MPU_REQUIRES_NON_OVERLAPPING_REGIONS */
+}
+
 
 #if defined(CONFIG_MPU_STACK_GUARD)
 /*