zephyrproject-rtos/zephyr

net: if: vlan: Add virtual lan support
This allows creation of virtual lan (VLAN) networks. VLAN support is
only available for ethernet network technology.

Fixes #3234

Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

include/net/ethernet.h
@@ -15,10 +15,13 @@
 
 #include <zephyr/types.h>
 #include <stdbool.h>
+#include <atomic.h>
 
 #include <net/net_ip.h>
 #include <net/net_pkt.h>
 #include <misc/util.h>
+#include <net/net_if.h>
+#include <net/vlan.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -36,6 +39,7 @@ extern "C" {
 #define NET_ETH_PTYPE_ARP		0x0806
 #define NET_ETH_PTYPE_IP		0x0800
 #define NET_ETH_PTYPE_IPV6		0x86dd
+#define NET_ETH_PTYPE_VLAN		0x8100
 
 #define NET_ETH_MINIMAL_FRAME_SIZE	60
 
@@ -67,6 +71,64 @@ struct net_eth_hdr {
 	struct net_eth_addr src;
 	u16_t type;
 } __packed;
+
+struct ethernet_vlan {
+	/** Network interface that has VLAN enabled */
+	struct net_if *iface;
+
+	/** VLAN tag */
+	u16_t tag;
+};
+
+#define NET_VLAN_MAX_COUNT CONFIG_NET_VLAN_COUNT
+
+/** Ethernet L2 context that is needed for VLAN */
+struct ethernet_context {
+#if defined(CONFIG_NET_VLAN)
+	struct ethernet_vlan vlan[NET_VLAN_MAX_COUNT];
+
+	/** Array that will help when checking if VLAN is enabled for
+	 * some specific network interface. Requires that VLAN count
+	 * NET_VLAN_MAX_COUNT is not smaller than the actual number
+	 * of network interfaces.
+	 */
+	ATOMIC_DEFINE(interfaces, NET_VLAN_MAX_COUNT);
+
+	/** Flag that tells whether how many VLAN tags are enabled for this
+	 * context. The same information can be dug from the vlan array but
+	 * this saves some time in RX path.
+	 */
+	s8_t vlan_enabled;
+#endif
+
+	/** Is this context already initialized */
+	bool is_init;
+};
+
+#define ETHERNET_L2_CTX_TYPE	struct ethernet_context
+
+/**
+ * @brief Initialize Ethernet L2 stack for a given interface
+ *
+ * @param iface A valid pointer to a network interface
+ */
+void ethernet_init(struct net_if *iface);
+
+#if defined(CONFIG_NET_VLAN)
+/* Separate header for VLAN as some of device interfaces might not
+ * support VLAN.
+ */
+struct net_eth_vlan_hdr {
+	struct net_eth_addr dst;
+	struct net_eth_addr src;
+	struct {
+		u16_t tpid; /* tag protocol id  */
+		u16_t tci;  /* tag control info */
+	} vlan;
+	u16_t type;
+} __packed;
+
+#endif /* CONFIG_NET_VLAN */
 
 static inline bool net_eth_is_addr_broadcast(struct net_eth_addr *addr)
 {
@@ -132,6 +194,109 @@ enum eth_hw_caps net_eth_get_hw_capabilities(struct net_if *iface)
 
 	return eth->get_capabilities(net_if_get_device(iface));
 }
+
+#if defined(CONFIG_NET_VLAN)
+/**
+ * @brief Add VLAN tag to the interface.
+ *
+ * @param iface Interface to use.
+ * @param tag VLAN tag to add
+ *
+ * @return 0 if ok, <0 if error
+ */
+int net_eth_vlan_enable(struct net_if *iface, u16_t tag);
+
+/**
+ * @brief Remove VLAN tag from the interface.
+ *
+ * @param iface Interface to use.
+ * @param tag VLAN tag to remove
+ *
+ * @return 0 if ok, <0 if error
+ */
+int net_eth_vlan_disable(struct net_if *iface, u16_t tag);
+
+/**
+ * @brief Return VLAN tag specified to network interface
+ *
+ * @param iface Network interface.
+ *
+ * @return VLAN tag for this interface or NET_VLAN_TAG_UNSPEC if VLAN
+ * is not configured for that interface.
+ */
+u16_t net_eth_get_vlan_tag(struct net_if *iface);
+
+/**
+ * @brief Return network interface related to this VLAN tag
+ *
+ * @param ctx Ethernet context
+ * @param tag VLAN tag
+ *
+ * @return Network interface related to this tag or NULL if no such interface
+ * exists.
+ */
+struct net_if *net_eth_get_vlan_iface(struct ethernet_context *ctx, u16_t tag);
+
+/**
+ * @brief Check if VLAN is enabled for a specific network interface.
+ *
+ * @param ctx Ethernet context
+ * @param iface Network interface
+ *
+ * @return True if VLAN is enabled for this network interface, false if not.
+ */
+bool net_eth_is_vlan_enabled(struct ethernet_context *ctx,
+			     struct net_if *iface);
+
+#define ETH_NET_DEVICE_INIT(dev_name, drv_name, init_fn,		 \
+			    data, cfg_info, prio, api, mtu)		 \
+	DEVICE_AND_API_INIT(dev_name, drv_name, init_fn, data,		 \
+			    cfg_info, POST_KERNEL, prio, api);		 \
+	NET_L2_DATA_INIT(dev_name, 0, NET_L2_GET_CTX_TYPE(ETHERNET_L2)); \
+	NET_IF_INIT(dev_name, 0, ETHERNET_L2, mtu, NET_VLAN_MAX_COUNT)
+
+#else /* CONFIG_NET_VLAN */
+
+#define ETH_NET_DEVICE_INIT(dev_name, drv_name, init_fn,		\
+			    data, cfg_info, prio, api, mtu)		\
+	NET_DEVICE_INIT(dev_name, drv_name, init_fn,			\
+			data, cfg_info, prio, api, ETHERNET_L2,		\
+			NET_L2_GET_CTX_TYPE(ETHERNET_L2), mtu)
+
+static inline int net_eth_vlan_enable(struct net_if *iface, u16_t vlan_tag)
+{
+	return -EINVAL;
+}
+
+static inline int net_eth_vlan_disable(struct net_if *iface, u16_t vlan_tag)
+{
+	return -EINVAL;
+}
+
+static inline u16_t net_eth_get_vlan_tag(struct net_if *iface)
+{
+	return NET_VLAN_TAG_UNSPEC;
+}
+#endif /* CONFIG_NET_VLAN */
+
+/**
+ * @brief Fill ethernet header in network packet.
+ *
+ * @param ctx Ethernet context
+ * @param pkt Network packet
+ * @param frag Ethernet header in packet
+ * @param ptype Upper level protocol type (in network byte order)
+ * @param src Source ethernet address
+ * @param dst Destination ethernet address
+ *
+ * @return Pointer to ethernet header struct inside net_buf.
+ */
+struct net_eth_hdr *net_eth_fill_header(struct ethernet_context *ctx,
+					struct net_pkt *pkt,
+					struct net_buf *frag,
+					u32_t ptype,
+					u8_t *src,
+					u8_t *dst);
 
 #ifdef __cplusplus
 }

include/net/net_pkt.h
@@ -26,6 +26,7 @@
 #include <net/net_ip.h>
 #include <net/net_if.h>
 #include <net/net_context.h>
+#include <net/vlan.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -149,6 +150,15 @@ struct net_pkt {
 	 */
 	u8_t priority;
 #endif
+
+#if defined(CONFIG_NET_VLAN)
+	/* VLAN TCI (Tag Control Information). This contains the Priority
+	 * Code Point (PCP), Drop Eligible Indicator (DEI) and VLAN
+	 * Identifier (VID, called more commonly VLAN tag). This value is
+	 * kept in host byte order.
+	 */
+	u16_t vlan_tci;
+#endif /* CONFIG_NET_VLAN */
 	/* @endcond */
 
 	/** Reference counter */
@@ -414,12 +424,87 @@ static inline u8_t net_pkt_priority(struct net_pkt *pkt)
 {
 	return 0;
 }
+#endif
 
-static inline void net_pkt_set_priority(struct net_pkt *pkt,
-					u8_t priority)
+#if defined(CONFIG_NET_VLAN)
+static inline u16_t net_pkt_vlan_tag(struct net_pkt *pkt)
+{
+	return net_eth_get_vid(pkt->vlan_tci);
+}
+
+static inline void net_pkt_set_vlan_tag(struct net_pkt *pkt, u16_t tag)
+{
+	pkt->vlan_tci = net_eth_set_vid(pkt->vlan_tci, tag);
+}
+
+static inline u8_t net_pkt_vlan_priority(struct net_pkt *pkt)
+{
+	return net_eth_get_pcp(pkt->vlan_tci);
+}
+
+static inline void net_pkt_set_vlan_priority(struct net_pkt *pkt,
+					     u8_t priority)
+{
+	pkt->vlan_tci = net_eth_set_pcp(pkt->vlan_tci, priority);
+}
+
+static inline bool net_pkt_vlan_dei(struct net_pkt *pkt)
+{
+	return net_eth_get_dei(pkt->vlan_tci);
+}
+
+static inline void net_pkt_set_vlan_dei(struct net_pkt *pkt, bool dei)
+{
+	pkt->vlan_tci = net_eth_set_dei(pkt->vlan_tci, dei);
+}
+
+static inline void net_pkt_set_vlan_tci(struct net_pkt *pkt, u16_t tci)
+{
+	pkt->vlan_tci = tci;
+}
+
+static inline u16_t net_pkt_vlan_tci(struct net_pkt *pkt)
+{
+	return pkt->vlan_tci;
+}
+#else
+static inline u16_t net_pkt_vlan_tag(struct net_pkt *pkt)
+{
+	return NET_VLAN_TAG_UNSPEC;
+}
+
+static inline void net_pkt_set_vlan_tag(struct net_pkt *pkt, u16_t tag)
 {
 	ARG_UNUSED(pkt);
-	ARG_UNUSED(priority);
+	ARG_UNUSED(tag);
+}
+
+static inline u8_t net_pkt_vlan_priority(struct net_pkt *pkt)
+{
+	ARG_UNUSED(pkt);
+	return 0;
+}
+
+static inline bool net_pkt_vlan_dei(struct net_pkt *pkt)
+{
+	return false;
+}
+
+static inline void net_pkt_set_vlan_dei(struct net_pkt *pkt, bool dei)
+{
+	ARG_UNUSED(pkt);
+	ARG_UNUSED(dei);
+}
+
+static inline u16_t net_pkt_vlan_tci(struct net_pkt *pkt)
+{
+	return NET_VLAN_TAG_UNSPEC; /* assumes priority is 0 */
+}
+
+static inline void net_pkt_set_vlan_tci(struct net_pkt *pkt, u16_t tci)
+{
+	ARG_UNUSED(pkt);
+	ARG_UNUSED(tci);
 }
 #endif
 

phytec/zephyr-phytec

include/net/ethernet.h
@@ -15,10 +15,13 @@
 
 #include <zephyr/types.h>
 #include <stdbool.h>
+#include <atomic.h>
 
 #include <net/net_ip.h>
 #include <net/net_pkt.h>
 #include <misc/util.h>
+#include <net/net_if.h>
+#include <net/ethernet_vlan.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -36,6 +39,7 @@ extern "C" {
 #define NET_ETH_PTYPE_ARP		0x0806
 #define NET_ETH_PTYPE_IP		0x0800
 #define NET_ETH_PTYPE_IPV6		0x86dd
+#define NET_ETH_PTYPE_VLAN		0x8100
 
 #define NET_ETH_MINIMAL_FRAME_SIZE	60
 
@@ -45,6 +49,9 @@ enum eth_hw_caps {
 
 	/** RX Checksum offloading supported */
 	ETH_HW_RX_CHKSUM_OFFLOAD  = BIT(1),
+
+	/** VLAN supported */
+	ETH_HW_VLAN  = BIT(2),
 };
 
 struct ethernet_api {
@@ -56,6 +63,13 @@ struct ethernet_api {
 
 	/** Get the device capabilities */
 	enum eth_hw_caps (*get_capabilities)(struct device *dev);
+
+	/** The IP stack will call this function when a VLAN tag is enabled
+	 * or disabled. If enable is set to true, then the VLAN tag was added,
+	 * if it is false then the tag was removed. The driver can utilize
+	 * this information if needed.
+	 */
+	int (*vlan_setup)(struct net_if *iface, u16_t tag, bool enable);
 };
 
 struct net_eth_addr {
@@ -67,6 +81,70 @@ struct net_eth_hdr {
 	struct net_eth_addr src;
 	u16_t type;
 } __packed;
+
+struct ethernet_vlan {
+	/** Network interface that has VLAN enabled */
+	struct net_if *iface;
+
+	/** VLAN tag */
+	u16_t tag;
+};
+
+#if defined(CONFIG_NET_VLAN_COUNT)
+#define NET_VLAN_MAX_COUNT CONFIG_NET_VLAN_COUNT
+#else
+/* Even thou there are no VLAN support, the minimum count must be set to 1.
+ */
+#define NET_VLAN_MAX_COUNT 1
+#endif
+
+/** Ethernet L2 context that is needed for VLAN */
+struct ethernet_context {
+#if defined(CONFIG_NET_VLAN)
+	struct ethernet_vlan vlan[NET_VLAN_MAX_COUNT];
+
+	/** Array that will help when checking if VLAN is enabled for
+	 * some specific network interface. Requires that VLAN count
+	 * NET_VLAN_MAX_COUNT is not smaller than the actual number
+	 * of network interfaces.
+	 */
+	ATOMIC_DEFINE(interfaces, NET_VLAN_MAX_COUNT);
+
+	/** Flag that tells whether how many VLAN tags are enabled for this
+	 * context. The same information can be dug from the vlan array but
+	 * this saves some time in RX path.
+	 */
+	s8_t vlan_enabled;
+
+	/** Is this context already initialized */
+	bool is_init;
+#endif
+};
+
+#define ETHERNET_L2_CTX_TYPE	struct ethernet_context
+
+/**
+ * @brief Initialize Ethernet L2 stack for a given interface
+ *
+ * @param iface A valid pointer to a network interface
+ */
+void ethernet_init(struct net_if *iface);
+
+#if defined(CONFIG_NET_VLAN)
+/* Separate header for VLAN as some of device interfaces might not
+ * support VLAN.
+ */
+struct net_eth_vlan_hdr {
+	struct net_eth_addr dst;
+	struct net_eth_addr src;
+	struct {
+		u16_t tpid; /* tag protocol id  */
+		u16_t tci;  /* tag control info */
+	} vlan;
+	u16_t type;
+} __packed;
+
+#endif /* CONFIG_NET_VLAN */
 
 static inline bool net_eth_is_addr_broadcast(struct net_eth_addr *addr)
 {
@@ -132,6 +210,116 @@ enum eth_hw_caps net_eth_get_hw_capabilities(struct net_if *iface)
 
 	return eth->get_capabilities(net_if_get_device(iface));
 }
+
+#if defined(CONFIG_NET_VLAN)
+/**
+ * @brief Add VLAN tag to the interface.
+ *
+ * @param iface Interface to use.
+ * @param tag VLAN tag to add
+ *
+ * @return 0 if ok, <0 if error
+ */
+int net_eth_vlan_enable(struct net_if *iface, u16_t tag);
+
+/**
+ * @brief Remove VLAN tag from the interface.
+ *
+ * @param iface Interface to use.
+ * @param tag VLAN tag to remove
+ *
+ * @return 0 if ok, <0 if error
+ */
+int net_eth_vlan_disable(struct net_if *iface, u16_t tag);
+
+/**
+ * @brief Return VLAN tag specified to network interface
+ *
+ * @param iface Network interface.
+ *
+ * @return VLAN tag for this interface or NET_VLAN_TAG_UNSPEC if VLAN
+ * is not configured for that interface.
+ */
+u16_t net_eth_get_vlan_tag(struct net_if *iface);
+
+/**
+ * @brief Return network interface related to this VLAN tag
+ *
+ * @param iface Master network interface. This is used to get the
+ *        pointer to ethernet L2 context
+ * @param tag VLAN tag
+ *
+ * @return Network interface related to this tag or NULL if no such interface
+ * exists.
+ */
+struct net_if *net_eth_get_vlan_iface(struct net_if *iface, u16_t tag);
+
+/**
+ * @brief Check if VLAN is enabled for a specific network interface.
+ *
+ * @param ctx Ethernet context
+ * @param iface Network interface
+ *
+ * @return True if VLAN is enabled for this network interface, false if not.
+ */
+bool net_eth_is_vlan_enabled(struct ethernet_context *ctx,
+			     struct net_if *iface);
+
+#define ETH_NET_DEVICE_INIT(dev_name, drv_name, init_fn,		 \
+			    data, cfg_info, prio, api, mtu)		 \
+	DEVICE_AND_API_INIT(dev_name, drv_name, init_fn, data,		 \
+			    cfg_info, POST_KERNEL, prio, api);		 \
+	NET_L2_DATA_INIT(dev_name, 0, NET_L2_GET_CTX_TYPE(ETHERNET_L2)); \
+	NET_IF_INIT(dev_name, 0, ETHERNET_L2, mtu, NET_VLAN_MAX_COUNT)
+
+#else /* CONFIG_NET_VLAN */
+
+#define ETH_NET_DEVICE_INIT(dev_name, drv_name, init_fn,		\
+			    data, cfg_info, prio, api, mtu)		\
+	NET_DEVICE_INIT(dev_name, drv_name, init_fn,			\
+			data, cfg_info, prio, api, ETHERNET_L2,		\
+			NET_L2_GET_CTX_TYPE(ETHERNET_L2), mtu)
+
+static inline int net_eth_vlan_enable(struct net_if *iface, u16_t vlan_tag)
+{
+	return -EINVAL;
+}
+
+static inline int net_eth_vlan_disable(struct net_if *iface, u16_t vlan_tag)
+{
+	return -EINVAL;
+}
+
+static inline u16_t net_eth_get_vlan_tag(struct net_if *iface)
+{
+	return NET_VLAN_TAG_UNSPEC;
+}
+
+static inline
+struct net_if *net_eth_get_vlan_iface(struct net_if *iface, u16_t tag)
+{
+	return NULL;
+}
+#endif /* CONFIG_NET_VLAN */
+
+/**
+ * @brief Fill ethernet header in network packet.
+ *
+ * @param ctx Ethernet context
+ * @param pkt Network packet
+ * @param frag Ethernet header in packet
+ * @param ptype Upper level protocol type (in network byte order)
+ * @param src Source ethernet address
+ * @param dst Destination ethernet address
+ *
+ * @return Pointer to ethernet header struct inside net_buf.
+ */
+struct net_eth_hdr *net_eth_fill_header(struct ethernet_context *ctx,
+					struct net_pkt *pkt,
+					struct net_buf *frag,
+					u32_t ptype,
+					u8_t *src,
+					u8_t *dst);
 
 #ifdef __cplusplus
 }

include/net/net_pkt.h
@@ -26,6 +26,7 @@
 #include <net/net_ip.h>
 #include <net/net_if.h>
 #include <net/net_context.h>
+#include <net/ethernet_vlan.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -149,6 +150,15 @@ struct net_pkt {
 	 */
 	u8_t priority;
 #endif
+
+#if defined(CONFIG_NET_VLAN)
+	/* VLAN TCI (Tag Control Information). This contains the Priority
+	 * Code Point (PCP), Drop Eligible Indicator (DEI) and VLAN
+	 * Identifier (VID, called more commonly VLAN tag). This value is
+	 * kept in host byte order.
+	 */
+	u16_t vlan_tci;
+#endif /* CONFIG_NET_VLAN */
 	/* @endcond */
 
 	/** Reference counter */
@@ -414,12 +424,87 @@ static inline u8_t net_pkt_priority(struct net_pkt *pkt)
 {
 	return 0;
 }
+#endif
 
-static inline void net_pkt_set_priority(struct net_pkt *pkt,
-					u8_t priority)
+#if defined(CONFIG_NET_VLAN)
+static inline u16_t net_pkt_vlan_tag(struct net_pkt *pkt)
+{
+	return net_eth_vlan_get_vid(pkt->vlan_tci);
+}
+
+static inline void net_pkt_set_vlan_tag(struct net_pkt *pkt, u16_t tag)
+{
+	pkt->vlan_tci = net_eth_vlan_set_vid(pkt->vlan_tci, tag);
+}
+
+static inline u8_t net_pkt_vlan_priority(struct net_pkt *pkt)
+{
+	return net_eth_vlan_get_pcp(pkt->vlan_tci);
+}
+
+static inline void net_pkt_set_vlan_priority(struct net_pkt *pkt,
+					     u8_t priority)
+{
+	pkt->vlan_tci = net_eth_vlan_set_pcp(pkt->vlan_tci, priority);
+}
+
+static inline bool net_pkt_vlan_dei(struct net_pkt *pkt)
+{
+	return net_eth_vlan_get_dei(pkt->vlan_tci);
+}
+
+static inline void net_pkt_set_vlan_dei(struct net_pkt *pkt, bool dei)
+{
+	pkt->vlan_tci = net_eth_vlan_set_dei(pkt->vlan_tci, dei);
+}
+
+static inline void net_pkt_set_vlan_tci(struct net_pkt *pkt, u16_t tci)
+{
+	pkt->vlan_tci = tci;
+}
+
+static inline u16_t net_pkt_vlan_tci(struct net_pkt *pkt)
+{
+	return pkt->vlan_tci;
+}
+#else
+static inline u16_t net_pkt_vlan_tag(struct net_pkt *pkt)
+{
+	return NET_VLAN_TAG_UNSPEC;
+}
+
+static inline void net_pkt_set_vlan_tag(struct net_pkt *pkt, u16_t tag)
 {
 	ARG_UNUSED(pkt);
-	ARG_UNUSED(priority);
+	ARG_UNUSED(tag);
+}
+
+static inline u8_t net_pkt_vlan_priority(struct net_pkt *pkt)
+{
+	ARG_UNUSED(pkt);
+	return 0;
+}
+
+static inline bool net_pkt_vlan_dei(struct net_pkt *pkt)
+{
+	return false;
+}
+
+static inline void net_pkt_set_vlan_dei(struct net_pkt *pkt, bool dei)
+{
+	ARG_UNUSED(pkt);
+	ARG_UNUSED(dei);
+}
+
+static inline u16_t net_pkt_vlan_tci(struct net_pkt *pkt)
+{
+	return NET_VLAN_TAG_UNSPEC; /* assumes priority is 0 */
+}
+
+static inline void net_pkt_set_vlan_tci(struct net_pkt *pkt, u16_t tci)
+{
+	ARG_UNUSED(pkt);
+	ARG_UNUSED(tci);
 }
 #endif
 