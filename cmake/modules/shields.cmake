kartben/zephyr

cmake: shields: introduce shield.yml
While legacy shields are still supported, this introduces a shield.yml
file similar to board.yml that allows to more explicitly declare a
shield and to set some useful metadata such as vendor and full name.

Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

cmake/modules/shields.cmake
@@ -31,6 +31,7 @@
 include_guard(GLOBAL)
 
 include(extensions)
+include(yaml)
 
 # Check that SHIELD has not changed.
 zephyr_check_cache(SHIELD WATCH)
@@ -48,26 +49,44 @@ set(SHIELD-NOTFOUND ${SHIELD_AS_LIST})
 
 foreach(root ${BOARD_ROOT})
   set(shield_dir ${root}/boards/shields)
-  # Match the Kconfig.shield files in the shield directories to make sure we are
-  # finding shields, e.g. x_nucleo_iks01a1/Kconfig.shield
+  # First look for shield.yml files
+  file(GLOB_RECURSE shield_yml_files ${shield_dir}/*/shield.yml)
+
+  foreach(shield_yml ${shield_yml_files})
+    get_filename_component(shield_path ${shield_yml} DIRECTORY)
+    get_filename_component(shield ${shield_path} NAME)
+
+    # Use a unique YAML context name for each shield
+    set(yaml_ctx_name "shield_data_${shield}")
+
+    yaml_load(FILE ${shield_yml} NAME ${yaml_ctx_name})
+    yaml_get(shield_data_content NAME ${yaml_ctx_name} KEY content)
+
+    if(shield_data_content.shields)
+      # Multiple shields format
+      foreach(shield_info ${shield_data_content.shields})
+        list(APPEND SHIELD_LIST ${shield_info.name})
+        set(SHIELD_DIR_${shield_info.name} ${shield_path})
+      endforeach()
+    else()
+      # Single shield format
+      list(APPEND SHIELD_LIST ${shield})
+      set(SHIELD_DIR_${shield} ${shield_path})
+    endif()
+  endforeach()
+
+  # Then look for Kconfig.shield files as fallback (legacy shields)
   file(GLOB_RECURSE shields_refs_list ${shield_dir}/*/Kconfig.shield)
 
-  # The above gives a list of Kconfig.shield files, like this:
-  #
-  # x_nucleo_iks01a1/Kconfig.shield;x_nucleo_iks01a2/Kconfig.shield
-  #
-  # we construct a list of shield names by extracting the directories
-  # from each file and looking for <shield>.overlay files in there.
-  # Each overlay corresponds to a shield. We obtain the shield name by
-  # removing the .overlay extension.
-  # We also create a SHIELD_DIR_${name} variable for each shield's directory.
   foreach(shields_refs ${shields_refs_list})
     get_filename_component(shield_path ${shields_refs} DIRECTORY)
     file(GLOB shield_overlays RELATIVE ${shield_path} ${shield_path}/*.overlay)
     foreach(overlay ${shield_overlays})
       get_filename_component(shield ${overlay} NAME_WE)
-      list(APPEND SHIELD_LIST ${shield})
-      set(SHIELD_DIR_${shield} ${shield_path})
+      if(NOT ${shield} IN_LIST SHIELD_LIST)
+        list(APPEND SHIELD_LIST ${shield})
+        set(SHIELD_DIR_${shield} ${shield_path})
+      endif()
     endforeach()
   endforeach()
 endforeach()

phytec/zephyr-phytec

cmake/modules/shields.cmake
@@ -31,6 +31,7 @@
 include_guard(GLOBAL)
 
 include(extensions)
+include(yaml)
 
 # Check that SHIELD has not changed.
 zephyr_check_cache(SHIELD WATCH)
@@ -48,21 +49,47 @@ set(SHIELD-NOTFOUND ${SHIELD_AS_LIST})
 
 foreach(root ${BOARD_ROOT})
   set(shield_dir ${root}/boards/shields)
-  # Match the Kconfig.shield files in the shield directories to make sure we are
-  # finding shields, e.g. x_nucleo_iks01a1/Kconfig.shield
+
+  # First, look for shield.yml files
+  file(GLOB_RECURSE shield_yml_files ${shield_dir}/*/shield.yml)
+
+  foreach(shield_yml ${shield_yml_files})
+    get_filename_component(shield_path ${shield_yml} DIRECTORY)
+    get_filename_component(shield ${shield_path} NAME)
+
+    set(yaml_ctx_name shield_data_${shield})
+    yaml_load(FILE ${shield_yml} NAME ${yaml_ctx_name})
+
+    # Check for multiple shields format first
+    yaml_get(shields_data NAME ${yaml_ctx_name} KEY shields)
+    if(shields_data)
+      yaml_length(num_shields NAME ${yaml_ctx_name} KEY shields)
+      if(${num_shields} GREATER 0)
+        math(EXPR shield_stop "${num_shields} - 1")
+        foreach(i RANGE 0 ${shield_stop})
+          yaml_get(shield_name NAME ${yaml_ctx_name} KEY shields ${i} name)
+          list(APPEND SHIELD_LIST ${shield_name})
+          set(SHIELD_DIR_${shield_name} ${shield_path})
+        endforeach()
+      endif()
+    else()
+      yaml_get(shield_data NAME ${yaml_ctx_name} KEY shield)
+      if(shield_data)
+        yaml_get(shield_name NAME ${yaml_ctx_name} KEY shield name)
+        list(APPEND SHIELD_LIST ${shield_name})
+        set(SHIELD_DIR_${shield_name} ${shield_path})
+      endif()
+    endif()
+  endforeach()
+
+  # Then, look for overlay files next to Kconfig.shield files as fallback (legacy shields)
   file(GLOB_RECURSE shields_refs_list ${shield_dir}/*/Kconfig.shield)
 
-  # The above gives a list of Kconfig.shield files, like this:
-  #
-  # x_nucleo_iks01a1/Kconfig.shield;x_nucleo_iks01a2/Kconfig.shield
-  #
-  # we construct a list of shield names by extracting the directories
-  # from each file and looking for <shield>.overlay files in there.
-  # Each overlay corresponds to a shield. We obtain the shield name by
-  # removing the .overlay extension.
-  # We also create a SHIELD_DIR_${name} variable for each shield's directory.
   foreach(shields_refs ${shields_refs_list})
     get_filename_component(shield_path ${shields_refs} DIRECTORY)
+    if(EXISTS "${shield_path}/shield.yml")
+      continue()
+    endif()
     file(GLOB shield_overlays RELATIVE ${shield_path} ${shield_path}/*.overlay)
     foreach(overlay ${shield_overlays})
       get_filename_component(shield ${overlay} NAME_WE)