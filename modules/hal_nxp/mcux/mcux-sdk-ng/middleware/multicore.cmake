zephyrproject-rtos/zephyr

soc: nxp: mcxw70: enable BLE support
- Add NBU pinmux to configure HDI mode with external radio
- Define nbu_handler to the MU interrupt handler
- Enable TSTMR0 clock at init
- Enable connectivity framework, MCMGR, rpmsg-lite
- Enable MU driver when selecting NXP_RF_IMU
- Disable LPUART1 to avoid pin conflicts with radio interface

Signed-off-by: Axel Le Bourhis <axel.lebourhis@nxp.com>

modules/hal_nxp/mcux/mcux-sdk-ng/middleware/multicore.cmake
@@ -2,11 +2,13 @@ if(CONFIG_NXP_MULTICORE)
     set(CONFIG_MCUX_COMPONENT_middleware.multicore.mcmgr ON)
     set_variable_ifdef(CONFIG_SOC_MCXW716C      CONFIG_MCUX_COMPONENT_middleware.multicore.mcmgr.mcxw716)
     set_variable_ifdef(CONFIG_SOC_MCXW727C_CPU0 CONFIG_MCUX_COMPONENT_middleware.multicore.mcmgr.mcxw727)
+    set_variable_ifdef(CONFIG_SOC_MCXW70AC      CONFIG_MCUX_COMPONENT_middleware.multicore.mcmgr.kw43b43)
 
     set(CONFIG_MCUX_COMPONENT_middleware.multicore.rpmsg-lite ON)
     set(CONFIG_MCUX_COMPONENT_middleware.multicore.rpmsg-lite.zephyr ON)
     set_variable_ifdef(CONFIG_SOC_MCXW716C      CONFIG_MCUX_COMPONENT_middleware.multicore.rpmsg-lite.mcxw71x)
     set_variable_ifdef(CONFIG_SOC_MCXW727C_CPU0 CONFIG_MCUX_COMPONENT_middleware.multicore.rpmsg-lite.mcxw72x)
+    set_variable_ifdef(CONFIG_SOC_MCXW70AC      CONFIG_MCUX_COMPONENT_middleware.multicore.rpmsg-lite.kw43b43)
 
     add_subdirectory(${MCUX_SDK_NG_DIR}/middleware/mcuxsdk-middleware-multicore
         ${CMAKE_CURRENT_BINARY_DIR}/mcuxsdk-middleware-multicore

phytec/zephyr-phytec

modules/hal_nxp/mcux/mcux-sdk-ng/middleware/multicore.cmake
@@ -2,11 +2,13 @@ if(CONFIG_NXP_MULTICORE)
     set(CONFIG_MCUX_COMPONENT_middleware.multicore.mcmgr ON)
     set_variable_ifdef(CONFIG_SOC_MCXW716C      CONFIG_MCUX_COMPONENT_middleware.multicore.mcmgr.mcxw716)
     set_variable_ifdef(CONFIG_SOC_MCXW727C      CONFIG_MCUX_COMPONENT_middleware.multicore.mcmgr.mcxw727)
+    set_variable_ifdef(CONFIG_SOC_MCXW70AC      CONFIG_MCUX_COMPONENT_middleware.multicore.mcmgr.mcxw70)
 
     set(CONFIG_MCUX_COMPONENT_middleware.multicore.rpmsg-lite ON)
     set(CONFIG_MCUX_COMPONENT_middleware.multicore.rpmsg-lite.zephyr ON)
     set_variable_ifdef(CONFIG_SOC_MCXW716C      CONFIG_MCUX_COMPONENT_middleware.multicore.rpmsg-lite.mcxw71x)
     set_variable_ifdef(CONFIG_SOC_MCXW727C      CONFIG_MCUX_COMPONENT_middleware.multicore.rpmsg-lite.mcxw72x)
+    set_variable_ifdef(CONFIG_SOC_MCXW70AC      CONFIG_MCUX_COMPONENT_middleware.multicore.rpmsg-lite.mcxw70)
 
     add_subdirectory(${MCUX_SDK_NG_DIR}/middleware/mcuxsdk-middleware-multicore
         ${CMAKE_CURRENT_BINARY_DIR}/mcuxsdk-middleware-multicore