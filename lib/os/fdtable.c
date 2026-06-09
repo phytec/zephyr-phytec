jukkar/zephyr

net: sockets: Cleanup socket properly if POSIX API is enabled
The sock_obj_core_dealloc() was not called if close() is called
instead of zsock_close(). This happens if POSIX API is enabled.

Fix this by calling zvfs_close() from zsock_close() and then
pass the socket number to zsock_close_ctx() so that the cleanup
can be done properly.

Reported-by: Andreas Ålgård <aal@ixys.no>
Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

lib/os/fdtable.c
@@ -389,7 +389,14 @@ int zvfs_close(int fd)
 	(void)k_mutex_lock(&fdtable[fd].lock, K_FOREVER);
 	if (fdtable[fd].vtable->close != NULL) {
 		/* close() is optional - e.g. stdinout_fd_op_vtable */
-		res = fdtable[fd].vtable->close(fdtable[fd].obj);
+		if (fdtable[fd].mode & ZVFS_MODE_CLOSE2) {
+			/* Network socket needs to know socket number so pass
+			 * it via close2() call.
+			 */
+			res = fdtable[fd].vtable->close2(fdtable[fd].obj, fd);
+		} else {
+			res = fdtable[fd].vtable->close(fdtable[fd].obj);
+		}
 	}
 	k_mutex_unlock(&fdtable[fd].lock);
 

phytec/zephyr-phytec

lib/os/fdtable.c
@@ -389,7 +389,14 @@ int zvfs_close(int fd)
 	(void)k_mutex_lock(&fdtable[fd].lock, K_FOREVER);
 	if (fdtable[fd].vtable->close != NULL) {
 		/* close() is optional - e.g. stdinout_fd_op_vtable */
-		res = fdtable[fd].vtable->close(fdtable[fd].obj);
+		if (fdtable[fd].mode & ZVFS_MODE_IFSOCK) {
+			/* Network socket needs to know socket number so pass
+			 * it via close2() call.
+			 */
+			res = fdtable[fd].vtable->close2(fdtable[fd].obj, fd);
+		} else {
+			res = fdtable[fd].vtable->close(fdtable[fd].obj);
+		}
 	}
 	k_mutex_unlock(&fdtable[fd].lock);
 