Balaklaka/zephyr

Bluetooth: Mesh: Shell support for remote provisioning
Adds commands in the mesh shell for remote provisioning client
operation, including scanning, provisioning and reprovisioning.

Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>

tests/bluetooth/mesh_shell/src/main.c
@@ -37,6 +37,12 @@ static struct bt_mesh_model root_models[] = {
 	BT_MESH_MODEL_BLOB_CLI(&bt_mesh_shell_blob_cli),
 #endif
 #endif /* CONFIG_BT_MESH_DFD_SRV */
+#if defined(CONFIG_BT_MESH_RPR_CLI)
+	BT_MESH_MODEL_RPR_CLI(&bt_mesh_shell_rpr_cli),
+#endif
+#if defined(CONFIG_BT_MESH_RPR_SRV)
+	BT_MESH_MODEL_RPR_SRV,
+#endif
 };
 
 static struct bt_mesh_elem elements[] = {

phytec/zephyr-phytec

Co-authored-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

tests/bluetooth/mesh_shell/src/main.c
@@ -41,6 +41,12 @@ static struct bt_mesh_model root_models[] = {
 	BT_MESH_MODEL_BLOB_CLI(&bt_mesh_shell_blob_cli),
 #endif
 #endif /* CONFIG_BT_MESH_DFD_SRV */
+#if defined(CONFIG_BT_MESH_SHELL_RPR_CLI)
+	BT_MESH_MODEL_RPR_CLI(&bt_mesh_shell_rpr_cli),
+#endif
+#if defined(CONFIG_BT_MESH_RPR_SRV)
+	BT_MESH_MODEL_RPR_SRV,
+#endif
 };
 
 static struct bt_mesh_elem elements[] = {