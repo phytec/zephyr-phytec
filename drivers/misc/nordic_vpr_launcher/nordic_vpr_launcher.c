ravi-sharma-adi/zephyr

soc: nordic: Introduce the nRF54L05 and nRF54L10
These two new ICs are variants of the nRF54L15 with different memory
sizes:

- nRF54L05: 500KB RRAM, 96KB RAM
- nRF54L10: 1022KB RRAM, 192KB RAM
- nRF54L15: 1524KB RRAM, 256KB RAM

Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

drivers/misc/nordic_vpr_launcher/nordic_vpr_launcher.c
@@ -14,7 +14,7 @@
 #include <zephyr/toolchain.h>
 
 #include <hal/nrf_vpr.h>
-#if defined(CONFIG_SOC_NRF54L15_CPUAPP) && !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
+#if defined(CONFIG_SOC_NRF54L_05_10_15_CPUAPP) && !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
 #include <hal/nrf_spu.h>
 #endif
 
@@ -41,7 +41,7 @@ static int nordic_vpr_launcher_init(const struct device *dev)
 	}
 #endif
 
-#if defined(CONFIG_SOC_NRF54L15_CPUAPP) && !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
+#if defined(CONFIG_SOC_NRF54L_05_10_15_CPUAPP) && !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
 	nrf_spu_periph_perm_secattr_set(NRF_SPU00, nrf_address_slave_get((uint32_t)config->vpr),
 					true);
 #endif

phytec/zephyr-phytec

drivers/misc/nordic_vpr_launcher/nordic_vpr_launcher.c
@@ -14,7 +14,7 @@
 #include <zephyr/toolchain.h>
 
 #include <hal/nrf_vpr.h>
-#if defined(CONFIG_SOC_NRF54L15_CPUAPP) && !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
+#if defined(CONFIG_SOC_NRF54L_CPUAPP_COMMON) && !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
 #include <hal/nrf_spu.h>
 #endif
 
@@ -41,7 +41,7 @@ static int nordic_vpr_launcher_init(const struct device *dev)
 	}
 #endif
 
-#if defined(CONFIG_SOC_NRF54L15_CPUAPP) && !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
+#if defined(CONFIG_SOC_NRF54L_CPUAPP_COMMON) && !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
 	nrf_spu_periph_perm_secattr_set(NRF_SPU00, nrf_address_slave_get((uint32_t)config->vpr),
 					true);
 #endif