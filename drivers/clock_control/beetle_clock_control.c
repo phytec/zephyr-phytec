zephyrproject-rtos/zephyr

drivers: beetle_clock_control: Get CPU clock frequency from DTS
The clock control initialization code used system clock
frequency as a CPU clock frequency. This commit corrects
that by obtaining the needed value from DTS.

Signed-off-by: Piotr Zięcik <piotr.ziecik@nordicsemi.no>

drivers/clock_control/beetle_clock_control.c
@@ -233,7 +233,7 @@ static int beetle_clock_control_init(struct device *dev)
 
 static const struct beetle_clock_control_cfg_t beetle_cc_cfg = {
 	.clock_control_id = 0,
-	.freq = CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC,
+	.freq = DT_CPUS_CPU_0_CLOCK_FREQUENCY,
 };
 
 /**

phytec/zephyr-phytec

drivers/clock_control/beetle_clock_control.c
@@ -233,7 +233,7 @@ static int beetle_clock_control_init(struct device *dev)
 
 static const struct beetle_clock_control_cfg_t beetle_cc_cfg = {
 	.clock_control_id = 0,
-	.freq = CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC,
+	.freq = DT_ARM_CORTEX_M3_0_CLOCK_FREQUENCY,
 };
 
 /**