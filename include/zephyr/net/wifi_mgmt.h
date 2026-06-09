Akadimia-Robot/zephyr

net: wifi: shell: add btwt feature support
Add btwt_setup cmd for sap.

Signed-off-by: Rex Chen <rex.chen_1@nxp.com>

include/zephyr/net/wifi_mgmt.h
@@ -85,6 +85,8 @@ enum net_request_wifi_cmd {
 	NET_REQUEST_WIFI_CMD_PS,
 	/** Setup or teardown TWT flow */
 	NET_REQUEST_WIFI_CMD_TWT,
+	/** Setup BTWT flow */
+	NET_REQUEST_WIFI_CMD_BTWT,
 	/** Get power save config */
 	NET_REQUEST_WIFI_CMD_PS_CONFIG,
 	/** Set or get regulatory domain */
@@ -193,6 +195,11 @@ NET_MGMT_DEFINE_REQUEST_HANDLER(NET_REQUEST_WIFI_PS);
 	(_NET_WIFI_BASE | NET_REQUEST_WIFI_CMD_TWT)
 
 NET_MGMT_DEFINE_REQUEST_HANDLER(NET_REQUEST_WIFI_TWT);
+
+#define NET_REQUEST_WIFI_BTWT			\
+	(_NET_WIFI_BASE | NET_REQUEST_WIFI_CMD_BTWT)
+
+NET_MGMT_DEFINE_REQUEST_HANDLER(NET_REQUEST_WIFI_BTWT);
 
 /** Request a Wi-Fi power save configuration */
 #define NET_REQUEST_WIFI_PS_CONFIG				\
@@ -744,6 +751,23 @@ struct wifi_twt_params {
 			/** TWT exponent */
 			uint8_t exponent;
 		} setup;
+		/** Setup specific parameters */
+		struct {
+			/** Broadcast TWT AP config */
+			uint16_t sub_id;
+			/** Range 64-255 */
+			uint8_t nominal_wake;
+			/** Max STA support */
+			uint8_t max_sta_support;
+			/** TWT interval */
+			uint16_t twt_interval;
+			/** TWT offset */
+			uint16_t twt_offset;
+			/** TWT exponent */
+			uint8_t twt_exponent;
+			/** SP gap */
+			uint8_t sp_gap;
+		} btwt;
 		/** Teardown specific parameters */
 		struct {
 			/** Teardown all flows */
@@ -1350,6 +1374,14 @@ struct wifi_mgmt_ops {
 	 * @return 0 if ok, < 0 if error
 	 */
 	int (*set_twt)(const struct device *dev, struct wifi_twt_params *params);
+	/** Setup BTWT flow
+	 *
+	 * @param dev Pointer to the device structure for the driver instance.
+	 * @param params BTWT parameters
+	 *
+	 * @return 0 if ok, < 0 if error
+	 */
+	int (*set_btwt)(const struct device *dev, struct wifi_twt_params *params);
 	/** Get power save config
 	 *
 	 * @param dev Pointer to the device structure for the driver instance.

phytec/zephyr-phytec

include/zephyr/net/wifi_mgmt.h
@@ -85,6 +85,8 @@ enum net_request_wifi_cmd {
 	NET_REQUEST_WIFI_CMD_PS,
 	/** Setup or teardown TWT flow */
 	NET_REQUEST_WIFI_CMD_TWT,
+	/** Setup BTWT flow */
+	NET_REQUEST_WIFI_CMD_BTWT,
 	/** Get power save config */
 	NET_REQUEST_WIFI_CMD_PS_CONFIG,
 	/** Set or get regulatory domain */
@@ -193,6 +195,11 @@ NET_MGMT_DEFINE_REQUEST_HANDLER(NET_REQUEST_WIFI_PS);
 	(_NET_WIFI_BASE | NET_REQUEST_WIFI_CMD_TWT)
 
 NET_MGMT_DEFINE_REQUEST_HANDLER(NET_REQUEST_WIFI_TWT);
+
+#define NET_REQUEST_WIFI_BTWT			\
+	(_NET_WIFI_BASE | NET_REQUEST_WIFI_CMD_BTWT)
+
+NET_MGMT_DEFINE_REQUEST_HANDLER(NET_REQUEST_WIFI_BTWT);
 
 /** Request a Wi-Fi power save configuration */
 #define NET_REQUEST_WIFI_PS_CONFIG				\
@@ -750,6 +757,23 @@ struct wifi_twt_params {
 			 */
 			uint32_t twt_wake_ahead_duration;
 		} setup;
+		/** Setup specific parameters */
+		struct {
+			/** Broadcast TWT AP config */
+			uint16_t sub_id;
+			/** Range 64-255 */
+			uint8_t nominal_wake;
+			/** Max STA support */
+			uint8_t max_sta_support;
+			/** TWT mantissa */
+			uint16_t twt_mantissa;
+			/** TWT offset */
+			uint16_t twt_offset;
+			/** TWT exponent */
+			uint8_t twt_exponent;
+			/** SP gap */
+			uint8_t sp_gap;
+		} btwt;
 		/** Teardown specific parameters */
 		struct {
 			/** Teardown all flows */
@@ -1365,6 +1389,14 @@ struct wifi_mgmt_ops {
 	 * @return 0 if ok, < 0 if error
 	 */
 	int (*set_twt)(const struct device *dev, struct wifi_twt_params *params);
+	/** Setup BTWT flow
+	 *
+	 * @param dev Pointer to the device structure for the driver instance.
+	 * @param params BTWT parameters
+	 *
+	 * @return 0 if ok, < 0 if error
+	 */
+	int (*set_btwt)(const struct device *dev, struct wifi_twt_params *params);
 	/** Get power save config
 	 *
 	 * @param dev Pointer to the device structure for the driver instance.