zephyrproject-rtos/zephyr

drivers: clock: stm32: add PLLSAI handling (common/F4/F7)
Add code handling the pllsai. It is similar to the pllsai1
pllsai2 which can be found on some other socs, except, depending
on the socs the fact that pllsai source can be or not common with
other plls and moreover it can also have additional DIV_DIVQ
and DIV_DIVR additional dividers.
Choice is made to add PLLSAI instead of add further support to
PLLSAI1, in order to stick to the proper naming of the PLLs.

Signed-off-by: Alain Volmat <alain.volmat@foss.st.com>

include/zephyr/drivers/clock_control/stm32_clock_control.h
@@ -220,6 +220,31 @@
 #define STM32_PLLI2S_R_DIVISOR		DT_PROP_OR(DT_NODELABEL(plli2s), div_r, 1)
 #endif
 
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pllsai), st_stm32f4_pllsai_clock, okay) || \
+	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pllsai), st_stm32f7_pllsai_clock, okay)
+#define STM32_PLLSAI_ENABLED	1
+#define STM32_PLLSAI_M_DIVISOR		DT_PROP(DT_NODELABEL(pllsai), div_m)
+#define STM32_PLLSAI_N_MULTIPLIER	DT_PROP(DT_NODELABEL(pllsai), mul_n)
+#define STM32_PLLSAI_P_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_p)
+#define STM32_PLLSAI_P_DIVISOR		DT_PROP_OR(DT_NODELABEL(pllsai), div_p, 1)
+#define STM32_PLLSAI_Q_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_q)
+#define STM32_PLLSAI_DIVQ_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_divq)
+#if (STM32_PLLSAI_Q_ENABLED && !STM32_PLLSAI_DIVQ_ENABLED) || \
+	(!STM32_PLLSAI_Q_ENABLED && STM32_PLLSAI_DIVQ_ENABLED)
+#error "On STM32F4/STM32F7, both div_q and div_divq must be present if one of them is present"
+#endif
+#define STM32_PLLSAI_Q_DIVISOR		DT_PROP_OR(DT_NODELABEL(pllsai), div_q, 1)
+#define STM32_PLLSAI_DIVQ_DIVISOR	DT_PROP_OR(DT_NODELABEL(pllsai), div_divq, 1)
+#define STM32_PLLSAI_R_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_r)
+#define STM32_PLLSAI_DIVR_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_divr)
+#if (STM32_PLLSAI_R_ENABLED && !STM32_PLLSAI_DIVR_ENABLED) || \
+	(!STM32_PLLSAI_R_ENABLED && STM32_PLLSAI_DIVR_ENABLED)
+#error "On STM32F4/STM32F7, both div_r and div_divr must be present if one of them is present"
+#endif
+#define STM32_PLLSAI_R_DIVISOR		DT_PROP_OR(DT_NODELABEL(pllsai), div_r, 1)
+#define STM32_PLLSAI_DIVR_DIVISOR	DT_PROP_OR(DT_NODELABEL(pllsai), div_divr, 1)
+#endif
+
 #if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pllsai1), st_stm32l4_pllsai_clock, okay)
 #define STM32_PLLSAI1_ENABLED	1
 #define STM32_PLLSAI1_M_DIVISOR		DT_PROP(DT_NODELABEL(pllsai1), div_m)
@@ -435,6 +460,19 @@
 
 #endif
 
+/** PLLSAI clock source */
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(pllsai), okay) && \
+	DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), clocks)
+#define DT_PLLSAI_CLOCKS_CTRL	DT_CLOCKS_CTLR(DT_NODELABEL(pllsai))
+#if DT_SAME_NODE(DT_PLLSAI_CLOCKS_CTRL, DT_NODELABEL(clk_hsi))
+#define STM32_PLLSAI_SRC_HSI	1
+#endif
+#if DT_SAME_NODE(DT_PLLSAI_CLOCKS_CTRL, DT_NODELABEL(clk_hse))
+#define STM32_PLLSAI_SRC_HSE	1
+#endif
+
+#endif
+
 /** PLLSAI1 clock source */
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(pllsai1), okay) && \
 	DT_NODE_HAS_PROP(DT_NODELABEL(pllsai1), clocks)
@@ -465,6 +503,20 @@
 #define STM32_PLLSAI2_SRC_HSE	1
 #endif
 
+#endif
+
+/* On STM32F4 series - PLL and PLLSAI share the same source */
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32f4_pll_clock, okay) && \
+	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pllsai), st_stm32f4_pllsai_clock, okay) && \
+	!DT_SAME_NODE(DT_PLL_CLOCKS_CTRL, DT_PLLSAI_CLOCKS_CTRL)
+#error "On STM32F4 series, PLL and PLLSAI must have the same source"
+#endif
+
+/* On STM32F7 series - PLL and PLLSAI share the same source */
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32f7_pll_clock, okay) && \
+	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pllsai), st_stm32f7_pllsai_clock, okay) && \
+	!DT_SAME_NODE(DT_PLL_CLOCKS_CTRL, DT_PLLSAI_CLOCKS_CTRL)
+#error "On STM32F7 series, PLL and PLLSAI must have the same source"
 #endif
 
 /* On STM32L4 series - PLL / PLLSAI1 and PLLSAI2 shared same source */

phytec/zephyr-phytec

include/zephyr/drivers/clock_control/stm32_clock_control.h
@@ -220,6 +220,30 @@
 #define STM32_PLLI2S_R_DIVISOR		DT_PROP_OR(DT_NODELABEL(plli2s), div_r, 1)
 #endif
 
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pllsai), st_stm32fx_pllsai_clock, okay)
+#define STM32_PLLSAI_ENABLED	1
+#define STM32_PLLSAI_M_DIVISOR		DT_PROP(DT_NODELABEL(pllsai), div_m)
+#define STM32_PLLSAI_N_MULTIPLIER	DT_PROP(DT_NODELABEL(pllsai), mul_n)
+#define STM32_PLLSAI_P_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_p)
+#define STM32_PLLSAI_P_DIVISOR		DT_PROP_OR(DT_NODELABEL(pllsai), div_p, 1)
+#define STM32_PLLSAI_Q_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_q)
+#define STM32_PLLSAI_DIVQ_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_divq)
+#if (STM32_PLLSAI_Q_ENABLED && !STM32_PLLSAI_DIVQ_ENABLED) || \
+	(!STM32_PLLSAI_Q_ENABLED && STM32_PLLSAI_DIVQ_ENABLED)
+#error "On STM32F4/STM32F7, both div_q and div_divq must be present if one of them is present"
+#endif
+#define STM32_PLLSAI_Q_DIVISOR		DT_PROP_OR(DT_NODELABEL(pllsai), div_q, 1)
+#define STM32_PLLSAI_DIVQ_DIVISOR	DT_PROP_OR(DT_NODELABEL(pllsai), div_divq, 1)
+#define STM32_PLLSAI_R_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_r)
+#define STM32_PLLSAI_DIVR_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_divr)
+#if (STM32_PLLSAI_R_ENABLED && !STM32_PLLSAI_DIVR_ENABLED) || \
+	(!STM32_PLLSAI_R_ENABLED && STM32_PLLSAI_DIVR_ENABLED)
+#error "On STM32F4/STM32F7, both div_r and div_divr must be present if one of them is present"
+#endif
+#define STM32_PLLSAI_R_DIVISOR		DT_PROP_OR(DT_NODELABEL(pllsai), div_r, 1)
+#define STM32_PLLSAI_DIVR_DIVISOR	DT_PROP_OR(DT_NODELABEL(pllsai), div_divr, 1)
+#endif
+
 #if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pllsai1), st_stm32l4_pllsai_clock, okay)
 #define STM32_PLLSAI1_ENABLED	1
 #define STM32_PLLSAI1_M_DIVISOR		DT_PROP(DT_NODELABEL(pllsai1), div_m)
@@ -435,6 +459,19 @@
 
 #endif
 
+/** PLLSAI clock source */
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(pllsai), okay) && \
+	DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), clocks)
+#define DT_PLLSAI_CLOCKS_CTRL	DT_CLOCKS_CTLR(DT_NODELABEL(pllsai))
+#if DT_SAME_NODE(DT_PLLSAI_CLOCKS_CTRL, DT_NODELABEL(clk_hsi))
+#define STM32_PLLSAI_SRC_HSI	1
+#endif
+#if DT_SAME_NODE(DT_PLLSAI_CLOCKS_CTRL, DT_NODELABEL(clk_hse))
+#define STM32_PLLSAI_SRC_HSE	1
+#endif
+
+#endif
+
 /** PLLSAI1 clock source */
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(pllsai1), okay) && \
 	DT_NODE_HAS_PROP(DT_NODELABEL(pllsai1), clocks)
@@ -465,6 +502,20 @@
 #define STM32_PLLSAI2_SRC_HSE	1
 #endif
 
+#endif
+
+/* On STM32F4 series - PLL and PLLSAI share the same source */
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32f4_pll_clock, okay) && \
+	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pllsai), st_stm32fx_pllsai_clock, okay) && \
+	!DT_SAME_NODE(DT_PLL_CLOCKS_CTRL, DT_PLLSAI_CLOCKS_CTRL)
+#error "On STM32F4 series, PLL and PLLSAI must have the same source"
+#endif
+
+/* On STM32F7 series - PLL and PLLSAI share the same source */
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32f7_pll_clock, okay) && \
+	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pllsai), st_stm32fx_pllsai_clock, okay) && \
+	!DT_SAME_NODE(DT_PLL_CLOCKS_CTRL, DT_PLLSAI_CLOCKS_CTRL)
+#error "On STM32F7 series, PLL and PLLSAI must have the same source"
 #endif
 
 /* On STM32L4 series - PLL / PLLSAI1 and PLLSAI2 shared same source */