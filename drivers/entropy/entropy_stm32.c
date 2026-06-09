egostm/zephyr

drivers: entropy: Add STM32G4X RNG support
Add RNG driver support for STM32G4X SoC series.

Signed-off-by: Richard Osterloh <richard.osterloh@gmail.com>

drivers/entropy/entropy_stm32.c
@@ -17,8 +17,8 @@
 #include <drivers/clock_control.h>
 #include <clock_control/stm32_clock_control.h>
 
-#if !defined(CONFIG_SOC_SERIES_STM32L4X) && !defined(CONFIG_SOC_SERIES_STM32F4X) && !defined(CONFIG_SOC_SERIES_STM32F7X)
-#error RNG only available on STM32F4, STM32F7 and STM32L4 series
+#if !defined(CONFIG_SOC_SERIES_STM32L4X) && !defined(CONFIG_SOC_SERIES_STM32F4X) && !defined(CONFIG_SOC_SERIES_STM32F7X) && !defined(CONFIG_SOC_SERIES_STM32G4X)
+#error RNG only available on STM32F4, STM32F7, STM32L4 and STM32G4 series
 #elif defined(CONFIG_SOC_STM32F401XE)
 #error RNG not available on STM32F401 based SoCs
 #elif defined(CONFIG_SOC_STM32F411XE)
@@ -182,6 +182,14 @@ static int entropy_stm32_rng_init(struct device *dev)
 	 *  Linear Feedback Shift Register
 	 */
 	 LL_RCC_SetRNGClockSource(LL_RCC_RNG_CLKSOURCE_PLLSAI1);
+#elif CONFIG_SOC_SERIES_STM32G4X
+	/* Use the HSI48 for the RNG */
+	LL_RCC_HSI48_Enable();
+	while (!LL_RCC_HSI48_IsReady()) {
+		/* Wait for HSI48 to become ready */
+	}
+
+	LL_RCC_SetRNGClockSource(LL_RCC_USB_CLKSOURCE_HSI48);
 #endif /* CONFIG_SOC_SERIES_STM32L4X */
 
 	dev_data->clock = device_get_binding(STM32_CLOCK_CONTROL_NAME);

phytec/zephyr-phytec

drivers/entropy/entropy_stm32.c
@@ -17,8 +17,8 @@
 #include <drivers/clock_control.h>
 #include <clock_control/stm32_clock_control.h>
 
-#if !defined(CONFIG_SOC_SERIES_STM32L4X) && !defined(CONFIG_SOC_SERIES_STM32F4X) && !defined(CONFIG_SOC_SERIES_STM32F7X)
-#error RNG only available on STM32F4, STM32F7 and STM32L4 series
+#if !defined(CONFIG_SOC_SERIES_STM32L4X) && !defined(CONFIG_SOC_SERIES_STM32F4X) && !defined(CONFIG_SOC_SERIES_STM32F7X) && !defined(CONFIG_SOC_SERIES_STM32G4X)
+#error RNG only available on STM32F4, STM32F7, STM32L4 and STM32G4 series
 #elif defined(CONFIG_SOC_STM32F401XE)
 #error RNG not available on STM32F401 based SoCs
 #elif defined(CONFIG_SOC_STM32F411XE)
@@ -182,6 +182,14 @@ static int entropy_stm32_rng_init(struct device *dev)
 	 *  Linear Feedback Shift Register
 	 */
 	 LL_RCC_SetRNGClockSource(LL_RCC_RNG_CLKSOURCE_PLLSAI1);
+#elif CONFIG_SOC_SERIES_STM32G4X
+	/* Use the HSI48 for the RNG */
+	LL_RCC_HSI48_Enable();
+	while (!LL_RCC_HSI48_IsReady()) {
+		/* Wait for HSI48 to become ready */
+	}
+
+	LL_RCC_SetRNGClockSource(LL_RCC_RNG_CLKSOURCE_HSI48);
 #endif /* CONFIG_SOC_SERIES_STM32L4X */
 
 	dev_data->clock = device_get_binding(STM32_CLOCK_CONTROL_NAME);