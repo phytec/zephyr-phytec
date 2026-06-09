tbursztyka/zephyr

drivers/sensors: Switch adxl362 driver to new SPI API
Replacing legacy API calls by news ones.

Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

drivers/sensor/adxl362/adxl362.h
@@ -155,12 +155,11 @@
 #define ADXL362_RESET_KEY               0x52
 
 struct adxl362_data {
-	struct device *spi;
+	struct spi_config spi;
 	s32_t acc_x;
 	s32_t acc_y;
 	s32_t acc_z;
 	s32_t temp;
-	u8_t spi_slave;
 	u8_t selected_range;
 };
 

phytec/zephyr-phytec

drivers/sensor/adxl362/adxl362.h
@@ -156,11 +156,11 @@
 
 struct adxl362_data {
 	struct device *spi;
+	struct spi_config spi_cfg;
 	s32_t acc_x;
 	s32_t acc_y;
 	s32_t acc_z;
 	s32_t temp;
-	u8_t spi_slave;
 	u8_t selected_range;
 };
 