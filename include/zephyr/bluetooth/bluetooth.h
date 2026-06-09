hermabe/sdk-zephyr

Bluetooth: Host: Add support for PAwR Advertiser
Adds API for Periodic Advertising with Responses - Advertiser:
 - Configure parameters
 - Receive subevent data requests
 - Set subevent data
 - Receive response reports

The support is enabled by CONFIG_BT_PER_ADV_RSP, and requires
a controller that selects CONFIG_BT_CTLR_ADV_PERIODIC_RSP_SUPPORT.

Signed-off-by: Herman Berget <herman.berget@nordicsemi.no>

include/zephyr/bluetooth/bluetooth.h
@@ -73,6 +73,39 @@ struct bt_le_ext_adv_scanned_info {
 	bt_addr_le_t *addr;
 };
 
+struct bt_le_per_adv_data_request {
+	/** The first subevent data can be set for */
+	uint8_t start;
+
+	/** The number of subevents data can be set for */
+	uint8_t count;
+};
+
+struct bt_le_per_adv_response_info {
+	/** The subevent the response was received in */
+	uint8_t subevent;
+
+	/** @brief Status of the subevent indication.
+	 *
+	 * 0 if subevent indication was transmitted.
+	 * 1 if subevent indication was not transmitted.
+	 * All other values RFU.
+	 */
+	uint8_t tx_status;
+
+	/** The TX power of the response in dBm */
+	int8_t tx_power;
+
+	/** The RSSI of the response in dBm */
+	int8_t rssi;
+
+	/** The Constant Tone Extension (CTE) of the advertisement (@ref bt_df_cte_type) */
+	uint8_t cte_type;
+
+	/** The slot the response was received in */
+	uint8_t response_slot;
+};
+
 struct bt_le_ext_adv_cb {
 	/**
 	 * @brief The advertising set has finished sending adv data.
@@ -128,6 +161,32 @@ struct bt_le_ext_adv_cb {
 	 */
 	bool (*rpa_expired)(struct bt_le_ext_adv *adv);
 #endif /* defined(CONFIG_BT_PRIVACY) */
+
+#if defined(CONFIG_BT_PER_ADV_RSP)
+	/**
+	 * @brief The Controller indicates it is ready to transmit one or more subevent.
+	 *
+	 * This callback notifies the application that the controller has requested
+	 * data for upcoming subevents.
+	 *
+	 * @param adv     The advertising set object.
+	 * @param request Information about the upcoming subevents.
+	 */
+	void (*request)(struct bt_le_ext_adv *adv,
+			const struct bt_le_per_adv_data_request *request);
+	/**
+	 * @brief The Controller indicates that one or more synced devices have
+	 * responded to a periodic advertising subevent indication.
+	 *
+	 * @param adv  The advertising set object.
+	 * @param info Information about the responses received.
+	 * @param buf  The received data. NULL if the controller reported
+	 *             that it did not receive any response.
+	 */
+	void (*response)(struct bt_le_ext_adv *adv, struct bt_le_per_adv_response_info *info,
+			 struct net_buf_simple *buf);
+
+#endif /* defined(CONFIG_BT_PER_ADV_RSP) */
 };
 
 /**
@@ -690,6 +749,43 @@ struct bt_le_per_adv_param {
 
 	/** Bit-field of periodic advertising options */
 	uint32_t options;
+
+#if defined(CONFIG_BT_PER_ADV_RSP)
+	/**
+	 * @brief Number of subevents
+	 *
+	 * If zero, the periodic advertiser will be a broadcaster, without responses.
+	 */
+	uint8_t num_subevents;
+
+	/**
+	 * @brief Interval between subevents (N * 1.25 ms)
+	 *
+	 * Shall be between 7.5ms and 318.75 ms.
+	 */
+	uint8_t subevent_interval;
+
+	/**
+	 * @brief Time between the advertising packet in a subevent and the
+	 * first response slot (N * 1.25 ms)
+	 *
+	 */
+	uint8_t response_slot_delay;
+
+	/**
+	 * @brief Time between response slots (N * 0.125 ms)
+	 *
+	 * Shall be between 0.25 and 31.875 ms.
+	 */
+	uint8_t response_slot_spacing;
+
+	/**
+	 * @brief Number of subevent response slots
+	 *
+	 * If zero, response_slot_delay and response_slot_spacing are ignored.
+	 */
+	uint8_t num_response_slots;
+#endif /* CONFIG_BT_PER_ADV_RSP */
 };
 
 /**
@@ -1156,6 +1252,38 @@ int bt_le_per_adv_set_param(struct bt_le_ext_adv *adv,
  */
 int bt_le_per_adv_set_data(const struct bt_le_ext_adv *adv,
 			   const struct bt_data *ad, size_t ad_len);
+
+struct bt_le_per_adv_subevent_data_params {
+	/** The subevent to set data for */
+	uint8_t subevent;
+
+	/** The first response slot to listen to */
+	uint8_t response_slot_start;
+
+	/** The number of response slots to listen to */
+	uint8_t response_slot_count;
+
+	/** The data to send */
+	const struct net_buf_simple *data;
+};
+
+/**
+ * @brief Set the periodic advertising with response subevent data.
+ *
+ * Set the data for one or more subevents of a Periodic Advertising with
+ * Responses Advertiser in reply data request.
+ *
+ * @pre There are @p num_subevents elements in @p params.
+ * @pre The controller has requested data for the subevents in @p params.
+ *
+ * @param adv           The extended advertiser the PAwR train belongs to.
+ * @param num_subevents The number of subevents to set data for.
+ * @param params        Subevent parameters.
+ *
+ * @return Zero on success or (negative) error code otherwise.
+ */
+int bt_le_per_adv_set_subevent_data(const struct bt_le_ext_adv *adv, uint8_t num_subevents,
+				    const struct bt_le_per_adv_subevent_data_params *params);
 
 /**
  * @brief Starts periodic advertising.

subsys/bluetooth/host/adv.c
@@ -1736,12 +1736,25 @@ static void adv_timeout(struct k_work *work)
 int bt_le_per_adv_set_param(struct bt_le_ext_adv *adv,
 			    const struct bt_le_per_adv_param *param)
 {
+#if defined(CONFIG_BT_PER_ADV_RSP)
+	struct bt_hci_cp_le_set_per_adv_param_v2 *cp;
+#else
 	struct bt_hci_cp_le_set_per_adv_param *cp;
+#endif /* CONFIG_BT_PER_ADV_RSP */
+
+	uint16_t opcode;
+	uint16_t size;
 	struct net_buf *buf;
 	int err;
 	uint16_t props = 0;
 
-	if (!BT_FEAT_LE_EXT_PER_ADV(bt_dev.le.features)) {
+	if (IS_ENABLED(CONFIG_BT_PER_ADV_RSP) && BT_FEAT_LE_PAWR_ADVERTISER(bt_dev.le.features)) {
+		opcode = BT_HCI_OP_LE_SET_PER_ADV_PARAM_V2;
+		size = sizeof(struct bt_hci_cp_le_set_per_adv_param_v2);
+	} else if (BT_FEAT_LE_EXT_PER_ADV(bt_dev.le.features)) {
+		opcode = BT_HCI_OP_LE_SET_PER_ADV_PARAM;
+		size = sizeof(struct bt_hci_cp_le_set_per_adv_param);
+	} else {
 		return -ENOTSUP;
 	}
 
@@ -1764,13 +1777,13 @@ int bt_le_per_adv_set_param(struct bt_le_ext_adv *adv,
 		return -ENOTSUP;
 	}
 
-	buf = bt_hci_cmd_create(BT_HCI_OP_LE_SET_PER_ADV_PARAM, sizeof(*cp));
+	buf = bt_hci_cmd_create(opcode, size);
 	if (!buf) {
 		return -ENOBUFS;
 	}
 
-	cp = net_buf_add(buf, sizeof(*cp));
-	(void)memset(cp, 0, sizeof(*cp));
+	cp = net_buf_add(buf, size);
+	(void)memset(cp, 0, size);
 
 	cp->handle = adv->handle;
 	cp->min_interval = sys_cpu_to_le16(param->interval_min);
@@ -1781,7 +1794,18 @@ int bt_le_per_adv_set_param(struct bt_le_ext_adv *adv,
 	}
 
 	cp->props = sys_cpu_to_le16(props);
-	err = bt_hci_cmd_send_sync(BT_HCI_OP_LE_SET_PER_ADV_PARAM, buf, NULL);
+
+#if defined(CONFIG_BT_PER_ADV_RSP)
+	if (opcode == BT_HCI_OP_LE_SET_PER_ADV_PARAM_V2) {
+		cp->num_subevents = param->num_subevents;
+		cp->subevent_interval = param->subevent_interval;
+		cp->response_slot_delay = param->response_slot_delay;
+		cp->response_slot_spacing = param->response_slot_spacing;
+		cp->num_response_slots = param->num_response_slots;
+	}
+#endif /* CONFIG_BT_PER_ADV_RSP */
+
+	err = bt_hci_cmd_send_sync(opcode, buf, NULL);
 	if (err) {
 		return err;
 	}
@@ -1827,6 +1851,50 @@ int bt_le_per_adv_set_data(const struct bt_le_ext_adv *adv,
 	}
 
 	return hci_set_per_adv_data(adv, ad, ad_len);
+}
+
+int bt_le_per_adv_set_subevent_data(const struct bt_le_ext_adv *adv, uint8_t num_subevents,
+				    const struct bt_le_per_adv_subevent_data_params *params)
+{
+	struct bt_hci_cp_le_set_pawr_subevent_data *cp;
+	struct bt_hci_cp_le_set_pawr_subevent_data_element *element;
+	struct net_buf *buf;
+	size_t cmd_length = sizeof(*cp);
+
+	if (!BT_FEAT_LE_PAWR_ADVERTISER(bt_dev.le.features)) {
+		return -ENOTSUP;
+	}
+
+	for (size_t i = 0; i < num_subevents; i++)
+	{
+		cmd_length += sizeof(struct bt_hci_cp_le_set_pawr_subevent_data_element);
+		cmd_length += params[i].data->len;
+	}
+
+	if (cmd_length > 0xFF) {
+		return -EINVAL;
+	}
+
+	buf = bt_hci_cmd_create(BT_HCI_OP_LE_SET_PER_ADV_SUBEVENT_DATA, (uint8_t)cmd_length);
+	if (!buf) {
+		return -ENOBUFS;
+	}
+
+	cp = net_buf_add(buf, sizeof(*cp));
+	cp->adv_handle = adv->handle;
+	cp->num_subevents = num_subevents;
+
+	for (size_t i = 0; i < num_subevents; i++)
+	{
+		element = net_buf_add(buf, sizeof(*element));
+		element->subevent = params[i].subevent;
+		element->response_slot_start = params[i].response_slot_start;
+		element->response_slot_count = params[i].response_slot_count;
+		element->subevent_data_length = params[i].data->len;
+		net_buf_add_mem(buf, params[i].data->data, params[i].data->len);
+	}
+
+	return bt_hci_cmd_send_sync(BT_HCI_OP_LE_SET_PER_ADV_SUBEVENT_DATA, buf, NULL);
 }
 
 static int bt_le_per_adv_enable(struct bt_le_ext_adv *adv, bool enable)
@@ -1888,6 +1956,102 @@ int bt_le_per_adv_start(struct bt_le_ext_adv *adv)
 int bt_le_per_adv_stop(struct bt_le_ext_adv *adv)
 {
 	return bt_le_per_adv_enable(adv, false);
+}
+
+void bt_hci_le_per_adv_subevent_data_request(struct net_buf *buf)
+{
+	struct bt_hci_evt_le_per_adv_subevent_data_request *evt;
+	struct bt_le_per_adv_data_request request;
+	struct bt_le_ext_adv *adv;
+
+	if (buf->len < sizeof(struct bt_hci_evt_le_per_adv_subevent_data_request)) {
+		LOG_ERR("Invalid data request");
+
+		return;
+	}
+
+	evt = net_buf_pull_mem(buf, sizeof(struct bt_hci_evt_le_per_adv_subevent_data_request));
+	adv = bt_adv_lookup_handle(evt->adv_handle);
+	if (!adv) {
+		LOG_ERR("Unknown advertising handle %d", evt->adv_handle);
+
+		return;
+	}
+
+	request.start = evt->subevent_start;
+	request.count = evt->subevent_data_count;
+
+	if (adv->cb && adv->cb->request) {
+		adv->cb->request(adv, &request);
+	}
+}
+
+void bt_hci_le_per_adv_response_report(struct net_buf *buf)
+{
+	struct bt_hci_evt_le_per_adv_response_report *evt;
+	struct bt_hci_evt_le_per_adv_response *response;
+	struct bt_le_ext_adv *adv;
+	struct bt_le_per_adv_response_info info;
+	struct net_buf_simple data;
+
+	if (buf->len < sizeof(struct bt_hci_evt_le_per_adv_response_report)) {
+		LOG_ERR("Invalid response report");
+
+		return;
+	}
+
+	evt = net_buf_pull_mem(buf, sizeof(struct bt_hci_evt_le_per_adv_response_report));
+	adv = bt_adv_lookup_handle(evt->adv_handle);
+	if (!adv) {
+		LOG_ERR("Unknown advertising handle %d", evt->adv_handle);
+
+		return;
+	}
+
+	info.subevent = evt->subevent;
+	info.tx_status = evt->tx_status;
+
+	for (uint8_t i = 0; i < evt->num_responses; i++) {
+		if (buf->len < sizeof(struct bt_hci_evt_le_per_adv_response)) {
+			LOG_ERR("Invalid response report");
+
+			return;
+		}
+
+		response = net_buf_pull_mem(buf, sizeof(struct bt_hci_evt_le_per_adv_response));
+		info.tx_power = response->tx_power;
+		info.rssi = response->rssi;
+		info.cte_type = BIT(response->cte_type);
+		info.response_slot = response->response_slot;
+
+		if (buf->len < response->data_length) {
+			LOG_ERR("Invalid response report");
+
+			return;
+		}
+
+		if (response->data_status == BT_HCI_LE_ADV_EVT_TYPE_DATA_STATUS_PARTIAL) {
+			LOG_WRN("Incomplete response report received, discarding");
+			(void)net_buf_pull_mem(buf, response->data_length);
+		} else if (response->data_status == BT_HCI_LE_ADV_EVT_TYPE_DATA_STATUS_RX_FAILED) {
+			(void)net_buf_pull_mem(buf, response->data_length);
+
+			if (adv->cb && adv->cb->response) {
+				adv->cb->response(adv, &info, NULL);
+			}
+		} else if (response->data_status == BT_HCI_LE_ADV_EVT_TYPE_DATA_STATUS_COMPLETE) {
+			net_buf_simple_init_with_data(&data,
+						      net_buf_pull_mem(buf, response->data_length),
+						      response->data_length);
+
+			if (adv->cb && adv->cb->response) {
+				adv->cb->response(adv, &info, &data);
+			}
+		} else {
+			LOG_ERR("Invalid data status %d", response->data_status);
+			(void)net_buf_pull_mem(buf, response->data_length);
+		}
+	}
 }
 
 #if defined(CONFIG_BT_PER_ADV_SYNC_TRANSFER_SENDER)

phytec/zephyr-phytec

include/zephyr/bluetooth/bluetooth.h
@@ -74,6 +74,39 @@ struct bt_le_ext_adv_scanned_info {
 	bt_addr_le_t *addr;
 };
 
+struct bt_le_per_adv_data_request {
+	/** The first subevent data can be set for */
+	uint8_t start;
+
+	/** The number of subevents data can be set for */
+	uint8_t count;
+};
+
+struct bt_le_per_adv_response_info {
+	/** The subevent the response was received in */
+	uint8_t subevent;
+
+	/** @brief Status of the subevent indication.
+	 *
+	 * 0 if subevent indication was transmitted.
+	 * 1 if subevent indication was not transmitted.
+	 * All other values RFU.
+	 */
+	uint8_t tx_status;
+
+	/** The TX power of the response in dBm */
+	int8_t tx_power;
+
+	/** The RSSI of the response in dBm */
+	int8_t rssi;
+
+	/** The Constant Tone Extension (CTE) of the advertisement (@ref bt_df_cte_type) */
+	uint8_t cte_type;
+
+	/** The slot the response was received in */
+	uint8_t response_slot;
+};
+
 struct bt_le_ext_adv_cb {
 	/**
 	 * @brief The advertising set has finished sending adv data.
@@ -129,6 +162,32 @@ struct bt_le_ext_adv_cb {
 	 */
 	bool (*rpa_expired)(struct bt_le_ext_adv *adv);
 #endif /* defined(CONFIG_BT_PRIVACY) */
+
+#if defined(CONFIG_BT_PER_ADV_RSP)
+	/**
+	 * @brief The Controller indicates it is ready to transmit one or more subevent.
+	 *
+	 * This callback notifies the application that the controller has requested
+	 * data for upcoming subevents.
+	 *
+	 * @param adv     The advertising set object.
+	 * @param request Information about the upcoming subevents.
+	 */
+	void (*pawr_data_request)(struct bt_le_ext_adv *adv,
+				  const struct bt_le_per_adv_data_request *request);
+	/**
+	 * @brief The Controller indicates that one or more synced devices have
+	 * responded to a periodic advertising subevent indication.
+	 *
+	 * @param adv  The advertising set object.
+	 * @param info Information about the responses received.
+	 * @param buf  The received data. NULL if the controller reported
+	 *             that it did not receive any response.
+	 */
+	void (*pawr_response)(struct bt_le_ext_adv *adv, struct bt_le_per_adv_response_info *info,
+			      struct net_buf_simple *buf);
+
+#endif /* defined(CONFIG_BT_PER_ADV_RSP) */
 };
 
 /**
@@ -735,6 +794,43 @@ struct bt_le_per_adv_param {
 
 	/** Bit-field of periodic advertising options */
 	uint32_t options;
+
+#if defined(CONFIG_BT_PER_ADV_RSP)
+	/**
+	 * @brief Number of subevents
+	 *
+	 * If zero, the periodic advertiser will be a broadcaster, without responses.
+	 */
+	uint8_t num_subevents;
+
+	/**
+	 * @brief Interval between subevents (N * 1.25 ms)
+	 *
+	 * Shall be between 7.5ms and 318.75 ms.
+	 */
+	uint8_t subevent_interval;
+
+	/**
+	 * @brief Time between the advertising packet in a subevent and the
+	 * first response slot (N * 1.25 ms)
+	 *
+	 */
+	uint8_t response_slot_delay;
+
+	/**
+	 * @brief Time between response slots (N * 0.125 ms)
+	 *
+	 * Shall be between 0.25 and 31.875 ms.
+	 */
+	uint8_t response_slot_spacing;
+
+	/**
+	 * @brief Number of subevent response slots
+	 *
+	 * If zero, response_slot_delay and response_slot_spacing are ignored.
+	 */
+	uint8_t num_response_slots;
+#endif /* CONFIG_BT_PER_ADV_RSP */
 };
 
 /**
@@ -1201,6 +1297,38 @@ int bt_le_per_adv_set_param(struct bt_le_ext_adv *adv,
  */
 int bt_le_per_adv_set_data(const struct bt_le_ext_adv *adv,
 			   const struct bt_data *ad, size_t ad_len);
+
+struct bt_le_per_adv_subevent_data_params {
+	/** The subevent to set data for */
+	uint8_t subevent;
+
+	/** The first response slot to listen to */
+	uint8_t response_slot_start;
+
+	/** The number of response slots to listen to */
+	uint8_t response_slot_count;
+
+	/** The data to send */
+	const struct net_buf_simple *data;
+};
+
+/**
+ * @brief Set the periodic advertising with response subevent data.
+ *
+ * Set the data for one or more subevents of a Periodic Advertising with
+ * Responses Advertiser in reply data request.
+ *
+ * @pre There are @p num_subevents elements in @p params.
+ * @pre The controller has requested data for the subevents in @p params.
+ *
+ * @param adv           The extended advertiser the PAwR train belongs to.
+ * @param num_subevents The number of subevents to set data for.
+ * @param params        Subevent parameters.
+ *
+ * @return Zero on success or (negative) error code otherwise.
+ */
+int bt_le_per_adv_set_subevent_data(const struct bt_le_ext_adv *adv, uint8_t num_subevents,
+				    const struct bt_le_per_adv_subevent_data_params *params);
 
 /**
  * @brief Starts periodic advertising.

subsys/bluetooth/host/adv.c
@@ -1779,12 +1779,29 @@ static void adv_timeout(struct k_work *work)
 int bt_le_per_adv_set_param(struct bt_le_ext_adv *adv,
 			    const struct bt_le_per_adv_param *param)
 {
+#if defined(CONFIG_BT_PER_ADV_RSP)
+	/* The v2 struct can be used even if we end up sending a v1 command
+	 * because they have the same layout for the common fields.
+	 * V2 simply adds fields at the end of the v1 command.
+	 */
+	struct bt_hci_cp_le_set_per_adv_param_v2 *cp;
+#else
 	struct bt_hci_cp_le_set_per_adv_param *cp;
+#endif /* CONFIG_BT_PER_ADV_RSP */
+
+	uint16_t opcode;
+	uint16_t size;
 	struct net_buf *buf;
 	int err;
 	uint16_t props = 0;
 
-	if (!BT_FEAT_LE_EXT_PER_ADV(bt_dev.le.features)) {
+	if (IS_ENABLED(CONFIG_BT_PER_ADV_RSP) && BT_FEAT_LE_PAWR_ADVERTISER(bt_dev.le.features)) {
+		opcode = BT_HCI_OP_LE_SET_PER_ADV_PARAM_V2;
+		size = sizeof(struct bt_hci_cp_le_set_per_adv_param_v2);
+	} else if (BT_FEAT_LE_EXT_PER_ADV(bt_dev.le.features)) {
+		opcode = BT_HCI_OP_LE_SET_PER_ADV_PARAM;
+		size = sizeof(struct bt_hci_cp_le_set_per_adv_param);
+	} else {
 		return -ENOTSUP;
 	}
 
@@ -1807,13 +1824,13 @@ int bt_le_per_adv_set_param(struct bt_le_ext_adv *adv,
 		return -ENOTSUP;
 	}
 
-	buf = bt_hci_cmd_create(BT_HCI_OP_LE_SET_PER_ADV_PARAM, sizeof(*cp));
+	buf = bt_hci_cmd_create(opcode, size);
 	if (!buf) {
 		return -ENOBUFS;
 	}
 
-	cp = net_buf_add(buf, sizeof(*cp));
-	(void)memset(cp, 0, sizeof(*cp));
+	cp = net_buf_add(buf, size);
+	(void)memset(cp, 0, size);
 
 	cp->handle = adv->handle;
 	cp->min_interval = sys_cpu_to_le16(param->interval_min);
@@ -1824,7 +1841,18 @@ int bt_le_per_adv_set_param(struct bt_le_ext_adv *adv,
 	}
 
 	cp->props = sys_cpu_to_le16(props);
-	err = bt_hci_cmd_send_sync(BT_HCI_OP_LE_SET_PER_ADV_PARAM, buf, NULL);
+
+#if defined(CONFIG_BT_PER_ADV_RSP)
+	if (opcode == BT_HCI_OP_LE_SET_PER_ADV_PARAM_V2) {
+		cp->num_subevents = param->num_subevents;
+		cp->subevent_interval = param->subevent_interval;
+		cp->response_slot_delay = param->response_slot_delay;
+		cp->response_slot_spacing = param->response_slot_spacing;
+		cp->num_response_slots = param->num_response_slots;
+	}
+#endif /* CONFIG_BT_PER_ADV_RSP */
+
+	err = bt_hci_cmd_send_sync(opcode, buf, NULL);
 	if (err) {
 		return err;
 	}
@@ -1870,6 +1898,48 @@ int bt_le_per_adv_set_data(const struct bt_le_ext_adv *adv,
 	}
 
 	return hci_set_per_adv_data(adv, ad, ad_len);
+}
+
+int bt_le_per_adv_set_subevent_data(const struct bt_le_ext_adv *adv, uint8_t num_subevents,
+				    const struct bt_le_per_adv_subevent_data_params *params)
+{
+	struct bt_hci_cp_le_set_pawr_subevent_data *cp;
+	struct bt_hci_cp_le_set_pawr_subevent_data_element *element;
+	struct net_buf *buf;
+	size_t cmd_length = sizeof(*cp);
+
+	if (!BT_FEAT_LE_PAWR_ADVERTISER(bt_dev.le.features)) {
+		return -ENOTSUP;
+	}
+
+	for (size_t i = 0; i < num_subevents; i++) {
+		cmd_length += sizeof(struct bt_hci_cp_le_set_pawr_subevent_data_element);
+		cmd_length += params[i].data->len;
+	}
+
+	if (cmd_length > 0xFF) {
+		return -EINVAL;
+	}
+
+	buf = bt_hci_cmd_create(BT_HCI_OP_LE_SET_PER_ADV_SUBEVENT_DATA, (uint8_t)cmd_length);
+	if (!buf) {
+		return -ENOBUFS;
+	}
+
+	cp = net_buf_add(buf, sizeof(*cp));
+	cp->adv_handle = adv->handle;
+	cp->num_subevents = num_subevents;
+
+	for (size_t i = 0; i < num_subevents; i++) {
+		element = net_buf_add(buf, sizeof(*element));
+		element->subevent = params[i].subevent;
+		element->response_slot_start = params[i].response_slot_start;
+		element->response_slot_count = params[i].response_slot_count;
+		element->subevent_data_length = params[i].data->len;
+		net_buf_add_mem(buf, params[i].data->data, params[i].data->len);
+	}
+
+	return bt_hci_cmd_send_sync(BT_HCI_OP_LE_SET_PER_ADV_SUBEVENT_DATA, buf, NULL);
 }
 
 static int bt_le_per_adv_enable(struct bt_le_ext_adv *adv, bool enable)
@@ -1932,6 +2002,104 @@ int bt_le_per_adv_stop(struct bt_le_ext_adv *adv)
 {
 	return bt_le_per_adv_enable(adv, false);
 }
+
+#if defined(CONFIG_BT_PER_ADV_RSP)
+void bt_hci_le_per_adv_subevent_data_request(struct net_buf *buf)
+{
+	struct bt_hci_evt_le_per_adv_subevent_data_request *evt;
+	struct bt_le_per_adv_data_request request;
+	struct bt_le_ext_adv *adv;
+
+	if (buf->len < sizeof(struct bt_hci_evt_le_per_adv_subevent_data_request)) {
+		LOG_ERR("Invalid data request");
+
+		return;
+	}
+
+	evt = net_buf_pull_mem(buf, sizeof(struct bt_hci_evt_le_per_adv_subevent_data_request));
+	adv = bt_adv_lookup_handle(evt->adv_handle);
+	if (!adv) {
+		LOG_ERR("Unknown advertising handle %d", evt->adv_handle);
+
+		return;
+	}
+
+	request.start = evt->subevent_start;
+	request.count = evt->subevent_data_count;
+
+	if (adv->cb && adv->cb->pawr_data_request) {
+		adv->cb->pawr_data_request(adv, &request);
+	}
+}
+
+void bt_hci_le_per_adv_response_report(struct net_buf *buf)
+{
+	struct bt_hci_evt_le_per_adv_response_report *evt;
+	struct bt_hci_evt_le_per_adv_response *response;
+	struct bt_le_ext_adv *adv;
+	struct bt_le_per_adv_response_info info;
+	struct net_buf_simple data;
+
+	if (buf->len < sizeof(struct bt_hci_evt_le_per_adv_response_report)) {
+		LOG_ERR("Invalid response report");
+
+		return;
+	}
+
+	evt = net_buf_pull_mem(buf, sizeof(struct bt_hci_evt_le_per_adv_response_report));
+	adv = bt_adv_lookup_handle(evt->adv_handle);
+	if (!adv) {
+		LOG_ERR("Unknown advertising handle %d", evt->adv_handle);
+
+		return;
+	}
+
+	info.subevent = evt->subevent;
+	info.tx_status = evt->tx_status;
+
+	for (uint8_t i = 0; i < evt->num_responses; i++) {
+		if (buf->len < sizeof(struct bt_hci_evt_le_per_adv_response)) {
+			LOG_ERR("Invalid response report");
+
+			return;
+		}
+
+		response = net_buf_pull_mem(buf, sizeof(struct bt_hci_evt_le_per_adv_response));
+		info.tx_power = response->tx_power;
+		info.rssi = response->rssi;
+		info.cte_type = BIT(response->cte_type);
+		info.response_slot = response->response_slot;
+
+		if (buf->len < response->data_length) {
+			LOG_ERR("Invalid response report");
+
+			return;
+		}
+
+		if (response->data_status == BT_HCI_LE_ADV_EVT_TYPE_DATA_STATUS_PARTIAL) {
+			LOG_WRN("Incomplete response report received, discarding");
+			(void)net_buf_pull_mem(buf, response->data_length);
+		} else if (response->data_status == BT_HCI_LE_ADV_EVT_TYPE_DATA_STATUS_RX_FAILED) {
+			(void)net_buf_pull_mem(buf, response->data_length);
+
+			if (adv->cb && adv->cb->pawr_response) {
+				adv->cb->pawr_response(adv, &info, NULL);
+			}
+		} else if (response->data_status == BT_HCI_LE_ADV_EVT_TYPE_DATA_STATUS_COMPLETE) {
+			net_buf_simple_init_with_data(&data,
+						      net_buf_pull_mem(buf, response->data_length),
+						      response->data_length);
+
+			if (adv->cb && adv->cb->pawr_response) {
+				adv->cb->pawr_response(adv, &info, &data);
+			}
+		} else {
+			LOG_ERR("Invalid data status %d", response->data_status);
+			(void)net_buf_pull_mem(buf, response->data_length);
+		}
+	}
+}
+#endif /* CONFIG_BT_PER_ADV_RSP */
 
 #if defined(CONFIG_BT_PER_ADV_SYNC_TRANSFER_SENDER)
 int bt_le_per_adv_set_info_transfer(const struct bt_le_ext_adv *adv,