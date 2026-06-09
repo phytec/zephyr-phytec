vanti/zephyr

net: sockets: Remove socket offloading interface
Instead of using a custom offloading interface, users can use
`NET_SOCKET_REGISTER` macro to register custom socket API provider. This
solution removes a limitation, that only one offloaded interface can be
registered and that it cannot be used together with native IP stack.

The only exception remainig are DNS releated operations -
`getaddrinfo`/`freeaddrinfo`, which, when offloaded, have to be
registered specifically.

Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

include/net/socket_offload.h
@@ -12,153 +12,38 @@
 #ifndef ZEPHYR_INCLUDE_NET_SOCKET_OFFLOAD_H_
 #define ZEPHYR_INCLUDE_NET_SOCKET_OFFLOAD_H_
 
-#include <net/socket_offload_ops.h>
+#include <net/net_ip.h>
+#include <net/socket.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-extern const struct socket_offload *socket_ops;
+/**
+ * @brief An offloaded Socket DNS API interface
+ *
+ * It is assumed that these offload functions follow the
+ * POSIX socket API standard for arguments, return values and setting of errno.
+ */
+struct socket_dns_offload {
+	int (*getaddrinfo)(const char *node, const char *service,
+			   const struct addrinfo *hints,
+			   struct addrinfo **res);
+	void (*freeaddrinfo)(struct addrinfo *res);
+};
 
-static inline int socket(int family, int type, int proto)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->socket);
+/**
+ * @brief Register an offloaded socket DNS API interface.
+ *
+ * @param ops A pointer to the offloaded socket DNS API interface.
+ */
+void socket_offload_dns_register(const struct socket_dns_offload *ops);
 
-	return socket_ops->socket(family, type, proto);
-}
+int socket_offload_getaddrinfo(const char *node, const char *service,
+			       const struct zsock_addrinfo *hints,
+			       struct zsock_addrinfo **res);
 
-static inline int close(int sock)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->close);
-
-	return socket_ops->close(sock);
-}
-
-static inline int accept(int sock, struct sockaddr *addr,
-			 socklen_t *addrlen)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->accept);
-
-	return socket_ops->accept(sock, addr, addrlen);
-}
-
-
-static inline int bind(int sock, const struct sockaddr *addr,
-		       socklen_t addrlen)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->bind);
-
-	return socket_ops->bind(sock, addr, addrlen);
-}
-
-static inline int listen(int sock, int backlog)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->listen);
-
-	return socket_ops->listen(sock, backlog);
-}
-
-static inline int connect(int sock, const struct sockaddr *addr,
-			  socklen_t addrlen)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->connect);
-
-	return socket_ops->connect(sock, addr, addrlen);
-}
-
-static inline int poll(struct pollfd *fds, int nfds, int timeout)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->poll);
-
-	return socket_ops->poll(fds, nfds, timeout);
-}
-
-static inline int setsockopt(int sock, int level, int optname,
-			     const void *optval,
-			     socklen_t optlen)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->setsockopt);
-
-	return socket_ops->setsockopt(sock, level, optname, optval, optlen);
-}
-
-static inline int getsockopt(int sock, int level, int optname,
-			     void *optval, socklen_t *optlen)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->getsockopt);
-
-	return socket_ops->getsockopt(sock, level, optname, optval, optlen);
-}
-
-static inline ssize_t recv(int sock, void *buf, size_t max_len,
-				      int flags)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->recv);
-
-	return socket_ops->recv(sock, buf, max_len, flags);
-}
-
-static inline ssize_t recvfrom(int sock, void *buf,
-			       short int len,
-			       short int flags,
-			       struct sockaddr *from,
-			       socklen_t *fromlen)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->recvfrom);
-
-	return socket_ops->recvfrom(sock, buf, len, flags, from, fromlen);
-}
-
-static inline ssize_t send(int sock, const void *buf, size_t len,
-			   int flags)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->send);
-
-	return socket_ops->send(sock, buf, len, flags);
-}
-
-static inline ssize_t sendto(int sock, const void *buf,
-			     size_t len, int flags,
-			     const struct sockaddr *to,
-			     socklen_t tolen)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->sendto);
-
-	return socket_ops->sendto(sock, buf, len, flags, to, tolen);
-}
-
-static inline int getaddrinfo(const char *node, const char *service,
-			      const struct addrinfo *hints,
-			      struct addrinfo **res)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->getaddrinfo);
-
-	return socket_ops->getaddrinfo(node, service, hints, res);
-}
-
-static inline void freeaddrinfo(struct addrinfo *res)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->freeaddrinfo);
-
-	return socket_ops->freeaddrinfo(res);
-}
-
-int fcntl(int fd, int cmd, ...);
+void socket_offload_freeaddrinfo(struct zsock_addrinfo *res);
 
 #ifdef __cplusplus
 }

phytec/zephyr-phytec

include/net/socket_offload.h
@@ -12,153 +12,38 @@
 #ifndef ZEPHYR_INCLUDE_NET_SOCKET_OFFLOAD_H_
 #define ZEPHYR_INCLUDE_NET_SOCKET_OFFLOAD_H_
 
-#include <net/socket_offload_ops.h>
+#include <net/net_ip.h>
+#include <net/socket.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-extern const struct socket_offload *socket_ops;
+/**
+ * @brief An offloaded Socket DNS API interface
+ *
+ * It is assumed that these offload functions follow the
+ * POSIX socket API standard for arguments, return values and setting of errno.
+ */
+struct socket_dns_offload {
+	int (*getaddrinfo)(const char *node, const char *service,
+			   const struct zsock_addrinfo *hints,
+			   struct zsock_addrinfo **res);
+	void (*freeaddrinfo)(struct zsock_addrinfo *res);
+};
 
-static inline int socket(int family, int type, int proto)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->socket);
+/**
+ * @brief Register an offloaded socket DNS API interface.
+ *
+ * @param ops A pointer to the offloaded socket DNS API interface.
+ */
+void socket_offload_dns_register(const struct socket_dns_offload *ops);
 
-	return socket_ops->socket(family, type, proto);
-}
+int socket_offload_getaddrinfo(const char *node, const char *service,
+			       const struct zsock_addrinfo *hints,
+			       struct zsock_addrinfo **res);
 
-static inline int close(int sock)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->close);
-
-	return socket_ops->close(sock);
-}
-
-static inline int accept(int sock, struct sockaddr *addr,
-			 socklen_t *addrlen)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->accept);
-
-	return socket_ops->accept(sock, addr, addrlen);
-}
-
-
-static inline int bind(int sock, const struct sockaddr *addr,
-		       socklen_t addrlen)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->bind);
-
-	return socket_ops->bind(sock, addr, addrlen);
-}
-
-static inline int listen(int sock, int backlog)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->listen);
-
-	return socket_ops->listen(sock, backlog);
-}
-
-static inline int connect(int sock, const struct sockaddr *addr,
-			  socklen_t addrlen)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->connect);
-
-	return socket_ops->connect(sock, addr, addrlen);
-}
-
-static inline int poll(struct pollfd *fds, int nfds, int timeout)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->poll);
-
-	return socket_ops->poll(fds, nfds, timeout);
-}
-
-static inline int setsockopt(int sock, int level, int optname,
-			     const void *optval,
-			     socklen_t optlen)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->setsockopt);
-
-	return socket_ops->setsockopt(sock, level, optname, optval, optlen);
-}
-
-static inline int getsockopt(int sock, int level, int optname,
-			     void *optval, socklen_t *optlen)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->getsockopt);
-
-	return socket_ops->getsockopt(sock, level, optname, optval, optlen);
-}
-
-static inline ssize_t recv(int sock, void *buf, size_t max_len,
-				      int flags)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->recv);
-
-	return socket_ops->recv(sock, buf, max_len, flags);
-}
-
-static inline ssize_t recvfrom(int sock, void *buf,
-			       short int len,
-			       short int flags,
-			       struct sockaddr *from,
-			       socklen_t *fromlen)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->recvfrom);
-
-	return socket_ops->recvfrom(sock, buf, len, flags, from, fromlen);
-}
-
-static inline ssize_t send(int sock, const void *buf, size_t len,
-			   int flags)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->send);
-
-	return socket_ops->send(sock, buf, len, flags);
-}
-
-static inline ssize_t sendto(int sock, const void *buf,
-			     size_t len, int flags,
-			     const struct sockaddr *to,
-			     socklen_t tolen)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->sendto);
-
-	return socket_ops->sendto(sock, buf, len, flags, to, tolen);
-}
-
-static inline int getaddrinfo(const char *node, const char *service,
-			      const struct addrinfo *hints,
-			      struct addrinfo **res)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->getaddrinfo);
-
-	return socket_ops->getaddrinfo(node, service, hints, res);
-}
-
-static inline void freeaddrinfo(struct addrinfo *res)
-{
-	__ASSERT_NO_MSG(socket_ops);
-	__ASSERT_NO_MSG(socket_ops->freeaddrinfo);
-
-	return socket_ops->freeaddrinfo(res);
-}
-
-int fcntl(int fd, int cmd, ...);
+void socket_offload_freeaddrinfo(struct zsock_addrinfo *res);
 
 #ifdef __cplusplus
 }