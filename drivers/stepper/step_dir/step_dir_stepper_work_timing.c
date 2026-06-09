faxe1008/zephyr

drivers: stepper: Change stepper velocity to step interval
Change the stepper API to instead of changing the stepper speed based on
the velocity in microsteps per second to use the delay in usec between
successive steps. Also remove the velocity from the `stepper_run` function
as typical API usage is enable -> set step interval -> run.

Signed-off-by: Fabian Blatz <fabianblatz@gmail.com>

drivers/stepper/step_dir/step_dir_stepper_work_timing.c
@@ -10,11 +10,11 @@ static k_timeout_t stepper_movement_delay(const struct device *dev)
 {
 	const struct step_dir_stepper_common_data *data = dev->data;
 
-	if (data->max_velocity == 0) {
+	if (data->step_interval_us == 0) {
 		return K_FOREVER;
 	}
 
-	return K_USEC(USEC_PER_SEC / data->max_velocity);
+	return K_USEC(data->step_interval_us);
 }
 
 static void stepper_work_step_handler(struct k_work *work)
@@ -35,10 +35,10 @@ int step_work_timing_source_init(const struct device *dev)
 	return 0;
 }
 
-int step_work_timing_source_update(const struct device *dev, const uint32_t velocity)
+int step_work_timing_source_update(const struct device *dev, const uint64_t step_interval_us)
 {
 	ARG_UNUSED(dev);
-	ARG_UNUSED(velocity);
+	ARG_UNUSED(step_interval_us);
 	return 0;
 }
 

tests/drivers/stepper/stepper_api/src/main.c
@@ -83,7 +83,7 @@ ZTEST_F(stepper, test_target_position)
 {
 	int32_t pos = 100u;
 
-	(void)stepper_set_max_velocity(fixture->dev, 100u);
+	(void)stepper_set_step_interval(fixture->dev, 10000);
 
 	/* Pass the function name as user data */
 	(void)stepper_set_event_callback(fixture->dev, fixture->callback, &fixture);

phytec/zephyr-phytec

drivers/stepper/step_dir/step_dir_stepper_work_timing.c
@@ -10,11 +10,11 @@ static k_timeout_t stepper_movement_delay(const struct device *dev)
 {
 	const struct step_dir_stepper_common_data *data = dev->data;
 
-	if (data->max_velocity == 0) {
+	if (data->microstep_interval_ns == 0) {
 		return K_FOREVER;
 	}
 
-	return K_USEC(USEC_PER_SEC / data->max_velocity);
+	return K_NSEC(data->microstep_interval_ns);
 }
 
 static void stepper_work_step_handler(struct k_work *work)
@@ -35,10 +35,10 @@ int step_work_timing_source_init(const struct device *dev)
 	return 0;
 }
 
-int step_work_timing_source_update(const struct device *dev, const uint32_t velocity)
+int step_work_timing_source_update(const struct device *dev, const uint64_t microstep_interval_ns)
 {
 	ARG_UNUSED(dev);
-	ARG_UNUSED(velocity);
+	ARG_UNUSED(microstep_interval_ns);
 	return 0;
 }
 

tests/drivers/stepper/stepper_api/src/main.c
@@ -83,7 +83,7 @@ ZTEST_F(stepper, test_target_position)
 {
 	int32_t pos = 100u;
 
-	(void)stepper_set_max_velocity(fixture->dev, 100u);
+	(void)stepper_set_microstep_interval(fixture->dev, 10000);
 
 	/* Pass the function name as user data */
 	(void)stepper_set_event_callback(fixture->dev, fixture->callback, &fixture);