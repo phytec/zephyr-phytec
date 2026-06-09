hillLang/zephyr

kernel/sched: arch/x86_64: Support synchronous k_thread_abort() in SMP
Currently thread abort doesn't work if a thread is currently scheduled
on a different CPU, because we have no way of delivering an interrupt
to the other CPU to force the issue.  This patch adds a simple
framework for an architecture to provide such an IPI, implements it
for x86_64, and uses it to implement a spin loop in abort for the case
where a thread is currently scheduled elsewhere.

On SMP architectures (xtensa) where no such IPI is implemented, we
fall back to waiting on an arbitrary interrupt to occur.  This "works"
for typical code (and all current tests), but of course it cannot be
guaranteed on such an architecture that k_thread_abort() will return
in finite time (e.g. the other thread on the other CPU might have
taken a spinlock and entered an infinite loop, so it will never
receive an interrupt to terminate itself)!

On non-SMP architectures this patch changes no code paths at all.

Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

kernel/sched.c
@@ -464,13 +464,6 @@ void _unpend_thread(struct k_thread *thread)
 	(void)_abort_thread_timeout(thread);
 }
 
-/* FIXME: this API is glitchy when used in SMP.  If the thread is
- * currently scheduled on the other CPU, it will silently set it's
- * priority but nothing will cause a reschedule until the next
- * interrupt.  An audit seems to show that all current usage is to set
- * priorities on either _current or a pended thread, though, so it's
- * fine for now.
- */
 void _thread_priority_set(struct k_thread *thread, int prio)
 {
 	bool need_sched = 0;
@@ -488,6 +481,11 @@ void _thread_priority_set(struct k_thread *thread, int prio)
 		}
 	}
 	sys_trace_thread_priority_set(thread);
+
+	if (IS_ENABLED(CONFIG_SMP) &&
+	    !IS_ENABLED(CONFIG_SCHED_IPI_SUPPORTED)) {
+		z_sched_ipi();
+	}
 
 	if (need_sched && _current->base.sched_locked == 0) {
 		_reschedule_unlocked();
@@ -593,6 +591,15 @@ void *_get_next_switch_handle(void *interrupted)
 	sys_trace_thread_switched_in();
 #endif
 #endif
+
+	/* Some architectures don't have a working IPI, so the best we
+	 * can do there is check the abort status of the current
+	 * thread here on ISR exit
+	 */
+	if (IS_ENABLED(CONFIG_SMP) &&
+	    !IS_ENABLED(CONFIG_SCHED_IPI_SUPPORTED)) {
+		z_sched_ipi();
+	}
 
 	_check_stack_sentinel();
 
@@ -935,7 +942,60 @@ void _impl_k_wakeup(k_tid_t thread)
 	if (!_is_in_isr()) {
 		_reschedule_unlocked();
 	}
+
+	if (IS_ENABLED(CONFIG_SMP) &&
+	    !IS_ENABLED(CONFIG_SCHED_IPI_SUPPORTED)) {
+		z_sched_ipi();
+	}
 }
+
+#ifdef CONFIG_SMP
+/* Called out of the scheduler interprocessor interrupt.  All it does
+ * is flag the current thread as dead if it needs to abort, so the ISR
+ * return into something else and the other thread which called
+ * k_thread_abort() can finish its work knowing the thing won't be
+ * rescheduled.
+ */
+void z_sched_ipi(void)
+{
+	LOCKED(&sched_lock) {
+		if (_current->base.thread_state & _THREAD_ABORTING) {
+			_current->base.thread_state |= _THREAD_DEAD;
+			_current_cpu->swap_ok = true;
+		}
+	}
+}
+
+void z_sched_abort(struct k_thread *thread)
+{
+	if (thread == _current) {
+		_remove_thread_from_ready_q(thread);
+		return;
+	}
+
+	/* First broadcast an IPI to the other CPUs so they can stop
+	 * it locally.  Not all architectures support that, alas.  If
+	 * we don't have it, we need to wait for some other interrupt.
+	 */
+	thread->base.thread_state |= _THREAD_ABORTING;
+#ifdef CONFIG_SCHED_IPI_SUPPORTED
+	z_arch_sched_ipi();
+#endif
+
+	/* Wait for it to be flagged dead either by the CPU it was
+	 * running on or because we caught it idle in the queue
+	 */
+	while ((thread->base.thread_state & _THREAD_DEAD) == 0) {
+		LOCKED(&sched_lock) {
+			if (_is_thread_queued(thread)) {
+				_current->base.thread_state |= _THREAD_DEAD;
+				_priq_run_remove(&_kernel.ready_q.runq, thread);
+				_mark_thread_as_not_queued(thread);
+			}
+		}
+	}
+}
+#endif
 
 #ifdef CONFIG_USERSPACE
 Z_SYSCALL_HANDLER1_SIMPLE_VOID(k_wakeup, K_OBJ_THREAD, k_tid_t);

phytec/zephyr-phytec

kernel/sched.c
@@ -464,13 +464,6 @@ void z_unpend_thread(struct k_thread *thread)
 	(void)z_abort_thread_timeout(thread);
 }
 
-/* FIXME: this API is glitchy when used in SMP.  If the thread is
- * currently scheduled on the other CPU, it will silently set it's
- * priority but nothing will cause a reschedule until the next
- * interrupt.  An audit seems to show that all current usage is to set
- * priorities on either _current or a pended thread, though, so it's
- * fine for now.
- */
 void z_thread_priority_set(struct k_thread *thread, int prio)
 {
 	bool need_sched = 0;
@@ -488,6 +481,11 @@ void z_thread_priority_set(struct k_thread *thread, int prio)
 		}
 	}
 	sys_trace_thread_priority_set(thread);
+
+	if (IS_ENABLED(CONFIG_SMP) &&
+	    !IS_ENABLED(CONFIG_SCHED_IPI_SUPPORTED)) {
+		z_sched_ipi();
+	}
 
 	if (need_sched && _current->base.sched_locked == 0) {
 		z_reschedule_unlocked();
@@ -593,6 +591,15 @@ void *z_get_next_switch_handle(void *interrupted)
 	sys_trace_thread_switched_in();
 #endif
 #endif
+
+	/* Some architectures don't have a working IPI, so the best we
+	 * can do there is check the abort status of the current
+	 * thread here on ISR exit
+	 */
+	if (IS_ENABLED(CONFIG_SMP) &&
+	    !IS_ENABLED(CONFIG_SCHED_IPI_SUPPORTED)) {
+		z_sched_ipi();
+	}
 
 	z_check_stack_sentinel();
 
@@ -957,7 +964,60 @@ void z_impl_k_wakeup(k_tid_t thread)
 	if (!z_is_in_isr()) {
 		z_reschedule_unlocked();
 	}
+
+	if (IS_ENABLED(CONFIG_SMP) &&
+	    !IS_ENABLED(CONFIG_SCHED_IPI_SUPPORTED)) {
+		z_sched_ipi();
+	}
 }
+
+#ifdef CONFIG_SMP
+/* Called out of the scheduler interprocessor interrupt.  All it does
+ * is flag the current thread as dead if it needs to abort, so the ISR
+ * return into something else and the other thread which called
+ * k_thread_abort() can finish its work knowing the thing won't be
+ * rescheduled.
+ */
+void z_sched_ipi(void)
+{
+	LOCKED(&sched_spinlock) {
+		if (_current->base.thread_state & _THREAD_ABORTING) {
+			_current->base.thread_state |= _THREAD_DEAD;
+			_current_cpu->swap_ok = true;
+		}
+	}
+}
+
+void z_sched_abort(struct k_thread *thread)
+{
+	if (thread == _current) {
+		z_remove_thread_from_ready_q(thread);
+		return;
+	}
+
+	/* First broadcast an IPI to the other CPUs so they can stop
+	 * it locally.  Not all architectures support that, alas.  If
+	 * we don't have it, we need to wait for some other interrupt.
+	 */
+	thread->base.thread_state |= _THREAD_ABORTING;
+#ifdef CONFIG_SCHED_IPI_SUPPORTED
+	z_arch_sched_ipi();
+#endif
+
+	/* Wait for it to be flagged dead either by the CPU it was
+	 * running on or because we caught it idle in the queue
+	 */
+	while ((thread->base.thread_state & _THREAD_DEAD) == 0) {
+		LOCKED(&sched_spinlock) {
+			if (z_is_thread_queued(thread)) {
+				_current->base.thread_state |= _THREAD_DEAD;
+				_priq_run_remove(&_kernel.ready_q.runq, thread);
+				z_mark_thread_as_not_queued(thread);
+			}
+		}
+	}
+}
+#endif
 
 #ifdef CONFIG_USERSPACE
 Z_SYSCALL_HANDLER1_SIMPLE_VOID(k_wakeup, K_OBJ_THREAD, k_tid_t);