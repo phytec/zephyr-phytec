zephyrproject-rtos/zephyr

logging: Add missing compiler barrier after k_is_user_context
Remove the k_is_user_context call from the Z_LOG_LEVEL_ALL_CHECK macro.
Add the Z_LOG_LEVEL_ALL_CHECK_BREAK macro, which safely checks whether
logging is performed in user context before checking the dynamic log level
that requires access to kernel structure.

Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

subsys/logging/frontends/stmesp/zephyr_custom_log.h
@@ -40,9 +40,7 @@ extern "C" {
  */
 #define Z_LOG_STMESP_0(_level, _source, ...)                                                       \
 	do {                                                                                       \
-		if (!Z_LOG_LEVEL_ALL_CHECK(_level, __log_current_const_data, _source)) {           \
-			break;                                                                     \
-		}                                                                                  \
+		Z_LOG_LEVEL_ALL_CHECK_BREAK(_level, _inst, _source)                                \
 		LOG_FRONTEND_STMESP_LOG0(_source, STRINGIFY(_level) __VA_ARGS__);                  \
 	} while (0)
 
@@ -72,9 +70,7 @@ extern "C" {
 				(Z_LOG(_level, __VA_ARGS__)));                                     \
 			break;                                                                     \
 		}                                                                                  \
-		if (!Z_LOG_LEVEL_ALL_CHECK(_level, __log_current_const_data, _source)) {           \
-			break;                                                                     \
-		}                                                                                  \
+		Z_LOG_LEVEL_ALL_CHECK_BREAK(_level, _inst, _source)                                \
 		LOG_FRONTEND_STMESP_LOG1(_source, STRINGIFY(_level) __VA_ARGS__, dummy);           \
 	} while (0)
 

phytec/zephyr-phytec

subsys/logging/frontends/stmesp/zephyr_custom_log.h
@@ -40,9 +40,7 @@ extern "C" {
  */
 #define Z_LOG_STMESP_0(_level, _source, ...)                                                       \
 	do {                                                                                       \
-		if (!Z_LOG_LEVEL_ALL_CHECK(_level, __log_current_const_data, _source)) {           \
-			break;                                                                     \
-		}                                                                                  \
+		Z_LOG_LEVEL_ALL_CHECK_BREAK(_level, __log_current_const_data, _source)             \
 		LOG_FRONTEND_STMESP_LOG0(_source, STRINGIFY(_level) __VA_ARGS__);                  \
 	} while (0)
 
@@ -72,9 +70,7 @@ extern "C" {
 				(Z_LOG(_level, __VA_ARGS__)));                                     \
 			break;                                                                     \
 		}                                                                                  \
-		if (!Z_LOG_LEVEL_ALL_CHECK(_level, __log_current_const_data, _source)) {           \
-			break;                                                                     \
-		}                                                                                  \
+		Z_LOG_LEVEL_ALL_CHECK_BREAK(_level, __log_current_const_data, _source)             \
 		LOG_FRONTEND_STMESP_LOG1(_source, STRINGIFY(_level) __VA_ARGS__, dummy);           \
 	} while (0)
 